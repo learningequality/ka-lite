593234de00f41257fc2e64d3390590ea7f80b09b
Jamie Alexandre <jamalex@gmail.com>
2012-10-14 23:27:18 -0700

    Added indices to users/ids in logging models.

--- kalite/main/models.py
+++ kalite/main/models.py
@@ -3,15 +3,16 @@ from securesync.models import SyncedModel, FacilityUser
 
 
 class VideoLog(SyncedModel):
-    user = models.ForeignKey(FacilityUser, blank=True, null=True)
-    youtube_id = models.CharField(max_length=11)
+    user = models.ForeignKey(FacilityUser, blank=True, null=True, db_index=True)
+    youtube_id = models.CharField(max_length=11, db_index=True)
     seconds_watched = models.IntegerField()
     total_seconds_watched = models.IntegerField(default=0)
+    complete = models.BooleanField(default=False)
 
 
 class ExerciseLog(SyncedModel):
-    user = models.ForeignKey(FacilityUser, blank=True, null=True)
-    exercise_id = models.CharField(max_length=50)
+    user = models.ForeignKey(FacilityUser, blank=True, null=True, db_index=True)
+    exercise_id = models.CharField(max_length=50, db_index=True)
     answer = models.TextField(blank=True)
     correct = models.BooleanField()
     seed = models.IntegerField(blank=True, null=True)
