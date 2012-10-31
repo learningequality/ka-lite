80a7b6491a5aa2adecdfacd89f0d574f338e5041
Jamie Alexandre <jamalex@gmail.com>
2012-10-31 16:13:31 -0700

    Missing file from last commit.

---
+++ kalite/central/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from models import *
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "url")
+admin.site.register(Organization, OrganizationAdmin)
+
+
+class ZoneOrganizationAdmin(admin.ModelAdmin):
+    list_display = ("zone", "organization",)
+    list_filter = ("zone", "organization",)
+admin.site.register(ZoneOrganization, ZoneOrganizationAdmin)
+
+
+class OrganizationUserAdmin(admin.ModelAdmin):
+    list_display = ("user", "organization",)
+    list_filter = ("user", "organization",)
+admin.site.register(OrganizationUser, OrganizationUserAdmin)
