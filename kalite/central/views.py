1cc739f12b2ab76e9a4110ab15e2cf736f30a657
Dylan Barth <dlnbarth@gmail.com>
2012-11-05 12:27:04 -0800

    Added permission checks.

--- kalite/central/views.py
+++ kalite/central/views.py
@@ -1,5 +1,5 @@
 import re, json
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponseNotAllowed
 from django.shortcuts import render_to_response, get_object_or_404, redirect, get_list_or_404
 from django.template import RequestContext
 from annoying.decorators import render_to
@@ -7,19 +7,30 @@ from central.models import Organization
 from central.forms import OrganizationForm, ZoneForm
 from securesync.models import Zone
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
 
 import settings
 
 @render_to("central/homepage.html")
 def homepage(request):
+    if not request.user.is_authenticated():
+        return landing_page(request)
     organizations = request.user.get_profile().get_organizations()
     context = {'organizations': organizations}
     return context
-      
+
+@render_to("central/landing_page.html")
+def landing_page(request):
+    return {}
+ 
+@login_required     
 @render_to("central/organization_form.html")
 def organization_form(request, id=None):
     if id != "new":
-        org = Organization.objects.get(pk=id)
+        org = get_object_or_404(Organization, pk=id)
+        if org.users.filter(pk=request.user.pk).count() == 0:
+            return HttpResponseNotAllowed("You do not have permissions for this organization.")
     else:
         org = None
     if request.method == 'POST':
@@ -36,11 +47,17 @@ def organization_form(request, id=None):
         'form': form
     } 
 
+
+@login_required
 @render_to("central/zone_form.html")
 def zone_form(request, id=None, org_id=None):
     org = get_object_or_404(Organization, pk=org_id)
+    if org.users.filter(pk=request.user.pk).count() == 0:
+        return HttpResponseNotAllowed("You do not have permissions for this organization.")
     if id != "new":
-        zone = Zone.objects.get(pk=id)
+        zone = get_object_or_404(Zone, pk=id)
+        if org.zones.filter(pk=zone.pk).count() == 0:
+            return HttpResponseNotAllowed("This organization does not have permissions for this zone.")
     else:
         zone = None
     if request.method == 'POST':
