b0077938a64a591d6e23f614afdca689f9a15657
Jamie Alexandre <jamalex@gmail.com>
2012-11-13 14:37:17 -0800

    Simplified doRequest function.

--- kalite/static/js/khan-lite.js
+++ kalite/static/js/khan-lite.js
@@ -29,11 +29,11 @@ $.ajaxSetup({
     }
 });
 
-function doRequest(url, data, method) {
+function doRequest(url, data) {
     return $.ajax({
         url: url,
-        type: method || "GET",
-        data: JSON.stringify(data),
+        type: data ? "POST" : "GET",
+        data: data ? JSON.stringify(data) : "",
         contentType: "application/json",
         dataType: "json"
     });
@@ -44,7 +44,7 @@ $(function() {
     // load progress data for all videos linked on page, and render progress circles
     var youtube_ids = $.map($(".progress-circle[data-youtube-id]"), function(el) { return $(el).data("youtube-id") });
     if (youtube_ids.length > 0) {
-        doRequest("/api/get_video_logs", youtube_ids, "POST").success(function(data) {
+        doRequest("/api/get_video_logs", youtube_ids).success(function(data) {
             $.each(data, function(ind, video) {
                 var newClass = video.complete ? "complete" : "partial";
                 $("[data-youtube-id='" + video.youtube_id + "']").addClass(newClass);
@@ -55,7 +55,7 @@ $(function() {
     // load progress data for all exercises linked on page, and render progress circles
     var exercise_ids = $.map($(".progress-circle[data-exercise-id]"), function(el) { return $(el).data("exercise-id") });
     if (exercise_ids.length > 0) {
-        doRequest("/api/get_exercise_logs", exercise_ids, "POST").success(function(data) {
+        doRequest("/api/get_exercise_logs", exercise_ids).success(function(data) {
             $.each(data, function(ind, exercise) {
                 var newClass = exercise.complete ? "complete" : "partial";
                 $("[data-exercise-id='" + exercise.exercise_id + "']").addClass(newClass);
