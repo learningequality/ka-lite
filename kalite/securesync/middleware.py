da22ee2437848376b033403dc74a1de3cd4730d3
Jamie Alexandre <jamalex@gmail.com>
2012-11-14 19:20:26 -0800

    Fixes to login checking.

--- kalite/securesync/middleware.py
+++ kalite/securesync/middleware.py
@@ -5,14 +5,16 @@ class AuthFlags:
         request.is_logged_in = False
         request.is_django_user = False
 
-        if request.session.get('facility_user'):
-            if request.session.get('facility_user').is_teacher:
+        if request.user.is_authenticated():
+            request.is_logged_in = True
+            request.is_django_user = True
+            if request.user.is_superuser:
+                request.is_admin = True
+            if "facility_user" in request.session:
+                del request.session["facility_user"]
+        elif "facility_user" in request.session:
+            if request.session["facility_user"].is_teacher:
                 request.is_admin = True
                 request.is_teacher = True
-                request.is_logged_in = True
-        elif request.user.is_superuser:
-            request.is_admin = True
-            request.is_django_user = True
-        if request.user.is_authenticated():
             request.is_logged_in = True
 
