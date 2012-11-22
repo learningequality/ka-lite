50f31214dcfc23cdb7236f5a04788cf80e4295f1
Jamie Alexandre <jamalex@gmail.com>
2012-11-21 23:43:33 -0800

    Tweak to Windows script.

--- stop.bat
+++ stop.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo The stop command is not yet cleanly implemented for Windows.
-echo We will have to kill all processes called "wscript.exe" and "python.exe".
+echo We will have to kill all processes called "python.exe".
 
 :choice
 set /P c=Do you wish to proceed [Y/N]?
