df429aa03c080c576d6c4c56f5f3b6334155fbea
Jamie Alexandre <jamalex@gmail.com>
2012-11-28 15:32:46 -0800

    Searching for nodes by key now recurses, to handle duplicate keys (YT ids).

--- kalite/utils/topic_tools.py
+++ kalite/utils/topic_tools.py
@@ -1,16 +1,16 @@
 from main import topicdata
 
-def find_video_by_youtube_id(youtube_id, node=topicdata.TOPICS):
+def find_videos_by_youtube_id(youtube_id, node=topicdata.TOPICS):
+    videos = []
     if node.get("youtube_id", "") == youtube_id:
-        return node
+        videos.append(node)
     for child in node.get("children", []):
-        result = find_video_by_youtube_id(youtube_id, child)
-        if result:
-            print str(result) + "\n"
+        videos += find_videos_by_youtube_id(youtube_id, child)
+    return videos
 
 # find_video_by_youtube_id("NSSoMafbBqQ")
 
-def get_all_youtube_ids(node):
+def get_all_youtube_ids(node=topicdata.TOPICS):
     if node.get("youtube_id", ""):
         return [node.get("youtube_id", "")]
     ids = []
@@ -18,3 +18,11 @@ def get_all_youtube_ids(node):
         ids += get_all_youtube_ids(child)
     return ids
 
+def get_dups(threshold=2):
+    ids = get_all_youtube_ids()
+    return [id for id in set(ids) if ids.count(id) >= threshold]
+    
+def print_videos(youtube_id):
+    print "Videos with YouTube ID '%s':" % youtube_id
+    for node in find_videos_by_youtube_id(youtube_id):
+        print " > ".join(node["path"].split("/")[1:-3] + [node["title"]])
