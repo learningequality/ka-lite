fb1e681491ba86977feb752b48c7bc7c3607acf9
Jamie Alexandre <jamalex@gmail.com>
2012-11-28 13:37:13 -0800

    Fix to zone query for non-superusers on central server.

--- kalite/central/admin.py
+++ kalite/central/admin.py
@@ -9,14 +9,3 @@ admin.site.register(Organization, OrganizationAdmin)
 class UserProfileAdmin(admin.ModelAdmin):
 	list_display = ("user",)
 admin.site.register(UserProfile, UserProfileAdmin)
-
-# class ZoneOrganizationAdmin(admin.ModelAdmin):
-#     list_display = ("zone", "organization",)
-#     list_filter = ("zone", "organization",)
-# admin.site.register(ZoneOrganization, ZoneOrganizationAdmin)
-
-
-# class OrganizationUserAdmin(admin.ModelAdmin):
-#     list_display = ("user", "organization",)
-#     list_filter = ("user", "organization",)
-# admin.site.register(OrganizationUser, OrganizationUserAdmin)
