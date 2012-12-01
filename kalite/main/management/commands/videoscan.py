b5a5612ddf4dfcfc5df172a44f2d7b8bae54feac
Jamie Alexandre <jamalex@gmail.com>
2012-11-30 19:06:33 -0800

    Updating videoscan command to be more efficient (in progress).

--- kalite/main/management/commands/videoscan.py
+++ kalite/main/management/commands/videoscan.py
@@ -12,26 +12,44 @@ class Command(BaseCommand):
     help = "Sync up the database's version of what videos have been downloaded with the actual folder contents"
 
     def handle(self, *args, **options):
+
+        # delete VideoFile objects that are not marked as in progress, but are neither 0% nor 100% done; they're broken
+        VideoFile.objects.filter(download_in_progress=False, percent_complete__gt=0, percent_complete__lt=100).delete()
+
         files = glob.glob(settings.VIDEO_PATH + "*.mp4")
-        existing_videos = set([path.replace("\\", "/").split("/")[-1].split(".")[0] for path in files])
-        marked_videos = set([video.youtube_id for video in VideoFile.objects.filter(percent_complete=100)])
-        partial_videos = set([video.youtube_id for video in VideoFile.objects.filter(download_in_progress=True)])
+        videos_marked_at_all = set([video.youtube_id for video in VideoFile.objects.all()])
+        videos_marked_as_in_progress = set([video.youtube_id for video in VideoFile.objects.filter(download_in_progress=True)])
+        videos_marked_as_unstarted = set([video.youtube_id for video in VideoFile.objects.filter(percent_complete=0, download_in_progress=False)])
         
-        new_videos = existing_videos - marked_videos - partial_videos
-        deleted_videos = marked_videos - existing_videos
+        max_vars = 500
+        videos_in_filesystem = set([path.replace("\\", "/").split("/")[-1].split(".")[0] for path in files])
+        videos_in_filesystem_list = list(videos_in_filesystem)
+        videos_in_filesystem_chunked = [videos_in_filesystem_list[i*max_vars:(i+1)*max_vars] for i in range(0, len(videos_in_filesystem_list), max_vars)]
         
-        for youtube_id in new_videos:
-            video = get_object_or_None(VideoFile, youtube_id=youtube_id) or VideoFile(youtube_id=youtube_id)
-            if video.download_in_progress or video.percent_complete == 100:
-                continue
-            video.percent_complete = 100
-            video.flagged_for_download = False
-            video.save()
-            # print "Marked %s as complete." % youtube_id
+        for chunk in videos_in_filesystem_chunked:
+            video_files_needing_model_update = VideoFile.objects.filter(percent_complete=0, download_in_progress=False, youtube_id__in=chunk)
+            print "Updating %d models" % video_files_needing_model_update.count()
+            video_files_needing_model_update.update(percent_complete=100, flagged_for_download=False)
         
-        for youtube_id in deleted_videos:
-            video = get_object_or_None(VideoFile, youtube_id=youtube_id)
-            if not video:
-                continue
-            video.delete()
-            # print "Deleted %s." % youtube_id
+        video_ids_needing_model_creation = list(videos_in_filesystem - videos_marked_at_all)
+        print "Creating %d models" % len(video_ids_needing_model_creation)
+        VideoFile.objects.bulk_create([VideoFile(youtube_id=youtube_id, percent_complete=100) for youtube_id in video_ids_needing_model_creation])
+        
+        for chunk in videos_in_filesystem_chunked:
+            video_files_needing_model_deletion = VideoFile.objects.filter(percent_complete=100).exclude(youtube_id__in=chunk)
+            print "Deleting %d models" % video_files_needing_model_update.count()
+            video_files_needing_model_deletion.delete()
+        
+        # for youtube_id in new_video_files_needing_model:
+        #     video = get_object_or_None(VideoFile, youtube_id=youtube_id) or VideoFile(youtube_id=youtube_id)
+        #     video.percent_complete = 100
+        #     video.flagged_for_download = False
+        #     video.save()
+        #     # print "Marked %s as complete." % youtube_id
+        
+        # for youtube_id in deleted_videos:
+        #     video = get_object_or_None(VideoFile, youtube_id=youtube_id)
+        #     if not video:
+        #         continue
+        #     video.delete()
+        #     # print "Deleted %s." % youtube_id
