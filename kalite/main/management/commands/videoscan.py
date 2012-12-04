8759ec67f272f8913e6153653e0fa85a99ed602a
Jamie Alexandre <jamalex@gmail.com>
2012-12-03 17:50:49 -0800

    Fixed efficient version of the videoscan command, using batch updates.

--- kalite/main/management/commands/videoscan.py
+++ kalite/main/management/commands/videoscan.py
@@ -17,39 +17,50 @@ class Command(BaseCommand):
         VideoFile.objects.filter(download_in_progress=False, percent_complete__gt=0, percent_complete__lt=100).delete()
 
         files = glob.glob(settings.VIDEO_PATH + "*.mp4")
+        subtitle_files = glob.glob(settings.VIDEO_PATH + "*.srt")
         videos_marked_at_all = set([video.youtube_id for video in VideoFile.objects.all()])
         videos_marked_as_in_progress = set([video.youtube_id for video in VideoFile.objects.filter(download_in_progress=True)])
         videos_marked_as_unstarted = set([video.youtube_id for video in VideoFile.objects.filter(percent_complete=0, download_in_progress=False)])
         
         max_vars = 500
+        
         videos_in_filesystem = set([path.replace("\\", "/").split("/")[-1].split(".")[0] for path in files])
         videos_in_filesystem_list = list(videos_in_filesystem)
-        videos_in_filesystem_chunked = [videos_in_filesystem_list[i*max_vars:(i+1)*max_vars] for i in range(0, len(videos_in_filesystem_list), max_vars)]
+        videos_in_filesystem_chunked = [videos_in_filesystem_list[i:i+max_vars] for i in range(0, len(videos_in_filesystem_list), max_vars)]
+
+        subtitles_in_filesystem = set([path.replace("\\", "/").split("/")[-1].split(".")[0] for path in subtitle_files])
+        subtitles_in_filesystem_list = list(subtitles_in_filesystem)
+        subtitles_in_filesystem_chunked = [subtitles_in_filesystem_list[i:i+max_vars] for i in range(0, len(subtitles_in_filesystem_list), max_vars)]
         
+        count = 0
         for chunk in videos_in_filesystem_chunked:
             video_files_needing_model_update = VideoFile.objects.filter(percent_complete=0, download_in_progress=False, youtube_id__in=chunk)
-            print "Updating %d models" % video_files_needing_model_update.count()
+            count += video_files_needing_model_update.count()
             video_files_needing_model_update.update(percent_complete=100, flagged_for_download=False)
+        if count:
+            print "Updated %d VideoFile models (to mark them as complete, since the files exist)" % count
         
         video_ids_needing_model_creation = list(videos_in_filesystem - videos_marked_at_all)
-        print "Creating %d models" % len(video_ids_needing_model_creation)
-        VideoFile.objects.bulk_create([VideoFile(youtube_id=youtube_id, percent_complete=100) for youtube_id in video_ids_needing_model_creation])
+        count = len(video_ids_needing_model_creation)
+        if count:
+            VideoFile.objects.bulk_create([VideoFile(youtube_id=youtube_id, percent_complete=100) for youtube_id in video_ids_needing_model_creation])
+            print "Created %d VideoFile models (to mark them as complete, since the files exist)" % count
         
-        for chunk in videos_in_filesystem_chunked:
-            video_files_needing_model_deletion = VideoFile.objects.filter(percent_complete=100).exclude(youtube_id__in=chunk)
-            print "Deleting %d models" % video_files_needing_model_update.count()
+        count = 0
+        videos_needing_model_deletion_list = list(videos_marked_at_all - videos_in_filesystem)
+        videos_needing_model_deletion_chunked = [videos_needing_model_deletion_list[i:i+max_vars] for i in range(0, len(videos_needing_model_deletion_list), max_vars)]
+        for chunk in videos_needing_model_deletion_chunked:
+            video_files_needing_model_deletion = VideoFile.objects.filter(youtube_id__in=chunk)
+            count += video_files_needing_model_deletion.count()
             video_files_needing_model_deletion.delete()
+        if count:
+            print "Deleted %d VideoFile models (because the videos didn't exist in the filesystem)" % count
+
+        count = 0
+        for chunk in subtitles_in_filesystem_chunked:
+            video_files_needing_model_update = VideoFile.objects.filter(subtitle_download_in_progress=False, subtitles_downloaded=False, youtube_id__in=chunk)
+            count += video_files_needing_model_update.count()
+            video_files_needing_model_update.update(subtitles_downloaded=True)
+        if count:
+            print "Updated %d VideoFile models (marked them as having subtitles)" % count
         
-        # for youtube_id in new_video_files_needing_model:
-        #     video = get_object_or_None(VideoFile, youtube_id=youtube_id) or VideoFile(youtube_id=youtube_id)
-        #     video.percent_complete = 100
-        #     video.flagged_for_download = False
-        #     video.save()
-        #     # print "Marked %s as complete." % youtube_id
-        
-        # for youtube_id in deleted_videos:
-        #     video = get_object_or_None(VideoFile, youtube_id=youtube_id)
-        #     if not video:
-        #         continue
-        #     video.delete()
-        #     # print "Deleted %s." % youtube_id
