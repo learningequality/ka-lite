6465b0acad1014ea55c27642649e1e935b81ad56
Jamie Alexandre <jamalex@gmail.com>
2012-12-04 19:01:44 -0800

    Added non-Django script to check waiting job count, for cron efficiency.

--- kalite/cronserver.py
+++ kalite/cronserver.py
@@ -1,7 +1,10 @@
 import threading, subprocess
+from croncount import get_count
 
 def cron():
-    subprocess.call(["python", "manage.py", "cron"])
+    # only bother spinning up the full cron management command if there's something waiting to be run
+    if get_count():
+        subprocess.call(["python", "manage.py", "cron"])
     threading.Timer(5, cron).start()
 
 if __name__ == "__main__":
