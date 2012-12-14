55a6d43b919813fe8c74b5f07c6e4368e0e8e71c
rtibbles <rtibbles@ucsd.edu>
2012-12-14 13:40:04 -0800

    Updates to maplayout, and icons for overall map view.

--- kalite/utils/knowledgemap.py
+++ kalite/utils/knowledgemap.py
@@ -2,13 +2,29 @@ import requests, json, os, sys
 
 baseurl = "http://www.khanacademy.org/api/v1/"
 
+iconbaseurl = "http://www.khanacademy.org"
+
 download_path = os.path.dirname(os.path.realpath(__file__)) + "/../static/data/"
+
+icon_download_path = os.path.dirname(os.path.realpath(__file__)) + "/../static"
 
 maplayout = requests.get(baseurl + "topicversion/default/maplayout")
 
 maplayoutfile = file(download_path+"maplayout_data.json","w")
 
 maplayout_data = json.loads(maplayout.content)
+
+iconfilepath = "/images/power-mode/badges/"
+iconextension = "-40x40.png"
+
+for key,value in maplayout_data["topics"].items():
+    if value.has_key("icon_url"):
+        value["icon_url"] = iconfilepath + value["id"] + iconextension
+        maplayout_data["topics"][key] = value
+        icon = requests.get(iconbaseurl + value["icon_url"])
+        iconfile = file(icon_download_path + value["icon_url"],"w")
+        iconfile.write(icon.content)
+    
 
 json.dump(maplayout_data,maplayoutfile)
 
@@ -21,7 +37,6 @@ exceptions = {
 
 for key,value in maplayout_data["topics"].items():
     topicget = requests.get(baseurl + "topic/%s/exercises" % key)
-    print topicget.content
     topicdata = json.loads(topicget.content)
     
     if exceptions.has_key(key):
