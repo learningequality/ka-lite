1125f2a75025c8f7667274904f4c8a414210da80
Jamie Alexandre <jamalex@gmail.com>
2012-12-18 17:28:36 -0800

    Suppress output while moving content files in batch file.

--- start.bat
+++ start.bat
@@ -2,7 +2,7 @@
 cd kalite
 
 REM transfer any previously downloaded content from the old location to the new
-move static\videos\* ..\content
+move static\videos\* ..\content > nul 2> nul
 
 echo Starting the cron server in the background.
 start /B runhidden.vbs "cronstart.bat"
