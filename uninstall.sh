3cffdf844a3a0b9c7bd9823fd4613a94c1bd6ca0
mr.Shu <mr@shu.io>
2013-04-29 08:23:45 +0200

    comments incorporated

--- uninstall.sh
+++ uninstall.sh
@@ -2,20 +2,20 @@
 
 current_dir=`dirname "${BASH_SOURCE[0]}"`
 if [ ! `id -u` -eq `stat -c "%u" $current_dir` ]; then
-	echo "-------------------------------------------------------------------"
-	echo "You are not the owner of this directory!"
-	echo "We don't think it's a good idea to uninstall something you"
+        echo "-------------------------------------------------------------------"
+        echo "You are not the owner of this directory!"
+        echo "We don't think it's a good idea to uninstall something you"
         echo "do not own."
-	echo "-------------------------------------------------------------------"
-	exit 1
+        echo "-------------------------------------------------------------------"
+        exit 1
 fi
 
 if [ ! -w $current_dir/kalite ]; then
-	echo "-------------------------------------------------------------------"
-	echo "You do not have permission to write to this directory!"
+        echo "-------------------------------------------------------------------"
+        echo "You do not have permission to write to this directory!"
         echo "Thus, it's not possible to remove it =)"
-	echo "-------------------------------------------------------------------"
-	exit 1
+        echo "-------------------------------------------------------------------"
+        exit 1
 fi
 
 echo
@@ -24,6 +24,7 @@ read CONFIRM
 
 case $CONFIRM in 
         y|Y)
+                $current_dir/stop.sh
                 echo "You are about to remove the service which started KA Lite server"
                 echo "in the background (this will require root/sudo privileges)"
                 sudo $current_dir/kalite/donotrunatboot.sh
