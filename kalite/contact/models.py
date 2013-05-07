e92fb35c131298946c8cb4698af41cbe8acb4d43
Ben Cipollini <ben@learningequality.org>
2013-05-07 03:54:19 -0700

    adding admin, adding email bodies, sending to addresses within settings.py

--- kalite/contact/models.py
+++ kalite/contact/models.py
@@ -1,21 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Contact(models.Model):
-    CONTACT_TYPE_DEPLOYMENT = 'depl'
-    CONTACT_TYPE_SUPPORT    = 'supp'
+    CONTACT_TYPE_DEPLOYMENT = 'deployment'
+    CONTACT_TYPE_SUPPORT    = 'support'
     CONTACT_TYPE_INFO       = 'info'
     CONTACT_TYPES = ((CONTACT_TYPE_DEPLOYMENT, 'Deployment'),
                      (CONTACT_TYPE_SUPPORT, 'Support'),
                      (CONTACT_TYPE_INFO, 'General Inquiries'))
-    user      = models.ForeignKey(User, blank=True)
-    name      = models.CharField(verbose_name="Contact name", max_length=100)
-    type      = models.CharField(verbose_name="Contact type", max_length=4, choices=CONTACT_TYPES)
-    email     = models.EmailField(verbose_name="Contact email", max_length=100)
-    org_name  = models.CharField(verbose_name="Organization name", max_length=100, blank=True)
-    org_url   = models.URLField(verbose_name="Organziation_url URL", help_text="<br/>(optional)", blank=True)
+    user      = models.ForeignKey(User, blank=True, null=True)
+    name      = models.CharField(verbose_name="Contact Name", max_length=100)
+    type      = models.CharField(verbose_name="Reason for Contact", max_length=12, choices=CONTACT_TYPES)
+    email     = models.EmailField(verbose_name="Contact Email", max_length=100)
+    org_name  = models.CharField(verbose_name="Organization Name", max_length=100, blank=True)
+    org_url   = models.URLField(verbose_name="Organization URL", blank=True)
+    contact_date= models.DateField(auto_now_add=True)
 
+    def __unicode__(self):
+        return "%s inquiry from %s @ %s on %s (%s)"%(self.type, self.name, self.org_name, self.contact_date, self.email)
+        
+        
 class Deployment(models.Model):
     contact                 = models.ForeignKey(Contact)
     countries               = models.CharField(max_length=100, blank=True)
@@ -25,18 +29,29 @@ class Deployment(models.Model):
     low_cost_bundle         = models.BooleanField(blank=True)
     other                   = models.TextField(blank=True)
 
+    def __unicode__(self):
+        return "Inquiry from %s @ %s on %s (%s)"%(self.contact.name, self.contact.org_name, self.contact.contact_date, self.contact.email)
+
+
 class Support(models.Model):
-    SUPPORT_TYPES = (('instl', 'Installation'),
-                     ('setup', 'Post-install setup'),
-                     ('dwnld', 'Downloading videos'),
-                     ('corpt', 'Coach reports'),
-                     ('other', 'Other'))
+    SUPPORT_TYPES = (('installation', 'Installation'),
+                     ('setup',        'Post-install setup'),
+                     ('downloading',  'Downloading videos'),
+                     ('reporting',    'Coach reports'),
+                     ('other',        'Other'))
 
     contact  = models.ForeignKey(Contact)
     type     = models.CharField(max_length=5, choices=SUPPORT_TYPES)
     issue    = models.TextField(blank=True)
 
+    def __unicode__(self):
+        return "%s inquiry from %s @ %s on %s (%s)"%(self.type, self.contact.name, self.contact.org_name, self.contact.contact_date, self.contact.email)
+
+
 class Info(models.Model):
     contact  = models.ForeignKey(Contact)
     issue    = models.TextField(blank=True)
+    
+    def __unicode__(self):
+        return "Inquiry from %s @ %s on %s (%s)"%(self.contact.name, self.contact.org_name, self.contact.contact_date, self.contact.email)
 
--- kalite/contact/views.py
+++ kalite/contact/views.py
@@ -1,7 +1,11 @@
 from annoying.decorators import render_to
 from forms  import ContactForm, DeploymentForm, SupportForm, InfoForm
 from models import Contact, Deployment, Support, Info
-from django.core.mail import send_mail
+from central.models import Organization
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 import settings
 
@@ -17,39 +21,51 @@ def contact_wizard(request):
     
     # handle a submitted contact form
     if request.method == "POST":
-        contact_form = ContactForm(data=request.POST)
-        deployment_form = DeploymentForm(data=request.POST)
-        support_form = SupportForm(data=request.POST)
-        info_form = InfoForm(data=request.POST)
-        
+        contact_form = ContactForm(prefix="cf", data=request.POST)
+        deployment_form = DeploymentForm(prefix="df", data=request.POST)
+        support_form = SupportForm(prefix="sf", data=request.POST)
+        info_form = InfoForm(prefix="if", data=request.POST)
+
         if contact_form.is_valid():
+            if request.user.is_authenticated():
+                contact_form.instance.user = request.user
+                
             # Deployment
             if contact_form.cleaned_data["type"]  ==Contact.CONTACT_TYPE_DEPLOYMENT:
                 if deployment_form.is_valid():
-                    handle_deployment(request)
+                    return handle_contact(request, contact_form, deployment_form, settings.CENTRAL_DEPLOYMENT_EMAIL, "deployment")
 
             # Support
             elif contact_form.cleaned_data["type"]==Contact.CONTACT_TYPE_SUPPORT:
                 if support_form.is_valid():
-                    raise Exception('Support not yet implemented')
+                    return handle_contact(request, contact_form, support_form, settings.CENTRAL_SUPPORT_EMAIL, "support")
                     
             # Info
             elif contact_form.cleaned_data["type"]==Contact.CONTACT_TYPE_INFO:
                 if info_form.is_valid():
-                    raise Exception('Support not yet implemented')
-                    
+                    return handle_contact(request, contact_form, info_form, settings.CENTRAL_INFO_EMAIL, "info")
+
             else:
                 raise Exception("Unknown contact type: %s"%(contact_form.cleaned_data["type"]))
     
     # A GET request.  Create empty forms, fill in user details if available
     else:
-        deployment_form = DeploymentForm()
-        support_form = SupportForm()
-        info_form = InfoForm()
+        deployment_form = DeploymentForm(prefix="df")
+        support_form = SupportForm(prefix="sf")
+        info_form = InfoForm(prefix="if")
+        
+        # Use the user's information, if available
         if request.user.is_authenticated():
-            contact_form = ContactForm(user=request.user, name=request.user.name)
+            if request.user.owned_organizations.count()>0:
+                org = request.user.owned_organizations.get()
+            elif request.user.organization_set.count()>0:
+                org = request.user.organization_set.get()
+            else:
+                org = Organization()
+            
+            contact_form = ContactForm(prefix="cf", instance=Contact(user=request.user, name="%s %s"%(request.user.first_name, request.user.last_name), email=request.user.email, org_name=org.name, org_url=org.url))
         else:
-            contact_form = ContactForm()
+            contact_form = ContactForm(prefix="cf")
 
     return {
         "central_contact_email": settings.CENTRAL_CONTACT_EMAIL,
@@ -61,24 +77,23 @@ def contact_wizard(request):
     }
     
     
-def handle_deployment(request):
+def handle_contact(request, contact_form, details_form, list_email, email_template_prefix):
         # Save data to database
         contact_form.save()
-        deployment_form.instance.contact = contact_form.instance
-        deployment_form.save()
+        details_form.instance.contact = contact_form.instance
+        details_form.save()
         
         # Send email
-        to_email = "ben@learningequality.org"
         context = {
-            'contact_form':    contact_form,
-            'deployment_form': deployment_form,
+            'contact':    contact_form.instance,
+            'details': details_form.instance,
             'central_server_host': settings.CENTRAL_SERVER_HOST,
         }
-        subject = render_to_string('contact/deployment_subject.txt', context)
-        body = render_to_string('contact/deployment_body.txt', context)
+        subject = '[KA Lite] %s'%render_to_string('contact/%s_subject.txt'%email_template_prefix, context)
+        body = render_to_string('contact/%s_body.txt'%email_template_prefix, context)
 
-        email = EmailMessage(subject, body, settings.CENTRAL_SERVER_HOST,
-                    [contact_form.instance.email], [],
-                    headers = {'Reply-To': contact_form.instance.email})
+        email = EmailMessage(subject=subject, body=body, from_email=settings.CENTRAL_FROM_EMAIL,
+                    to=[list_email], headers = {'Reply-To': contact_form.instance.email})
         email.send()
         return HttpResponseRedirect(reverse("contact_thankyou"))
+
