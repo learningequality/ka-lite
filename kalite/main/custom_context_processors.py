9e08fa6167614dcf4cad0c29006dd7c93732d69e
Ben Cipollini <ben@learningequality.org>
2013-05-31 14:11:27 -0700

    allow csrf to be ignored (set to blank); for use in some distributed server situations

--- kalite/main/custom_context_processors.py
+++ kalite/main/custom_context_processors.py
@@ -6,10 +6,11 @@ def custom(request):
     return {
         "central_server_host": settings.CENTRAL_SERVER_HOST,
         "is_central": settings.CENTRAL_SERVER,
-        "base_template": settings.CENTRAL_SERVER and "base_central.html" or "base_distributed.html",
+        "base_template": "base_central.html" if settings.CENTRAL_SERVER else "base_distributed.html",
         "CONTENT_ROOT": settings.CONTENT_ROOT,
         "CONTENT_URL": settings.CONTENT_URL,
         "DATA_PATH": settings.DATA_PATH,
+        "use_csrf": settings.USE_CSRF,
     }
 
 
