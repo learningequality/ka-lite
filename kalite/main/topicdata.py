788762284154aee7289457b4e264fe5fa13c075c
Ben Cipollini <ben@learningequality.org>
2013-06-04 17:01:01 -0700

    add methods for removing data cruft
    Conflicts:
    	kalite/main/management/commands/package_for_download.py

--- kalite/main/topicdata.py
+++ kalite/main/topicdata.py
@@ -1,8 +1,10 @@
 import json
 import settings
+import copy
+from utils.topics import node_cache_from_topic
 
 TOPICS = json.loads(open(settings.DATA_PATH + "topics.json").read())
-NODE_CACHE = json.loads(open(settings.DATA_PATH + "nodecache.json").read())
+NODE_CACHE = node_cache_from_topic(TOPICS)
 EXERCISE_TOPICS = json.loads(open(settings.DATA_PATH + "maplayout_data.json").read())
 LANGUAGE_LOOKUP = json.loads(open(settings.DATA_PATH + "languages.json").read())
 LANGUAGE_LIST = json.loads(open(settings.DATA_PATH + "listedlanguages.json").read())
--- kalite/utils/topics.py
+++ kalite/utils/topics.py
@@ -107,3 +107,40 @@ def download_topictree():
     with open(data_path + "nodecache.json", "w") as fp:
         fp.write(json.dumps(node_cache, indent=2))
 
+
+
+def scrub_topics(topic_node):
+    """Seems that topics.json has cruft in it.  Scrub it!"""
+    global kind_blacklist
+    
+    if topic_node["kind"] in kind_blacklist:
+#        if topic_node["kind"] == "Separator":
+#            print topic_node
+#        else:
+#            import pdb; pdb.set_trace()
+        return None
+    elif topic_node["slug"] in slug_blacklist:
+        return None
+        
+    for ci,child in enumerate(topic_node.get("children", [])):
+        if not scrub_topics(child):
+            topic_node['children'].pop(ci)
+
+    return topic_node  
+    
+    
+def node_cache_from_topic(topic_node, node_cache={}):
+    """Get the node_cache from the topics list (saves disk space!)"""
+    
+    kind = topic_node["kind"]
+    
+    node_cache[kind] = node_cache.get(kind, {})
+    node_copy = copy.copy(topic_node)
+    if "children" in node_copy:
+        del node_copy["children"]
+    node_cache[kind][node_copy["slug"]] = node_copy
+
+    for child in topic_node.get("children", []):
+        node_cache_from_topic(child, node_cache)
+
+    return node_cache
