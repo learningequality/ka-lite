0a5dbf77d32d91a458be39e95231bc7ac597cd66
Ben Cipollini <bcipolli@ucsd.edu>
2013-06-12 22:17:48 -0700

    changes to get server stop working

--- kalite/serverstop.py
+++ kalite/serverstop.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python
 
-import os, signal
+import os
+import sys
 
-with open('runcherrypyserver.pid') as file:
-    pid = int(file.readline().strip())
-    os.kill(pid, signal.SIGTERM)
-    os.remove('runcherrypyserver.pid')
+# Set up the paths
+script_dir = os.path.dirname(os.path.realpath(__file__))
+python_packages_dir = script_dir + "/../python-packages/"
+sys.path = sys.path + [ python_packages_dir]
+
+# Import the server
+from django_cherrypy_wsgiserver import cherrypyserver
+
+# Call stop!
+pid_file = os.path.realpath(script_dir + "/runcherrypyserver.pid")
+cherrypyserver.stop_server(pid_file)
