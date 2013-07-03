fe1b55e3efdd352e58a3e12c7307f224724e1d64
ruimalheiro <rmpmalheiro@gmail.com>
2013-07-03 10:35:25 -0700

    Changed the design by extending SessionStorage

--- kalite/utils/django_utils.py
+++ kalite/utils/django_utils.py
@@ -3,7 +3,8 @@ import sys
 from cStringIO import StringIO
 
 from django.core.management import call_command
-
+from django.contrib.messages.storage.base import Message
+from django.contrib.messages.storage.session import SessionStorage
 
 def call_command_with_output(cmd, *args, **kwargs): 
     """Run call_command while capturing stdout/stderr and calls to sys.exit"""
@@ -37,3 +38,27 @@ def call_command_with_output(cmd, *args, **kwargs):
         sys.stdout = backups[0]
         sys.stderr = backups[1]
         sys.exit   = backups[2]
+
+class NoDuplicateMessagesSessionStorage(SessionStorage):
+    
+    def add(self, level, message, extra_tags=''):
+        """
+        Queues a message to be stored.
+
+        The message is only queued if it contained something and its level is
+        not less than the recording level (``self.level``).
+        """
+        for m in self._queued_messages:
+            if m.message == message:
+                return
+
+        if not message:
+            return
+        # Check that the message level is not less than the recording level.
+        level = int(level)
+        if level < self.level:
+            return
+        # Add the message.
+        self.added_new = True
+        message = Message(level, message, extra_tags=extra_tags)
+        self._queued_messages.append(message)
