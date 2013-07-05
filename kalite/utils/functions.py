0f5ef73a9bf47c83d0208d139fab809daf939322
Ben Cipollini <bcipolli@ucsd.edu>
2013-07-04 18:02:19 -0700

    combine general functions to one file. Test UserLog functionality on new datediff function.

--- kalite/utils/functions.py
+++
@@ -1,8 +0,0 @@
-def isnumeric(obj):
-    """Returns whether an object is itself numeric, or can be converted to numeric"""
-    
-    try:
-        float(obj)
-        return True
-    except:
-        return False
--- kalite/utils/general.py
+++ kalite/utils/general.py
@@ -1,3 +1,8 @@
+"""
+General string, integer, date functions.
+"""
+
+
 def break_into_chunks(bigiterator, chunksize=500):
     """
     Given an iterator, separates the iterator into a list of iterators,
@@ -5,6 +10,19 @@ def break_into_chunks(bigiterator, chunksize=500):
     """
     biglist = list(bigiterator)
     return [biglist[i:i+chunksize] for i in range(0, len(biglist), chunksize)]
+
+
+def isnumeric(obj):
+    """
+    Returns whether an object is itself numeric, or can be converted to numeric
+    """
+
+    try:
+        float(obj)
+        return True
+    except:
+        return False
+
 
 def datediff(*args, **kwargs):
     """
