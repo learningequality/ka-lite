0c701476b417ce48285b8901ab0b76c760ecd0ad
Ben Cipollini <ben@learningequality.org>
2013-07-05 07:54:48 -0700

    adding comments for use of OrderedDict

--- kalite/central/models.py
+++ kalite/central/models.py
@@ -1,6 +1,6 @@
-import collections
 import random
 import datetime
+from collections import OrderedDict
 
 from django.db import models, transaction
 from django.contrib.auth.models import User
@@ -17,7 +17,7 @@ from securesync.models import Zone
 def get_or_create_user_profile(user):
     assert not user.is_anonymous(), "Should not be calling get_or_create_user_profile with an anonymous user."
     assert user.is_authenticated(), "Should not be calling get_or_create_user_profile with an anonymous user."
-    
+
     return UserProfile.objects.get_or_create(user=user)[0]
 
 class Organization(models.Model):
@@ -32,7 +32,7 @@ class Organization(models.Model):
     owner = models.ForeignKey(User, related_name="owned_organizations")
 
     dummy_name = "Headless Zones"
-    
+
     def get_zones(self):
         return list(self.zones.all())
 
@@ -51,25 +51,25 @@ class Organization(models.Model):
     def save(self, owner=None, *args, **kwargs):
         if not self.owner_id:
             self.owner = owner
-            
+
         # Make org unique by name, for dummy name only.
         if self.name==Organization.dummy_name:
             dummy_orgs = Organization.objects.filter(name=Organization.dummy_name)
             if len(dummy_orgs)>0 and self.pk not in [d.pk for d in dummy_orgs]:
                 raise Exception("Cannot add more than one dummy org!")
-                
+
         super(Organization, self).save(*args, **kwargs)
 
     @classmethod
     def from_zone(cls, zone):
         """Given a zone, figure out which organization is the parent."""
-    
+
         return Organization.objects.filter(zones__pk=zone.pk)
-    
+
     @classmethod
     def get_dummy_organization(cls, user):
         assert user.is_superuser, "only super-users can call this method!"
-        
+
         orgs = cls.objects.filter(name=cls.dummy_name)
         if not orgs:
             org = Organization(name=cls.dummy_name, owner=user)
@@ -78,7 +78,7 @@ class Organization(models.Model):
         else:
             assert len(orgs)==1, "Cannot have multiple dummy organizations"
             return orgs[0]
-        
+
     @classmethod
     def update_dummy_organization(cls, user):
         assert user.is_superuser, "only super-users can call this method!"
@@ -89,20 +89,22 @@ class Organization(models.Model):
                 dummy_org.zones.add(zone)
             dummy_org.save()
         return dummy_org
-            
-class UserProfile(models.Model):  
+
+
+class UserProfile(models.Model):
     user = models.OneToOneField(User)
 
     def __unicode__(self):
         return self.user.username
 
     def get_organizations(self):
-        orgs = collections.OrderedDict() # no dictionary comprehensions, so have to loop
-        for org in self.user.organization_set.all():
+        orgs = OrderedDict()  # no dictionary comprehensions, so have to loop
+        for org in self.user.organization_set.all():  # add in order queries (alphabetical?)
             orgs[org.pk] = org
-        
+
         # Add a dummy organization for superusers, containing
-        #   any headless zones
+        #   any headless zones.
+        # Make sure this is at the END of the list, so it is clearly special.
         if self.user.is_superuser:
             dummy_org = Organization.update_dummy_organization(self.user)
             if dummy_org.zones:
@@ -110,7 +112,7 @@ class UserProfile(models.Model):
 
         return orgs
 
-    
+
 class OrganizationInvitation(models.Model):
     email_to_invite = models.EmailField(verbose_name="Email of invitee", max_length=75)
     invited_by = models.ForeignKey(User)
@@ -125,7 +127,7 @@ class OrganizationInvitation(models.Model):
         cdict = {
             'organization': self.organization,
             'invited_by': self.invited_by,
-            'central_server_host': request.META.get('HTTP_HOST', settings.CENTRAL_SERVER_HOST), # for central server actions, determine DYNAMICALLY to be safe
+            'central_server_host': request.META.get('HTTP_HOST', settings.CENTRAL_SERVER_HOST),  # for central server actions, determine DYNAMICALLY to be safe
 
         }
         # Invite an existing user
@@ -152,10 +154,10 @@ class FeedListing(models.Model):
     description = models.TextField(blank=True)
     posted_date = models.DateTimeField()
     url = models.URLField()
-    
+
     def get_absolute_url(self):
         return self.url
-    
+
 class Subscription(models.Model):
     email = models.EmailField()
     timestamp = models.DateTimeField(auto_now_add=True)
