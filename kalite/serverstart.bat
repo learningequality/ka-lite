1a311ab517027ee423b47cc73a1b55471380544e
ruimalheiro <rmpmalheiro@gmail.com>
2013-07-08 17:45:12 -0700

    Fix PORT

--- kalite/serverstart.bat
+++ kalite/serverstart.bat
@@ -8,5 +8,5 @@ set OLD_DIR=%CD%
 set SCRIPT_DIR=%~dp0
 cd "%SCRIPT_DIR%"
 echo.
-start /b python.exe manage.py runcherrypyserver host=0.0.0.0 port=8008 daemonize=True threads=50 pidfile="%SCRIPT_DIR%\runcherrypyserver.pid"
+start /b python.exe manage.py runcherrypyserver host=0.0.0.0 port=%PORT% daemonize=True threads=50 pidfile="%SCRIPT_DIR%\runcherrypyserver.pid"
 CD "%OLD_DIR%"
--- start.bat
+++ start.bat
@@ -25,13 +25,13 @@ if exist database\data.sqlite (
 	echo Running the web server in the background, on port %PORT%.
 	start /B runhidden.vbs "serverstart.bat %PORT%"
 
-	echo The server should now be accessible locally at: http://127.0.0.1:8008/
+	echo The server should now be accessible locally at: http://127.0.0.1:%PORT%/
 	echo To access it from another connected computer, try the following:
 
 	for /f "tokens=2 delims=:" %%i in ('ipconfig ^| findstr /R /C:"IPv4 Address" /C:"IP Address"') do (
-    	for /f "tokens=1 delims= " %%a in ("%%i") do echo http://%%a:8008/
+    	for /f "tokens=1 delims= " %%a in ("%%i") do echo http://%%a:%PORT%/
 	)
-	
+
 ) else (
 	echo Please run install.bat first!
 )
