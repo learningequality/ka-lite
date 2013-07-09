2730b0d83a5473366d0523696b6d36455202b6a1
ruimalheiro <rmpmalheiro@gmail.com>
2013-07-09 15:28:42 -0700

    Update in stop mechanism.

--- stop.bat
+++ stop.bat
@@ -1,12 +1,8 @@
 @echo off
 
 cd kalite
-set /p cronserverpid=<cronserver.pid
-set /p runcherrypyserverpid=<runcherrypyserver.pid
-
-taskkill /f /pid %cronserverpid% > nul 2>&1 && echo Cron server was stopped!
-tskill %cronserverpid% > nul 2>&1 && echo Cron server was stopped!
-
-taskkill /f /pid %runcherrypyserverpid% > nul 2>&1 && echo CherryPy server was stopped!
-tskill %runcherrypyserverpid% > nul 2>&1 && echo CherryPy server was stopped!
-cd ..
+call cronstop.bat
+call serverstop.bat
+echo.
+echo The KA Lite server was stopped.
+cd ..	
