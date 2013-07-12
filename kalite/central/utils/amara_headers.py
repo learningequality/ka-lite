40a913a67bb95dd4a5c9ac52331276f684b51fcb
Dylan Barth <dylan.j.barth@gmail.com>
2013-07-12 14:04:45 -0700

    Adding amara headers.

---
+++ kalite/central/utils/amara_headers.py
@@ -0,0 +1,6 @@
+headers = {
+    # "X-api-username": "kalite",
+    # "X-apikey": "9931eb077687297823e8a23fd6c2bfafae25c543",
+    "X-api-username": "dyl",
+    "X-apikey": "6a7e0af81ce95d6b810761041b72412043851804",
+}
