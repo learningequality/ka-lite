9501ed4fb8f0e0069eeae12cdf2ff33ebc7970da
Jamie Alexandre <jamalex@gmail.com>
2013-07-18 18:21:08 -0700

    Enable tests modules to auto-import all test files in their directories.

--- kalite/main/tests/__init__.py
+++ kalite/main/tests/__init__.py
@@ -1,5 +1,19 @@
-from main.tests.browser_tests import *
-from main.tests.cache_tests import *
-from main.tests.fixture_tests import *
-from main.tests.internet_tests import *
-from main.tests.url_tests import *
+import os, glob
+
+# load the names of all the test modules in the current directory
+module_names = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
+
+# effectively do a `from <module> import *` for all the test modules, so all tests will be included
+for module_name in module_names:
+
+    # skip special modules
+    if module_name.startswith("_"):
+        continue
+
+    # import the module by name, passing globals so it can make use of Django stuff
+    # http://www.diveintopython.net/functional_programming/dynamic_import.html
+    module = __import__(module_name, globals=globals())
+
+    # bring all the contents of the module into the local namespace (ala `from ... import *`)
+    for name in dir(module):
+        locals()[name] = getattr(module, name)
--- kalite/securesync/tests/__init__.py
+++ kalite/securesync/tests/__init__.py
@@ -1,1 +1,19 @@
-from securesync.tests.crypto_tests import *
+import os, glob
+
+# load the names of all the test modules in the current directory
+module_names = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__) + "/*.py")]
+
+# effectively do a `from <module> import *` for all the test modules, so all tests will be included
+for module_name in module_names:
+
+    # skip special modules
+    if module_name.startswith("_"):
+        continue
+
+    # import the module by name, passing globals so it can make use of Django stuff
+    # http://www.diveintopython.net/functional_programming/dynamic_import.html
+    module = __import__(module_name, globals=globals())
+
+    # bring all the contents of the module into the local namespace (ala `from ... import *`)
+    for name in dir(module):
+        locals()[name] = getattr(module, name)
