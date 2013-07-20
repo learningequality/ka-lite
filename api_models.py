96c7caacb5321ed52d344dc346bd2de61a2d9e4a
Richard <richard.tibbles@gmail.com>
2013-07-20 11:58:01 -0700

    PEP-8 Linting.

--- api_models.py
+++ api_models.py
@@ -1,28 +1,30 @@
 import requests
 import json
 
+
 class AttrDict(dict):
 
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
-        
+
     def __getattr__(self, name):
         value = self[name]
-        if type(value) == dict:
+        if isinstance(value, dict):
             value = AttrDict(value)
-        if type(value) == list:
+        if isinstance(value, list):
             for i in range(len(value)):
-                if type(value[i]) == dict:
+                if isinstance(value[i], dict):
                     value[i] = AttrDict(value[i])
         return value
 
     def __setattr__(self, name, value):
         self[name] = value
 
+
 class APIModel(AttrDict):
-    
-    _related_field_types = {} # this is a dummy; do not use directly
-    
+
+    _related_field_types = {}  # this is a dummy; do not use directly
+
     API_attributes = {}
 
     def __getattr__(self, name):
@@ -34,12 +36,10 @@ class APIModel(AttrDict):
                 convert_items(name, self)
                 return self[name]
 
-
-
     def __init__(self, *args, **kwargs):
-        
+
         super(APIModel, self).__init__(*args, **kwargs)
-                
+
         for name in getattr(self, "_related_field_types", {}):
             if name in self:
                 convert_items(name, self)
@@ -48,12 +48,16 @@ class APIModel(AttrDict):
         return self.base_url + "/" + self.id + self.API_attributes[name]
 
 # usage : api_call("v1", "/badges")
+
+
 def api_call(target_version, target_api_url):
     try:
-        json_object = json.loads(requests.get("http://www.khanacademy.org/api/" + target_version + target_api_url).content)
+        json_object = json.loads(requests.get(
+            "http://www.khanacademy.org/api/" + target_version + target_api_url).content)
     except:
         return {}
-    return json_object                    
+    return json_object
+
 
 def class_by_kind(node):
     return kind_to_class_map[node["kind"]](node)
@@ -64,11 +68,12 @@ def convert_list_to_classes(nodelist, class_converter=class_by_kind):
     Convert each element of the list (in-place) into an instance of a subclass of APIModel.
     You can pass a particular class to `class_converter` if you want to, or it will auto-select by kind.
     """
-    
+
     for i in range(len(nodelist)):
         nodelist[i] = class_converter(nodelist[i])
-    
-    return nodelist # just for good measure; it's already been changed
+
+    return nodelist  # just for good measure; it's already been changed
+
 
 def convert_items(name, self):
     """
@@ -77,11 +82,12 @@ def convert_items(name, self):
     """
 
     # convert dicts to the related type
-    if type(self[name]) == dict:
+    if isinstance(self[name], dict):
         self[name] = self._related_field_types[name](self[name])
     # convert every item in related list to correct type
-    elif type(self[name]) == list:
-        convert_list_to_classes(self[name], class_converter=self._related_field_types[name])
+    elif isinstance(self[name], list):
+        convert_list_to_classes(self[
+                                name], class_converter=self._related_field_types[name])
 
 
 class Video(APIModel):
@@ -99,12 +105,13 @@ class Exercise(APIModel):
 
     base_url = "/exercises"
 
-    API_attributes = {"related_videos": "/videos", "followup_exercises": "/followup_exercises"}
+    API_attributes = {"related_videos": "/videos",
+                      "followup_exercises": "/followup_exercises"}
     pass
 
 
 class Topic(APIModel):
-    
+
     _related_field_types = {
         "children": class_by_kind,
     }
@@ -126,7 +133,7 @@ class UserVideo(APIModel):
     pass
 
 
-#ProblemLog and VideoLog API calls return multiple entities in a list
+# ProblemLog and VideoLog API calls return multiple entities in a list
 
 class ProblemLog(APIModel):
     pass
@@ -140,39 +147,39 @@ kind_to_class_map = {
     "video": Video,
     "exercise": Exercise,
     "topic": Topic,
-}    
+}
 
 t = Topic({
-    "name": "Penguin Watchers", 
+    "name": "Penguin Watchers",
     "children":
-        [
+    [
             {
                 "kind": "video",
                 "name": "Waddling"
-            }, 
-            {
-                "kind": "exercise",
-                "name": "Waddle Test"
             },
-            {
-                "kind": "topic",
-                "name": "More stuff",
-                "children": [
+    {
+    "kind": "exercise",
+    "name": "Waddle Test"
+    },
+        {
+            "kind": "topic",
+            "name": "More stuff",
+            "children": [
                     {
                         "kind": "video",
                         "name": "Deep Secrets"
                     }
-                ]
-            },
-        ],
+            ]
+        },
+    ],
 })
 
 if __name__ == "__main__":
-    #print t.name
-    #print t.children
-    #print t.children[0].__class__
-    #print t.children[1].__class__
-    #print api_call("v1", "/videos");
-    #print api_call("nothing");
-    #Video.get_video("adding-subtracting-negative-numbers")
+    # print t.name
+    # print t.children
+    # print t.children[0].__class__
+    # print t.children[1].__class__
+    # print api_call("v1", "/videos");
+    # print api_call("nothing");
+    # Video.get_video("adding-subtracting-negative-numbers")
     Video.get_video("C38B33ZywWs")
