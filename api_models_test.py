cefd16af2fa6ebb05871a2ea6f7d80a42366bd12
Rui Malheiro <rui@WOLF.local>
2013-07-21 15:49:04 -0700

    update on test cases

--- api_models_test.py
+++ api_models_test.py
@@ -26,6 +26,21 @@ class ApiCallExerciseTest(unittest.TestCase):
 		"""Tests if the result object contains the requested Exercise ID."""
 		self.assertEqual("logarithms_1", self.exercise_object.name)
 
+	def test_get_exercise_related_videos(self):
+		"""Tests if the result is and empty list or if it is a list of Video objects."""
+		if not self.exercise_object.related_videos:
+			self.assertListEqual(self.exercise_object.related_videos, [])
+		else:
+			for obj in self.exercise_object.related_videos:
+				self.assertIsInstance(obj, Video)
+
+	def test_get_exercise_followup_exercises(self):
+		"""Tests if the result is and empty list or if it is a list of Exercise objects."""
+		if not self.exercise_object.followup_exercises:
+			self.assertListEqual(self.exercise_object.followup_exercises, [])
+		else:
+			for obj in self.exercise_object.followup_exercises:
+				self.assertIsInstance(obj, Exercise)
 
 
 class ApiCallBadgeTest(unittest.TestCase):
@@ -72,16 +87,67 @@ class ApiCallUserTest(unittest.TestCase):
 
 	Attributes:
 		user_object: An User object that is created after the user login.
+		badges_object: A Badge object that cointains UserBadge objects if the user is logged in.
 	"""
 
 	def setUp(self):
 		"""Prepares the objects that will be tested."""
 		self.user_object = User.get_user()
+		self.badges_object = Badge.get_badges()
 
 	def test_get_user(self):
 		"""Tests if the result is an instance of User. The object is created if the result of the API call is a success."""
 		self.assertIsInstance(self.user_object, User)
 
+	def test_get_user_videos(self):
+		"""Tests if the result is an emty list or if it is a list of UserVideo objects.
+		   For each UserVideo object check if log contains VideoLog objects.
+		"""
+		if not self.user_object.videos:
+			self.assertListEqual(self.user_object.videos, [])
+		else:
+			for obj in self.user_object.videos:
+				self.assertIsInstance(obj, UserVideo)
+				if not obj.log:
+					self.assertListEqual(obj.log, [])
+				else:
+					for l_obj in obj.log:
+						self.assertIsInstance(l_obj, VideoLog)
+
+	def test_get_user_exercises(self):
+		"""Tests if the result is an emty list or if it is a list of UserExercise objects.
+		   For each UserExercise object, checks if log attribute only contains ProblemLog objects
+		   and if followup_exercises attribute only contains UserExercise objects.
+		"""
+		if not self.user_object.exercises:
+			self.assertListEqual(self.user_object.exercises, [])
+		else:
+			for obj in self.user_object.exercises:
+				self.assertIsInstance(obj, UserExercise)
+				if not obj.log:
+					self.assertListEqual(obj.log, [])
+				else:
+					for l_obj in obj.log:
+						self.assertIsInstance(l_obj, ProblemLog)
+				if not obj.followup_exercises:
+					self.assertListEqual(obj.followup_exercises, [])
+				else:
+					for f_obj in obj.followup_exercises:
+						self.assertIsInstance(f_obj, UserExercise)
+
+	def test_get_user_badges(self):
+		"""Tests if the result is an emty list or if it is a list of Badge objects. 
+		   Then for each Badge, if it contains the user_badges key, it must be an instance of User Badges.
+		"""
+		if not self.badges_object:
+			self.assertListEqual(self.badges_object, [])
+		else:
+			for obj in self.badges_object:
+				if not obj.__contains__("user_badges"):
+					continue
+				else:
+					for u_obj in obj.user_badges:
+						self.assertIsInstance(u_obj, UserBadge)
 
 
 class ApiCallTopicTest(unittest.TestCase):
@@ -157,11 +223,11 @@ def prepare_suites_from_test_cases(case_class_list):
 # Add or remove test cases as needed.
 test_cases = [
 
-	ApiCallExerciseTest,
-	ApiCallBadgeTest,
+	#ApiCallExerciseTest,
+	#ApiCallBadgeTest,
 	ApiCallUserTest,
-	ApiCallTopicTest,
-	ApiCallVideoTest,
+	#ApiCallTopicTest,
+	#ApiCallVideoTest,
 
 ]
 
