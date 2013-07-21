624eb7ab3f196473e446e060df44f854277e438d
Rui Malheiro <rui@WOLF.local>
2013-07-21 14:15:17 -0700

    Better mechanism to add TestCases and added comments.

--- api_models_test.py
+++ api_models_test.py
@@ -2,12 +2,20 @@ import unittest
 from api_models import *
 
 class ApiCallExerciseTest(unittest.TestCase):
+	"""Performs an API call to fetch exercises and verifies the result.
+
+	Attributes:
+		exercises_list_object: A list of Exercise objects.
+		exercise_object: An Exercise object that was specifically requested.
+	"""
 
 	def setUp(self):
+		"""Prepares the objects that will be tested."""
 		self.exercises_list_object = Exercise.get_exercises()
 		self.exercise_object = Exercise.get_exercise("logarithms_1")
 	
 	def test_get_exercises(self):
+		"""Tests if the result is an empty list or if it is a list of Exercise objects."""
 		if not self.exercises_list_object:
 			self.assertListEqual(self.exercises_list_object, [])
 		else:
@@ -15,17 +23,28 @@ class ApiCallExerciseTest(unittest.TestCase):
 				self.assertIsInstance(obj, Exercise)
 
 	def test_get_exercise(self):
+		"""Tests if the result object contains the requested Exercise ID."""
 		self.assertEqual("logarithms_1", self.exercise_object.name)
 
 
+
 class ApiCallBadgeTest(unittest.TestCase):
+	"""Performs an API call to fetch badges and verifies the result.
+
+	Attributes:
+		badges_list_object: A list of Badge objects.
+		badges_category_object: A BadgeCategory object that was specifically requested.
+		badges_category_list_object: A list of BadgeCategory objects.
+	"""
 
 	def setUp(self):
+		"""Prepares the objects that will be tested."""
 		self.badges_list_object = Badge.get_badges()
 		self.badges_category_object = Badge.get_category(1)
 		self.badges_category_list_object = Badge.get_category()
 
 	def test_get_badges(self):
+		"""Tests if the result is an empty list or if it is a list of Bagde objects."""
 		if not self.badges_list_object:
 			self.assertListEqual(self.badges_list_object, [])
 		else:
@@ -33,9 +52,11 @@ class ApiCallBadgeTest(unittest.TestCase):
 				self.assertIsInstance(obj, Badge)
 
 	def test_get_category(self):
+		"""Tests if the result object contains the requested Badge category."""
 		self.assertEqual(self.badges_category_object.category, 1)
 
 	def test_get_category_list(self):
+		"""Tests if the result is an emty list or if it is a list of BadgeCategory objects."""
 		if not self.badges_category_list_object:
 			self.assertListEqual(self.badges_category_list_object, [])
 		else:
@@ -43,30 +64,51 @@ class ApiCallBadgeTest(unittest.TestCase):
 				self.assertIsInstance(obj, BadgeCategory)
 
 
+
 class ApiCallUserTest(unittest.TestCase):
+	"""Performs an API call to fetch user data and verifies the result.
+
+	This test will require login in Khan Academy.
+
+	Attributes:
+		user_object: An User object that is created after the user login.
+	"""
 
 	def setUp(self):
-		self.user_get_user_object = User.get_user()
+		"""Prepares the objects that will be tested."""
+		self.user_object = User.get_user()
 
 	def test_get_user(self):
-		self.assertIsInstance(self.user_get_user_object, User)
+		"""Tests if the result is an instance of User. The object is created if the result of the API call is a success."""
+		self.assertIsInstance(self.user_object, User)
+
 
 
 class ApiCallTopicTest(unittest.TestCase):
+	"""Performs an API call to fetch Topic data and verifies the result.
+
+	Attributes:
+		topic_tree_object: A Topic object that represents the entire Topic tree.
+		topic_subtree_object: A Topic object that was specifically requested. It represents a subtree. 
+	"""
 
 	def setUp(self):
+		"""Prepares the objects that will be tested."""
 		self.topic_tree_object = Topic.get_tree()
 		self.topic_subtree_object = Topic.get_tree("addition-subtraction")
 		self.topic_exercises_list_object = Topic.get_topic_exercises("addition-subtraction")
 		self.topic_videos_list_object = Topic.get_topic_videos("addition-subtraction")
 	
 	def test_get_tree(self):
+		"""Tests if the result is an instance of Topic."""
 		self.assertIsInstance(self.topic_tree_object, Topic)
 	
 	def test_get_subtree(self):
+		"""Tests if the result object contains the requested topic slug."""
 		self.assertEqual("addition-subtraction", self.topic_subtree_object.slug)
 
 	def test_get_topic_exercises(self):
+		"""Tests if the result is an emty list or if it is a list of Exercise objects."""
 		if not self.topic_exercises_list_object:
 			self.assertListEqual(self.topic_exercises_list_object, [])
 		else:
@@ -74,6 +116,7 @@ class ApiCallTopicTest(unittest.TestCase):
 				self.assertIsInstance(obj, Exercise)	
 
 	def test_get_topic_videos(self):
+		"""Tests if the result is an emty list or if it is a list of Video objects."""
 		if not self.topic_videos_list_object:
 			self.assertListEqual(self.topic_videos_list_object, [])
 		else:
@@ -81,36 +124,50 @@ class ApiCallTopicTest(unittest.TestCase):
 				self.assertIsInstance(obj, Video)
 
 
+
 class ApiCallVideoTest(unittest.TestCase):
+	"""Performs an API call to fetch video data and verifies the result.
+
+	Attributes:
+		video_object: A Video object that was specifically requested.
+	"""
 
 	def setUp(self):
+		"""Prepares the objects that will be tested."""
 		self.video_object = Video.get_video("adding-subtracting-negative-numbers")
 	
 	def test_get_video(self):
+		"""Tests if the result object contains the requested video readable id."""
 		self.assertEqual("adding-subtracting-negative-numbers", self.video_object.readable_id)
 
 
 
 def prepare_suites_from_test_cases(case_class_list):
+	"""
+	This function prepares a list of suites to be tested.
+	"""
 	test_suites = []
 	for cls in case_class_list:
 		test_suites.append(unittest.TestLoader().loadTestsFromTestCase(cls))
 	return test_suites
 
 
-# test_cases contain the classes that will be tested. 
+
+# "test_cases" contains the classes that will be tested. 
+# Add or remove test cases as needed.
 test_cases = [
 
-	#ApiCallExerciseTest,
-	#ApiCallBadgeTest,
-	#ApiCallUserTest,
-	#ApiCallTopicTest,
-	#ApiCallVideoTest,
+	ApiCallExerciseTest,
+	ApiCallBadgeTest,
+	ApiCallUserTest,
+	ApiCallTopicTest,
+	ApiCallVideoTest,
 
 ]
 
-
+# Prepares a set of suites.
 all_tests = unittest.TestSuite(prepare_suites_from_test_cases(test_cases))
 
+# Runs all tests on suites passed as an argument.
 unittest.TextTestRunner(verbosity=2).run(all_tests)
 		
