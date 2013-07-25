dab59131e38fe843aa358c68dc8a29c32e67b5f1
gimick <bbtgaf@googlemail.com>
2013-07-25 23:36:54 +0100

    new katest management command "clean_db"

--- kalite/management/commands/katest.py
+++ kalite/management/commands/katest.py
@@ -1,10 +1,15 @@
 """
 This is a command-line tool to execute functions helpful to testing.
 """
+
+import logging
+from optparse import make_option
+
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 from config.models import Settings
-from securesync.models import Device, DeviceZone, Zone
+from securesync.models import Device, DeviceZone, Zone, Facility
 
 
 def unregister_distributed_server():
@@ -27,16 +32,41 @@ def unregister_distributed_server():
     # Delete central server
     Device.objects.filter(devicemetadata__is_trusted=True).delete()
 
-
+def clean_db():
+    """
+    Delete kalite data associated with Zone, Facility and Device
+    Does not remove the django admin accounts etc.
+    """
+    settings.LOG.info("Cleaning Zone")
+    Zone.objects.all().delete()
+    settings.LOG.info("Cleaning Facility")
+    Facility.objects.all().delete()
+    settings.LOG.info("Cleaning Device")
+    Device.objects.all().delete()
+        
+    
 class Command(BaseCommand):
     help = "KA Lite test help"
+    option_list = BaseCommand.option_list + (
+    make_option('--noinput', action='store_false', dest='interactive', default=True,
+            help='Tells Django to NOT prompt the user for input of any kind.'),)
 
     def handle(self, *args, **options):
+        interactive = options.get('interactive')
         if not args:
             raise CommandError("Must specify a test-only method to run..")
 
         elif args[0] == "unregister":
             unregister_distributed_server()
+            
+        elif args[0] == "clean_db":
+            if interactive:
+                confirm = raw_input("""Confirm data will be deleted
+    Type 'yes' to continue, or 'no' to cancel: """)
+            else:
+                confirm = 'yes'
+            if confirm == 'yes':
+                clean_db()
 
         else:
             raise CommandError("Unrecognized test-only method: %s" % args[0])
