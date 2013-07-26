3d0508efa32fc2da405f4a578380324b8d29dcee
Ben Cipollini <ben@learningequality.org>
2013-07-25 22:26:00 -0700

    adding empty string as default value for forms.

--- kalite/securesync/forms.py
+++ kalite/securesync/forms.py
@@ -41,8 +41,8 @@ class FacilityUserForm(forms.ModelForm):
         }
 
     def clean(self):
-        facility = self.cleaned_data.get('facility')
-        username = self.cleaned_data.get('username')
+        facility = self.cleaned_data.get('facility', "")
+        username = self.cleaned_data.get('username', "")
         
         # Now validate again, as lowercase
         if FacilityUser.objects.filter(username__iexact=username, facility=facility).count() > 0:
@@ -70,7 +70,7 @@ class FacilityGroupForm(forms.ModelForm):
         fields = ("name",)
         
     def clean(self):
-        name = self.cleaned_data.get("name")
+        name = self.cleaned_data.get("name", "")
         ungrouped = re.compile("[uU]+ngroup")
         
         if ungrouped.match(name):
@@ -92,9 +92,9 @@ class LoginForm(forms.ModelForm):
         self.fields['facility'].queryset = Facility.objects.all()
 
     def clean(self):
-        username = self.cleaned_data.get('username')
-        facility = self.cleaned_data.get('facility')
-        password = self.cleaned_data.get('password')
+        username = self.cleaned_data.get('username', "")
+        facility = self.cleaned_data.get('facility', "")
+        password = self.cleaned_data.get('password', "")
 
         # Coerce
         users = FacilityUser.objects.filter(username__iexact=username, facility=facility)
