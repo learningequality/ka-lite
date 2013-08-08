5cc620d2f071253e72eb456d57dcbc30aeb979bf
Dylan Barth <dylan.j.barth@gmail.com>
2013-08-07 17:14:04 -0700

    Migrated central server scripts for subtitle downloading to this branch.

---
+++ kalite/utils/subtitles/generate_subtitle_counts.py
@@ -0,0 +1,86 @@
+import json
+import os
+import sys
+
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+
+import subtitle_utils 
+
+PROJECT_PATH = os.path.dirname(os.path.realpath(__file__)) + "/../../"
+sys.path = [PROJECT_PATH] + sys.path
+
+from main import topicdata
+import settings
+
+LANGUAGE_LOOKUP = topicdata.LANGUAGE_LOOKUP
+LANGUAGE_LIST   = topicdata.LANGUAGE_LIST
+
+data_path = settings.DATA_PATH + "subtitledata/"
+
+logger = subtitle_utils.setup_logging("generate_subtitle_counts")
+
+
+class LanguageNameDoesNotExist(Exception):
+
+    def __str__(value):
+        return "The language name doesn't exist yet. Please add it to the lookup dictionary located at static/data/languages.json"
+
+
+def get_new_counts():
+    """Return dictionary of srt file counts in respective locale folders"""
+    locale_path = settings.LOCALE_PATHS[0]  # ka-lite/locale/
+
+    subtitle_counts = {}
+    # index into ka-lite/locale/
+    for (dirpath, languages, filenames) in os.walk(locale_path):
+        for lang_code in languages:
+            subtitles_path = "%s%s/subtitles/" % (locale_path, lang_code) 
+            try:      
+                count = len(os.listdir(subtitles_path))
+            except:
+                logger.info("No subs for %s" % lang_code)
+                continue
+            else: 
+                lang_name = get_language_name(lang_code) 
+                subtitle_counts[lang_name] = {}
+                subtitle_counts[lang_name]["count"] = count
+                subtitle_counts[lang_name]["code"] = lang_code
+    write_new_json(subtitle_counts)
+    update_language_list(subtitle_counts)
+
+
+def get_language_name(lang_code):
+    """Return full language name from ISO 639-1 language code, raise exception if it isn't hardcoded yet"""
+    language_name = LANGUAGE_LOOKUP.get(lang_code)
+    if language_name:
+        logger.info("%s: %s" %(lang_code, language_name))
+        return language_name
+    else: 
+        raise LanguageNameDoesNotExist()
+
+
+def write_new_json(subtitle_counts):
+    """Write JSON to file in static/data/subtitledata"""
+    filename = "subtitle_counts.json"
+    filepath = data_path + filename
+    logger.info("Writing fresh srt counts to %s" % filepath)
+    with open(filepath, 'wb') as fp:
+        json.dump(subtitle_counts, fp)
+
+
+def update_language_list(sub_counts):
+    """Update hardcoded language codes if any supported subtitle languages aren't there."""
+    for data in sub_counts.values():
+        lang_code = data.get("code")
+        if lang_code not in LANGUAGE_LIST:
+            logger.info("Adding %s to language code list" % lang_code)
+            LANGUAGE_LIST.append(lang_code)
+    with open(os.path.join(settings.DATA_PATH, "listedlanguages.json"), 'wb') as fp:
+        json.dump(LANGUAGE_LIST, fp)
+
+
+if __name__ == "__main__":
+    get_new_counts()
+
+
