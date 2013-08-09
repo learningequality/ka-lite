9bd5d63ada2ad26dec8c34b06cee19c046253957
Ben Cipollini <ben@learningequality.org>
2013-08-09 06:58:49 -0700

    updating comments, debug-based Exception forcing, to complete.

--- kalite/main/management/commands/videodownload.py
+++ kalite/main/management/commands/videodownload.py
@@ -35,38 +35,43 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         caching_enabled = settings.CACHE_TIME != 0
-        handled_video_ids = []
-        failed_video_ids = []
+        handled_video_ids = []  # stored to deal with caching
+        failed_video_ids = []  # stored to avoid requerying failures.
 
         while True: # loop until the method is aborted
             
             if VideoFile.objects.filter(download_in_progress=True).count() > 0:
                 self.stderr.write("Another download is still in progress; aborting.\n")
                 break
-            
+
+            # Grab any video that hasn't been tried yet
             videos = VideoFile.objects.filter(flagged_for_download=True, download_in_progress=False).exclude(youtube_id__in=failed_video_ids)
             if videos.count() == 0:
                 self.stdout.write("Nothing to download; aborting.\n")
                 break
 
             video = videos[0]
-            
+
+            # User intervention
             if video.cancel_download == True:
                 video.download_in_progress = False
                 video.save()
                 self.stdout.write("Download cancelled; aborting.\n")
                 break
-            
+
+            # Grab a video as OURS to handle, set fields to indicate to others that we're on it!
             video.download_in_progress = True
             video.percent_complete = 0
             video.save()
             
             self.stdout.write("Downloading video '%s'...\n" % video.youtube_id)
             try:
-                xxx
                 download_video(video.youtube_id, callback=download_progress_callback(self, video))
+                handled_video_ids.append(video.youtube_id)
                 self.stdout.write("Download is complete!\n")
             except Exception as e:
+                # On error, report the error, mark the video as not downloaded,
+                #   and allow the loop to try other videos.
                 self.stderr.write("Error in downloading %s: %s\n" % (video.youtube_id, e))
                 video.download_in_progress = False
                 video.save()
@@ -74,13 +79,13 @@ class Command(BaseCommand):
                 failed_video_ids.append(video.youtube_id)
                 continue
 
-            handled_video_ids.append(video.youtube_id)
-            
             # Expire, but don't regenerate until the very end, for efficiency.
             if caching_enabled:
                 caching.invalidate_cached_topic_hierarchies(video_id=video.youtube_id)
-    
-        # Regenerate all pages, efficiently
+
+        # After all is done, regenerate all pages
+        #   since this is computationally intensive, only do it after we're sure
+        #   nothing more will change (so that we don't regenerate something that is
+        #   later invalidated by another video downloaded in the loop)
         if caching_enabled:
             caching.regenerate_cached_topic_hierarchies(handled_video_ids)
-        
