a1ee53699872f4d2665782166ea0d96fd5ca16e0
Aron Fyodor Asor <aronasorman@gmail.com>
2013-08-12 17:07:28 -0700

    check if username given is in User tables

---
+++ kalite/securesync/tests/form_tests.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from securesync.forms import FacilityUserForm
+from securesync.models import Facility
+
+class UserRegistration(TestCase):
+
+    def setUp(self):
+        self.f = Facility.objects.create(name='testfac')
+        password = make_password('insecure')
+        self.admin = User.objects.create(username='testadmin',
+                                              password=password)
+
+    def test_admin_and_user_no_common_username(self):
+        data = {'username': self.admin.username,
+                'facility': self.f,
+                'group': None,
+                'password': 'doesntmatter',
+                'password_recheck': 'doesntmatter',
+        }
+        response = self.client.post('/securesync/addstudent/', data)
+        self.assertFormError(response, 'form', 'username', 'A user with this username at this facility already exists. Please choose a new username and try again.')
