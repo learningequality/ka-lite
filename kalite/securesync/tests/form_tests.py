2693248e902515872c8dd5e321fc821b5ed34f33
Aron Fyodor Asor <aronasorman@gmail.com>
2013-08-14 11:00:13 -0700

    cleanup form tests

--- kalite/securesync/tests/form_tests.py
+++ kalite/securesync/tests/form_tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 
 from securesync.forms import FacilityUserForm
@@ -14,17 +15,15 @@ class UserRegistration(TestCase):
                                               password=password)
         self.data = {'username': 'testuser',
                 'facility': self.f.id,
-                # 'group': None,
                 'password': 'doesntmatter',
                 'password_recheck': 'doesntmatter',
         }
 
     def test_facility_user_form_works(self):
-        response = self.client.post('/securesync/addstudent/', self.data)
-        import pdb; pdb.set_trace()
+        response = self.client.post(reverse('add_facility_student'), self.data)
         FacilityUser.objects.get(username=self.data['username']) # should not raise error
 
     def test_admin_and_user_no_common_username(self):
         self.data['username'] = self.admin.username
-        response = self.client.post('/securesync/addstudent/', self.data)
+        response = self.client.post(reverse('add_facility_student'), self.data)
         self.assertFormError(response, 'form', 'username', 'The specified username is unavailable. Please choose a new username and try again.')
