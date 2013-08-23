3d6b809e48261da31a7bbcb22ca53ff723579581
Ben Cipollini <ben@learningequality.org>
2013-08-23 14:38:17 -0700

    mucking around with loadtesting

--- kalite/__init__.py
+++ kalite/__init__.py
@@ -1,10 +1,14 @@
 # testing isn't always available; just ignore if not
 try:
-    import tests.testrunner
-    import tests.loadtesting
-except:
+    import utils.testing.testrunner
+except Exception as e:
     pass
-    
+try:
+    import tests.loadtesting as loadtesting
+except Exception as e:
+    pass
+
+
 import version
 VERSION = version.VERSION
 
