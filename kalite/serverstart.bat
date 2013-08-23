2b16801aefd51122c702a39789dc95ba22913c03
Ben Cipollini <ben@learningequality.org>
2013-08-23 12:18:06 -0700

    fixing bug on windows script.

--- kalite/serverstart.bat
+++ kalite/serverstart.bat
@@ -10,5 +10,5 @@ set OLD_DIR=%CD%
 set SCRIPT_DIR=%~dp0
 cd "%SCRIPT_DIR%"
 echo.
-start /b python.exe manage.py runcherrypyserver host=0.0.0.0 port=%PORT% daemonize=True threads=%CHERRYPY_THREAD_COUNT% pidfile="%SCRIPT_DIR%\runcherrypyserver.pid"
+start /b python.exe manage.py runcherrypyserver host=0.0.0.0 port=%PORT% daemonize=True threads=%NTHREADS% pidfile="%SCRIPT_DIR%\runcherrypyserver.pid"
 CD "%OLD_DIR%"
