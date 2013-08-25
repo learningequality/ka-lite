778f6bcd403724aa93f9f0a170841c365ca1feae
Ben Cipollini <ben@learningequality.org>
2013-08-25 11:16:31 -0700

    JsonResponse chokes on datetime; no longer\! Use it for coachreports API.  Clean API error handling.

--- kalite/utils/internet/classes.py
+++ kalite/utils/internet/classes.py
@@ -1,9 +1,9 @@
 """
 For functions mucking with internet access
 """
+import datetime;
 from django.http import HttpResponse
 from django.utils import simplejson
-
 
 class StatusException(Exception):
     """Class used for turning a HTTP response error into an exception"""
@@ -20,13 +20,16 @@ class CsvResponse(HttpResponse):
         super(CsvResponse, self).__init__(content, content_type='text/csv', *args, **kwargs)
 
 
+def _dthandler(obj):
+    "Handler for object types that are not Json-serializable"
+    return  obj.isoformat() if isinstance(obj, datetime.datetime) else None
+
 class JsonResponse(HttpResponse):
     """Wrapper class for generating a HTTP response with JSON data"""
     def __init__(self, content, *args, **kwargs):
         if not isinstance(content, basestring):
-            content = simplejson.dumps(content, ensure_ascii=False)
+            content = simplejson.dumps(content, ensure_ascii=False, default=_dthandler)
         super(JsonResponse, self).__init__(content, content_type='application/json', *args, **kwargs)
-
 
 class JsonpResponse(HttpResponse):
     """Wrapper class for generating a HTTP response with JSONP data"""
