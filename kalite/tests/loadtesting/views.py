66856458a48b87f377a078d635ba5e2bf77b0db2
Ben Cipollini <ben@learningequality.org>
2013-08-30 00:22:47 -0700

    implement multiple users; pre-create them in the view.

--- kalite/tests/loadtesting/views.py
+++ kalite/tests/loadtesting/views.py
@@ -1,8 +1,22 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, HttpResponseServerError
-from django.shortcuts import render_to_response, get_object_or_404, redirect, get_list_or_404
 from annoying.decorators import render_to
-from config.models import Settings
 
+from securesync.models import Facility, FacilityUser
+
+
+users_are_created = False
 @render_to("loadtesting/load_test.html")
 def load_test(request):
+    global users_are_created
+
+    if not users_are_created:
+        if not Facility.objects.count() > 0:
+            fac = Facility.create({ "name": "fac" })
+        fac = Facility.objects.all()[0]
+        for sidx in range(1, 11):
+            unpw = "s%d" % sidx
+            user = FacilityUser.get_or_initialize(username=unpw, facility=fac)
+            user.set_password(unpw)
+            user.save()
+        users_are_created = True
+
     return {}
