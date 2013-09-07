f8893a5217cb6988293f4583dad6bdcda002479e
gimick <bbtgaf@googlemail.com>
2013-09-07 20:42:48 +0100

    performance: optimize the pragma setup code

--- kalite/__init__.py
+++ kalite/__init__.py
@@ -23,26 +23,28 @@ except:
         OS = ""
 
 #####
-#
-# Implement sqlite PRAGMA from settings
+# Implement sqlite PRAGMA using values in settings
 #
 def activate_pragma(sender, connection, **kwargs):
+
+    if connection.vendor != 'sqlite':
+        return
     try:
         # try: because target hardware may not have sqlite3
         import sqlite3
         if sqlite3.sqlite_version < "3.7":
-            return False
+            return
     except:
-        return False
-    if connection.vendor == 'sqlite':
-        from settings import SQLITE_PRAGMA_SYNCHRONOUS
-        from settings import SQLITE_PRAGMA_JOURNAL_MODE
-        from settings import SQLITE_PRAGMA_AUTOCHECKPOINT
-        print SQLITE_PRAGMA_SYNCHRONOUS, SQLITE_PRAGMA_JOURNAL_MODE, str(SQLITE_PRAGMA_AUTOCHECKPOINT)
-        cursor = connection.cursor()
-        cursor.execute('PRAGMA synchronous=' + SQLITE_PRAGMA_SYNCHRONOUS + ';')
-        cursor.execute('PRAGMA journal_mode=' + SQLITE_PRAGMA_JOURNAL_MODE + ';')
-        cursor.execute('PRAGMA wal_autocheckpoint=' + str(SQLITE_PRAGMA_AUTOCHECKPOINT) + ';')
+        return
+
+    cursor = connection.cursor()
+    from settings import SQLITE_PRAGMA_SYNCHRONOUS
+    from settings import SQLITE_PRAGMA_JOURNAL_MODE
+    cursor.execute('PRAGMA synchronous=' + SQLITE_PRAGMA_SYNCHRONOUS + ';')
+    cursor.execute('PRAGMA journal_mode=' + SQLITE_PRAGMA_JOURNAL_MODE + ';')
+    if SQLITE_PRAGMA_JOURNAL_MODE == 'WAL':
+        from settings import SQLITE_PRAGMA_WAL_AUTOCHECKPOINT
+        cursor.execute('PRAGMA wal_autocheckpoint=' + str(SQLITE_PRAGMA_WAL_AUTOCHECKPOINT) + ';')
 
 from django.db.backends.signals import connection_created
 connection_created.connect(activate_pragma)
