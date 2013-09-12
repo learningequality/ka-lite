a36068fb00da1c381edbed7769c9b30873551b1f
Ben Cipollini <ben@learningequality.org>
2013-09-12 07:46:01 -0700

    migrating cron scripts to use manage.py

--- scripts/cronstart.bat
+++ scripts/cronstart.bat
@@ -1,12 +1,9 @@
 @echo off
-rem TODO(ruimalheiro): Create a more general python script to be crossplatform.
-
 rem have to change directory, in order to make sure we can import settings
-set OLD_DIR=%CD%
 set SCRIPT_DIR=%~dp0
-cd "%SCRIPT_DIR%\..\kalite"
+set KALITE_DIR=%SCRIPT_DIR%\..\kalite
+
+for /f %%a in ('"%SCRIPT_DIR%\get_setting.bat" CRONSERVER_FREQUENCY') do set FREQ=%%a
 
 echo.
-start /b python.exe -c "import subprocess; sp=subprocess.Popen(['python', 'cronserver.py']);f=open('cronserver.pid','w'); f.write(str(sp.pid)); f.close(); exit();"
-
-cd "%OLD_DIR%"
+start /b python.exe -c "import sys, subprocess; sp=subprocess.Popen([sys.executable, '%KALITE_DIR%\manage.py', 'cronserver', %FREQ%]);f=open('cronserver.pid','w'); f.write(str(sp.pid)); f.close(); exit();"
--- scripts/cronstart.sh
+++ scripts/cronstart.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 SCRIPT_DIR=`dirname "${BASH_SOURCE[0]}"`
+KALITE_DIR=$SCRIPT_DIR/../kalite
 pyexec=`"$SCRIPT_DIR/python.sh"`
 
-pushd "$SCRIPT_DIR/../kalite" > /dev/null
-pids=`ps aux | grep cronserver.py | grep -v "grep" | awk '{print $2}'`
+pids=`ps aux | grep cronserver | grep manage | grep -v "grep" | awk '{print $2}'`
 
 if [ "$pids" ]; then
     echo "(Warning: Cron server may still be running; stopping old process ($pids) first)"
-    kill $pids
+    source "$SCRIPT_DIR/cronstop.sh"
 fi
 
-echo "Starting the cron server in the background."
-
-"$pyexec" cronserver.py &
-popd > /dev/null
+freq=`"$SCRIPT_DIR/get_setting.sh" CRONSERVER_FREQUENCY`
+"$pyexec" "$KALITE_DIR/manage.py" cronserver "$freq" &
