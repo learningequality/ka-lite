061d2588537b10029c8c053ddb07534ab71a8a2c
Dylan Barth <dylan.j.barth@gmail.com>
2013-09-18 16:09:29 -0700

    Added command to run make messages and automatically copy new files to static dir where they will be exposed for download.

---
+++ kalite/i18n/management/commands/update_po.py
@@ -0,0 +1,30 @@
+"""
+Update po files, write them to exposed URL.
+"""
+
+import os
+import shutil
+
+from optparse import make_option
+from django.core import management
+from django.core.management.base import BaseCommand, CommandError
+
+import settings
+
+class Command(BaseCommand):
+	option_list = BaseCommand.option_list + (
+		# make_option('--verbose', '-v', dest='', help=''),
+	)
+	help = 'Run command to generate a fresh pot file and expose it via the central/api.'
+
+	def handle(self, **options):
+		# Change to project root dir
+		os.chdir(os.path.join(settings.LOCALE_PATHS[0], "..")
+		# Generate english po file
+		management.call_command('makemessages', locale='en')
+		# Generate english po file from javascript
+		management.call_command('makemessages', domain='djangojs', locale='en')
+
+		# cp new files into static dir (which should be exposed for download) 
+		shutil.copy(os.path.join(settings.LOCALE_PATHS[0], "en/LC_MESSAGES/django.po"), os.path.join(settings.STATIC_ROOT, "pot/kalite.pot"))
+		shutil.copy(os.path.join(settings.LOCALE_PATHS[0], "en/LC_MESSAGES/djangojs.po"), os.path.join(settings.STATIC_ROOT, "pot/kalitejs.pot"))
