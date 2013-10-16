6737a5a71e983fcdeae39aa3ee415a61ac5f83d4
Aron Fyodor Asor <aronasorman@gmail.com>
2013-10-16 01:40:34 -0700

    use localStorage on compatible browsers to cache topic tree

--- kalite/static/js/search_autocomplete.js
+++ kalite/static/js/search_autocomplete.js
@@ -1,20 +1,41 @@
+function isLocalStorageAvailable() {
+    var testString = "hello peeps"
+    try {
+	localStorage[testString] = testString;
+	return true;
+    } catch(e) {
+	return false;
+    }
+}
+
 $(document).ready(function() {
-    var results = [];
-    
-    $.ajax({ 
-        url: "/api/flat_topic_tree", 
-        cache: true,
-        dataType: "json",
-        success: function(categories) {
-            for (var category_name in categories) { // category is either Video, Exercise or Topic
-                var category = categories[category_name];
-                for (var node_name in category) {
-                    node = category[node_name];
-                    results.push(node.title);
-                }
-            }
-        }
-    });
+    var results = null;
+
+    if (isLocalStorageAvailable()) {
+	results = JSON.parse(localStorage.getItem("flat_topic_tree")); // coerce string back to JSON
+    }
+
+    if (results === null) {
+	$.ajax({
+	    url: "/api/flat_topic_tree",
+	    cache: true,
+	    dataType: "json",
+	    success: function(categories) {
+		results = [];
+		for (var category_name in categories) { // category is either Video, Exercise or Topic
+		    var category = categories[category_name];
+		    for (var node_name in category) {
+			node = category[node_name];
+			results.push(node.title);
+		    }
+		}
+		if (isLocalStorageAvailable()) {
+		    localStorage.setItem("flat_topic_tree", JSON.stringify(results)); // we can only store strings in localStorage
+		}
+	    }
+	});
+    }
+
     $("#search").autocomplete({
         minLength: 3,
         source: function(request, response) {
