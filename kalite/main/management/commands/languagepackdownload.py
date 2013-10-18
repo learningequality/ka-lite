1e77535426a9ab5ee0eaab915ec9eaa289ece6eb
Dylan Barth <dylan.j.barth@gmail.com>
2013-10-18 14:48:48 -0700

    Untested command to download l-packs from central server

---
+++ kalite/main/management/commands/languagepackdownload.py
@@ -0,0 +1,97 @@
+import json
+import os
+import optparse 
+import requests
+import zipfile
+
+from django.core.management.base import BaseCommand, CommandError
+
+import settings
+from settings import LOG as logging
+from utils.general import ensure_dir
+from main.models import LanguagePack
+
+
+LOCALE_ROOT = settings.LOCALE_PATHS[0]
+
+class Command(BaseCommand):
+    help = "Download language pack requested from central server"
+
+    option_list = BaseCommand.option_list + (
+        make_option('-l', '--language',
+                    action='store',
+                    dest='lang_code',
+                    default=None,
+                    metavar="LANG_CODE",
+                    help="Specify a particular language code to download language pack for."),
+    )
+
+    def handle(self, *args, **options):
+        code = options.get("lang_code")
+        if not code:
+            raise CommandError("You must specify a language to download a language pack for.")
+
+        ## Download the language pack
+        zip_file = get_language_pack(code)
+
+        ## Unpack into locale directory
+        unpack_language(code, zip_file)
+
+        ## Update database with meta info
+        update_database(code)
+
+
+def get_language_pack(code):
+    """Download language pack for specified language"""
+
+    logging.info("Retrieving language pack: %s" % code)
+    request_url = "%s/static/language_packs/%s.zip" % (settings.CENTRAL_SERVER_HOST, code)
+    r = requests.get(request_url)
+    try:
+        r.raise_for_status()
+    except Exception as e:
+        logging.error(e)
+
+    return r.content
+
+def unpack_language(code, zip_file):
+    """Unpack zipped language pack into locale directory"""
+
+    logging.info("Unpacking new translations")
+    ensure_dir(os.path.join(LOCALE_ROOT, code, "LC_MESSAGES"))
+    
+    ## Unpack into temp dir
+    z = zipfile.ZipFile(StringIO.StringIO(zip_file))
+    z.extractall(os.path.join(LOCALE_ROOT, code))
+
+
+def update_database(code):
+    """Create/update LanguagePack table in database based on given languages metadata"""
+
+    try:
+        metadata = json.loads(open(os.path.join(LOCALE_ROOT, code, "%s_metadata.json" % code)))
+    except Exception as e:
+        logging.error(e)
+
+    logging.info("Updating database for language pack: %s" % code)
+    try:
+        pack, created = LanguagePack.objects.get_or_create(code=code, name=metadata.get("name"))
+        pack.phrases = metadata.get("phrases")
+        pack.approved_translations = metadata.get("translations_approved")
+        pack.percent_translated = metadata.get("percent_approved_translations")
+        pack.version = metadata.get("version")
+
+        pack.save()
+    except Exception as e:
+        logging.error(e)
+    else:
+        logging.info("Successfully updated database.")
+
+
+
+
+
+
+
+
+
