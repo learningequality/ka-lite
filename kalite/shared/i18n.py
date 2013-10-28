1ee81582c10ad0b06d28ed82ffd183b67f5f8bea
Ben Cipollini <ben@learningequality.org>
2013-10-28 16:37:52 -0700

    Update file to loop through all locale paths, and skip directories that don't exist.

--- kalite/shared/i18n.py
+++ kalite/shared/i18n.py
@@ -1,34 +1,33 @@
 """
 Utility functions for i18n related tasks on the distributed server
 """
-
 import os
 import json
 
+from django.utils.translation import ugettext as _
+
 import settings
 
-class MetaDataNotFound(Exception):
-
-    def __str__(value):
-        return "Meta data JSON file not found"
 
 def get_installed_languages():
-	"""Return dictionary of currently installed languages and meta data"""
+    """Return dictionary of currently installed languages and meta data"""
 
-	installed_languages = []
+    installed_languages = []
 
-	# Loop through locale folder
-	locale_dir = settings.LOCALE_PATHS[0]
+    # Loop through locale folders
+    for locale_dir in settings.LOCALE_PATHS:
+        if not os.path.exists(locale_dir):
+            continue
 
-	for lang in os.listdir(locale_dir):
-		# Inside each folder, read from the JSON file - language name, % UI trans, version number
-		try:
-			lang_meta = json.loads(open(os.path.join(locale_dir, lang, "%s_metadata.json" % lang)).read())
-		except:
-			lang_meta = {}
-			# raise MetaDataNotFound()
-		lang = lang_meta
-		installed_languages.append(lang)
+        # Loop through folders in each locale dir
+        for lang in os.listdir(locale_dir):
+            # Inside each folder, read from the JSON file - language name, % UI trans, version number
+            try:
+                lang_meta = json.loads(open(os.path.join(locale_dir, lang, "%s_metadata.json" % lang)).read())
+            except:
+                lang_meta = {}
+            lang = lang_meta
+            installed_languages.append(lang)
 
-	# return installed_languages
-	return installed_languages
+    # return installed_languages
+    return installed_languages
