991ad24688fca6cc1544fd5edb166a2e9a718718
Aron Fyodor Asor <aronasorman@gmail.com>
2013-10-28 11:50:30 -0700

    disable checking of raw_password if it's None

--- kalite/securesync/users/models.py
+++ kalite/securesync/users/models.py
@@ -121,7 +121,8 @@ class FacilityUser(DeferredCountSyncedModel):
         assert hashed_password is None or settings.DEBUG, "Only use hashed_password in debug mode."
         assert raw_password is not None or hashed_password is not None, "Must be passing in raw or hashed password"
         assert not (raw_password is not None and hashed_password is not None), "Must be specifying only one--not both."
-        assert len(raw_password) >= settings.MIN_PASSWORD_LENGTH, "Password must be greater than %d characters." % settings.MIN_PASSWORD_LENGTH
+        if raw_password:
+            assert len(raw_password) >= settings.MIN_PASSWORD_LENGTH, "Password must be greater than %d characters." % settings.MIN_PASSWORD_LENGTH
 
         if hashed_password:
             self.password = hashed_password
