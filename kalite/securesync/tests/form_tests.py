fe242e6aab5038a79aa8ecf7208924a9df78ee41
Aron Fyodor Asor <aronasorman@gmail.com>
2013-11-04 11:19:40 -0800

    make invalid password generation dynamic

--- kalite/securesync/tests/form_tests.py
+++ kalite/securesync/tests/form_tests.py
@@ -34,6 +34,8 @@ class UserRegistration(KALiteTestCase):
         self.assertFormError(response, 'form', 'username', 'The specified username is unavailable. Please choose a new username and try again.')
 
     def test_password_length_enforced(self):
-        self.data['password'] = self.data['password_recheck'] =  'short'
+        # always make passwd shorter than passwd min length setting
+        self.data['password'] = self.data['password_recheck'] =  self.data['password'][:settings.PASSWORD_CONSTRAINTS['min_length']-1]
+
         response = self.client.post(reverse('add_facility_student'), self.data)
         self.assertFormError(response, 'form', 'password', "Password should be at least %d characters." % settings.PASSWORD_CONSTRAINTS['min_length'])
