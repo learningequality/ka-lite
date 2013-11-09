9e55a66b0c890e5e82b8cb2629627b9c97eff93c
Ben Cipollini <ben@learningequality.org>
2013-11-09 07:56:26 -0800

    Refactoring code fully, removing cruft, documenting TODOs

--- kalite/control_panel/urls.py
+++ kalite/control_panel/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('control_panel.views',
     url(r'zone/(?P<zone_id>\w+)/device/(?P<device_id>\w+)/$', 'device_management', {}, 'device_management'),
 
     # Facility
-    url(r'zone/(?P<zone_id>\w+)/facility/$', 'facility_management', {}, 'facility_management'),
     url(r'zone/(?P<zone_id>\w+)/facility/(?P<facility_id>\w+)/edit$', 'facility_form', {}, 'facility_form'),
     url(r'zone/(?P<zone_id>\w+)/facility/(?P<facility_id>\w+)/usage/$', 'facility_usage', {}, 'facility_usage'),
 #    url(r'zone/(?P<zone_id>\w+)/facility/(?P<facility_id>\w+)/users/manage/$', 'facility_user_management', {}, 'facility_user_management'),
--- kalite/control_panel/views.py
+++ kalite/control_panel/views.py
@@ -9,7 +9,7 @@ from django.core.exceptions import ValidationError
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
 from django.db.models import Sum, Max
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext as _
@@ -36,37 +36,35 @@ from utils.internet import CsvResponse, render_to_csv
 @require_authorized_admin
 @render_to("control_panel/zone_form.html")
 def zone_form(request, zone_id, org_id=None):
-    org = get_object_or_None(Organization, pk=org_id) if org_id else None
-    zone = get_object_or_404(Zone, pk=zone_id) if zone_id != "new" else None
+    context = control_panel_context(request, org_id=org_id, zone_id=zone_id)
 
     if request.method == "POST":
         form = ZoneForm(data=request.POST, instance=zone)
         if form.is_valid():
             form.instance.save()
-            if org:
-                org.zones.add(form.instance)
+            if context["org"]:
+                context["org"].zones.add(form.instance)
             if zone_id == "new":
                 zone_id = form.instance.pk
             return HttpResponseRedirect(reverse("zone_management", kwargs={ "org_id": org_id, "zone_id": zone_id }))
     else:
-        form = ZoneForm(instance=zone)
-    return {
-        "org": org,
-        "zone": zone,
-        "form": form,
-    }
+        form = ZoneForm(instance=context["zone"])
+
+    context.update({"form": form})
+    return context
 
 
 @require_authorized_admin
 @render_to("control_panel/zone_management.html")
 def zone_management(request, zone_id, org_id=None):
-    org = get_object_or_None(Organization, pk=org_id) if org_id else None
-    zone = get_object_or_404(Zone, pk=zone_id)# if zone_id != "new" else None
+    context = control_panel_context(request, org_id=org_id, zone_id=zone_id)
     own_device = Device.get_own_device()
 
     # Accumulate device data
     device_data = OrderedDict()
-    for device in Device.objects.filter(devicezone__zone=zone).order_by("devicemetadata__is_demo_device", "name"):
+    for device in list(Device.objects \
+        .filter(devicezone__zone=context["zone"]) \
+        .order_by("devicemetadata__is_demo_device", "name")):
 
         user_activity = UserLogSummary.objects.filter(device=device)
         sync_sessions = SyncSession.objects.filter(client_device=device)
@@ -86,7 +84,7 @@ def zone_management(request, zone_id, org_id=None):
 
     # Accumulate facility data
     facility_data = OrderedDict()
-    for facility in Facility.objects.by_zone(zone).order_by("name"):
+    for facility in list(Facility.objects.by_zone(context["zone"]).order_by("name")):
 
         user_activity = UserLogSummary.objects.filter(user__facility=facility)
         exercise_activity = ExerciseLog.objects.filter(user__facility=facility)
@@ -99,44 +97,29 @@ def zone_management(request, zone_id, org_id=None):
             "last_time_used":   exercise_activity.order_by("-completion_timestamp")[0:1] if user_activity.count() == 0 else user_activity.order_by("-end_datetime")[0],
         }
 
-    return {
-        "org": org,
-        "zone": zone,
+    context.update({
         "facilities": facility_data,
         "devices": device_data,
         "upload_form": UploadFileForm(),
         "own_device_is_trusted": Device.get_own_device().get_metadata().is_trusted,
-    }
-
-
-
-#TODO(bcipolli) I think this will be deleted on the central server side
-@require_authorized_admin
-@render_to("control_panel/facility_management.html")
-def facility_management(request, zone_id, org_id=None):
-    facilities = Facility.objects.by_zone(zone_id)
-    return {
-        "zone_id": zone_id,
-        "facilities": facilities,
-    }
+    })
+    return context
 
 
 @require_authorized_admin
 @render_to("control_panel/facility_usage.html")
 @render_to_csv(["students", "teachers"], key_label="user_id", order="stacked")
 def facility_usage(request, facility_id, org_id=None, zone_id=None, frequency=None):
+    context = control_panel_context(request, org_id=org_id, zone_id=zone_id, facility_id=facility_id)
 
     # Basic data
-    org = get_object_or_None(Organization, pk=org_id) if org_id else None
-    zone = get_object_or_None(Zone, pk=zone_id) if zone_id else None
-    facility = get_object_or_404(Facility, pk=facility_id)
-    groups = FacilityGroup.objects.filter(facility=facility).order_by("name")
+    groups = FacilityGroup.objects.filter(facility=context["facility"]).order_by("name")
     students = FacilityUser.objects \
-        .filter(facility=facility, is_teacher=False) \
+        .filter(facility=context["facility"], is_teacher=False) \
         .order_by("last_name", "first_name", "username") \
         .prefetch_related("group")
     teachers = FacilityUser.objects \
-        .filter(facility=facility, is_teacher=True) \
+        .filter(facility=context["facility"], is_teacher=True) \
         .order_by("last_name", "first_name", "username") \
         .prefetch_related("group")
     
@@ -157,17 +140,108 @@ def facility_usage(request, facility_id, org_id=None, zone_id=None, frequency=No
         period_start = None
         period_end = None
 
-    return {
-        "org": org,
-        "zone": zone,
-        "facility": facility,
+    context.update({
         "groups": group_data,
         "students": student_data,
         "teachers": teacher_data,
         "date_range": [period_start, period_end] if frequency else [None, None],
-    }
+    })
+    return context
 
 
+@require_authorized_admin
+@render_to("control_panel/device_management.html")
+def device_management(request, device_id, org_id=None, zone_id=None, n_sessions=10):
+    context = control_panel_context(request, org_id=org_id, zone_id=zone_id, device_id=device_id)
+
+    # Retrieve sync sessions
+    all_sessions = SyncSession.objects.filter(client_device=context["device"])
+    total_sessions = all_sessions.count()
+    shown_sessions = list(all_sessions.order_by("-timestamp")[:n_sessions])
+
+    context.update({
+        "shown_sessions": shown_sessions,
+        "total_sessions": total_sessions,
+    })
+
+    # If local, get device metadata
+    if device_id == Device.get_own_device().id:
+        context.update(local_device_context(request))
+
+    return context
+
+
+@require_authorized_admin
+@render_to("control_panel/facility_form.html")
+def facility_form(request, facility_id, org_id=None, zone_id=None):
+    context = control_panel_context(request, org_id=org_id, zone_id=zone_id, facility_id=facility_id)
+
+    if request.method != "POST":
+        form = FacilityForm(instance=context["facility"])
+
+    else:
+        form = FacilityForm(data=request.POST, instance=context["facility"])
+        if form.is_valid():
+            form.instance.zone_fallback = get_object_or_404(Zone, pk=zone_id)
+            form.save()
+            return HttpResponseRedirect(reverse("zone_management", kwargs={"org_id": org_id, "zone_id": zone_id}))
+
+    context.update({"form": form})
+    return context
+
+
+@require_authorized_admin
+@render_to("control_panel/group_report.html")
+def group_report(request, facility_id, group_id=None, org_id=None, zone_id=None):
+    context = group_report_context(
+        facility_id=facility_id,
+        group_id=group_id or request.REQUEST.get("group", ""),
+        topic_id=request.REQUEST.get("topic", ""),
+        org_id=org_id,
+        zone_id=zone_id
+    )
+
+    context.update(control_panel_context(request, org_id=org_id, zone_id=zone_id, facility_id=facility_id, group_id=group_id))
+    return context
+
+
+@require_authorized_admin
+@render_to("control_panel/group_users_management.html")
+def facility_user_management(request, facility_id, group_id="", org_id=None, zone_id=None):
+    group_id=group_id or request.REQUEST.get("group","")  # TODO(bcipolli) remove the need for this
+
+    context = user_management_context(
+        request=request,
+        facility_id=facility_id,
+        group_id=group_id,
+        page=request.REQUEST.get("page","1"),
+    )
+
+    context.update(control_panel_context(request, org_id=org_id, zone_id=zone_id, facility_id=facility_id, group_id=group_id))
+    return context
+
+
+@require_authorized_access_to_student_data
+@render_to("control_panel/account_management.html")
+def account_management(request):
+ 
+    # Only log 'coachreport' activity for students, 
+    #   (otherwise it's hard to compare teachers)
+    if "facility_user" in request.session and not request.session["facility_user"].is_teacher and reverse("login") not in request.META.get("HTTP_REFERER", ""):
+        try:
+            # Log a "begin" and end here
+            user = request.session["facility_user"]
+            UserLog.begin_user_activity(user, activity_type="coachreport")
+            UserLog.update_user_activity(user, activity_type="login")  # to track active login time for teachers
+            UserLog.end_user_activity(user, activity_type="coachreport")
+        except ValidationError as e:
+            # Never report this error; don't want this logging to block other functionality.
+            logging.error("Failed to update student userlog activity: %s" % e)
+
+    return student_view_context(request)
+
+
+# data functions
 
 def _get_date_range(frequency):
     """
@@ -280,112 +354,6 @@ def _get_user_usage_data(users, period_start=None, period_end=None):
     return (user_data, group_data)
 
 
-@require_authorized_admin
-@render_to("control_panel/device_management.html")
-def device_management(request, device_id, org_id=None, zone_id=None, n_sessions=10):
-    org = get_object_or_None(Organization, pk=org_id) if org_id else None
-    zone = get_object_or_None(Zone, pk=zone_id) if zone_id else None
-    device = get_object_or_404(Device, pk=device_id)
-
-    # Retrieve sync sessions
-    all_sessions = SyncSession.objects.filter(client_device=device)
-    total_sessions = all_sessions.count()
-    shown_sessions = list(all_sessions.order_by("-timestamp")[:n_sessions])
-
-    context = {
-        "org": org,
-        "zone": zone,
-        "device": device,
-        "shown_sessions": shown_sessions,
-        "total_sessions": total_sessions,
-    }
-
-    # If local, get device metadata
-    if device_id == Device.get_own_device().id:
-        context.update(local_device_context(request))
-
-    return context
-
-
-@require_authorized_admin
-@render_to("control_panel/facility_form.html")
-def facility_form(request, facility_id, org_id=None, zone_id=None):
-    org = get_object_or_None(Organization, pk=org_id) if org_id else None
-    zone = get_object_or_None(Zone, pk=zone_id) if zone_id else None
-    facil = get_object_or_404(Facility, pk=facility_id) if id != "new" else None
-
-    if request.method != "POST":
-        form = FacilityForm(instance=facil)
-
-    else:
-        form = FacilityForm(data=request.POST, instance=facil)
-        if form.is_valid():
-            form.instance.zone_fallback = get_object_or_404(Zone, pk=zone_id)
-            form.save()
-            return HttpResponseRedirect(reverse("zone_management", kwargs={"org_id": org_id, "zone_id": zone_id}))
-
-    return {
-        "org": org,
-        "zone": zone,
-        "facility": facil,
-        "form": form,
-    }
-
-
-@require_authorized_admin
-@render_to("control_panel/group_report.html")
-def group_report(request, facility_id, group_id=None, org_id=None, zone_id=None):
-    context = group_report_context(
-        facility_id=facility_id,
-        group_id=group_id or request.REQUEST.get("group", ""),
-        topic_id=request.REQUEST.get("topic", ""),
-        org_id=org_id,
-        zone_id=zone_id
-    )
-
-    context["org"] = get_object_or_None(Organization, pk=org_id) if org_id else None
-    context["zone"] = get_object_or_None(Zone, pk=zone_id) if zone_id else None
-    context["facility"] = get_object_or_404(Facility, pk=facility_id) if id != "new" else None
-    context["group"] = get_object_or_None(FacilityGroup, pk=group_id)
-
-    return context
-
-
-@require_authorized_admin
-@render_to("control_panel/group_users_management.html")
-def facility_user_management(request, facility_id, group_id="", org_id=None, zone_id=None):
-    group_id=group_id or request.REQUEST.get("group","")
-
-    context = user_management_context(
-        request=request,
-        facility_id=facility_id,
-        group_id=group_id,
-        page=request.REQUEST.get("page","1"),
-    )
-    context.update(control_panel_context(request, facility_id=facility_id, group_id=group_id, org_id=org_id, zone_id=zone_id))
-    return context
-
-
-@require_authorized_access_to_student_data
-@render_to("control_panel/account_management.html")
-def account_management(request, org_id=None):
-
-    # Only log 'coachreport' activity for students, 
-    #   (otherwise it's hard to compare teachers)
-    if "facility_user" in request.session and not request.session["facility_user"].is_teacher and reverse("login") not in request.META.get("HTTP_REFERER", ""):
-        try:
-            # Log a "begin" and end here
-            user = request.session["facility_user"]
-            UserLog.begin_user_activity(user, activity_type="coachreport")
-            UserLog.update_user_activity(user, activity_type="login")  # to track active login time for teachers
-            UserLog.end_user_activity(user, activity_type="coachreport")
-        except ValidationError as e:
-            # Never report this error; don't want this logging to block other functionality.
-            logging.error("Failed to update student userlog activity: %s" % e)
-
-    return student_view_context(request)
-
-
 # context functions
 
 def control_panel_context(request, **kwargs):
@@ -398,6 +366,8 @@ def control_panel_context(request, **kwargs):
         context["facility"] = get_object_or_404(Facility, pk=kwargs["facility_id"]) if kwargs["facility_id"] != "new" else None
     if "group_id" in kwargs:
         context["group"] = get_object_or_None(FacilityGroup, pk=kwargs["group_id"])
+    if "device_id" in kwargs:
+        context["device"] = get_object_or_404(Device, pk=kwargs["device_id"])
     return context
 
 
