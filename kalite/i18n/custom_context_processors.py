e7434e82c910cada96be25314df4c81f0147dce0
Ben Cipollini <ben@learningequality.org>
2013-11-12 08:02:18 -0800

    Fixing bug--sometimes context processors CAN be called before middleware!

--- kalite/i18n/custom_context_processors.py
+++ kalite/i18n/custom_context_processors.py
@@ -1,4 +1,7 @@
 def languages(request):
+    if "default_language" not in request.session:
+        return {}  # temporarily skipped middleware, but we'll get back here again.  Tricky Django...
+
     return {
         "DEFAULT_LANGUAGE": request.session["default_language"],
         "language_choices": request.session["language_choices"],
