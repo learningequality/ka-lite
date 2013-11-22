6b2eab1eec6451a82679f25a8a284f0aae6f33a3
Richard <richard.tibbles@gmail.com>
2013-11-22 09:24:12 -0800

    First pass model for dumping research data into.

--- kalite/research/models.py
+++ kalite/research/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 from utils.django_utils import ExtendedModel
 
+class Experiment(ExtendedModel):
+
+	minversion = "0.9.4"
+
+    user = models.ForeignKey(FacilityUser, blank=False, null=False, db_index=True)
+    activity_stage = models.IntegerField(blank=False, null=False)
+    start_datetime = models.DateTimeField(blank=False, null=False)
+    end_datetime = models.DateTimeField(blank=True, null=True)
+
+    
