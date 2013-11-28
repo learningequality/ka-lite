50b973e16e076c821f38d614fbffc9212d23207b
Jamie Alexandre <jamalex@gmail.com>
2013-11-27 18:39:37 -0800

    Added build id to static paths for some CSS/JS files that have changed.

--- kalite/main/custom_context_processors.py
+++ kalite/main/custom_context_processors.py
@@ -1,4 +1,9 @@
 import settings
+import version
+
+# TODO(jamalex): this should be calculated more intelligently, and incorporated into a template tag
+# (see https://github.com/learningequality/ka-lite/issues/1161)
+BUILD_ID = version.VERSION_INFO[version.VERSION]["git_commit"][0:8]
 
 def custom(request):
     return {
@@ -10,5 +15,6 @@ def custom(request):
         "DATA_PATH": settings.DATA_PATH,
         "settings": settings,
         "is_central": False,
-        "restricted": settings.package_selected("UserRestricted")
+        "restricted": settings.package_selected("UserRestricted"),
+        "BUILD_ID": BUILD_ID,
     }
