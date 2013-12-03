6ce981866b4b30c65b2d2cb381b4c856ac530fdd
Ben Cipollini <ben@learningequality.org>
2013-12-02 21:10:41 -0800

    Fix for #1211, by refreshing (for free) cached facility info

--- kalite/securesync/users/middleware.py
+++ kalite/securesync/users/middleware.py
@@ -9,13 +9,16 @@ class AuthFlags:
         request.is_django_user = False
 
         if request.user.is_authenticated():
+            # Django user
             request.is_logged_in = True
             request.is_django_user = True
             if request.user.is_superuser:
                 request.is_admin = True
             if "facility_user" in request.session:
                 del request.session["facility_user"]
+
         elif "facility_user" in request.session:
+            # Facility user
             if request.session["facility_user"].is_teacher:
                 request.is_admin = True
                 request.is_teacher = True
@@ -27,7 +30,8 @@ class FacilityCheck:
         Cache facility data in the session,
           while making sure anybody who can create facilities sees the real (non-cached) data
         """
-        if not "facility_exists" in request.session or request.is_admin:   # always refresh for admins
+        if not "facility_exists" in request.session or request.is_admin:
+            # always refresh for admins, or when no facility exists yet.
             request.session["facility_count"] = Facility.objects.count()
             request.session["facility_exists"] = request.session["facility_count"] > 0
 
