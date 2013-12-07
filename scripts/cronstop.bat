6369a11b3c5d6b4ac43770a7f518298d79f48341
ruimalheiro <rmpmalheiro@gmail.com>
2013-12-07 17:07:26 +0000

    small change when deleting files

--- scripts/cronstop.bat
+++ scripts/cronstop.bat
@@ -10,7 +10,7 @@ if exist "%KALITE_DIR%\cronserver.pid" (
     for /f "tokens=*" %%i in (cronserver.pid) do (
         taskkill /f /pid %%i > nul 2>&1 && echo Cron server was stopped!
         tskill %%i > nul 2>&1 && echo Cron server was stopped!
-        del "%KALITE_DIR%\cronserver.pid" > nul 2>&1
+        del cronserver.pid > nul 2>&1
         cd "%CURRENT_DIR%"
     )
 )
--- scripts/serverstop.bat
+++ scripts/serverstop.bat
@@ -9,7 +9,7 @@ if exist "%KALITE_DIR%\runcherrypyserver.pid" (
   for /f "tokens=*" %%i in (runcherrypyserver.pid) do (
     taskkill /f /pid %%i > nul 2>&1 && echo CherryPy server was stopped!
     tskill %%i > nul 2>&1 && echo CherryPy server was stopped!
-    del "%KALITE_DIR%\runcherrypyserver.pid" > nul 2>&1
+    del runcherrypyserver.pid > nul 2>&1
     cd "%CURRENT_DIR%"
   )
 )
