4bec3d8b70adc5e881c0da4527d5f89c207c3430
Ben Cipollini <bcipolli@ucsd.edu>
2013-12-09 21:25:19 -0800

    Final fixes for #1240, #1234

--- scripts/cronstop.bat
+++ scripts/cronstop.bat
@@ -1,16 +1,17 @@
 @echo off
 
-rem have to change directory, in order to make sure we can access pid files
 set SCRIPT_DIR=%~dp0
-set KALITE_DIR="%SCRIPT_DIR%..\kalite"
-set CURRENT_DIR=%CD%
+set KALITE_DIR=%SCRIPT_DIR%..\kalite
 
 if exist "%KALITE_DIR%\cronserver.pid" (
+    rem have to change directory, in order to make sure we can access pid files
+    set CURRENT_DIR=%CD%
     cd "%KALITE_DIR%"
+
     for /f "tokens=*" %%i in ( cronserver.pid ) do (
         taskkill /f /pid %%i > nul 2>&1 && echo Cron server was stopped!
         tskill %%i > nul 2>&1 && echo Cron server was stopped!
-        del cronserver.pid > nul 2>&1
-        cd "%CURRENT_DIR%"
     )
+    del cronserver.pid > nul 2>&1
+    cd "%CURRENT_DIR%"
 )
