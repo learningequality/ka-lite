cd41a297543bd3497ffef6276cac3aa30de5f88a
Ben Cipollini <ben@learningequality.org>
2013-12-12 09:24:58 -0800

    Reorg script code, add area for metadata.

--- kalite/static/js/update_languages.js
+++ kalite/static/js/update_languages.js
@@ -1,55 +1,70 @@
-var installable_languages = null;
+var installable_languages = [];
+var installed_languages = [];
 
-
-// this function doesn't look good. One too many callbacks!
-$(function() {
-    // basic flow: check with central server what we can install
-    // if that's successful, check with local server of what we have installed
-    // then dont show languages in dropdown box if already installed
-    var url = "http://" + CENTRAL_SERVER_HOST +  "/api/i18n/language_packs/available";
+function get_available_languages() {
+    var url = AVAILABLE_LANGUAGEPACK_URL;
     var request = $.ajax({
         url: url,
+        cache: false,
         dataType: "jsonp",
     }).success(function(languages) {
         installable_languages = languages;
-        display_languages(languages);
+        display_languages();
     }).error(function(data, status, error) {
         installable_languages = [];
-        display_languages([]);
+        display_languages();
         handleFailedAPI(data, [status, error].join(" "), "id_languagepackdownload");
     });
-});
+}
 
-function display_languages(installables) {
-
+function get_installed_languages() {
     $.ajax({
         url: installed_languages_url,
+        cache: false,
         datatype: "json",
     }).success(function(installed) {
+        installed_languages = installed;
+        display_languages();
+    }).error(function(data, status, error) {
+        installed_languages = [];
+        display_languages();
+        handleFailedAPI(data, [status, error].join(" "), "id_languagepackdownload");
+    });
+}
 
-        //
-        // show list of installed languages
-        //
-        $("div.installed-languages").empty();
-        installed.forEach(function(lang, index) {
-            if (lang['name']) { // nonempty name
-                var link_text;
-                if (!(lang['code'] === defaultLanguage)) {
-                    link_text = sprintf("<a href='?set_default_language=%(lang.code)s'>(%(link_text)s)</a>", {
-                        lang: lang,
-                        link_text: gettext("Set as default")
-                    });
-                } else {
-                    link_text = "(Default)";
-                }
-                var lang_name = sprintf("<b>%(name)s</b> (%(code)s)", lang);
-                var lang_data = sprintf(gettext("%(subtitle_count)d Subtitles / %(percent_translated)d%% Translated"), lang);
-                var lang_description = sprintf("<div class='lang-link'>%s </div><div class='lang-name'>%s</div><div class='lang-data'> - %s</div>", link_text, lang_name, lang_data);
+function display_languages() {
+    //this will work regardless of download state; whatever finishes first
+    // will run this function first.
 
-                // check if there's a new version of the languagepack, if so, add an "UPGRADE NOW!" option
-                // NOTE: N^2 algorithm right here, but meh
-                var matching_installable = installables.filter(function(installable_lang) { return lang.code === installable_lang.code; })[0];
-                if (matching_installable) {
+    var installables = installable_languages;
+    var installed = installed_languages;
+
+    //
+    // show list of installed languages
+    //
+    $("div.installed-languages").empty();
+    installed.forEach(function(lang, index) {
+        if (lang['name']) { // nonempty name
+            var link_text;
+            if (!(lang['code'] === defaultLanguage)) {
+                link_text = sprintf("<a href='?set_default_language=%(lang.code)s'>(%(link_text)s)</a>", {
+                    lang: lang,
+                    link_text: gettext("Set as default")
+                });
+            } else {
+                link_text = "(Default)";
+            }
+            var lang_name = sprintf("<b>%(name)s</b> (%(code)s)", lang);
+            var lang_data = sprintf(gettext("%(subtitle_count)d Subtitles / %(percent_translated)d%% Translated"), lang);
+            var lang_description = sprintf("<div class='lang-link'>%s </div><div class='lang-name'>%s</div><div class='lang-data'> - %s</div>", link_text, lang_name, lang_data);
+
+            // check if there's a new version of the languagepack, if so, add an "UPGRADE NOW!" option
+            // NOTE: N^2 algorithm right here, but meh
+            if (installables.length > 0) {
+                var matching_installable = installables.filter(function(installable_lang) { return lang.code === installable_lang.code; });
+                if (matching_installable.length != 0) {
+                    matching_installable = matching_installable[0];
+
                     var upgradeable = matching_installable.language_pack_version > lang.language_pack_version;
                     if (upgradeable) {
                         //add upgrade link here
@@ -66,28 +81,30 @@ function display_languages(installables) {
                         });
                     }
                 }
-                lang_description += "<div class='clear'></div>";
-
-                $("div.installed-languages").append(lang_description);
             }
-        });
+            lang_description += "<div class='clear'></div>";
 
-        //
-        // show list of installable languages in the dropdown box
-        //
-        installables.forEach(function(langdata, langindex) {
-            var srtcount = langdata["subtitle_count"];
-            var percent_translated = langdata["percent_translated"];
-            var langcode = langdata["code"];
+            $("div.installed-languages").append(lang_description);
+        }
+    });
 
-            // if language already installed, dont show in dropdown box
-            var installed_languages = installed.map(function(elem) { return elem['code']; });
-            if ($.inArray(langcode, installed_languages) === -1) { // lang not yet installed
-                if (percent_translated > 0 || srtcount > 0) {
-                    $('#language-packs').append(sprintf('<option id="option-%(code)s" value="%(code)s">%(name)s (%(code)s) %(subtitle_count)d srts / %(percent_translated)d %% translated</option>', langdata));
-                }
+    //
+    // show list of installable languages in the dropdown box
+    //
+    $('#language-packs').find('option').remove()
+    $('#language-packs').append("<option value='' selected=''>--</option>");
+    installables.forEach(function(langdata, langindex) {
+        var srtcount = langdata["subtitle_count"];
+        var percent_translated = langdata["percent_translated"];
+        var langcode = langdata["code"];
+
+        // if language already installed, dont show in dropdown box
+        var installed_languages = installed.map(function(elem) { return elem['code']; });
+        if ($.inArray(langcode, installed_languages) === -1) { // lang not yet installed
+            if (percent_translated > 0 || srtcount > 0) {
+                $('#language-packs').append(sprintf('<option id="option-%(code)s" value="%(code)s">%(name)s (%(code)s)</option>', langdata));
             }
-        });
+        }
     });
 }
 
@@ -95,23 +112,6 @@ function display_languages(installables) {
 // Messy UI stuff incoming
 //
 
-// when we make a selection on the language pack select box, enable the 'Get Language' Button
-// TODO: change so that if they already have a valid selection, activate button anyway
-$(function() {
-    $("#language-packs").change(function(event) {
-        $("#get-language-button").removeAttr("disabled");
-    });
-});
-
-var language_downloading = null;
-
-// start download process once button is clicked
-$(function () {
-    $("#get-language-button").click(function(event) {
-        language_downloading = $("#language-packs").val();
-        start_languagepack_download(language_downloading);
-    });
-});
 
 function start_languagepack_download(lang_code) {
     // tell server to start languagepackdownload job
@@ -138,15 +138,52 @@ function start_languagepack_download(lang_code) {
     });
 }
 
+// when we make a selection on the language pack select box, enable the 'Get Language' Button
+// TODO: change so that if they already have a valid selection, activate button anyway
+$(function() {
+    $("#language-packs").change(function(event) {
+        var lang_code = $("#language-packs").val();
+        var found = false;
+
+        $("#get-language-button").removeAttr("disabled");
+        var matching_installable = installable_languages.filter(function(installable_lang) { return lang_code === installable_lang.code; });
+        var found = (matching_installable.length != 0);
+
+        if (found) {
+            var langdata = matching_installable[0];
+            $("#lp-num-srts").text(sprintf("%d", langdata["subtitle_count"]));
+            $("#lp-pct-trans").text(sprintf("%5.2f%%", langdata["percent_translated"]));
+            $("#lp-down-size").text(sprintf("%5.2f MB", langdata["zip_size"]/1.0E6));
+            $("#lp-disk-size").text(sprintf("%5.2f MB", langdata["package_size"]/1.0E6));
+        }
+        $("#langpack-details").toggle(found);
+    });
+});
+
+var language_downloading = null;
+
+// start download process once button is clicked
+$(function () {
+    $("#get-language-button").click(function(event) {
+        language_downloading = $("#language-packs").val();
+        start_languagepack_download(language_downloading);
+    });
+});
+
 function languagepack_reset_callback(progress, resp) {
-    $.ajax({url: "/api/languagepacks/refresh", async: false});
-    display_languages(installable_languages);
-    $("#option-" + language_downloading).remove();
+    // This will get the latest list of installed languages, and refresh the display.
+    get_installed_languages();
 }
 
 var languagepack_callbacks = {
     reset: languagepack_reset_callback
 };
 
-// Show the (locally) installed languages, while we wait for the central server
-$(function() { display_languages([]); });
+
+$(function() {
+    // basic flow: check with central server what we can install
+    // if that's successful, check with local server of what we have installed
+    // then dont show languages in dropdown box if already installed
+    get_available_languages();
+    get_installed_languages();
+});
