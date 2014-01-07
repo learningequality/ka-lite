2376fc188209550b6a41f104a796ae9b6d74e34d
Ben Cipollini <ben@learningequality.org>
2014-01-06 20:35:43 -0800

    Force update to launch commands as external processes.
    Conflicts:
    	kalite/updates/management/commands/update.py

--- kalite/updates/management/commands/classes.py
+++ kalite/updates/management/commands/classes.py
@@ -42,8 +42,8 @@ class UpdatesDynamicCommand(UpdatesCommand):
         if not self.progress_log.total_stages:
             raise Exception("Must set num-stages (through __init__ or set_stages()) before starting.")
         self.check_if_cancel_requested()
-        self.progress_log.update_stage(stage_name=stage_name, stage_percent=0, notes=notes)
         self.display_notes(notes, ignore_same=False)
+        self.progress_log.update_stage(stage_name=stage_name, stage_percent=0, notes=notes)
 
     def started(self):
         self.check_if_cancel_requested()
@@ -54,29 +54,29 @@ class UpdatesDynamicCommand(UpdatesCommand):
         Allow dynamic resetting of stages.
         """
         self.check_if_cancel_requested()
-        self.progress_log.update_total_stages(num_stages)
         self.display_notes(notes)
+        self.progress_log.update_total_stages(num_stages)
 
     def next_stage(self, stage_name=None, notes=None):
         assert self.started(), "Must call start() before moving to a next stage!"
         self.check_if_cancel_requested()
-        self.update_stage(stage_name=stage_name, stage_percent=0., notes=notes)
         self.display_notes(notes or "")  # blank out old notes, if necessary
+        self.update_stage(stage_name=stage_name, stage_percent=0., notes=notes)
 
     def update_stage(self, stage_name=None, stage_percent=None, stage_status=None, notes=None):
         self.check_if_cancel_requested()
-        self.progress_log.update_stage(stage_name=stage_name, stage_percent=stage_percent, stage_status=stage_status, notes=notes)
         self.display_notes(notes)
+        self.progress_log.update_stage(stage_name=stage_name, stage_percent=stage_percent, stage_status=stage_status, notes=notes)
 
     def cancel(self, stage_status=None, notes=None):
         self.check_if_cancel_requested()
-        self.progress_log.cancel_progress(stage_status=stage_status, notes=notes)
         self.display_notes(notes)
+        self.progress_log.cancel_progress(stage_status=stage_status, notes=notes)
 
     def complete(self, notes=None):
         self.check_if_cancel_requested()
-        self.progress_log.mark_as_completed(notes=notes)
         self.display_notes(notes)
+        self.progress_log.mark_as_completed(notes=notes)
 
     def check_if_cancel_requested(self):
         if self.progress_log.cancel_requested:
@@ -97,13 +97,13 @@ class UpdatesStaticCommand(UpdatesCommand):
         assert self.stages, "Stages must be set before starting."
         assert self.progress_log.current_stage is None, "Must not call start while already in progress."
         self.progress_log.update_total_stages(len(self.stages))
-        self.progress_log.update_stage(stage_name=self.stages[0], stage_percent=0, notes=notes)
         self.display_notes(notes, ignore_same=False)
+        self.progress_log.update_stage(stage_name=self.stages[0], stage_percent=0, notes=notes)
 
     def restart(self, notes=None):
         self.progress_log.restart()
-        self.start(notes=notes)
         self.display_notes(notes)
+        self.start(notes=notes)
 
     def started(self):
         return self.progress_log.current_stage is not None
@@ -111,17 +111,17 @@ class UpdatesStaticCommand(UpdatesCommand):
     def next_stage(self, notes=None):
         assert self.progress_log.current_stage is not None, "Must call start function before next_stage()"
         assert self.progress_log.current_stage < len(self.stages), "Must not be at the last stage already."
-        self.progress_log.update_stage(stage_name=self.stages[self.progress_log.current_stage], stage_percent=0, notes=notes)
         self.display_notes(notes)
+        self.progress_log.update_stage(stage_name=self.stages[self.progress_log.current_stage], stage_percent=0, notes=notes)
 
     def update_stage(self, stage_percent, stage_status=None, notes=None):
-        self.progress_log.update_stage(stage_name=self.stages[self.progress_log.current_stage - 1], stage_percent=stage_percent, stage_status=stage_status, notes=notes)
         self.display_notes(notes)
+        self.progress_log.update_stage(stage_name=self.stages[self.progress_log.current_stage - 1], stage_percent=stage_percent, stage_status=stage_status, notes=notes)
 
     def cancel(self, stage_status=None, notes=None):
-        self.progress_log.cancel_progress(stage_status=stage_status, notes=notes)
         self.display_notes(notes)
+        self.progress_log.cancel_progress(stage_status=stage_status, notes=notes)
 
     def complete(self, notes=None):
-        self.progress_log.mark_as_completed(notes=notes)
         self.display_notes(notes)
+        self.progress_log.mark_as_completed(notes=notes)
