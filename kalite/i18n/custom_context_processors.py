529e43c72a6b27312176deb1d5b8f0143ab76962
Ben Cipollini <ben@learningequality.org>
2014-01-31 06:41:27 -0800

    Show which language is currently selected for video download

--- kalite/i18n/custom_context_processors.py
+++ kalite/i18n/custom_context_processors.py
@@ -1,4 +1,4 @@
-from shared.i18n import lcode_to_ietf
+from shared.i18n import lcode_to_ietf, get_language_name
 
 
 def languages(request):
@@ -9,4 +9,6 @@ def languages(request):
         "default_language": lcode_to_ietf(request.session["default_language"]),
         "language_choices": request.session["language_choices"],
         "current_language": request.language,
+        "current_language_native_name": get_language_name(request.language, native=True),
+        "current_language_name": get_language_name(request.language),
     }
