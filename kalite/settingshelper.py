5e73f2607c40cac41c70af7b6c0d0ab37871f9d4
Ben Cipollini <ben@learningequality.org>
2014-02-01 07:21:42 -0800

    Fix for #1404

--- kalite/settingshelper.py
+++ kalite/settingshelper.py
@@ -5,8 +5,14 @@ import os
 def get_language_alist(localepaths):
     for localepath in localepaths:
         for langdir in os.listdir(localepath):
-            mdfile = glob.glob(os.path.join(localepath, langdir, '*_metadata.json'))[0]
-            with open(mdfile) as f:
-                metadata = json.load(f)
-            lc = metadata['code'].lower() # django reads language codes in lowercase
-            yield (lc, metadata['name'])
+            try:
+                mdfiles = glob.glob(os.path.join(localepath, langdir, '*_metadata.json'))
+                for mdfile in mdfiles:
+                    with open(mdfile) as f:
+                        metadata = json.load(f)
+                    lc = metadata['code'].lower() # django reads language codes in lowercase
+                    yield (lc, metadata['name'])
+                    break
+            except Exception as e:
+                logging.error("Error loading metadata: %s" % e)
+
