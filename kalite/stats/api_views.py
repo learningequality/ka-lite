192923d763d036b06dd7759ff9d1cbf8c4fa48a0
Ben Cipollini <bcipolli@ucsd.edu>
2014-02-20 14:37:42 -0800

    Preliminary code to fix #1570 - log access to videos to file, via the logging module.

---
+++ kalite/stats/api_views.py
@@ -0,0 +1,44 @@
+import datetime
+import os
+
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+
+import settings
+from i18n import get_language_pack_filepath
+from utils.django_utils import get_request_ip
+from utils.videos import OUTSIDE_DOWNLOAD_BASE_URL  # for video download redirects
+
+from settings import LOG as logger  #temporary, while testing
+#logger = logging.getLogger("stats")
+
+
+def download_video(request, video_path):
+    """Dummy function for capturing a video download request and logging
+    to output, so we can collect stats."""
+
+    # Log the info
+    youtube_id = video_path.split(".")[0]
+    logger.info("%s;%s;%s" % (get_request_ip(request), datetime.datetime.now(), youtube_id))
+
+    # Redirect to amazon
+    return HttpResponseRedirect(OUTSIDE_DOWNLOAD_BASE_URL + video_path)
+
+
+def download_language_pack(request, version, lang_code):
+    """Dummy function for capturing a language pack download request and logging
+    to output, so we can collect stats."""
+
+    # Log the event
+    logger.info("%s;%s;%s;%s" % (get_request_ip(request), datetime.datetime.now(), lang_code, version))
+
+    # Find the file to return
+    zip_file = get_language_pack_filepath(lang_code, version=version)
+    if not os.path.exists(zip_file):
+        raise Http404
+
+    # Stream it back to the user
+    zh = open(zip_file,"rb")
+    response = HttpResponse(content=zh, mimetype='application/zip', content_type='application/zip')
+    response['Content-Disposition'] = 'attachment; filename="%s"' % (lang_code + ".zip")
+
+    return response
