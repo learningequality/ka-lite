1b721dd98711dadcd683e977c2216a1b7ab00764
Aron Fyodor Asor <aronasorman@gmail.com>
2014-02-21 05:00:15 -0800

    sprinkle in log calls that record IP address for video and lang pack downloads

--- kalite/stats/__init__.py
+++ kalite/stats/__init__.py
@@ -8,6 +8,7 @@ from utils.general import ensure_dir
 
 STATS_LOG_FILEPATH = os.path.join(settings.PROJECT_PATH, 'logs', 'stats.log')
 
+# Note: beta level logger. Move to config file/dictionary format
 def stats_logger(loggername="kalite.stats"):
     ensure_dir(os.path.dirname(STATS_LOG_FILEPATH))
 
