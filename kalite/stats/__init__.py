b247f427b6620683d60c1a1615fb33766146e04f
Aron Fyodor Asor <aronasorman@gmail.com>
2014-02-20 22:54:40 -0800

    create function that automatically sets up the logger

---
+++ kalite/stats/__init__.py
@@ -0,0 +1,22 @@
+import logging
+import os
+
+from django.conf import settings
+
+from utils.general import ensure_dir
+
+
+STATS_LOG_FILEPATH = os.path.join(settings.PROJECT_PATH, 'logs', 'stats.log')
+
+def stats_logger(loggername="kalite.stats"):
+    ensure_dir(os.path.dirname(STATS_LOG_FILEPATH))
+
+    logger = logging.getLogger(loggername)
+    logger.addHandler(_file_handler())
+
+    return logger
+
+def _file_handler():
+    handler = logging.FileHandler(STATS_LOG_FILEPATH, encoding='utf-8', delay=True)
+    handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    return handler
