1957e14417127e519490b9f42f7920f193f0e53a
him-28 <himanshupillai@gmail.com>
2014-02-24 20:30:18 +0530

    Update fixture_tests.py
    changed docstring

--- kalite/main/tests/fixture_tests.py
+++ kalite/main/tests/fixture_tests.py
@@ -10,19 +10,19 @@ from django.utils import unittest
 import settings
 from testing import distributed_server_test, KALiteTestCase
 
-# To automate the task of assigning a fixture for the test case
+" To automate the task of assigning a fixture for the test case "
 @distributed_server_test
 class FixtureTestCases(KALiteTestCase):
     """ """
-    # Searches for and loads the contents of the named fixture into the database
+    " Searches for and loads the contents of the named fixture into the database "
     def test_loaddata(self):
         cur_dir = os.path.split(__file__)[0]
 
         fixture_file = cur_dir + "/main_fixture.json"
         out = call_command("loaddata", fixture_file, "Just make sure that loaddata doesn't throw an error, for now")
     
-    # If no application name is provided, all installed applications will be dumped.
-    # The output of dumpdata can be used as input for loaddata.
+    " If no application name is provided, all installed applications will be dumped. "
+    "The output of dumpdata can be used as input for loaddata. "
     def test_dumpdata(self):
 
         #
