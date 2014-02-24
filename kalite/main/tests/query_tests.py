c373bc9ec546615683cf541be97ee8828f87667d
him-28 <himanshupillai@gmail.com>
2014-02-24 17:26:59 +0530

    Update query_tests.py
    general comments added to query_tests.py

--- kalite/main/tests/query_tests.py
+++ kalite/main/tests/query_tests.py
@@ -21,15 +21,18 @@ class QueryTest(KALiteDistributedWithFacilityBrowserTestCase):
         random.seed('ben') # to make password reproducible
         self.persistent_browser = False
         super(QueryTest, self).__init__(*args, **kwargs)
-
+    
+    # generating a valid password
     @staticmethod
     def _gen_valid_password():
         return ''.join(random.sample(string.ascii_lowercase, settings.PASSWORD_CONSTRAINTS['min_length']))
-
+    
+    # query to execute admin login
     def test_query_login_admin(self):
         with self.assertNumQueries(38 + 0*UserLog.is_enabled()):
             self.browser_login_admin()
-
+    
+    # query to execute teacher login
     def test_query_login_teacher(self):
         """Check the # of queries when logging in as a teacher."""
         teacher = FacilityUser(is_teacher=True, username="t1", facility=self.facility)
@@ -39,7 +42,8 @@ class QueryTest(KALiteDistributedWithFacilityBrowserTestCase):
 
         with self.assertNumQueries(39 + 3*UserLog.is_enabled()):
             self.browser_login_teacher("t1", passwd, self.facility)
-
+    
+    # query to execute student login
     def test_query_login_student(self):
         """Check the # of queries when logging in as a student."""
         student = FacilityUser(is_teacher=False, username="s1", facility=self.facility)
@@ -50,7 +54,7 @@ class QueryTest(KALiteDistributedWithFacilityBrowserTestCase):
         with self.assertNumQueries(39 + 3*UserLog.is_enabled()):
             self.browser_login_student("s1", passwd, self.facility)
 
-
+    # query to determine status
     def test_query_status_admin(self):
         """"""
         self.test_query_login_admin()
@@ -69,7 +73,7 @@ class QueryTest(KALiteDistributedWithFacilityBrowserTestCase):
         with self.assertNumQueries(0):
             self.browse_to(self.reverse("status"))
 
-
+    # query determining logout
     def test_query_logout_admin(self):
         """"""
         self.test_query_login_admin()
