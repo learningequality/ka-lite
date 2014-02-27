0e6d9bbb5bd2e18d9bdceea2352e31f992386e71
Ben Cipollini <bcipolli@ucsd.edu>
2014-02-27 06:37:51 -0800

    Fix for #1638 : show localized node descriptions

--- kalite/updates/api_views.py
+++ kalite/updates/api_views.py
@@ -249,7 +249,7 @@ def annotate_topic_tree(node, level=0, statusdict=None, remote_sizes=None, lang_
 
         return {
             "title": _(node["title"]),
-            "tooltip": re.sub(r'<[^>]*?>', '', node["description"] or ""),
+            "tooltip": re.sub(r'<[^>]*?>', '', _(node["description"]) or ""),
             "isFolder": True,
             "key": node["id"],
             "children": children,
@@ -282,7 +282,7 @@ def annotate_topic_tree(node, level=0, statusdict=None, remote_sizes=None, lang_
 
         return {
             "title": _(node["title"]),
-            "tooltip": re.sub(r'<[^>]*?>', '', node.get("description") or ""),
+            "tooltip": re.sub(r'<[^>]*?>', '', _(node.get("description")) or ""),
             "key": youtube_id,
             "addClass": status,
             "size": vid_size,
