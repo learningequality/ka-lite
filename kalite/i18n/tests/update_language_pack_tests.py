dedae682335816e8ddf466636f94bd90fc1cc7ff
Aron Fyodor Asor <aronasorman@gmail.com>
2014-02-27 22:41:14 -0800

    add test checking for 401 unauthorized from crowdin

--- kalite/i18n/tests/update_language_pack_tests.py
+++ kalite/i18n/tests/update_language_pack_tests.py
@@ -1,5 +1,7 @@
-from mock import patch
+import requests
+from mock import Mock, patch
 
+from django.core.management.base import CommandError
 from django.conf import settings
 from django.utils import unittest
 
@@ -10,7 +12,17 @@ class DownloadLatestTranslationTests(unittest.TestCase):
     def test_success(self):
         self.assertTrue(True)
 
+    def setUp(self):
+        self.download_translation_args = {'project_id': 'doesntmatter',
+                                          'project_key': 'doesntmatter'}
+
     def test_return_none_if_no_crowdin_credentials(self):
         with patch('settings.CROWDIN_PROJECT_ID', return_value=None, create=True):
             with patch('settings.CROWDIN_PROJECT_KEY', return_value=None, create=True):
                 self.assertIsNone(ulp.download_latest_translations())
+
+    @patch.object(requests, 'get', Mock(requests.models.Response))
+    def test_error_if_401_unauthorized(self):
+        requests.get.return_value.raise_for_status.side_effect = Exception
+        requests.get.return_value.status_code = 401
+        self.assertRaisesRegexp(CommandError, '401 Unauthorized', lambda: ulp.download_latest_translations(**self.download_translation_args))
