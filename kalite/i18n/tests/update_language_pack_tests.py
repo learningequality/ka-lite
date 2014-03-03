26d1622b5fa1fb143c496093ef8b473c64471122
Aron Fyodor Asor <aronasorman@gmail.com>
2014-03-03 13:49:49 -0800

    fix usage of patch.object

--- kalite/i18n/tests/update_language_pack_tests.py
+++ kalite/i18n/tests/update_language_pack_tests.py
@@ -1,28 +1,31 @@
 import requests
-from mock import Mock, patch
+from mock import MagicMock as Mock, patch
+from requests.models import Response
 
 from django.core.management.base import CommandError
-from django.conf import settings
 from django.utils import unittest
 
+import settings
 import i18n.management.commands.update_language_packs as ulp
 
 
 class DownloadLatestTranslationTests(unittest.TestCase):
-    def test_success(self):
-        self.assertTrue(True)
-
     def setUp(self):
         self.download_translation_args = {'project_id': 'doesntmatter',
                                           'project_key': 'doesntmatter'}
 
-    def test_return_none_if_no_crowdin_credentials(self):
-        with patch('settings.CROWDIN_PROJECT_ID', return_value=None, create=True):
-            with patch('settings.CROWDIN_PROJECT_KEY', return_value=None, create=True):
-                self.assertIsNone(ulp.download_latest_translations())
+    @patch.object(settings, 'CROWDIN_PROJECT_ID', None, create=True)
+    @patch.object(settings, 'CROWDIN_PROJECT_KEY', None, create=True)
+    @patch.object(requests, 'get')
+    def test_return_none_if_no_crowdin_credentials(self, get_method):
+        get_method.return_value = Mock(Response, status_code=404)
+        get_method.return_value.raise_for_status = Mock(side_effect=requests.exceptions.ConnectionError)
 
-    @patch.object(requests, 'get', Mock(requests.models.Response))
-    def test_error_if_401_unauthorized(self):
-        requests.get.return_value.raise_for_status.side_effect = Exception
-        requests.get.return_value.status_code = 401
+        self.assertIsNone(ulp.download_latest_translations())
+
+    @patch.object(requests, 'get')
+    def test_error_if_401_unauthorized(self, get_method):
+        get_method.return_value = Mock(Response, status_code=401)
+        get_method.return_value.raise_for_status = Mock(side_effect=requests.exceptions.ConnectionError)
+
         self.assertRaisesRegexp(CommandError, '401 Unauthorized', lambda: ulp.download_latest_translations(**self.download_translation_args))
