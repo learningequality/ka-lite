b2c827a21ec8e228e163059f6e85b5ce8dd52172
Aron Fyodor Asor <aronasorman@gmail.com>
2014-03-07 06:41:16 -0800

    fix import of chronograph.Job model

--- python-packages/fle_utils/chronograph/management/commands/cron.py
+++ python-packages/fle_utils/chronograph/management/commands/cron.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 
+from fle_utils.chronograph.models import Job
+
 class Command(BaseCommand):
     help = 'Runs all jobs that are due.'
-    
+
     def handle(self, *args, **options):
-        from chronograph.models import Job
         for job in Job.objects.due():
             job.run()
