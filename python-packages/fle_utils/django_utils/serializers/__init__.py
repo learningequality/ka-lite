a2e49af695f389e66c25ce778d3adc559da0b2be
Ben Cipollini <ben@learningequality.org>
2014-03-06 21:34:39 -0800

    Fixing bug in serializer reference.

--- python-packages/fle_utils/django_utils/serializers/__init__.py
+++ python-packages/fle_utils/django_utils/serializers/__init__.py
@@ -1,4 +1,4 @@
 from django.core.serializers import *
-BUILTIN_SERIALIZERS["versioned-json"]   = "utils.django_utils.serializers.versioned_json"
-BUILTIN_SERIALIZERS["versioned-python"] = "utils.django_utils.serializers.versioned_python"
+BUILTIN_SERIALIZERS["versioned-json"]   = "fle_utils.django_utils.serializers.versioned_json"
+BUILTIN_SERIALIZERS["versioned-python"] = "fle_utils.django_utils.serializers.versioned_python"
 
