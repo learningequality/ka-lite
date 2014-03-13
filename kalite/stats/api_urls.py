92b40c48205244fd81bfa9bbc65a059c9eddbe5b
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-12 21:37:50 -0700

    Default to latest version (not text 'latest'), get from 'downloads'

--- kalite/stats/api_urls.py
+++ kalite/stats/api_urls.py
@@ -8,7 +8,9 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('stats.api_views',
     url(r'^media/language_packs/(?P<version>.*)/(?P<lang_code>.*).zip$', 'download_language_pack'),
     url(r'^download/videos/(?P<video_path>.*)$', 'download_video'),
+
+    url(r'^download/installer/windows/$', 'download_windows_installer'),
+    url(r'^download/installer/windows/(?P<version>.*)$', 'download_windows_installer'),
+
     url(r'^static/srt/(?P<lang_code>.*)/subtitles/(?P<youtube_id>.*).srt$', 'download_subtitle', {}), # v0.10.0: fetching subtitles.
-    url(r'^static/installer/windows$', 'download_windows_installer', {'version': 'latest'}),
-    url(r'^static/installer/windows/(?P<version>.*)$', 'download_windows_installer', {}),
 )
