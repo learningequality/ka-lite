fcdf512360be1163a4f0287a7a08b1938b1a1851
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-15 07:38:09 -0700

    Moving updates templates.

--- kalite/updates/settings.py
+++ kalite/updates/settings.py
@@ -1,3 +1,5 @@
+import os
+
 try:
     import local_settings
 except ImportError:
@@ -14,3 +16,5 @@ CONTENT_URL    = getattr(local_settings, "CONTENT_URL", "/content/")
 # Should be a function that receives a video file (youtube ID), and returns a URL to a video stream
 BACKUP_VIDEO_SOURCE = getattr(local_settings, "BACKUP_VIDEO_SOURCE", None)
 BACKUP_THUMBNAIL_SOURCE = getattr(local_settings, "BACKUP_THUMBNAIL_SOURCE", None)
+
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(__file__), "templates"),)
