8d7c101e205fe64c7997222252c7cefcacda72d6
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-25 15:07:50 -0700

    5 coaches per page; 25 for users if in group view, 10 if in full facility view.

--- kalite/control_panel/views.py
+++ kalite/control_panel/views.py
@@ -194,8 +194,10 @@ def facility_management(request, facility, group_id=None, zone_id=None, frequenc
     context = control_panel_context(request, zone_id=zone_id, facility_id=facility.id)
 
     #Get pagination details
-    coach_page=request.REQUEST.get("coaches_page","1")
-    student_page=request.REQUEST.get("students_page","1")
+    coach_page = request.REQUEST.get("coaches_page", "1")
+    coach_per_page = request.REQUEST.get("coaches_per_page", "5")
+    student_page = request.REQUEST.get("students_page", "1")
+    student_per_page = request.REQUEST.get("students_per_page", "25" if group_id else "10")
 
     # This could be moved into a function shared across files, if necessary.
     #   For now, moving into function, as outside if function it looks more
@@ -302,8 +304,8 @@ def facility_management(request, facility, group_id=None, zone_id=None, frequenc
 
         return users, page_urls
 
-    coach_data, coach_urls = paginate_users(coach_data, "coaches", page=coach_page)
-    student_data, student_urls = paginate_users(student_data, "students", page=student_page)
+    coach_data, coach_urls = paginate_users(coach_data, "coaches", page=coach_page, per_page=coach_per_page)
+    student_data, student_urls = paginate_users(student_data, "students", page=student_page, per_page=student_per_page)
 
     page_urls = {
         "coaches": coach_urls,
