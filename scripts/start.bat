a11cfbd714842cfc4053095eb9e1f18cf81e2f53
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-27 13:50:57 -0700

    kaserve takes care of non-installed system, so don't check in OS script.

--- scripts/start.bat
+++ scripts/start.bat
@@ -10,32 +10,24 @@ if exist "%SCRIPT_DIR%\python.bat" (
     set "KALITE_DIR=%SCRIPT_DIR%\kalite"
 )
 
-if not exist "%KALITE_DIR%\database\data.sqlite" (
-    echo Please run install.bat first!
-) else (
-    REM transfer any previously downloaded content from the old location to the new
-    move "%KALITE_DIR%\static\videos\*" "%KALITE_DIR%\..\content" > nul 2> nul
+REM transfer any previously downloaded content from the old location to the new
+move "%KALITE_DIR%\static\videos\*" "%KALITE_DIR%\..\content" > nul 2> nul
 
-    set "file_exist="
-    if exist "%KALITE_DIR%\cronserver.pid" set file_exist=0
-    if exist "%KALITE_DIR%\runcherrypyserver.pid" set file_exist=0
-    if defined file_exist (
-        echo -------------------------------------------------------------------
-        echo KA Lite server is still running.
-        echo Please run stop.bat and then start.bat again.
-        echo -------------------------------------------------------------------
-        exit /b
-    )
+set "file_exist="
+if exist "%KALITE_DIR%\cronserver.pid" set file_exist=0
+if exist "%KALITE_DIR%\runcherrypyserver.pid" set file_exist=0
+if defined file_exist (
+    call "%SCRIPT_DIR%\stop.bat"
+)
 
-    call "%SCRIPT_DIR%\python.bat"
-    if !ERRORLEVEL! EQU 1 (
-        echo -------------------------------------------------------------------
-        echo Error: You do not seem to have Python installed.
-        echo Please install version 2.6 or 2.7, and re-run this script.
-        echo -------------------------------------------------------------------
-        exit /b
-    )
-
+call "%SCRIPT_DIR%\python.bat"
+if !ERRORLEVEL! EQU 1 (
+    echo -------------------------------------------------------------------
+    echo Error: You do not seem to have Python installed.
+    echo Please install version 2.6 or 2.7, and re-run this script.
+    echo -------------------------------------------------------------------
+    exit /b
+)
 
 echo Starting the cron server in the background.
 call "%SCRIPT_DIR%\cronstart.bat"
