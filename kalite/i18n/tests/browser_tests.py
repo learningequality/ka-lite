b072b0ea9855003b14cadbfb236690c7b9c625e6
Ben Cipollini <bcipolli@ucsd.edu>
2014-03-30 10:56:15 -0700

    Fixing i18n references to zone_management.

--- kalite/i18n/tests/browser_tests.py
+++ kalite/i18n/tests/browser_tests.py
@@ -130,7 +130,7 @@ class KALiteDistributedBrowserTestCase(BrowserTestCase):
 
         self.browser_login_user(username=username, password=password, expect_success=expect_success)
         if expect_success:
-            self.assertIn(reverse("zone_management"), self.browser.current_url, "Login browses to zone_management page" )
+            self.assertIn(reverse("zone_management", kwargs={zone_id: "None"}), self.browser.current_url, "Login browses to zone_management page" )
 
     def browser_login_teacher(self, username, password, facility_name=None, expect_success=True):
         self.browser_login_user(username=username, password=password, facility_name=facility_name, expect_success=expect_success)
