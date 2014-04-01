0888fdbba31aac7f5901dcf7ae6a11a26c367341
anuragkanungo <anuragkanungo@gmail.com>
2014-04-02 01:00:40 +0530

    Changes only admin specific

--- kalite/i18n/custom_context_processors.py
+++ kalite/i18n/custom_context_processors.py
@@ -4,8 +4,10 @@ from . import lcode_to_ietf, get_language_name, get_installed_language_packs
 def languages(request):
     if "default_language" not in request.session:
         return {}  # temporarily skipped middleware, but we'll get back here again.  Tricky Django...
-
-    language_choices = get_installed_language_packs(force=True)
+    if request.is_admin:
+        language_choices = get_installed_language_packs(force=True)
+    else:
+        language_choices = get_installed_language_packs()
 
     return {
         "default_language": lcode_to_ietf(request.session["default_language"]),
