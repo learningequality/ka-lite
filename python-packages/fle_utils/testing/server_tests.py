1df561fb0c23fbea86f8e7200e234299a039918d
Aron Fyodor Asor <aronasorman@gmail.com>
2014-04-01 11:47:46 -0700

    add test for restarting dev server

--- python-packages/fle_utils/testing/server_tests.py
+++ python-packages/fle_utils/testing/server_tests.py
@@ -5,15 +5,26 @@ import unittest
 import cherrypy
 from mock import patch, MagicMock as Mock
 
-from ..server import server_restart
+sys.path += [os.path.realpath('..'), os.path.realpath('.')]
+
+from server import server_restart
 
 
 class ServerRestartTests(unittest.TestCase):
+
     @patch.object(cherrypy.engine, 'restart')
     def test_cherrypy_restart_called(self, restart_method):
         server_restart('cherrypy')
 
         restart_method.assert_called_once_with()
+
+    @patch.object(os, 'utime')
+    def test_file_touched_for_dev_server(self, utime_method):
+        filename = __file__.replace('.pyc', '.py')
+
+        server_restart('wsgiserver')
+
+        assert utime_method.call_count == 1
 
 
 if __name__ == '__main__':
