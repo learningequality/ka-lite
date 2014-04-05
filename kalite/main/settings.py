5a88508b024a1620b417c01f72395179125fd210
Ben Cipollini <bcipolli@ucsd.edu>
2014-04-05 14:37:05 -0700

    Adding main-specific setting for data directory, moving main data there.

--- kalite/main/settings.py
+++ kalite/main/settings.py
@@ -1,3 +1,5 @@
+import os
+
 try:
     import local_settings
 except ImportError:
@@ -12,3 +14,5 @@ except ImportError:
 #   NOTE: None means no limit (infinite)
 USER_LOG_MAX_RECORDS_PER_USER = getattr(local_settings, "USER_LOG_MAX_RECORDS_PER_USER", 1)
 USER_LOG_SUMMARY_FREQUENCY = getattr(local_settings, "USER_LOG_SUMMARY_FREQUENCY", (1,"months"))
+
+MAIN_DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
