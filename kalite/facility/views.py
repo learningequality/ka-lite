89ae39a3cf885814d54159a7e8128f3a7cad3f7e
Dylan Barth <dylan.j.barth@gmail.com>
2014-04-09 17:20:59 -0700

    Couple small fixes

--- kalite/facility/views.py
+++ kalite/facility/views.py
@@ -35,7 +35,10 @@ from securesync.devices.models import Zone
 @render_to("facility/facility_edit.html")
 def facility_edit(request, id=None, zone_id=None):
     facil = (id != "new" and get_object_or_404(Facility, pk=id)) or None
-    zone = facil.get_zone() or get_object_or_None(Zone, pk=zone_id)
+    if facil:
+        zone = facil.get_zone()
+    else:
+        zone = get_object_or_None(Zone, pk=zone_id)
 
     if request.method != "POST":
         form = FacilityForm(instance=facil, initial={"zone_fallback": zone})
