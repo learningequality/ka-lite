ccce7a3c0e251b4ef299cf9ec3b7901f2e6217c3
Ben Cipollini <bcipolli@ucsd.edu>
2014-04-09 23:59:48 -0700

    Refactor paginate, increase informativeness of deployments.

--- python-packages/fle_utils/django_utils/paginate.py
+++ python-packages/fle_utils/django_utils/paginate.py
@@ -1,45 +1,45 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-def paginate_users(request, user_list, user_type, per_page=25, page=1):
+def paginate_data(request, data_list, data_type="cur", per_page=25, page=1):
     """
-    Create pagination for users
+    Create pagination for list
     """
-    if not user_list:
-        users = []
+    if not data_list:
+        paged_data = []
         page_urls = {}
     else:
         #Create a Django Pagintor from QuerySet
-        paginator = Paginator(user_list, per_page)
+        paginator = Paginator(data_list, per_page)
         try:
             #Try to render the page with the passed 'page' number
-            users = paginator.page(page)
+            paged_data = paginator.page(page)
             #Call pages_to_show function that selects a subset of pages to link to
             listed_pages = pages_to_show(paginator, page)
         except PageNotAnInteger:
             #If not a proper page number, render page 1
-            users = paginator.page(1)
+            paged_data = paginator.page(1)
             #Call pages_to_show function that selects a subset of pages to link to
             listed_pages = pages_to_show(paginator, 1)
         except EmptyPage:
             #If past the end of the page range, render last page
-            users = paginator.page(paginator.num_pages)
+            paged_data = paginator.page(paginator.num_pages)
             #Call pages_to_show function that selects a subset of pages to link to
             listed_pages = pages_to_show(paginator, paginator.num_pages)
 
-    if users:
+    if paged_data:
         #Generate URLs for pagination links
-        if users.has_previous():
+        if paged_data.has_previous():
             #If there are pages before the current page, generate a link for 'previous page'
             prevGETParam = request.GET.copy()
-            prevGETParam[user_type + "_page"] = users.previous_page_number()
+            prevGETParam[data_type + "_page"] = paged_data.previous_page_number()
             previous_page_url = "?" + prevGETParam.urlencode()
         else:
             previous_page_url = ""
-        if users.has_next():
+        if paged_data.has_next():
             #If there are pages after the current page, generate a link for 'next page'
             nextGETParam = request.GET.copy()
-            nextGETParam[user_type + "_page"] = users.next_page_number()
+            nextGETParam[data_type + "_page"] = paged_data.next_page_number()
             next_page_url = "?" + nextGETParam.urlencode()
         else:
             next_page_url = ""
@@ -50,12 +50,12 @@ def paginate_users(request, user_list, user_type, per_page=25, page=1):
             for listed_page in listed_pages:
                 if listed_page != -1:
                     GETParam = request.GET.copy()
-                    GETParam[user_type + "_page"] = listed_page
+                    GETParam[data_type + "_page"] = listed_page
                     page_urls.update({listed_page: "?" + GETParam.urlencode()})
-            users.listed_pages = listed_pages
-            users.num_listed_pages = len(listed_pages)
+            paged_data.listed_pages = listed_pages
+            paged_data.num_listed_pages = len(listed_pages)
 
-    return users, page_urls
+    return paged_data, page_urls
 
 
 
