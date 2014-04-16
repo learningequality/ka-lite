5cb21c60dd256b8e97ec93d47af38ff0af649091
Aron Fyodor Asor <aronasorman@gmail.com>
2014-04-15 19:42:02 -0700

    add settings for needed for updating through Git

--- kalite/updates/settings.py
+++ kalite/updates/settings.py
@@ -18,3 +18,8 @@ BACKUP_VIDEO_SOURCE = getattr(local_settings, "BACKUP_VIDEO_SOURCE", None)
 BACKUP_THUMBNAIL_SOURCE = getattr(local_settings, "BACKUP_THUMBNAIL_SOURCE", None)
 
 UPDATES_DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
+
+# settings for when we're updating the server through Git
+GIT_UPDATE_REPO_URL    = getattr(local_settings, "UPDATE_REPO_URL", "https://github.com/learningequality/ka-lite.git")
+GIT_UPDATE_BRANCH      = getattr(local_settings, "UPDATE_BRANCH", "master")
+GIT_UPDATE_REMOTE_NAME = getattr(local_settings, "GIT_UPDATE_REMOTE_NAME", "updates")
