08fd2c4ed52318d0cf8fe7df56a12b5ad75ffb36
Aron Fyodor Asor <aronasorman@gmail.com>
2014-04-18 21:58:12 -0700

    show the version according to git if we have a git repo!

--- kalite/updates/views.py
+++ kalite/updates/views.py
@@ -35,15 +35,19 @@ def update_context(request):
     device = Device.get_own_device()
     zone = device.get_zone()
     try:
-        is_git_repo = True if git.Repo(os.path.dirname(__file__)) else None
+        repo = git.Repo(os.path.dirname(__file__))
+        software_version = repo.git.describe()
+        is_git_repo = bool(repo)
     except git.exc.InvalidGitRepositoryError:
         is_git_repo = False
+        software_version = None
 
     context = {
         "registered": Device.get_own_device().is_registered(),
         "zone_id": zone.id if zone else None,
         "device_id": device.id,
         "is_git_repo": str(is_git_repo).lower(), # lower to make it look like JS syntax
+        "git_software_version": software_version,
     }
     return context
 
