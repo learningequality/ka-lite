1a0cd1ef7d929cd62c60baaea2bf3501eccb9fe0
Aron Fyodor Asor <aronasorman@gmail.com>
2014-04-23 16:10:43 -0700

    create playlist app and move playlist models there

---
+++ kalite/playlist/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from fle_utils.django_utils import ExtendedModel
+
+# Create your models here.
+
+class Playlist(ExtendedModel):
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    creator = models.ForeignKey(FacilityUser, blank=True, null=True)
+
+    def __unicode__(self):
+        return "%(title)s:%(desc)s" % {'title': self.title, 'desc': self.description}
+
+
+class PlaylistEntity(ExtendedModel):
+    ENTITY_KINDS = (
+        ('video', 'Video'),
+        ('exercise', 'Exercise'),
+        ('quiz', 'Quiz'),
+    )
+
+    entity_kind = models.CharField(max_length=10, choices=ENTITY_KINDS)
+    entity_id = models.CharField(max_length=50)
+    playlist = models.ForeignKey(Playlist)
+    sort_order = models.IntegerField()
