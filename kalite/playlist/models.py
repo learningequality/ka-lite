390ad8d03c21386340f988b33c6bbae9da6d52c0
Aron Fyodor Asor <aronasorman@gmail.com>
2014-04-26 07:44:44 -0700

    continue fleshing out playlist models

--- kalite/playlist/models.py
+++ kalite/playlist/models.py
@@ -9,20 +9,35 @@ from kalite.facility.models import FacilityUser
 class Playlist(ExtendedModel):
     title = models.CharField(max_length=30)
     description = models.TextField(blank=True)
-    creator = models.ForeignKey(FacilityUser, blank=True, null=True)
+
 
     def __unicode__(self):
         return "%(title)s:%(desc)s" % {'title': self.title, 'desc': self.description}
 
 
+    def add_entry(self, *args, **kwargs):
+        if 'sort_order' not in kwargs: # by default, append entry to the playlist
+            try:
+                last_entry = self.entries.order_by('-sort_order').all()[0]
+                kwargs['sort_order'] = last_entry.sort_order + 1
+            except IndexError:  # no entries yet
+                kwargs['sort_order'] = 0
+
+        return self.entries.create(*args, **kwargs)
+
+
 class PlaylistEntry(ExtendedModel):
     ENTITY_KINDS = (
-        ('video', 'Video'),
-        ('exercise', 'Exercise'),
-        ('quiz', 'Quiz'),
+        ('Video', 'Video'),
+        ('Exercise', 'Exercise'),
+        ('Quiz', 'Quiz'),
     )
 
     entity_kind = models.CharField(max_length=10, choices=ENTITY_KINDS)
     entity_id = models.CharField(max_length=50)
-    playlist = models.ForeignKey(Playlist)
     sort_order = models.IntegerField()
+    playlist = models.ForeignKey(Playlist, related_name='entries')
+
+
+    class Meta:
+        ordering = ['playlist', 'sort_order']
--- kalite/playlist/tests.py
+++ kalite/playlist/tests.py
@@ -7,10 +7,39 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
+from facility.models import FacilityUser
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from .models import Playlist
+
+
+class PlaylistTests(TestCase):
+    fixtures = ['single_student_testdata.json']
+
+    def setUp(self):
+        self.test_student = FacilityUser.objects.get(username='teststudent1')
+        self.p = Playlist.objects.create(
+            title="test playlist",
+            description="An empty playlist",
+        )
+
+    def test_can_create_valid_empty_playlist(self):
+        self.assertIsNone(self.p.full_clean()) # doesnt raise any error or anything
+
+
+    def test_can_add_playlist_entry(self):
+        self.p.add_entry(entity_kind='Video')
+
+
+    def test_add_first_entry_sort_order_is_zero(self):
+        entry = self.p.add_entry(entity_kind='Video')
+        self.assertEquals(entry.sort_order, 0)
+
+
+    def test_add_new_entry_sort_order_incremented(self):
+        self.p.add_entry(entity_kind='Video')
+        new_entry = self.p.add_entry(entity_kind='Video')
+        self.assertEquals(new_entry.sort_order, 1)
+
+
+    def test_add_new_entry_explicit_sort_order_other_entries_moved(self):
+        raise NotImplementedError
