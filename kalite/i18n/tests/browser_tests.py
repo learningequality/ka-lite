a83b241d6e5d741119e407fcf3f39adebcb37a3b
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-07 16:36:43 -0700

    create a test for switching the server language

--- kalite/i18n/tests/browser_tests.py
+++ kalite/i18n/tests/browser_tests.py
@@ -22,6 +22,13 @@ class BrowserLanguageSwitchingTests(KALiteDistributedBrowserTestCase, I18nTestCa
         self.admin = User.objects.create(username=self.ADMIN_USERNAME, password='nein')
         self.admin.set_password(self.ADMIN_PASSWORD)
         self.admin.save()
+
+        self.foreign_language_homepage_text_mappings = {
+            'it': "gratuita per chiunque ovunque",
+            'pt-BR': "Uma educação sem salas de aula para qualquer um em qualquer lugar",
+        }
+        assert sorted(self.TEST_LANGUAGES) == sorted(self.foreign_language_homepage_text_mappings.keys())
+
         super(KALiteDistributedBrowserTestCase, self).setUp()
         self.install_languages()
 
@@ -32,14 +39,9 @@ class BrowserLanguageSwitchingTests(KALiteDistributedBrowserTestCase, I18nTestCa
 
     def test_logged_out_homepage_language_switches_from_english_and_back(self):
         # response = self.client.get('/', data={'set_user_language': 'de'})
-        foreign_language_text_mappings = {
-            'it': "gratuita per chiunque ovunque",
-            'pt-BR': "Uma educação sem salas de aula para qualquer um em qualquer lugar",
-        }
-        assert sorted(self.TEST_LANGUAGES) == sorted(foreign_language_text_mappings.keys())
         english_text = "A free world-class education for anyone anywhere"
 
-        for lang_code, expected_text in foreign_language_text_mappings.iteritems():
+        for lang_code, expected_text in self.foreign_language_homepage_text_mappings.iteritems():
             self.browse_to(self.reverse("homepage"))
             self.browser.find_element_by_css_selector("#language_selector > option[value='%s']" % lang_code).click()
             self.assertIn(expected_text, self.browser.find_element_by_css_selector(".main-headline").text)
@@ -47,3 +49,15 @@ class BrowserLanguageSwitchingTests(KALiteDistributedBrowserTestCase, I18nTestCa
             # switch back to english, and test that we successfully switched back
             self.browser.find_element_by_css_selector("#language_selector > option[value='%s']" % "en").click()
             self.assertIn(english_text, self.browser.find_element_by_css_selector(".main-headline").text)
+
+
+    def test_set_server_default_language(self):
+        self.browser_login_admin()
+        self.register_device()
+
+        for lang, expected_text in self.foreign_language_homepage_text_mappings.iteritems():
+            self.browse_to(self.reverse("update_languages"))
+            self.browser.find_element_by_css_selector(".set_server_language[value='%s']" % lang).click()
+
+            self.browse_to(self.reverse("homepage"))
+            self.assertIn(expected_text, self.browser.find_element_by_css_selector(".main-headline").text)
