96f847dc2cd295ed7b121730c5ee10eaa41ebf95
Jomel Araniego <jomel150@gmail.com>
2014-05-11 22:03:32 +0800

    Makes homepage table dynamic

---
+++ kalite/distributed/static/js/distributed/homepage.js
@@ -0,0 +1,24 @@
+$(function() {
+  $("#playlist-table").hide();
+  $.ajax({
+    url: '/playlists/sample',
+    type: 'GET',
+    success: function(data) {
+      $("#playlist-table").show();
+      $(".loading").hide();
+      var playlists = $("#playlists");
+      data.map(function(result) {
+          var format = formatPlaylist(result)
+          $("#playlists").append(format)
+      });
+    }
+  });
+});
+
+function formatPlaylist(data) {
+    var format =
+      "<li><div><span class=\"block cell-content\">" + data.title +"</span><span class=\"pull-left\">5/2</span><span class=\"pull-right\">V:2 Ex:7</span></div></li>";
+    return format;
+
+
+}
