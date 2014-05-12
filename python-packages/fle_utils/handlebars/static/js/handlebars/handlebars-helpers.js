bada6d3d376082b5885b32a7161372ea8ab9bbe0
Jamie Alexandre <jamalex@gmail.com>
2014-05-12 16:44:45 -0700

    Handlebars helper "ifcond" for making specific boolean comparisons.

---
+++ python-packages/fle_utils/handlebars/static/js/handlebars/handlebars-helpers.js
@@ -0,0 +1,37 @@
+Handlebars.registerHelper("ifcond", function(v1, operator, v2, options) {
+    switch (operator)
+    {
+        case "==":
+            return (v1==v2)?options.fn(this):options.inverse(this);
+
+        case "!=":
+            return (v1!=v2)?options.fn(this):options.inverse(this);
+
+        case "===":
+            return (v1===v2)?options.fn(this):options.inverse(this);
+
+        case "!==":
+            return (v1!==v2)?options.fn(this):options.inverse(this);
+
+        case "&&":
+            return (v1&&v2)?options.fn(this):options.inverse(this);
+
+        case "||":
+            return (v1||v2)?options.fn(this):options.inverse(this);
+
+        case "<":
+            return (v1<v2)?options.fn(this):options.inverse(this);
+
+        case "<=":
+            return (v1<=v2)?options.fn(this):options.inverse(this);
+
+        case ">":
+            return (v1>v2)?options.fn(this):options.inverse(this);
+
+        case ">=":
+         return (v1>=v2)?options.fn(this):options.inverse(this);
+
+        default:
+            return eval(""+v1+operator+v2)?options.fn(this):options.inverse(this);
+    }
+});
