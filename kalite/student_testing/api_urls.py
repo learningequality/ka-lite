8d89ffcff78651d87700550b706a385801bbe3fc
Richard <richard.tibbles@gmail.com>
2014-05-12 14:46:16 -0700

    Change urls to use __package__ for api_urls import.

--- kalite/student_testing/api_urls.py
+++ kalite/student_testing/api_urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import patterns, url, include
 
-urlpatterns = patterns('student_testing.api_views',
+urlpatterns = patterns(__package__ + '.api_views',
     url(r'save_attempt_log$', 'save_attempt_log', {}, 'save_attempt_log'),
 )
