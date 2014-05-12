3a679750588bbc9ca5e87e2f144b236880c18869
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-12 13:17:01 -0700

    lint i18n middleware

--- kalite/i18n/middleware.py
+++ kalite/i18n/middleware.py
@@ -17,14 +17,8 @@ Other values set here:
   request.language - proxy for request.session["django_language"] / request.session[settings.LANGUAGE_COOKIE_NAME]
 """
 from django.conf import settings; logging = settings.LOG
-from django.core.exceptions import PermissionDenied
-from django.http import HttpResponseRedirect
-from django.utils import translation
-from django.utils.translation import ugettext_lazy as _
 
-from . import get_default_language, get_installed_language_packs, lcode_to_django_lang, lcode_to_ietf, select_best_available_language, set_default_language, set_request_language
-from fle_utils.config.models import Settings
-from fle_utils.internet import set_query_params
+from . import get_default_language, set_request_language, select_best_available_language
 
 
 def set_language_data_from_request(request):
@@ -32,7 +26,7 @@ def set_language_data_from_request(request):
     Process requests to set language, redirect to the same URL to continue processing
     without leaving the "set" in the browser history.
     """
-    if not "default_language" in request.session:
+    if "default_language" not in request.session:
         # default_language has the following priority:
         #   facility user's individual setting
         #   config.Settings object's value
