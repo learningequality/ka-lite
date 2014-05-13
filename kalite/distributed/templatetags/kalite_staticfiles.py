059dea478cc55e173361a616ab3cd343f3bc14c8
Cyril Pauya <cpauya@gmail.com>
2014-05-13 13:31:39 +0800

    Added option to use `fle_utils.config.models.Settings` that can be set at the admin site. Options for hash now starts with the `Settings`, then the `BUILD_ID`, then the `SHA256` hash.

--- kalite/distributed/templatetags/kalite_staticfiles.py
+++ kalite/distributed/templatetags/kalite_staticfiles.py
@@ -12,6 +12,10 @@ else:
 from kalite import version
 
 
+# Add this as key to `fle_utils.config.models.Settings`
+BUILD_HASH = 'BUILD_HASH'
+
+
 register = Library()
 
 
@@ -33,37 +37,57 @@ def static_with_build(path, with_build=True):
 
     REF: https://www.quora.com/What-are-the-best-practices-for-versioning-CSS-and-JS-files
 
-    Option 1: Use the BUILD as Get parameter.
+    Option 1: Use the `fle_utils.config.models.Settings`
+        Pros: It works and more flexible.
+        Cons: Requires us to update the BUILD_ID key at admin page.
+
+    Option 2: Use the BUILD as Get parameter.
         Pros: It works.
         Cons: We need to maintain the `kalite/version.py` hard-coded values
             every time we make a release.
 
-    Option 2: TODO(cpauya): Use the MD5/SHA hash of the file's bytes as Get paramater.
-        Pros: It works.
+    Option 3: Use the SHA hash of the file's bytes as querystring.
+        Pros: It works and more reliable.
         Cons: Requires us to open the static files to generate the hash.
-                TODO(cpauya): benchmark this!
+                TODO(cpauya): Benchmark this!
+                TODO(cpauya): Inefficient!  This is called everytime the static file is used.
 
-    We use Option 1 then try Option 2 if it works.
+    We try all options which does not throw exception.
     """
     new_path = static_lib(path)
     if with_build:
         build_id = ''
-        try:
-            # option 1: use the BUILD data
-            build_id = version.VERSION_INFO[version.VERSION]["git_commit"][0:8]
-        except:
-            pass
 
-        # attempt to use hash
+        # try fle_utils`s Settings
         try:
-            # option 2: use hash
-            # REF: http://stackoverflow.com/questions/16874598/how-do-i-calculate-the-md5-checksum-of-a-file-in-python
-            file_path = os.path.realpath(os.path.join(settings.STATIC_ROOT, path))
-            with open(file_path, 'rb') as file_to_check:
-                data = file_to_check.read()
-                build_id = hashlib.sha256(data).hexdigest()
+            from fle_utils.config.models import Settings
+            build_id = Settings.get(BUILD_HASH, '')
         except Exception as exc:
             pass
+
+        # try the BUILD data from `version.py`
+        if not build_id:
+            try:
+                build_id = version.VERSION_INFO[version.VERSION]["git_commit"][0:8]
+            except:
+                pass
+
+        # attempt to use hash
+        if not build_id:
+            try:
+                # REF: http://stackoverflow.com/questions/16874598/how-do-i-calculate-the-md5-checksum-of-a-file-in-python
+                file_path = os.path.realpath(os.path.join(settings.STATIC_ROOT, path))
+                with open(file_path, 'rb') as file_to_check:
+                    # REF: http://stackoverflow.com/questions/1131220/get-md5-hash-of-big-files-in-python
+                    filehash = hashlib.sha256()
+                    while True:
+                        data = file_to_check.read(8192)
+                        if not data:
+                            break
+                        filehash.update(data)
+                    build_id = filehash.hexdigest()
+            except Exception as exc:
+                pass
 
         if build_id:
             new_path = '%s?%s' % (new_path, build_id,)
