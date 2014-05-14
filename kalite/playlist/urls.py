85483480dba298af8ce1bbfe4be1b8ddbd36d53a
Jomel Araniego <jomel150@gmail.com>
2014-05-14 08:30:32 +0800

    Adds routes for playlist page

--- kalite/playlist/urls.py
+++ kalite/playlist/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url, include
 
 
 urlpatterns = patterns(__package__ + '.views',
+  url(r'^$', 'playlists'),
 	url(r'^sample', 'sample_json'),
 )
 
--- kalite/playlist/views.py
+++ kalite/playlist/views.py
@@ -2,9 +2,17 @@ import json
 import os
 
 from fle_utils.internet import JsonResponse, api_handle_error_with_json
+from annoying.decorators import render_to
 
 
 def sample_json(request):
 	jsonfile = os.path.join(os.path.dirname(__file__), 'test_playlist.json')
 	with open(jsonfile) as f:
 		return JsonResponse(json.load(f))
+
+@render_to("playlist/index.html")
+def playlists(request):
+    context = {
+        "title": "Playlists",
+    }
+    return context
