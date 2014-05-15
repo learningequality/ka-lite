3f5379042432a3e747df0aec9272405e0dcbe002
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-15 14:06:17 -0700

    make query tests more flexible

--- kalite/distributed/tests/browser_tests/query_tests.py
+++ kalite/distributed/tests/browser_tests/query_tests.py
@@ -5,7 +5,6 @@ import random
 import string
 
 from django.conf import settings
-from django.utils import unittest
 
 from .base import KALiteDistributedWithFacilityBrowserTestCase
 from kalite.facility.models import FacilityUser
@@ -61,7 +60,8 @@ class QueryTest(KALiteDistributedWithFacilityBrowserTestCase):
         student.set_password(passwd)
         student.save()
 
-        with self.assertNumQueries(23 + 3*UserLog.is_enabled()):
+        expected_num_queries = 23 + 3*UserLog.is_enabled()
+        with self.assertNumQueries(FuzzyInt(expected_num_queries - 3, expected_num_queries + 3)):
             self.browser_login_student("s1", passwd, self.facility)
 
     def test_query_status_admin(self):
@@ -73,7 +73,7 @@ class QueryTest(KALiteDistributedWithFacilityBrowserTestCase):
     def test_query_status_teacher(self):
         """"""
         self.test_query_login_teacher()
-        with self.assertNumQueries(FuzzyInt(4,6)):
+        with self.assertNumQueries(FuzzyInt(2, 6)):
             self.browse_to(self.reverse("status"))
 
     def test_query_status_student(self):
