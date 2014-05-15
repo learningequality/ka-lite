41bba63a6fc6b0a39c24fb54736db2847c034f83
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-15 11:20:24 -0700

    remove unused imports in caching.py

--- kalite/distributed/caching.py
+++ kalite/distributed/caching.py
@@ -11,28 +11,16 @@ For any app implementing cacheable data or writing to the web cache, the app sho
     across all apps, by calling invalidate_inmemory_caches
 * Call invalidate_web_cache (from fle_utils.internet.webcache)
 """
-import datetime
-import os
-from functools import partial
-
 from django.conf import settings; logging = settings.LOG
-from django.core.cache import cache, InvalidCacheBackendError
-from django.core.cache.backends.filebased import FileBasedCache
-from django.core.cache.backends.locmem import LocMemCache
 from django.core.urlresolvers import reverse
 from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
-from django.http import HttpRequest
 from django.test.client import Client
-from django.utils import translation
-from django.utils.cache import get_cache_key as django_get_cache_key, get_cache, _generate_cache_key
-from django.views.decorators.cache import cache_control
-from django.views.decorators.cache import cache_page
-from django.views.decorators.http import condition
 
 from fle_utils.internet import generate_all_paths
 from fle_utils.internet.webcache import *
 from kalite import i18n
+from kalite.distributed.templatetags import kalite_staticfiles
 from kalite.main import topic_tools
 from kalite.updates.models import VideoFile
 
