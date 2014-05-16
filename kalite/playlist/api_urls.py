cac8fe920b0eec407f925f1b393091f1d6a52b37
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-16 06:02:43 -0700

    add api for adding playlist to a group

--- kalite/playlist/api_urls.py
+++ kalite/playlist/api_urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns(__package__ + '.api_views',
     url(r'^sample', 'sample_json'),
+    url(r'^assign/group/(?P<group_id>\w+)/playlist/(?P<playlist_id>\w+)/$', 'assign_group_to_playlist', {}, 'assign_group_to_playlist'),
 )
--- kalite/playlist/api_views.py
+++ kalite/playlist/api_views.py
@@ -1,10 +1,26 @@
 import json
 import os
 
-from fle_utils.internet import JsonResponse
+from fle_utils.internet import JsonResponse, api_handle_error_with_json
+
+from .models import PlaylistsAssignedToGroup
+from kalite.facility.models import FacilityGroup
 
 
 def sample_json(request):
     jsonfile = os.path.join(os.path.dirname(__file__), 'test_playlist.json')
     with open(jsonfile) as f:
         return JsonResponse(json.load(f))
+
+
+@api_handle_error_with_json
+def assign_group_to_playlist(request, playlist_id, group_id):
+    playlist_to_group, was_created = PlaylistsAssignedToGroup.objects.get_or_create(
+        playlist=playlist_id,
+        group=FacilityGroup.objects.get(id=group_id),
+    )
+
+    if was_created:
+        return JsonResponse({"ok": "Assigned group %s to playlist %s" % (group_id, playlist_id)})
+    else:
+        return JsonResponse({"ok": "Playlist already assigned to group"})
