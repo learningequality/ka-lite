f99a7f4cf92b6027cbc5ffc6a55d0d2860021b0a
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-16 18:57:34 -0700

    rename playlist/index.html to playlist/assign_playlists.html

--- kalite/playlist/views.py
+++ kalite/playlist/views.py
@@ -1,7 +1,7 @@
 from annoying.decorators import render_to
 
 
-@render_to("playlist/index.html")
+@render_to("playlist/assign_playlists.html")
 def playlists(request):
     context = {
         "title": "Playlists",
