8cc00737b61877c180b70e9c7dfc54581c4fd5fe
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-16 19:18:48 -0700

    only allow teachers to access assign playlists page

--- kalite/playlist/views.py
+++ kalite/playlist/views.py
@@ -1,8 +1,16 @@
 from annoying.decorators import render_to
 
+from django.http import HttpResponseForbidden
 
+from kalite.shared.decorators import require_login
+
+
+@require_login
 @render_to("playlist/assign_playlists.html")
 def playlists(request):
+    if request.is_admin or not request.session['facility_user'].is_teacher:
+        return HttpResponseForbidden('Only teachers are allowed to access this page')
+
     context = {
         "title": "Playlists",
     }
