2136ce3f2317ea50f9dd5ee3fb5803f00fb357ac
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-21 15:23:56 -0700

    remove the old API for assigning groups to playlists

--- kalite/playlist/api_urls.py
+++ kalite/playlist/api_urls.py
@@ -4,8 +4,6 @@ from .api_resources import PlaylistResource
 
 
 urlpatterns = patterns(__package__ + '.api_views',
-    url(r'^assign/group/(?P<group_id>\w+)/playlist/(?P<playlist_id>\w+)/$', 'assign_group_to_playlist', {}, 'assign_group_to_playlist'),
-
     # For playlist management
     url(r'^', include(PlaylistResource().urls))
 )
