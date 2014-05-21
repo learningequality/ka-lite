8f42678d9fa79f49524631a76400503a7ebd9e1b
Jomel Araniego <jomel150@gmail.com>
2014-05-22 00:52:45 +0800

    Refactor assign_playlist.js

--- kalite/playlist/static/js/playlist/assign_playlists.js
+++ kalite/playlist/static/js/playlist/assign_playlists.js
@@ -1,33 +1,40 @@
-function appendStudentGrp(id, studentGrp) {
-  $("#student-grps-" + id + " ul").append('<li>'+studentGrp+'</li>');
+function appendStudentGrp(id, studentGrpName) {
+  var selector = sprintf("tr[playlist-id|=%s] ul", id);
+  $(selector).append('<li>'+studentGrpName+'</li>');
 }
+
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(ev) {
   var target = ev.currentTarget;
+  var studentGrpId = target.getAttribute('student-grp-id');
   var studentGrp = target.innerText || target.textContent;
-  ev.originalEvent.dataTransfer.setData('student-grp', studentGrp);
+  var dataTransfer = ev.originalEvent.dataTransfer;
+  dataTransfer.setData('student-grp', studentGrp);
+  dataTransfer.setData('student-grp-id', studentGrpId);
 }
 
 function drop(ev) {
-  var array = ev.currentTarget.id.split('-');
-  var length = array.length;
-  var id = array[length - 1];
+  var target = ev.currentTarget;
+  var playlistId = target.getAttribute('playlist-id');
+  var dataTransfer = ev.originalEvent.dataTransfer;
   ev.preventDefault();
-  var studentGrp = ev.originalEvent.dataTransfer.getData('student-grp');
-  appendStudentGrp(id, studentGrp);
+  var studentGrp = dataTransfer.getData('student-grp');
+  appendStudentGrp(playlistId, studentGrp);
 }
 
 // modify the groups assigned to a playlist
 function api_modify_groups(playlist_id, group_ids_assigned) {
+  console.log(ALL_PLAYLISTS_URL);
   var PLAYLIST_DETAIL_URL = ALL_PLAYLISTS_URL + playlist_id + "/"; // note: cleanup to something more readable
   var groups_assigned = group_ids_assigned.map(function (id) { return {"id": id}; });
   doRequest(PLAYLIST_DETAIL_URL,
             {"groups_assigned": groups_assigned},
             {"dataType": "text", // use text so jquery doesn't do the error callback since the server returns an empty response body
-             "type": "PATCH"}).success(function() {
+             "type": "PATCH"}).success(function(res) {
+               console.log(res);
                clear_messages();
               show_message("success", gettext("Successfully updated playlist groups."));
             }).error(function(e) {
@@ -38,41 +45,42 @@ function api_modify_groups(playlist_id, group_ids_assigned) {
             });
 }
 
-
-// get and display all groups
-$(function() {
+function displayGroups() {
     doRequest(ALL_GROUPS_URL).success(function(data) {
         data.objects.map(function(obj) {
-            $("table[id|=all-student-groups] tr:last").after(sprintf('<tr><td>%(name)s</td></tr>', obj));
+            $("#student-groups").append(sprintf('<tr><td student-grp-id="%(id)s" draggable="true">%(name)s</td></tr>', obj));
         });
+        $(".span3 td").on('dragstart', drag);
     });
-});
+}
 
-// get and display all playlists and their assigned groups
-$(function() {
+function appendPlaylistStudentGroupRow(playlist) {
+    return sprintf('<tr class="droppable" playlist-id="%(id)s"><td><ul></ul></td></tr>', playlist);
+}
+
+function displayPlaylists() {
     doRequest(ALL_PLAYLISTS_URL).success(function(data) {
         data.objects.map(function(playlist) {
-            $("table[id|=playlist-table] tr:last").after(sprintf("<tr class='playlist-title' playlist_id='%(id)s'><td>%(title)s</td></tr>", playlist));
-            $("tr[class|=playlist-title]:last").after("<tr class='playlist-groups-assigned'><td><ul></ul></td></tr>");
+            var playlistTitleRow = sprintf("<tr class='droppable title' playlist-id='%(id)s'><td>%(title)s</td></tr>", playlist);
+            var playlistStudentGroupsRow = appendPlaylistStudentGroupRow(playlist);
+            $("#playlists").append(playlistTitleRow + playlistStudentGroupsRow);
+            $("tr.title").click(function(e) {
+                $(e.currentTarget.nextSibling).toggle();
+            });
 
             playlist.groups_assigned.map(function(group) {
-                $("tr[class|=playlist-groups-assigned]:last ul").append(sprintf("<li group_id='%(id)s'>%(name)s</li>", group));
+                appendStudentGrp(group.id, group.name);
             });
+            $(".droppable").on('dragover', allowDrop);
+            $(".droppable").on('drop', drop);
         });
     });
-});
+}
 
 $(function() {
-    $(".span3 td").on('dragstart', drag);
-    $("tr[id|=student-grps]").on('dragover', allowDrop);
-    $("tr[id|=student-grps]").on('drop', drop);
-    $("tr[id|=title]").on('dragover', allowDrop);
-    $("tr[id|=title]").on('drop', drop);
-    $("tr[id|=student-grps]").hide();
-    $("tr[id|=title]").click(function(e) {
-        var target = e.target;
-        var targetId = e.currentTarget.id;
-        var idNum = targetId.split('-')[1];
-        $("#student-grps-" + idNum).toggle();
-    });
+    displayGroups();
+    displayPlaylists();
+
+    $("tr.title+tr").hide();
+
 });
