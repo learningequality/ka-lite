b8ff7d94fa7ea6c17ddeea72cbe98fcae53a1a8b
Cyril Pauya <cpauya@gmail.com>
2014-05-24 13:17:16 +0800

    Initial fix for the #2119 - Redirect users if exam mode is on on that device.

---
+++ kalite/student_testing/middleware.py
@@ -0,0 +1,52 @@
+import re
+
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from fle_utils.config.models import Settings
+
+
+SETTINGS_KEY_EXAM_MODE = 'EXAM_MODE_ON'
+
+
+class ExamModeCheck:
+    """
+    If exam-mode is on for the device, redirect user to the exam page set in the exam list page.
+    """
+    def process_request(self, request):
+
+        # TODO: users somehow does not login using django's auth module, so this is not working well
+        # if not request.user.is_authenticated():
+        #     return None
+
+        exam_mode_on = Settings.get(SETTINGS_KEY_EXAM_MODE, False)
+        print '==> EXAM_MODE_ON', exam_mode_on
+        if not exam_mode_on:
+            return None
+
+        print '====> process_request', request.user, request.is_admin, request.path
+
+        # TODO: How to check if logged-in user is a student?
+        if request.is_admin or request.is_teacher:
+            return None
+
+        # TODO: prevent redirect on certain url patterns
+        path = request.path
+        redirect_to = reverse("account_management")
+        url_exceptions = [
+            "^/api/*",
+            "^/securesync/*",
+            redirect_to
+        ]
+        for item in url_exceptions:
+            p = re.compile(item)
+            print '====> trying url ignore pattern', item, 'redirect_to==', redirect_to, p.match(redirect_to)
+            if p.match(path):
+                print '======> url ignore match', item
+                return None
+
+        print '====> redirecting to', redirect_to, 'from', request.path
+
+        # TODO: Redirect to the exam page set in the exam list page, for now, just redirect to user profile page.
+        response = HttpResponseRedirect(redirect_to)
+        return response
--- kalite/student_testing/settings.py
+++ kalite/student_testing/settings.py
@@ -11,3 +11,8 @@ except ImportError:
 #######################
 
 STUDENT_TESTING_DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
+
+MIDDLEWARE_CLASSES = getattr(local_settings, 'MIDDLEWARE_CLASSES', tuple())
+MIDDLEWARE_CLASSES = (
+    "kalite.student_testing.middleware.ExamModeCheck",
+) + MIDDLEWARE_CLASSES
