108a88e002a219c929ceaa2210a2f053ec42d7b8
Cyril Pauya <cpauya@gmail.com>
2014-05-24 18:31:41 +0800

    This fixes the `password should be at least 6 characters` issue when running tests. Some PEP-8 fixes.

--- kalite/facility/tests/form_tests.py
+++ kalite/facility/tests/form_tests.py
@@ -21,7 +21,7 @@ class FacilityTestCase(KALiteTestCase):
         super(FacilityTestCase, self).setUp()
         self.facility = Facility.objects.create(name='testfac')
         self.group = FacilityGroup.objects.create(name='testgroup', facility=self.facility)
-        self.admin = User.objects.create(username='testadmin',password=make_password('insecure'))
+        self.admin = User.objects.create(username='testadmin', password=make_password('insecure'))
         self.data = {
             'username': u'testuser',
             'first_name': u'fn',
@@ -42,27 +42,28 @@ class UserRegistrationTestCase(FacilityTestCase):
         self.assertNotIn("errorlist", response.content, "Must be no form errors")
         self.assertEqual(response.status_code, 302, "Status code must be 302")
 
-        FacilityUser.objects.get(username=self.data['username']) # should not raise error
+        FacilityUser.objects.get(username=self.data['username'])  # should not raise error
 
     def test_admin_and_user_no_common_username(self):
         """Post as username"""
         self.data['username'] = self.admin.username
         response = self.client.post(reverse('add_facility_student'), self.data)
         self.assertEqual(response.status_code, 200, "Status code must be 200")
-        self.assertFormError(response, 'form', 'username', 'The specified username is unavailable. Please choose a new username and try again.')
+        self.assertFormError(response, 'form', 'username',
+                             'The specified username is unavailable. Please choose a new username and try again.')
 
     def test_password_length_valid(self):
         response = self.client.post(reverse('add_facility_student'), self.data)
         self.assertNotIn("errorlist", response.content, "Must be no form errors")
         self.assertEqual(response.status_code, 302, "Status code must be 302")
 
-        FacilityUser.objects.get(username=self.data['username']) # should not raise error
+        FacilityUser.objects.get(username=self.data['username'])  # should not raise error
 
     def test_password_length_enforced(self):
         # always make passwd shorter than passwd min length setting
         min_length = settings.PASSWORD_CONSTRAINTS['min_length']
-        print '==> test_password_length_enforced', self, '====> min_length', min_length
-        self.data['password_first'] = self.data['password_recheck'] = self.data['password_first'][:min_length - 1]
+        d = self.data['password_first'][:min_length - 1]
+        self.data['password_first'] = self.data['password_recheck'] = d
 
         response = self.client.post(reverse('add_facility_student'), self.data)
         self.assertEqual(response.status_code, 200, "Status code must be 200")
@@ -70,13 +71,14 @@ class UserRegistrationTestCase(FacilityTestCase):
                              "Password should be at least %d characters." % min_length)
 
     def test_only_ascii_letters_allowed(self):
-        self.data['password_first'] = self.data['password_recheck'] = string.whitespace.join([self.data['password_first']] * 2)
+        l = [self.data['password_first']] * 2
+        self.data['password_first'] = self.data['password_recheck'] = string.whitespace.join(l)
 
         response = self.client.post(reverse('add_facility_student'), self.data)
         self.assertNotIn("errorlist", response.content, "Must be no form errors")
         self.assertEqual(response.status_code, 302, "Status code must be 302")
 
-        FacilityUser.objects.get(username=self.data['username']) # should not raise error
+        FacilityUser.objects.get(username=self.data['username'])  # should not raise error
 
 
 class DuplicateFacilityNameTestCase(FacilityTestCase):
@@ -192,7 +194,8 @@ class DuplicateFacilityUserTestCase(FacilityTestCase):
         self.data['first_name'] += '-different'
         user_form = FacilityUserForm(facility=self.facility, data=self.data)
         self.assertFalse(user_form.is_valid(), "Form must NOT be valid.")
-        self.assertIn('A user with this username already exists.', user_form.errors['username'][0], "Error message must contain # of users")
+        self.assertIn('A user with this username already exists.', user_form.errors['username'][0],
+                      "Error message must contain # of users")
 
     def test_form_duplicate_name_count(self):
         """Should have the proper duplicate user name count."""
