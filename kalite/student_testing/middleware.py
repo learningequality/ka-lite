af7c63732a21c11a86c4d59d0fb8318bb8ea1fcc
Cyril Pauya <cpauya@gmail.com>
2014-05-27 05:53:40 +0800

    Add the `/admin` url to exceptions when redirecting. Add the `student_testing.middleware` to `distributed.settings`. Refactor to check for url exceptions first before proceeding to check for the `EXAM_MODE_ON` setting.

--- kalite/student_testing/middleware.py
+++ kalite/student_testing/middleware.py
@@ -15,36 +15,27 @@ class ExamModeCheck:
     """
     def process_request(self, request):
 
-        if not request.is_logged_in:
-            return None
-
-        exam_mode_on = Settings.get(SETTINGS_KEY_EXAM_MODE, False)
-        # print '==> EXAM_MODE_ON', exam_mode_on
-        if not exam_mode_on:
-            return None
-
-        # print '====> process_request', request.user, request.is_admin, request.path
-
-        # TODO: How to check if logged-in user is a student?
-        if request.is_admin or request.is_teacher:
-            return None
-
         # MUST: prevent redirect on certain url patterns
         path = request.path
         redirect_to = reverse("account_management")
         url_exceptions = [
+            "^/admin/*",
             "^/api/*",
             "^/securesync/*",
             redirect_to
         ]
         for item in url_exceptions:
             p = re.compile(item)
-            # print '====> trying url ignore pattern', item, 'redirect_to==', redirect_to, p.match(redirect_to)
             if p.match(path):
-                # print '======> url ignore match', item
                 return None
 
-        # print '====> redirecting to', redirect_to, 'from', request.path
+        # check if logged-in user is a student
+        if not request.is_logged_in or request.is_admin or request.is_teacher:
+            return None
+
+        exam_mode_on = Settings.get(SETTINGS_KEY_EXAM_MODE, False)
+        if not exam_mode_on:
+            return None
 
         # TODO: Redirect to the exam page set in the exam list page, for now, just redirect to user profile page.
         response = HttpResponseRedirect(redirect_to)
--- kalite/student_testing/settings.py
+++ kalite/student_testing/settings.py
@@ -11,8 +11,3 @@ except ImportError:
 #######################
 
 STUDENT_TESTING_DATA_PATH = os.path.join(os.path.dirname(__file__), "data")
-
-MIDDLEWARE_CLASSES = getattr(local_settings, 'MIDDLEWARE_CLASSES', tuple())
-MIDDLEWARE_CLASSES = (
-    "kalite.student_testing.middleware.ExamModeCheck",
-) + MIDDLEWARE_CLASSES
