56bcebde4e2d616a4f24f4d6ca08357e4e4a46e7
Dylan Barth <dylan.j.barth@gmail.com>
2014-05-30 11:53:34 -0700

    Coach js now works, have framework for rest of refactor

--- kalite/control_panel/static/js/control_panel/facility_management.js
+++ kalite/control_panel/static/js/control_panel/facility_management.js
@@ -1,18 +1,21 @@
 function getSelectedItems(select) {
     // Retrieve a list of selected users.
-    var items = $(select).find("tr.selected").map(function () {
+    // var items = $(select).find("tr.selected").map(function () {
+    //     return $(this).attr("value");
+    // }).get();
+    var items = $(select).find("input:checked").parents("tr").map(function () {
         return $(this).attr("value");
     }).get();
-
     return items;
 }
 
 function setActionButtonState(select) {
     // argument to allow conditional selection of action buttons.
+
     if($(select).find("input:checked").length) {
-        $(select).find(".action button").removeAttr("disabled")
+        $('button[value="'+select+'"]').removeAttr("disabled");
     } else {
-        $(select).find(".action button").attr("disabled", "disabled")
+        $('button[value="'+select+'"]').attr("disabled", "disabled");
     }
 }
 
@@ -24,14 +27,18 @@ $(function() {
         window.location.href = setGetParamDict(window.location.href, GetParams);
     });
 
+    // Code for checkboxes
     $(".select-all").click(function(event){
-        // Select all users within local table
+        // Select all checkbxes within local table
+        var el = $(event.target.value);
         if(this.checked) {
-            $(event.target.value).find("input").prop("checked", true);
+            el.find("input").prop("checked", true);
         } else {
-            $(event.target.value).find("input").prop("checked", false);
+            el.find("input").prop("checked", false);
         }
-        
+    })
+
+    $("input:checkbox").click(function(event){
         // Only set action button state on related action buttons.
         setActionButtonState(event.target.value);
     })
@@ -40,8 +47,6 @@ $(function() {
         // Move users to the selected group
         var users = getSelectedItems(this.value);
         var group = $(this.value).find('.movegrouplist option:selected').val();
-
-        console.log(group);
 
         if (group==="----") {
             alert(gettext("Please choose a group to move users to."));
@@ -90,39 +95,52 @@ $(function() {
         }
     });
 
-    // This code is to allow rows of a selectable-table class table to be clicked for selection,
-    // and dragged across with mousedown for selection.
-    // When mouse is pressed over a row in the table body (not the header row), make mouseovers select.
-    $(".selectable-table").find("tbody").find("tr").mousedown(function(event){
-        $(this).toggleClass("selected");
-        // Only set action button state on related action buttons.
-        setActionButtonState("#" + $(event.currentTarget).attr("type"));
-        $(".selectable-table").find("tbody").find("tr").mouseover(function(event){
-            $(this).toggleClass("selected");
-            // This code is to toggle action buttons on only when items have been selected
-            // Works for now as only students have action buttons
-            // Only set action button state on related action buttons.
-            setActionButtonState("#" + $(event.currentTarget).attr("type"));
-        });
-    });
+    // // This code is to allow rows of a selectable-table class table to be clicked for selection,
+    // // and dragged across with mousedown for selection.
+    // // When mouse is pressed over a row in the table body (not the header row), make mouseovers select.
+    // var tableRows = $(".selectable-table").find("tbody").find("tr");
+    // tableRows.find('td:not(:has input)').mousedown(function(event){
+    //     $(this).toggleClass("selected");
+    //     var checkbox = $(this).find("input");
+    //     if (checkbox.prop("checked")) {
+    //         checkbox.prop("checked", false);
+    //     } else {
+    //         checkbox.prop("checked", true);
+    //     }
+    //     // Only set action button state on related action buttons.
+    //     setActionButtonState("#" + $(event.currentTarget).attr("type"));
+    //     $(".selectable-table").find("tbody").find("tr").mouseover(function(event){
+    //         $(this).toggleClass("selected");
+    //         var checkbox = $(this).find("input");
+    //         if (checkbox.prop("checked")) {
+    //             checkbox.prop("checked", false);
+    //         } else {
+    //             checkbox.prop("checked", true);
+    //         }
+    //         // This code is to toggle action buttons on only when items have been selected
+    //         // Works for now as only students have action buttons
+    //         // Only set action button state on related action buttons.
+    //         setActionButtonState("#" + $(event.currentTarget).attr("type"));
+    //     });
+    // });
 
-    // Unbind the mouseover selection once the button has been released.
-    $(".selectable-table").find("tbody").find("tr").mouseup(function(){
-        $(".selectable-table").find("tbody").find("tr").unbind("mouseover");
-    });
+    // // Unbind the mouseover selection once the button has been released.
+    // $(".selectable-table").find("tbody").find("tr").mouseup(function(){
+    //     $(".selectable-table").find("tbody").find("tr").unbind("mouseover");
+    // });
 
-    // If the mouse moves out of the table with the button still depressed, the above unbind will not fire.
-    // Unbind the mouseover once the mouse leaves the table.
-    // This means that moving the mouse out and then back in with the button depressed will not select.
-    $(".selectable-table").mouseleave(function(){
-        $(".selectable-table").find("tbody").find("tr").unbind("mouseover");
-    })
+    // // If the mouse moves out of the table with the button still depressed, the above unbind will not fire.
+    // // Unbind the mouseover once the mouse leaves the table.
+    // // This means that moving the mouse out and then back in with the button depressed will not select.
+    // $(".selectable-table").mouseleave(function(){
+    //     $(".selectable-table").find("tbody").find("tr").unbind("mouseover");
+    // })
 
-    // Prevent propagation of click events on links to limit confusing behaviour
-    // of rows being selected when links clicked.
-    $(".selectable-table").find("a").mousedown(function(event) {
-        event.stopPropagation();
-        return false;
-    });
+    // // Prevent propagation of click events on links to limit confusing behaviour
+    // // of rows being selected when links clicked.
+    // $(".selectable-table").find("a").mousedown(function(event) {
+    //     event.stopPropagation();
+    //     return false;
+    // });
 
 });
