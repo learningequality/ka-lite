a1fdf76becacf45025caef07e78b2e002edd2c2a
Aron Fyodor Asor <aronasorman@gmail.com>
2014-05-31 00:09:24 -0700

    partially port homepage playlists views to use backbone, and only show playlist table when logged in as user

--- kalite/distributed/static/js/distributed/homepage_playlists.js
+++ kalite/distributed/static/js/distributed/homepage_playlists.js
@@ -1,33 +1,73 @@
-$(function() {
-  $("#playlist-table").hide();
-  $.ajax({
-    url: ASSIGNED_PLAYLISTS_URL,
-    type: 'GET',
-    dataType: 'json',
-    success: function(data) {
-      $("#playlist-table").show();
-      $(".loading").hide();
-      var playlists = $("#playlists");
-      data.objects.map(function(result) {
-          var format = formatPlaylist(result);
-          $("#playlists").append(format);
-      });
-      $(".cell-content").dotdotdot({
-          ellipsis: '...',
-          wrap: 'word',
-        fallbackToLetter: true,
-        after: null,
-        watch: false,
-        height: null,
-        tolerance: 0
-      });
+var PlaylistTableView = Backbone.View.extend({
+
+    initialize: function() {
+
+
     }
-  });
+
 });
 
+var AppView = Backbone.View.extend({
+
+    initialize: function() {
+
+        $("#playlist-table").hide(); // hide the playlist table regardless
+        $(".loading").hide();
+
+        this.listenTo(window.statusModel, "sync", this.setupElementsForUser);
+    },
+
+    setupElementsForUser: function(statusModel) {
+
+        // check to see if we are a student. If so, render the playlist table
+        // and fetch the data to populate it
+        if (statusModel.get('is_logged_in') && !statusModel.get('is_admin')) { // we're a student
+
+            var playlists = new PlaylistList;
+
+            this.listenTo(playlists, "sync", this.displayPlaylistTable);
+
+            $(".loading").show();
+            playlists.fetch();
+        }
+    },
+
+    displayPlaylistTable: function(playlists) {
+
+        // TODO (aron): transfer to proper playlist view.  Did this to
+        // make things closer to how jomel implemented things
+        playlists.map(function(playlist) {
+            var format = formatPlaylist(playlist.toJSON());
+            $("#playlists").append(format);
+        });
+
+        $(".loading").hide();
+        $("#playlist-table").show();
+
+        $(".cell-content").dotdotdot({
+            ellipsis: '...',
+            wrap: 'word',
+            fallbackToLetter: true,
+            after: null,
+            watch: false,
+            height: null,
+            tolerance: 0
+        });
+
+    }
+
+});
+
+$(function() {
+
+    var app = new AppView;
+
+});
+
+
 function formatPlaylist(data) {
-    data.numVideos = data.entries.filter(function(e) { return e.entity_kind == 'Video'; }).length;
-    data.numExercises = data.entries.filter(function(e) { return e.entity_kind == 'Exercise'; }).length;
+    data.numVideos = data.entries.filter(function(e) { return e.get('entity_kind') == 'Video'; }).length;
+    data.numExercises = data.entries.filter(function(e) { return e.get('entity_kind') == 'Exercise'; }).length;
     var format = HB.template("playlists/homepage-playlists-table-cell");
     return format(data);
 
