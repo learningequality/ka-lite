c6fc75d2eff17232cd6e29317458edaa175360c2
Jamie Alexandre <jamalex@gmail.com>
2014-06-02 09:43:09 -0700

    Get videos loading on playlist page.

--- kalite/playlist/static/js/playlist/view_playlist.js
+++ kalite/playlist/static/js/playlist/view_playlist.js
@@ -32,15 +32,23 @@ window.PlaylistView = Backbone.View.extend({
 
     entry_requested: function(entry) {
         switch(entry.get("entity_kind")) {
+
             case "Exercise":
                 var view = new ExercisePracticeView({
-                    // el: $(".exercise-wrapper"),
                     exercise_id: entry.get("entity_id")
                 });
                 this.content_view.show_view(view);
-                // view.exercise_view.khan_loaded()
                 break;
 
+            case "Video":
+                var view = new VideoWrapperView({
+                    model: new VideoPlayerModel(
+                        // TODO-BLOCKER(jamalex): get actual video data loading for this
+                        {"ancestor_ids": ["root", "math", "arithmetic", "addition-subtraction", "basic_addition"], "available": true, "description": "Introduction to addition. Multiple visual ways to represent addition.", "duration": 462, "keywords": "Math, Addition, Khan, Academy, CC_1_OA_1, CC_1_OA_2, CC_1_OA_3, CC_1_OA_6", "related_exercise": {"path": "/math/arithmetic/addition-subtraction/basic_addition/e/addition_1/", "slug": "addition_1", "id": "addition_1", "title": "1-digit addition"}, "video_urls": {"stream": "/content/AuX7nPBqDts.mp4", "on_disk": true, "stream_type": "video/mp4", "language_name": "English", "thumbnail": "/content/AuX7nPBqDts.png", "subtitles": [{"url": "/static/srt/ar/subtitles/AuX7nPBqDts.srt", "code": "ar", "name": "\u0627\u0644\u0639\u0631\u0628\u064a\u0629"}, {"url": "/static/srt/es/subtitles/AuX7nPBqDts.srt", "code": "es", "name": "Espa\u00f1ol, Castellano"}]}, "dubs_available": true, "download_urls": {"mp4": "http://s3.amazonaws.com/KA-youtube-converted/AuX7nPBqDts.mp4/AuX7nPBqDts.mp4", "png": "http://s3.amazonaws.com/KA-youtube-converted/AuX7nPBqDts.mp4/AuX7nPBqDts.png", "m3u8": "http://s3.amazonaws.com/KA-youtube-converted/AuX7nPBqDts.m3u8/AuX7nPBqDts.m3u8"}, "path": "/math/arithmetic/addition-subtraction/basic_addition/v/basic-addition/", "availability": {"en": {"stream": "/content/AuX7nPBqDts.mp4", "on_disk": true, "stream_type": "video/mp4", "language_name": "English", "thumbnail": "/content/AuX7nPBqDts.png", "subtitles": [{"url": "/static/srt/ar/subtitles/AuX7nPBqDts.srt", "code": "ar", "name": "\u0627\u0644\u0639\u0631\u0628\u064a\u0629"}, {"url": "/static/srt/es/subtitles/AuX7nPBqDts.srt", "code": "es", "name": "Espa\u00f1ol, Castellano"}]}, "es": {"language_name": "Espa\u00f1ol, Castellano", "on_disk": true, "stream_type": "video/mp4", "thumbnail": "/content/yHeTx8SAaOQ.png", "stream": "/content/yHeTx8SAaOQ.mp4"}}, "selected_language": "en", "kind": "Video", "title": "Basic addition", "on_disk": true, "video_id": "AuX7nPBqDts", "slug": "basic-addition", "parent_id": "basic_addition", "id": "AuX7nPBqDts", "youtube_id": "AuX7nPBqDts", "subtitle_urls": [{"url": "/static/srt/ar/subtitles/AuX7nPBqDts.srt", "code": "ar", "name": "\u0627\u0644\u0639\u0631\u0628\u064a\u0629"}, {"url": "/static/srt/es/subtitles/AuX7nPBqDts.srt", "code": "es", "name": "Espa\u00f1ol, Castellano"}], "readable_id": "basic-addition"}
+                    )
+                });
+                this.content_view.show_view(view);
+                break;
         }
     }
 
