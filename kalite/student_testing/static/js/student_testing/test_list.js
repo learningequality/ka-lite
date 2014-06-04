1544ca64d09917505166ef6889e1b546882ce2ba
Cyril Pauya <cpauya@gmail.com>
2014-06-05 03:00:19 +0800

    Capture the click event for the button to be sent to the server.

--- kalite/student_testing/static/js/student_testing/test_list.js
+++ kalite/student_testing/static/js/student_testing/test_list.js
@@ -1,45 +1,48 @@
 var tests = new TestList;
 
 var TestView = Backbone.View.extend({
-
     tagName: "tr",
-
     className: "test-row",
-
     template: _.template($("#all-tests-list-entry-template").html()),
-
     initialize: function(options) {
         this.title = this.model.title;
         this.listenTo(this.model, 'change', this.render);
     },
-
     render: function() {
         var dict = this.model.toJSON();
         this.$el.html(this.template(dict));
         return this;
+    },
+    events: {
+        'click button': 'setExamMode'
+    },
+    setExamMode: function(ev) {
+        ev.preventDefault();
+        var examTitle = ev.target.dataset.examTitle;
+        console.log('==> setExamMode', ev, examTitle, this.model);
+//        var group = this.model;
+//        var parentModel = this.model.parentModel;
+//
+//        parentModel.get('groups_assigned').remove(group);
+//        parentModel.save();
     }
 
 });
 
 
 var AppView = Backbone.View.extend({
-
     initialize: function() {
         this.listenTo(tests, 'add', this.addNewTest);
         this.listenTo(tests, 'reset', this.addAllTests);
-
         tests.fetch();
     },
-
     addNewTest: function(test) {
         var view = new TestView({model: test});
         $("#tests").append(view.render().el);
     },
-
     addAllTests: function() {
         tests.each(this.addNewTest);
     }
-
 });
 
 
