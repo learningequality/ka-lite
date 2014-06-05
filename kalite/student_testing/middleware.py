27dcb881a8de5f58715596624e2412216994c6d1
Cyril Pauya <cpauya@gmail.com>
2014-06-06 02:14:31 +0800

    Redirect student to exam page of test set for exam-mode on Settings. Add "Tests" navigation link for admins which shows the test list page.

--- kalite/student_testing/middleware.py
+++ kalite/student_testing/middleware.py
@@ -1,4 +1,4 @@
-# import logging
+import logging
 import re
 
 from django.http import HttpResponseRedirect
@@ -17,34 +17,31 @@ class ExamModeCheck:
     """
     def process_request(self, request):
 
+        # check if logged-in user is a student
+        if not request.is_logged_in or request.is_admin or request.is_teacher:
+            return None
+
         # MUST: prevent redirect on certain url patterns
         path = request.path
-        redirect_to = '/'
-        try:
-            # This generates an exception during tests, not sure why, but we wrap it in an exception handler now.
-            redirect_to = reverse("account_management")
-        except Exception as exc:
-            pass
         url_exceptions = [
             "^/admin/*",
             "^/api/*",
             "^/securesync/*",
-            redirect_to
+            "^/test/*",
+            "^/static/*",
+            "^/handlebars/*",
         ]
         for item in url_exceptions:
             p = re.compile(item)
             if p.match(path):
                 return None
 
-        # check if logged-in user is a student
-        if not request.is_logged_in or request.is_admin or request.is_teacher:
-            return None
-
         exam_mode_on = Settings.get(SETTINGS_KEY_EXAM_MODE, '')
         if not exam_mode_on:
             return None
 
-        # TODO(cpauya): Redirect to the exam page set in the EXAM_MODE_ON Setting.
-        # redirect_to = reverse('test', args=[exam_mode_on])
+        redirect_to = reverse('test', args=[exam_mode_on])
+
+        # MUST: Redirect to the exam page set in the EXAM_MODE_ON Setting.
         response = HttpResponseRedirect(redirect_to)
         return response
