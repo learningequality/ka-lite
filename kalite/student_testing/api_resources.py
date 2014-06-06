4a8c5ef6111d687a5082cfffe04ef7d4a85a9882
Richard <richard.tibbles@gmail.com>
2014-06-06 11:30:47 -0700

    Complete conversion of Test API resource to using PK instead of title. Use file basename as PK.

--- kalite/student_testing/api_resources.py
+++ kalite/student_testing/api_resources.py
@@ -12,6 +12,7 @@ from .settings import STUDENT_TESTING_DATA_PATH
 from kalite.shared.api_auth import UserObjectsOnlyAuthorization
 from kalite.facility.api_resources import FacilityUserResource
 
+testscache = []
 
 class Test():
     def __init__(self, **kwargs):
@@ -20,6 +21,7 @@ class Test():
         self.playlist_ids = kwargs.get('playlist_ids')
         self.seed = kwargs.get('seed')
         self.repeats = kwargs.get('repeats')
+        self.pk = kwargs.get('pk')
 
 class TestLogResource(ModelResource):
 
@@ -29,7 +31,7 @@ class TestLogResource(ModelResource):
         queryset = TestLog.objects.all()
         resource_name = 'testlog'
         filtering = {
-            "title": ('exact', ),
+            "pk": ('exact', ),
             "user": ('exact', ),
         }
         authorization = UserObjectsOnlyAuthorization()
@@ -40,43 +42,34 @@ class TestResource(Resource):
     ids = fields.CharField(attribute='ids')
     seed = fields.IntegerField(attribute='seed')
     repeats = fields.IntegerField(attribute='repeats')
+    pk = fields.CharField(attribute='pk')
 
     class Meta:
         resource_name = 'test'
         object_class = Test
 
-    def _read_tests(self, title=None):
-        raw_tests = []
-        for testfile in glob.iglob(STUDENT_TESTING_DATA_PATH + "/*.json"):
-            with open(testfile) as f:
-                raw_tests.append(json.load(f))
+    def _read_tests(self, pk=None, force=False):
+        if not testscache or force:
+            for testfile in glob.iglob(STUDENT_TESTING_DATA_PATH + "/*.json"):
+                with open(testfile) as f:
+                    data = json.load(f)
+                    data["pk"] = os.path.splitext(os.path.basename(f.name))[0]
+                    testscache.append(Test(**data))
 
         # Coerce each test dict into a Test object
         # also add in the group IDs that are assigned to view this test
-        tests = []
-        for test_dict in raw_tests:
-            test = Test(**test_dict)
-            if title and test.title == title:
-                return test
-            tests.append(test)
-        if title:
+        if pk:
+            for test in testscache:
+                if pk and test.pk == pk:
+                    return test
             return None
 
-        return tests
+        return testscache
 
     def prepend_urls(self):
         return [
-            url(r"^(?P<resource_name>%s)/(?P<title>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
+            url(r"^(?P<resource_name>%s)/(?P<pk>[\w\d_.-]+)/$" % self._meta.resource_name, self.wrap_view('dispatch_detail'), name="api_dispatch_detail"),
         ]
-
-    def detail_uri_kwargs(self, bundle_or_obj):
-        kwargs = {}
-        if getattr(bundle_or_obj, 'obj', None):
-            kwargs['pk'] = bundle_or_obj.obj.title
-        else:
-            kwargs['pk'] = bundle_or_obj.title
-
-        return kwargs
 
     def get_object_list(self, request):
         '''Get the list of tests based from a request'''
@@ -86,12 +79,12 @@ class TestResource(Resource):
         return self.get_object_list(bundle.request)
 
     def obj_get(self, bundle, **kwargs):
-        title = kwargs.get("pk", None)
-        test = self._read_tests(title)
+        pk = kwargs.get("pk", None)
+        test = self._read_tests(pk)
         if test:
             return test
         else:
-            raise NotFound('Test with title %s not found' % title)
+            raise NotFound('Test with pk %s not found' % pk)
 
     def obj_create(self, request):
         raise NotImplemented("Operation not implemented yet for tests.")
