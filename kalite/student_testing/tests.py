9c4fb4c5b38acfb5f1f704ead6514729a61ba254
Cyril Pauya <cpauya@gmail.com>
2014-06-18 01:52:18 +0800

    Refactor and add more unit tests for exam page feature.

--- kalite/student_testing/tests.py
+++ kalite/student_testing/tests.py
@@ -1,18 +1,12 @@
-import json
-
 from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from django.test import Client, TestCase
-from django.utils import unittest
 
-from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoSuchElementException
 from tastypie.test import ResourceTestCase
 
-from kalite.distributed.tests.browser_tests.base import KALiteDistributedBrowserTestCase, \
-    KALiteDistributedWithFacilityBrowserTestCase
-from kalite.facility.models import Facility, FacilityGroup, FacilityUser
-from kalite.testing.client import KALiteClient
+# from kalite.distributed.tests.browser_tests.base import KALiteDistributedBrowserTestCase, \
+#     KALiteDistributedWithFacilityBrowserTestCase
+from kalite.facility.models import FacilityUser
 from kalite.testing.mixins.facility_mixins import CreateStudentMixin, CreateTeacherMixin, CreateFacilityMixin
 
 from .utils import get_exam_mode_on, set_exam_mode_on
@@ -32,6 +26,7 @@ class BaseTest(CreateStudentMixin, CreateTeacherMixin, CreateFacilityMixin, Reso
     student = None
     exam_id = '128'
     login_url = reverse('login')
+    logout_url = reverse('logout')
     test_list_url = reverse('test_list')
     exam_page_url = reverse('test', args=[exam_id])
     put_url = '/test/api/test/%s/' % exam_id
@@ -53,16 +48,10 @@ class BaseTest(CreateStudentMixin, CreateTeacherMixin, CreateFacilityMixin, Reso
                                            facility=self.facility)
         self.assertTrue(self.student)
 
-
-class CoreTests(BaseTest):
-
-    def setUp(self):
-        super(CoreTests, self).setUp()
-
     def tearDown(self):
-        super(CoreTests, self).tearDown()
+        super(BaseTest, self).tearDown()
 
-    # TODO(cpauya): some methods for Tastypie tests
+    # TODO(cpauya): some methods for Tastypie tests for future references
     # def get_credentials(self, username, password):
     #     return self.create_basic(username=username, password=password)
     #
@@ -72,22 +61,29 @@ class CoreTests(BaseTest):
     # def student_auth(self):
     #     return self.get_credentials(self.student_username, self.student_password)
 
-    def login_teacher(self, client=None):
+    def login_user(self, data, url):
+        response = self.client.post(self.login_url, data=data, follow=True)
+        self.assertEqual(response.request['PATH_INFO'], url)
+        logged_in = self.is_logged_in()
+        self.assertTrue(logged_in)
+        if logged_in:
+            fu = self.client.session.get('facility_user', None)
+            self.assertIsInstance(fu, FacilityUser)
+        return response
+
+    def login_teacher(self):
         data = {
             'username': self.teacher_username,
             'password': self.teacher_password,
             'facility': self.facility.id
         }
-        if not client:
-            client = self.client
-        response = client.post(self.login_url, data=data, follow=True)
         url = reverse('coach_reports')
-        self.assertEqual(response.request['PATH_INFO'], url)
+        response = self.login_user(data, url)
         # check content for teacher
         text = "Coach Reports"
         self.assertContains(response, text)
         # browse to the test list page to populate the test cache
-        response_tests = client.get(self.test_list_url, follow=True)
+        response_tests = self.client.get(self.test_list_url, follow=True)
         self.assertEqual(response_tests.request['PATH_INFO'], self.test_list_url)
         return response
 
@@ -97,18 +93,44 @@ class CoreTests(BaseTest):
             'password': self.student_password,
             'facility': self.facility.id
         }
-        response = self.client.post(self.login_url, data=data, follow=True)
-        self.assertEqual(response.request['PATH_INFO'], url)
-        if url == '/':
-            text = "Assigned Playlists"
-            self.assertContains(response, text)
+        response = self.login_user(data, url)
+        # if url == '/':
+        #     self.assertTrue(self.is_logged_in())
         if url == self.exam_page_url:
             text = '<title>Take Test | KA Lite</title>'
             self.assertContains(response, text)
         return response
 
+    def logout(self):
+        self.client.get(self.logout_url)
+        self.assertTrue(self.is_logged_out())
+
+    def is_logged_in(self):
+        logged_in = 'facility_user' in self.client.session
+        return logged_in
+
+    def is_logged_out(self):
+        return not self.is_logged_in()
+
+    def get_page_redirects_to_login_url(self, url):
+        response = self.client.get(url, follow=True)
+        text = "If you don't have a login, please"
+        self.assertContains(response, text)
+        self.assertEqual(response.request['PATH_INFO'], self.login_url)
+        self.assertEqual(response.status_code, 200)
+        return response
+
+
+class CoreTest(BaseTest):
+
+    def setUp(self):
+        super(CoreTest, self).setUp()
+
+    def tearDown(self):
+        super(CoreTest, self).tearDown()
+
     def test_teacher_can_access_test_list_page(self):
-        response = self.login_teacher()
+        self.login_teacher()
         # try to browse the test list page as teacher
         response = self.client.get(self.test_list_url, follow=True)
         self.assertEqual(response.request['PATH_INFO'], self.test_list_url)
@@ -116,36 +138,43 @@ class CoreTests(BaseTest):
     def test_student_cannot_access_test_list_page(self):
         # make sure we don't have exam mode on
         set_exam_mode_on('')
-        response = self.login_student()
+        self.login_student()
         # any attempt to browse the test list page as student will redirect to login page
-        response = self.client.get(self.test_list_url, follow=True)
-        self.assertEqual(response.request['PATH_INFO'], self.login_url)
+        self.get_page_redirects_to_login_url(self.test_list_url)
 
     def test_student_cannot_access_exam_page_if_not_exam_mode(self):
         # make sure we don't have exam mode on
         set_exam_mode_on('')
-        response = self.login_student()
+        self.login_student()
         # any attempt to browse the exam page as student will raise a 404
         response = self.client.get(self.exam_page_url, follow=True)
         self.assertEqual(response.status_code, 404)
 
+    def test_guest_cannot_access_exam_pages(self):
+        self.logout()
+        # try to access an exam page as guest
+        self.get_page_redirects_to_login_url(self.exam_page_url)
+        # try to access the test list page as guest
+        self.get_page_redirects_to_login_url(self.test_list_url)
+
     def test_teacher_toggle_exam_mode(self):
         data = {}
         set_exam_mode_on('')  # set exam mode
-        response = self.login_teacher()
-        response = self.client.get(self.put_url)  # this will fill the testcache
-        response = self.client.put(self.put_url, data=data, content_type='application/json')  # set exam mode
+        self.login_teacher()
+        self.client.get(self.put_url)  # this will fill the testcache
+        self.client.put(self.put_url, data=data, content_type='application/json')  # set exam mode
         # check student access on exam mode
-        response = self.login_student(url=self.exam_page_url)
+        self.login_student(url=self.exam_page_url)
         response = self.client.get(self.exam_page_url, follow=True)
         self.assertEqual(response.request['PATH_INFO'], self.exam_page_url)
+        self.logout()
 
         # reset exam mode
-        response = self.login_teacher()
-        response = self.client.get(self.put_url)  # this will fill the testcache
-        response = self.client.put(self.put_url, data=data, content_type='application/json')
+        self.login_teacher()
+        self.client.get(self.put_url)  # this will fill the testcache
+        self.client.put(self.put_url, data=data, content_type='application/json')
         # check student access if not on exam mode
-        response = self.login_student()
+        self.login_student()
         # any attempt to browse the exam page as student will raise a 404
         response = self.client.get(self.exam_page_url, follow=True)
         self.assertEqual(response.status_code, 404)
@@ -165,22 +194,22 @@ class CoreTests(BaseTest):
             self.assertEqual(get_exam_mode_on(), self.exam_id)
 
             # logged-in student must be redirected to the exam page
-            response = self.login_student(url)
+            resp = self.login_student(url)
             text = 'test_id: "%s"' % self.exam_id
-            self.assertContains(response, text)
-            self.assertEqual(response.request['PATH_INFO'], url)
+            self.assertContains(resp, text)
+            self.assertEqual(resp.request['PATH_INFO'], url)
 
-        # TODO(cpauya): option 1. set exam mode by code
-        response = self.login_teacher()
+        # option 1. set exam mode by code
+        self.login_teacher()
         set_exam_mode_on(self.exam_id)
         _check_student_access_to_exam()
 
-        # TODO(cpauya): option 2. set exam mode by client.put call
+        # option 2. set exam mode by client.put call
         data = {}
         set_exam_mode_on('')  # reset exam mode
-        response = self.login_teacher()
-        response = self.client.get(self.put_url)  # this will fill the testcache
-        response = self.client.put(self.put_url, data=data, content_type='application/json')  # set exam mode
+        self.login_teacher()
+        self.client.get(self.put_url)  # this will fill the testcache
+        self.client.put(self.put_url, data=data, content_type='application/json')  # set exam mode
         _check_student_access_to_exam()
 
         # TODO(cpauya): option 3. set exam mode by tastypie api
