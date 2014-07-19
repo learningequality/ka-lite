d0773a26221a0ae80e8ccf6b96c880cdea468787
Dylan Barth <dylan.j.barth@gmail.com>
2014-07-19 16:02:37 -0700

    First layer of topic tree backbone navigation working

--- kalite/distributed/static/js/distributed/topics.js
+++ kalite/distributed/static/js/distributed/topics.js
@@ -1,17 +1,69 @@
-window.TopicContainerView = Backbone.View.extend({
+// Models
+window.TopicNode = Backbone.Model.extend({
+    url: ALL_TOPICS_URL
+});
 
-    template: HB.template("topics/topic_container"),
+// Collections 
+window.TopicCollection = Backbone.Collection.extend({
+    model: TopicNode
+}); 
 
-    initialize: function() { 
+// Views
+window.TopicContainerOuter = Backbone.View.extend({
 
-        this.render();
+    class: "topic-container-outer",
 
+    initialize: function() {
+
+        _.bindAll(this);
+
+        this.model =  new TopicNode();
+        this.model.fetch().then(this.render);
     },
 
     render: function() {
+        this.inner_view = new TopicContainerInner({
+            model: this.model
+        });
+        this.$el.append(this.inner_view.el);
+    }
+});
 
+window.TopicContainerInner = Backbone.View.extend({
+
+    class: "topic-container-inner",
+
+    template: HB.template("topics/topic_container_inner"),
+
+    initialize: function() { 
+        this.model.set("children", new TopicCollection(this.model.get("children")));
+        this.render();
+    },
+
+    render: function() {
         this.$el.html(this.template());
+        var self = this;
+        
+        self.node_views = [];
+        this.model.get("children").each(function(node) {
+            var node_view = new TopicNodeView({ model: node });
+            self.$('.topic-container-node-list').append(node_view.el);
+        });
+    }
+});
 
+window.TopicNodeView = Backbone.View.extend({
+
+    template: HB.template("topics/topic_node"),
+
+
+
+    initialize: function() {
+        this.render()
+    },
+
+    render: function() {
+        this.$el.html(this.template(this.model.attributes));
     }
 
 });
