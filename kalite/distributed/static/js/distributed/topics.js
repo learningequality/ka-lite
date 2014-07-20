a2addb1ab3f75bf71cd28a64eca78fb3c7806326
Dylan Barth <dylan.j.barth@gmail.com>
2014-07-19 17:21:26 -0700

    Omg topic tree nav works when clicking into topics. Go backbone + Richard & jamie help

--- kalite/distributed/static/js/distributed/topics.js
+++ kalite/distributed/static/js/distributed/topics.js
@@ -17,15 +17,26 @@ window.TopicContainerOuter = Backbone.View.extend({
 
         _.bindAll(this);
 
+        this.inner_views = [];
         this.model =  new TopicNode();
         this.model.fetch().then(this.render);
     },
 
     render: function() {
-        this.inner_view = new TopicContainerInner({
-            model: this.model
+        this.show_new_topic(this.model);
+    },
+
+    show_new_topic: function(node) {
+        console.log('clciked')
+        var new_topic = new TopicContainerInner({
+            model: node
         });
-        this.$el.append(this.inner_view.el);
+        this.$el.append(new_topic.el);
+        if (this.inner_views.length > 0) {
+            this.inner_views[0].$el.hide();     
+        }
+        this.inner_views.unshift(new_topic); 
+        this.listenTo(new_topic, 'topic_node_clicked', this.show_new_topic);
     }
 });
 
@@ -48,7 +59,12 @@ window.TopicContainerInner = Backbone.View.extend({
         this.model.get("children").each(function(node) {
             var node_view = new TopicNodeView({ model: node });
             self.$('.topic-container-node-list').append(node_view.el);
+            self.listenTo(node_view, 'topic_node_clicked', self.topic_node_clicked)
         });
+    }, 
+
+    topic_node_clicked: function(node) {
+        this.trigger('topic_node_clicked', node)
     }
 });
 
@@ -56,14 +72,21 @@ window.TopicNodeView = Backbone.View.extend({
 
     template: HB.template("topics/topic_node"),
 
-
-
     initialize: function() {
         this.render()
     },
 
     render: function() {
         this.$el.html(this.template(this.model.attributes));
-    }
+    },
 
+    events: {
+        'click .topic-container-node-list-item': 'showChildCollection'
+    },
+
+    showChildCollection: function(ev) {
+        ev.preventDefault();
+        // Create the child elements and paste them on top
+        this.trigger('topic_node_clicked', this.model);
+    }
 });
