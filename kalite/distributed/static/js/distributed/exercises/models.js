eb2a92567a5aa5e30b9b93248e7357e2d50466e0
Jamie Alexandre <jamalex@gmail.com>
2014-07-19 17:11:17 -0700

    Partway through integrating Perseus rendering into Backbone views.

---
+++ kalite/distributed/static/js/distributed/exercises/models.js
@@ -0,0 +1,580 @@
+window.ExerciseParams = {
+    STREAK_CORRECT_NEEDED: 8,
+    STREAK_WINDOW: 10,
+    FIXED_BLOCK_EXERCISES: window.FIXED_BLOCK_EXERCISES || 0
+};
+
+
+window.ExerciseDataModel = Backbone.Model.extend({
+    /*
+    Contains data about an exercise itself, with no user-specific data.
+    */
+
+    defaults: {
+        basepoints: 0,
+        description: "",
+        title: "",
+        name: "",
+        seconds_per_fast_problem: 0,
+        author_name: "",
+        related_videos: [],
+        file_name: ""
+    },
+
+    initialize: function() {
+
+        _.bindAll(this);
+
+        // store the provided seed as an object attribute, so it will be available after a fetch
+        this.listenTo(this, "change:seed", function() { this.seed = this.get("seed") || this.seed; });
+
+    },
+
+    url: function () {
+        return "/api/exercise/" + this.get("exercise_id");
+    },
+
+    update_if_needed_then: function(callback) {
+        // TODO(jamalex): use a better method for checking status of lazy loading
+        if (this.get("exercise_id") !== this.get("name")) {
+            this.fetch().then(callback);
+        } else {
+            _.defer(callback);
+        }
+    },
+
+    // convert this data into the structure needed by khan-exercises
+    as_user_exercise: function () {
+        return {
+            "basepoints": this.get("basepoints"),
+            "description": this.get("description"),
+            "title": this.get("display_name"),
+            "seed": this.seed,
+            "lastCountHints": 0, // TODO: could store and pass down number of hints used
+            "exerciseModel": {
+                "displayName": this.get("display_name"),
+                "name": this.get("name"),
+                "secondsPerFastProblem": this.get("seconds_per_fast_problem"),
+                "authorName": this.get("author_name"),
+                "relatedVideos": this.get("related_videos"),
+                "fileName": this.get("file_name")
+            },
+            "exerciseProgress": {
+                "level": "" // needed to keep khan-exercises from blowing up
+            }
+        };
+    },
+
+    get_framework: function() {
+        return this.has("uses_assessment_items") ? "perseus" : "khan-exercises";
+    },
+
+    load_assessment_item: function(item_id) {
+
+    }
+
+});
+
+
+window.ExerciseLogModel = Backbone.Model.extend({
+    /*
+    Contains summary data about the user's history of interaction with the current exercise.
+    */
+
+    defaults: {
+        streak_progress: 0,
+        points: 0,
+        attempts: 0
+    },
+
+    initialize: function() {
+
+        _.bindAll(this);
+
+    },
+
+    save: function() {
+
+        var self = this;
+
+        var already_complete = this.get("complete");
+
+        if (this.get("attempts") > 20 && !this.get("complete")) {
+            this.set("struggling", true);
+        }
+
+        this.set("complete", this.get("streak_progress") >= 100);
+
+        if (!already_complete && this.get("complete")) {
+            this.set("struggling", false);
+            this.set("completion_timestamp", window.statusModel.get_server_time());
+            this.set("attempts_before_completion", this.get("attempts"));
+        }
+
+        // call the super method that will actually do the saving
+        return Backbone.Model.prototype.save.call(this);
+    },
+
+    attempts_since_completion: function() {
+        if (!this.get("complete")) {
+            return 0;
+        }
+        return this.get("attempts") - this.get("attempts_before_completion");
+    },
+
+    urlRoot: "/api/exerciselog/"
+
+});
+
+
+window.ExerciseLogCollection = Backbone.Collection.extend({
+
+    model: ExerciseLogModel,
+
+    initialize: function(models, options) {
+        this.exercise_id = options.exercise_id;
+    },
+
+    url: function() {
+        return "/api/exerciselog/?" + $.param({
+            "exercise_id": this.exercise_id,
+            "user": window.statusModel.get("user_id")
+        });
+    },
+
+    get_first_log_or_new_log: function() {
+        if (this.length > 0) {
+            return this.at(0);
+        } else { // create a new exercise log if none existed
+            return new ExerciseLogModel({
+                "exercise_id": this.exercise_id,
+                "user": window.statusModel.get("user_uri")
+            });
+        }
+    }
+
+});
+
+
+window.AttemptLogModel = Backbone.Model.extend({
+    /*
+    Contains data about the user's response to a particular exercise instance.
+    */
+
+    urlRoot: "/api/attemptlog/",
+
+    defaults: {
+        complete: false,
+        points: 0,
+        context_type: "",
+        context_id: "",
+        response_count: 0,
+        response_log: "[]"
+    },
+
+    initialize: function(options) {
+
+    },
+
+    add_response_log_event: function(ev) {
+
+        // inflate the stored JSON if needed
+        if (!this._response_log_cache) {
+            this._response_log_cache = JSON.parse(this.get("response_log") || "[]");
+        }
+
+        // set the timestamp to the current time
+        ev.timestamp = window.statusModel.get_server_time();
+
+        // add the event to the response log list
+        this._response_log_cache.push(ev);
+
+        // deflate the response log list so it will be saved along with the model later
+        this.set("response_log", JSON.stringify(this._response_log_cache));
+
+    }
+
+});
+
+
+window.AttemptLogCollection = Backbone.Collection.extend({
+
+    model: AttemptLogModel,
+
+    initialize: function(models, options) {
+        this.exercise_id = options.exercise_id;
+        this.context_type = options.context_type;
+    },
+
+    url: function() {
+        return "/api/attemptlog/?" + $.param({
+            "exercise_id": this.exercise_id,
+            "user": window.statusModel.get("user_id"),
+            "limit": ExerciseParams.STREAK_WINDOW,
+            "context_type": this.context_type
+        });
+    },
+
+    add_new: function(attemptlog) {
+        if (this.length == ExerciseParams.STREAK_WINDOW) {
+            this.pop();
+        }
+        this.unshift(attemptlog);
+    },
+
+    get_streak_progress: function() {
+        var count = 0;
+        this.forEach(function(model) {
+            count += model.get("correct") ? 1 : 0;
+        });
+        return count;
+    },
+
+    get_streak_progress_percent: function() {
+        var streak_progress = this.get_streak_progress();
+        return Math.min((streak_progress / ExerciseParams.STREAK_CORRECT_NEEDED) * 100, 100);
+    },
+
+    get_streak_points: function() {
+        // only include attempts that were correct (others won't have points)
+        var filtered_attempts = this.filter(function(attempt) { return attempt.get("correct"); });
+        // add up and return the total number of points represented by these attempts
+        // (only include the latest STREAK_CORRECT_NEEDED attempts, so the user doesn't get too many points)
+        var total = 0;
+        for (var i = 0; i < Math.min(ExerciseParams.STREAK_CORRECT_NEEDED, filtered_attempts.length); i++) {
+            total += filtered_attempts[i].get("points");
+        }
+        return total;
+    },
+
+    calculate_points_per_question: function(basepoints) {
+        // for comparability with the original algorithm (when a streak of 10 was needed),
+        // we calibrate the points awarded for each question (note that there are no random bonuses now)
+        return Math.round((basepoints * 10) / ExerciseParams.STREAK_CORRECT_NEEDED);
+    }
+
+});
+
+
+window.TestDataModel = Backbone.Model.extend({
+    /*
+    Contains data about a particular student test.
+    */
+
+    url: function() {
+        return "/test/api/test/" + this.get("test_id") + "/";
+    }
+});
+
+
+window.TestLogModel = Backbone.Model.extend({
+    /*
+    Contains summary data about the user's history of interaction with the current test.
+    */
+
+    defaults: {
+        index: 0,
+        complete: false,
+        started: false
+    },
+
+    init: function(options) {
+
+        _.bindAll(this);
+
+        var self = this;
+
+    },
+
+    get_item_data: function(test_data_model) {
+        /*
+        This function is designed to give a deterministic test sequence for an individual, based
+        on their userModel URI. As such, each individual will always have the same generated test
+        sequence, but it is, for all intents and purposes, randomized across individuals.
+        */
+
+        /*
+        Seed random generator here so that it increments all seed randomization blocks.
+        If seeded inside each call to the function, then the blocks of seeds for each user
+        would be identically shuffled.
+        */
+        if(typeof(test_data_model)==="object"){
+
+            var random = new Math.seedrandom(this.get("user"));
+
+            var items = $.parseJSON(test_data_model.get("ids"));
+
+            var initial_seed = test_data_model.get("seed");
+
+            var repeats = test_data_model.get("repeats");
+
+            var block_seeds = [];
+
+            // Create list of seeds incremented from initial_seed, one for every repeat.
+            for(i=0; i < repeats; i++){
+                block_seeds.push(initial_seed + i);
+            }
+
+            // Cache random shuffling of block seeds for each exercise_id.
+            var shuffled_block_seeds_gen = {};
+
+            // Final seed and item sequences.
+            this.seed_sequence = [];
+
+            this.item_sequence = [];
+
+            /*
+            Loop over every repeat, adding each exercise_id in turn to item_sequence.
+            On first loop, create shuffled copy of block_seeds for each exercise_id.
+            Add seed from shuffled block_seeds copy to seed_sequence.
+            This will have the net effect of a fixed sequence of exercise_ids, repeating
+            'repeats' times, with each exercise_id having a shuffled sequence of seeds across blocks.
+            */
+            for(j=0; j < repeats; j++){
+                for(i=0; i < items.length; i++){
+                    if(j===0){
+                        shuffled_block_seeds_gen[i] = seeded_shuffle(block_seeds, random);
+                    }
+                    this.item_sequence.push(items[i]);
+                    this.seed_sequence.push(shuffled_block_seeds_gen[i][j]);
+                }
+            }
+        }
+        return {
+            seed: this.seed_sequence[this.get("index")],
+            exercise_id: this.item_sequence[this.get("index")]
+        };
+    },
+
+    save: function() {
+
+        var self = this;
+
+        var already_complete = this.get("complete");
+
+        if(this.item_sequence){
+
+            if(!this.get("total_number")){
+                this.set({
+                    total_number: this.item_sequence.length
+                });
+            }
+
+            if((this.get("index") == this.item_sequence.length) && !already_complete){
+                this.set({
+                    complete: true
+                });
+                this.trigger("complete");
+            }
+        }
+
+        Backbone.Model.prototype.save.call(this);
+    },
+
+    urlRoot: "/test/api/testlog/"
+
+});
+
+
+window.TestLogCollection = Backbone.Collection.extend({
+
+    model: TestLogModel,
+
+    initialize: function(models, options) {
+        this.test_id = options.test_id;
+    },
+
+    url: function() {
+        return "/test/api/testlog/?" + $.param({
+            "test": this.test_id,
+            "user": window.statusModel.get("user_id")
+        });
+    },
+
+    get_first_log_or_new_log: function() {
+        if (this.length > 0) {
+            return this.at(0);
+        } else { // create a new exercise log if none existed
+            return new TestLogModel({
+                "user": window.statusModel.get("user_uri"),
+                "test": this.test_id
+            });
+        }
+    }
+
+});
+
+
+var QuizDataModel = Backbone.Model.extend({
+
+    defaults: {
+        repeats: 3
+    },
+
+    initialize: function() {
+        this.set({
+            ids: this.get_exercise_ids_from_playlist_entry(this.get("entry")),
+            quiz_id: this.get("entry").get("entity_id"),
+            seed: this.get("entry").get("seed") || null
+        });
+    },
+
+    get_exercise_ids_from_playlist_entry: function(entry) {
+        var temp_collection = entry.collection.slice(0, _.indexOf(entry.collection, entry));
+        var left_index = _.reduceRight(entry.collection.slice(0, _.indexOf(entry.collection, entry)), function(memo, value, index){
+                if(!memo && value.get("entity_kind")==="Quiz"){
+                    return index;
+                } else {
+                    return memo;
+                }
+            }, 0);
+        return _.map(new Backbone.Collection(temp_collection.slice(left_index)).where({"entity_kind": "Exercise"}), function(value){return value.get("entity_id");});
+    }
+});
+
+
+window.QuizLogModel = Backbone.Model.extend({
+    /*
+    Contains summary data about the user's history of interaction with the current test.
+    */
+
+    defaults: {
+        index: 0,
+        complete: false,
+        attempts: 0
+    },
+
+    init: function(options) {
+
+        _.bindAll(this);
+
+        var self = this;
+
+    },
+
+    get_item_data: function(quiz_data_model) {
+        /*
+        This function is designed to give a deterministic quiz sequence for an individual, based
+        on their userModel URI. As such, each individual will always have the same generated quiz
+        sequence, but it is, for all intents and purposes, randomized across individuals.
+        */
+
+        /*
+        Seed random generator here so that it increments all seed randomization blocks.
+        If seeded inside each call to the function, then the blocks of seeds for each user
+        would be identically shuffled.
+        */
+        if(typeof(quiz_data_model)==="object"){
+
+            var random = new Math.seedrandom(this.get("user") + this.get("attempts"));
+
+            var items = quiz_data_model.get("ids");
+
+            var repeats = quiz_data_model.get("repeats");
+
+            this.item_sequence = [];
+
+            for(j=0; j < repeats; j++){
+                this.item_sequence.push(items);
+            }
+
+            this.item_sequence = _.flatten(this.item_sequence);
+
+            this.item_sequence = seeded_shuffle(this.item_sequence, random);
+        }
+        return {
+            exercise_id: this.item_sequence[this.get("index")]
+        };
+    },
+
+    save: function() {
+
+        var self = this;
+
+        var already_complete = this.get("complete");
+
+        if(this.item_sequence){
+
+            if(!this.get("total_number")){
+                this.set({
+                    total_number: this.item_sequence.length
+                });
+            }
+
+            if((this.get("index") == this.item_sequence.length)){
+
+                this.set({
+                    index: 0,
+                    attempts: this.get("attempts") + 1
+                });
+
+                if(!already_complete) {
+                    this.set({
+                        complete: true
+
+                    });
+                }
+
+                this.trigger("complete");
+            }
+        }
+
+        Backbone.Model.prototype.save.call(this);
+    },
+
+    add_response_log_item: function(data) {
+
+        // inflate the stored JSON if needed
+        if (!this._response_log_cache) {
+            this._response_log_cache = JSON.parse(this.get("response_log") || "[]");
+        }
+
+        if(this._response_log_cache[this.get("attempts")]){
+            this._response_log_cache.push(0);
+        }
+        // add the event to the response log list
+        if(data.correct){
+            this._response_log_cache[this.get("attempts")] += 1;
+            if(this.get("attempts")==0) {
+                this.set({
+                    total_correct: this.get("total_correct") + 1
+                });
+            }
+        }
+
+        // deflate the response log list so it will be saved along with the model later
+        this.set("response_log", JSON.stringify(this._response_log_cache));
+
+    },
+
+    urlRoot: "/api/playlists/quizlog/"
+
+});
+
+
+window.QuizLogCollection = Backbone.Collection.extend({
+
+    model: QuizLogModel,
+
+    initialize: function(models, options) {
+        this.quiz = options.quiz;
+    },
+
+    url: function() {
+        return "/api/playlists/quizlog/?" + $.param({
+            "quiz": this.quiz,
+            "user": window.statusModel.get("user_id")
+        });
+    },
+
+    get_first_log_or_new_log: function() {
+        if (this.length > 0) {
+            return this.at(0);
+        } else { // create a new exercise log if none existed
+            return new QuizLogModel({
+                "user": window.statusModel.get("user_uri")
+            });
+        }
+    }
+
+});
---
+++ kalite/distributed/static/js/distributed/exercises/perseus-helpers.js
@@ -0,0 +1,81 @@
+// $(Exercises).trigger("clearExistingProblem");
+
+$.extend(Exercises.PerseusBridge, {
+
+    // this one needs to be here for khan-exercises
+    scoreInput: function() {
+        return Exercises.PerseusBridge.itemRenderer.scoreInput();
+    },
+
+    // this one needs to be here for khan-exercises
+    cleanupProblem: function(data) {
+        if (Exercises.PerseusBridge.itemMountNode) {
+            React.unmountComponentAtNode(Exercises.PerseusBridge.itemMountNode);
+            Exercises.PerseusBridge.itemMountNode = null;
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    // returns a Deferred that is resolved when Perseus is loaded and ready
+    load: function() {
+
+        // if we've already started loading Perseus, just return the existing Deferred
+        if (Exercises.PerseusBridge._loaded) {
+            return Exercises.PerseusBridge._loaded;
+        } else { // otherwise, create a Deferred
+            Exercises.PerseusBridge._loaded = $.Deferred();
+        }
+
+        // Load khan-exercises modules, then perseus
+        require([
+                STATIC_URL + "perseus/ke-deps.js",
+                STATIC_URL + "perseus/ke/interface.js"
+            ], function() {
+                require([STATIC_URL + "perseus/build/perseus-1.js"], Exercises.PerseusBridge._initialize);
+            }
+        );
+
+        return Exercises.PerseusBridge._loaded;
+    },
+
+    _initialize: function(Perseus) {
+
+        window.Perseus = Perseus;
+
+        Perseus.init({
+            skipMathJax: false
+        }).then(function() {
+            Exercises.PerseusBridge._loaded.resolve();
+        });
+
+    },
+
+    render_item: function(item_data) {
+
+        Exercises.PerseusBridge.itemMountNode = document.createElement("div");
+        var itemRenderer = Exercises.PerseusBridge.itemRenderer = Perseus.ItemRenderer({
+            item: item_data,
+            problemNum: Math.floor(Math.random() * 50) + 1,
+            initialHintsVisible: false, //&& item_data.hints && item_data.hints.length,
+            enabledFeatures: {
+                highlight: true,
+                toolTipFormats: true//,
+                //useMathQuill: true
+            },
+            apiOptions: {
+                interceptInputFocus: function() {}, // do nothing here; prevent keyboard from popping up
+                fancyDropdowns: true, // needed?
+                staticRender: true // needed?
+            },
+        }, null);
+        zk = React.renderComponent(
+            Exercises.PerseusBridge.itemRenderer,
+            Exercises.PerseusBridge.itemMountNode
+        );
+        zk.focus();
+
+    }
+
+});
