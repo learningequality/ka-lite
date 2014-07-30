c8375f341e1c95fef18f0f59c9023f45834303ff
Richard <richard.tibbles@gmail.com>
2014-07-30 15:48:49 -0700

    Initializes store app. Adds models for store app.

---
+++ kalite/store/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from kalite.facility.models import FacilityUser
+
+from securesync.models import DeferredCountSyncedModel
+
+# Create your models here.
+
+class StoreTransactionLog(DeferredCountSyncedModel):
+    """
+    Model to track student 'expenditure' of points.
+    """
+
+    # TODO-BLOCKER(rtibbles): Update this to "0.13.0" (or whatever the release version number is at the time this goes upstream)
+
+    minversion = "0.12.0"
+
+    user = models.ForeignKey(FacilityUser, db_index=True)
+    value = models.IntegerField(default=0)
+    context_id = models.CharField(max_length=100, blank=True) # e.g. the unit id that it was spent/earned during
+    context_type = models.CharField(max_length=100, blank=True)
+    # can this transaction be undone by user actions? Initially set by 'returnable' on StoreItem, but can be changed subsequently
+    reversible = models.BooleanField(default=False)
+    item = models.ForeignKey(StoreItem, db_index=True)
+
+    class Meta:  # needed to clear out the app_name property from SyncedClass.Meta
+        pass
+
+
+class StoreItem(DeferredCountSyncedModel):
+    """
+    Model to track items available for student purchase.
+    """
+
+    # TODO-BLOCKER(rtibbles): Update this to "0.13.0" (or whatever the release version number is at the time this goes upstream)
+
+    minversion = "0.12.0"
+
+    cost = models.IntegerField(default=0)
+    returnable = models.BooleanField(default=False) # can this item be returned to the store for a refund?
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, verbose_name=_("Description"))
+    image = models.TextField(blank=True) # data URI for image of item
+
+    class Meta:  # needed to clear out the app_name property from SyncedClass.Meta
+        pass
