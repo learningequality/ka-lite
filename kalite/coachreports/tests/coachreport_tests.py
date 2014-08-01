bc8bf914d68051ed06f2fd0d0e2b81b9660275fb
Aron Fyodor Asor <aronasorman@gmail.com>
2014-07-31 22:13:10 -0700

    turn all assertTrue calls in coachreport_tests.py into assertEqual to make the errors clearer

--- kalite/coachreports/tests/coachreport_tests.py
+++ kalite/coachreports/tests/coachreport_tests.py
@@ -35,9 +35,9 @@ class TestReportTests(FacilityMixins,
         self.browser_login_admin()
         self.browse_to(self.reverse('test_view'))
         student_score = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[2]/td[1]').text
-        self.assertTrue(student_score[0:3] == '50%')
+        self.assertEqual(student_score[0:3], '50%')
         empty_student = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr/td[1]').text
-        self.assertTrue(empty_student == '')
+        self.assertEqual(empty_student, '')
 
     def test_test_stats_display(self):
         """
@@ -69,13 +69,13 @@ class TestReportTests(FacilityMixins,
         self.browser_login_admin()
         self.browse_to(self.reverse('test_view'))
         stat_max = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[3]/td[1]').text
-        self.assertTrue(stat_max == '50%')
+        self.assertEqual(stat_max, '50%')
         stat_min = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[4]/td[1]').text
-        self.assertTrue(stat_min == '25%')
+        self.assertEqual(stat_min, '25%')
         stat_avg = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[5]/td[1]').text
-        self.assertTrue(stat_avg == '37%')
+        self.assertEqual(stat_avg, '37%')
         stat_std = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[6]/td[1]').text
-        self.assertTrue(stat_std == '12%')
+        self.assertEqual(stat_std, '12%')
 
     def test_student_stats_display(self):
         """
@@ -142,14 +142,14 @@ class TestReportTests(FacilityMixins,
         self.browser_login_admin()
         self.browse_to(self.reverse('test_detail_view', kwargs={'test_id': self.test_log.test}))
         student_score = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[1]/td[1]').text
-        self.assertTrue(student_score[0:4] == '100%')
+        self.assertEqual(student_score[0:4], '100%')
         stat_max = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[2]/td[1]').text
-        self.assertTrue(stat_max[0:4] == '100%')
+        self.assertEqual(stat_max[0:4], '100%')
         stat_min = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[3]/td[1]').text
-        self.assertTrue(stat_min[0:4] == '100%')
+        self.assertEqual(stat_min[0:4], '100%')
         stat_avg = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[4]/td[1]').text
-        self.assertTrue(stat_avg[0:4] == '100%')
+        self.assertEqual(stat_avg[0:4], '100%')
         stat_std = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[5]/td[1]').text
-        self.assertTrue(stat_std == '0.0%')
+        self.assertEqual(stat_std, '0.0%')
         overall = self.browser.find_element_by_xpath('//div[@class="results-table"]/table/tbody/tr[1]/td[7]').text
-        self.assertTrue(overall == '100.0%')
+        self.assertEqual(overall, '100.0%')
