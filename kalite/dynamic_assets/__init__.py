7ae89748a90e9161b6efd75949ccd65ca4753a4e
Aron Fyodor Asor <aronasorman@gmail.com>
2014-08-09 23:38:27 -0700

    load all dynamic settings if ever we import dynamic_assets

---
+++ kalite/dynamic_assets/__init__.py
@@ -0,0 +1,14 @@
+import importlib
+
+from django.conf import settings
+
+from .models import *
+
+
+for app in settings.INSTALLED_APPS:
+    module_name = '%s.dynamic_settings' % app
+
+    try:
+        importlib.import_module(module_name)
+    except ImportError:
+        continue
