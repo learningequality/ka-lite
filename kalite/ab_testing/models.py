74287f7616093464428bbadbc3bf1445407164d7
Aron Fyodor Asor <aronasorman@gmail.com>
2014-08-13 13:53:51 -0700

    add ab_testing custom dynamic settings that can be saved into the db

--- kalite/ab_testing/models.py
+++ kalite/ab_testing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from kalite.facility.models import FacilityUser
+
+
+class FacilityUserSpecificSetting(models.Model):
+    user = models.ForeignKey(FacilityUser, related_name='dynamic_settings')
+
+    turn_off_motivational_features = models.BooleanField()
