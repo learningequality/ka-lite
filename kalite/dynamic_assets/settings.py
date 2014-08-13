ccf325057e9ebbb8d27ddb5fe699f2fc8fd98ff3
Aron Fyodor Asor <aronasorman@gmail.com>
2014-08-13 13:53:09 -0700

    make DYNAMIC_ASSETS_PRIORITY_APPS into a tuple

--- kalite/dynamic_assets/settings.py
+++ kalite/dynamic_assets/settings.py
@@ -1,4 +1,4 @@
 # Dynamic settings within these apps override settings in all other
 # apps. If there are similarly-named settings within the apps below,
 # the apps in the end override the earlier defined apps
-DYNAMIC_SETTINGS_PRIORITY_APPS = ['kalite.ab_testing']
+DYNAMIC_SETTINGS_PRIORITY_APPS = ('kalite.ab_testing',)
