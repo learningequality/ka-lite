5d28edffd9ce546270439cafdeafe3106beeb748
Aron Fyodor Asor <aronasorman@gmail.com>
2014-08-13 06:24:55 -0700

    in dynamic_assets/decorators.py: return the value of viewfn

--- kalite/dynamic_assets/decorators.py
+++ kalite/dynamic_assets/decorators.py
@@ -5,6 +5,6 @@ def dynamic_settings(viewfn):
 
     def new_view_fn(request, *args, **kwargs):
         ds = load_dynamic_settings()
-        viewfn(request, ds, *args, **kwargs)
+        return viewfn(request, ds, *args, **kwargs)
 
     return new_view_fn
