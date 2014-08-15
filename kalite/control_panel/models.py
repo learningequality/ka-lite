76207a35932e1b222dfc1b4c6d9ef1112ee86113
Dylan Barth <dylan.j.barth@gmail.com>
2014-08-15 15:58:29 -0700

    API returning high level endpoint for playlists

--- kalite/control_panel/models.py
+++ kalite/control_panel/models.py
@@ -5,45 +5,39 @@ from kalite.main.models import ExerciseLog, VideoLog
 from kalite.playlist.models import VanillaPlaylist as Playlist, QuizLog
 from kalite.topic_tools import get_slug2id_map, get_id2slug_map, get_flat_topic_tree, convert_leaf_url_to_id
 
-class UserPlaylistProgress:
+class PlaylistProgress:
     """Users progress on playlists"""
 
     def __init__(self, **kwargs):
-        self.user_id = kwargs.get("user_id")
-        self.playlists = kwargs.get("playlists")
+        self.title = kwargs.get("title")
+        self.id = kwargs.get("id")
+        self.tag = kwargs.get("tag")
+        self.vid_pct_complete = kwargs.get("vid_pct_complete")
+        self.vid_pct_started = kwargs.get("vid_pct_started")
+        self.ex_pct_mastered = kwargs.get("ex_pct_mastered")
+        self.ex_pct_struggling = kwargs.get("ex_pct_struggling")
+        self.ex_pct_started = kwargs.get("ex_pct_started")
+        self.quiz_exists = kwargs.get("quiz_exists")
+        self.quiz_pct_score = kwargs.get("quiz_pct_score")
 
     @classmethod
-    def user_playlist_progress(cls, user_id):
+    def user_progress(cls, user_id):
         """
-        Return a dictionary of playlists that the user has started, with stats on completion_timestamp
-        Response schema:
+        Return a list of PlaylistProgress objects associated with the user.
             [
                 {
-                    "grade": "tag_name",
-                    "playlists": [
-                        {
-                            "insert_existing_playlist_data": "...",
-                            "...": "...",
-                            "vid_stats": {
-                                "pct_complete": "...",
-                                "pct_started": "...",
-                            },
-                            "ex_stats": {
-                                "pct_mastered": "...",
-                                "pct_inprogress": "...",
-                                "pct_struggling": "...",
-                                "pct_incomplete": "...",
-                            },
-                            "test_stats": {
-                                "pct_score": "...",
-                            }
-                        },
-                        {
-                            "insert_existing_playlist_data": "...",
-                            "...": "..."
-                        },
-                    ]
-                }
+                    "title": "...",
+                    "id": "...",
+                    "tag": "...",
+                    "vid_pct_complete": "...",
+                    "vid_pct_started": "...",
+                    "ex_pct_mastered": "...",
+                    "ex_pct_struggling": "...",
+                    "ex_pct_started": "...",
+                    "quiz_exists": "...",
+                    "quiz_pct_score": "...",
+                },
+                { ... etc ... }
             ]
         """
         user = FacilityUser.objects.filter(id=user_id)
@@ -75,41 +69,45 @@ class UserPlaylistProgress:
                         break
 
         # Store stats for each playlist
+        user_progress = list()
         for i, p in enumerate(user_playlists):
             # Playlist entry totals
             pl_video_ids = set([convert_leaf_url_to_id(entry["entity_id"]) for entry in p.get("entries") if entry.get("entity_kind") == "Video"])
             pl_exercise_ids = set([convert_leaf_url_to_id(entry["entity_id"]) for entry in p.get("entries") if entry.get("entity_kind") == "Exercise"])
-            n_pl_videos = len(pl_video_ids)
-            n_pl_exercises = len(pl_exercise_ids) 
+            n_pl_videos = float(len(pl_video_ids))
+            n_pl_exercises = float(len(pl_exercise_ids))
 
             # Vid & exercise logs in this playlist
             ex_logs = [ex_log for ex_log in exercise_logs if ex_log["exercise_id"] in pl_exercise_ids]
             vid_logs = [vid_log for vid_log in video_logs if id2slug_map.get(vid_log["video_id"]) in pl_video_ids]
-            n_ex_logs = len(ex_logs)
-            n_vid_logs = len(vid_logs)
 
             # Compute video stats 
-            vid_stats = {
-                "pct_complete": len([vid for vid in vid_logs if vid["complete"]])/float(n_pl_videos),
-                "pct_started": len([vid for vid in vid_logs if (vid["total_seconds_watched"] > 0) and (not vid["complete"])])/float(n_pl_videos),
-            }
+            vid_pct_complete = float(len([vid for vid in vid_logs if vid["complete"]])) / n_pl_videos
+            vid_pct_started = float(len([vid for vid in vid_logs if (vid["total_seconds_watched"] > 0) and (not vid["complete"])])) / n_pl_videos
+
             # Compute exercise stats 
-            ex_stats = {
-                "pct_mastered": len([ex for ex in ex_logs if ex["complete"]])/float(n_pl_exercises),
-                "pct_started": len([ex for ex in ex_logs if (ex["attempts"] > 0) and (not ex["complete"])])/float(n_pl_exercises),
-                "pct_struggling": len([ex for ex in ex_logs if ex["struggling"]])/float(n_pl_exercises),
-            }
+            ex_pct_mastered = float(len([ex for ex in ex_logs if ex["complete"]])) / n_pl_exercises
+            ex_pct_started = float(len([ex for ex in ex_logs if (ex["attempts"] > 0) and (not ex["complete"])])) / n_pl_exercises
+            ex_pct_struggling = float(len([ex for ex in ex_logs if ex["struggling"]])) / n_pl_exercises
+
             # Compute test stats
             quiz_exists = True if [entry for entry in p.get("entries") if entry["entity_kind"] == "Quiz"] else False
             quiz_result = None if not quiz_exists else QuizLog.objects.filter(user=user, quiz=p.get("id"))
-            quiz_stats = {
+            quiz_pct_score = 0 if not quiz_result else float(quiz_result.total_correct) / float(quiz_result.total_number)
+
+            progress = {
+                "title": p.get("title"),
+                "id": p.get("id"),
+                "tag": p.get("tag"),
+                "vid_pct_complete": vid_pct_complete,
+                "vid_pct_started": vid_pct_started,
+                "ex_pct_mastered": ex_pct_mastered,
+                "ex_pct_struggling": ex_pct_struggling,
+                "ex_pct_started": ex_pct_started,
                 "quiz_exists": quiz_exists,
-                "pct_score": 0 if not quiz_result else float(quiz_result.total_correct) / float(quiz_result.total_number),
+                "quiz_pct_score": quiz_pct_score,
             }
 
-            user_playlists[i]["vid_stats"] = vid_stats
-            user_playlists[i]["ex_stats"] = ex_stats
-            user_playlists[i]["quiz_stats"] = quiz_stats            
-
-        return user_playlists
-        # Create a new UserPlaylistProgress model and return it 
+            user_progress.append(cls(**progress))
+                    
+        return user_progress
