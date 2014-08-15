d37d5e10388216e7bb5fcd452b244cedd4151f8a
Cyril Pauya <cpauya@gmail.com>
2014-08-15 14:21:52 +0800

    Use handlebars for templates and some refactor of codes.

--- kalite/student_testing/static/js/student_testing/current_unit.js
+++ kalite/student_testing/static/js/student_testing/current_unit.js
@@ -1,16 +1,16 @@
 var units = new CurrentUnitList;
 
-var errorFunc = function(model, resp) {
-//    alert("Did not successfully set the current unit.  Try to reload the page.");
-//    console.log('==> errorFunc', 'model==', model, 'resp==', resp);
-    handleFailedAPI(resp)
+
+var error_func = function (model, resp) {
+//    console.log('==> error_func', 'model==', model, 'resp==', resp);
+    handleFailedAPI(resp);
     units.fetch({data: {'force': true}});
 };
 
-var successFunc = function(model, resp) {
+var success_func = function (model, resp) {
     // force to render the list
-//    console.log('==> successFunc', 'model==', model, 'resp==', resp);
-    handleSuccessAPI(resp)
+//    console.log('==> success_func', 'model==', model, 'resp==', resp);
+    handleSuccessAPI(resp);
     units.fetch({data: {'force': true}});
 };
 
@@ -18,17 +18,17 @@ var successFunc = function(model, resp) {
 var CurrentUnitView = Backbone.View.extend({
     tagName: "tr",
     className: "current-unit",
-    template: _.template($("#all-current-unit-entry-template").html()),
-    initialize: function(options) {
-//        console.log('==> initialize', this);
-//        this.facility_name = this.model.get('facility_name');
+    template: HB.template("student_testing/current-unit-row"),
+    initialize: function (options) {
         this.listenTo(this.model, 'change', this.render);
+//        console.log('======> CurrentUnitView initialize', this);
+        _.bindAll(this);
+        this.render();
     },
-    render: function() {
-        console.log('==> render', this, this.model.get('facility_name'));
-        var dict = this.model.toJSON();
-//        console.log('====> render', dict, this.template(dict));
-        this.$el.html(this.template(dict));
+    render: function () {
+        var variables = this.model.toJSON();
+        var template = this.$el.html(this.template(variables));
+//        console.log('======> CurrentUnitView render', this, 'template==', template);
         return this;
     },
     events: {
@@ -36,7 +36,7 @@ var CurrentUnitView = Backbone.View.extend({
         'click button.next': 'set_next',
         'click a.selected-unit': 'set_unit'
     },
-    set_current_unit: function(ev, is_previous, is_next) {
+    set_current_unit: function (ev, is_previous, is_next) {
         ev.preventDefault();
         var selected_unit = ev.target.dataset.selectedUnit;
         var current_unit = this.model.get('current_unit');
@@ -46,41 +46,38 @@ var CurrentUnitView = Backbone.View.extend({
             'current_unit': current_unit,
             'selected_unit': selected_unit
         };
-        this.model.save(data, {error: errorFunc, success: successFunc});
+        this.model.save(data, {error: error_func, success: success_func});
     },
-    set_unit: function(ev) {
+    set_unit: function (ev) {
         this.set_current_unit(ev, false, false);
     },
-    set_previous: function(ev) {
+    set_previous: function (ev) {
         this.set_current_unit(ev, true, false);
     },
-    set_next: function(ev) {
+    set_next: function (ev) {
         this.set_current_unit(ev, false, true);
     }
 });
 
 
 var AppView = Backbone.View.extend({
-    initialize: function() {
-        this.listenTo(units, 'add', this.addNewUnit);
-        this.listenTo(units, 'reset', this.addAllUnits);
-//        this.listenTo(units, 'sync', this.addAllUnits);
+    initialize: function () {
+        this.listenTo(units, 'add', this.add_new_unit);
+        this.listenTo(units, 'reset', this.add_all_units);
+//        this.listenTo(units, 'sync', this.add_all_units);
         units.fetch();
-        console.log('==> AppView.initialize', units);
+//        console.log('==> AppView.initialize', units);
     },
-    addNewUnit: function(unit) {
-        console.log('==> unit', unit);
+    add_new_unit: function (unit) {
         var view = new CurrentUnitView({model: unit});
         $("#current-units").append(view.render().el);
     },
-    addAllUnits: function() {
-        console.log('==> addAllUnits', units);
-        units.each(this.addNewUnit);
+    add_all_units: function () {
+        units.each(this.add_new_unit);
     }
 });
 
 
-$(function() {
-//    $("tr.facility_name+tr").hide();
+$(function () {
     var app = new AppView;
 });
