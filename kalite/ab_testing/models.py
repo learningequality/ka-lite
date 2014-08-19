bbbb448ef24c99d03e38c2bb49df3fe23d25e615
Jamie Alexandre <jamalex@gmail.com>
2014-08-19 14:32:31 -0700

    Remove model and migration for ab_testing app.

--- kalite/ab_testing/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-from kalite.facility.models import FacilityUser
-
-
-class FacilityUserSpecificSetting(models.Model):
-    user = models.ForeignKey(FacilityUser, related_name='dynamic_settings')
-
-    turn_off_motivational_features = models.BooleanField()
