4ab5baa1667b00a7e17baa3584df595413ca3ba6
Richard <richard.tibbles@gmail.com>
2014-08-20 14:53:20 -0700

    Adds data for gift_card store item.

---
+++ kalite/store/data/items.py
@@ -0,0 +1,8 @@
+STORE_ITEMS = {
+    "gift_card": {
+        "returnable" : False,
+        "title" : "Gift Card",
+        "description" : "Points rewarded for quizzes and tests",
+        "shown" : False
+    },
+}
