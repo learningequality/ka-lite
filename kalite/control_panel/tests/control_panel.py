bbaf135c0311b9eca5b7ae074c16f2d136cd9dfe
Aron Fyodor Asor <aronasorman@gmail.com>
2014-09-08 14:53:54 -0700

    simplify test_delete_facility test

--- kalite/control_panel/tests/control_panel.py
+++ kalite/control_panel/tests/control_panel.py
@@ -24,13 +24,8 @@ class FacilityControlTests(FacilityMixins,
         self.browse_to(self.reverse('zone_redirect'))  # zone_redirect so it will bring us to the right zone
 
         # assert that our facility exists
-        facility_row = self.browser.find_element_by_xpath('//tr[@facility-id="%s"]' % self.fac.id)
-        facility_delete_link = facility_row.find_element_by_xpath('//a[@class="facility-delete-link"]/span')
-        facility_delete_link.click()
-        alert = self.browser.switch_to_alert()
-        alert.send_keys("should-be-deleted")
-        alert.accept()
-        time.sleep(5)
+        selector = 'tr[facility-id="%s"] > td > a.facility-delete-link > span' % self.fac.id
+        self.browser_click_and_accept(selector, text=facility_name)
 
         with self.assertRaises(NoSuchElementException):
             self.browser.find_element_by_xpath('//tr[@facility-id="%s"]' % self.fac.id)
