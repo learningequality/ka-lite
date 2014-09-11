c308f75d5b17980f71260d1a4845c4d7f73fe29f
Cyril Pauya <cyril@mrpau.com>
2014-09-12 00:14:30 +0800

    Use separate sqlite3 database for screenshots.

--- kalite/distributed/management/commands/screenshot.py
+++ kalite/distributed/management/commands/screenshot.py
@@ -5,8 +5,11 @@ import os
 
 from django.conf import settings
 from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
 from django.core.urlresolvers import reverse
+from django.utils.six import StringIO
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -20,22 +23,20 @@ try:
 except ImportError:
     local_settings = object()
 
-SCREENSHOT_PATH = os.path.join(os.path.realpath(getattr(local_settings, "PROJECT_PATH", settings.PROJECT_PATH)),
-                               "..", "data", "screenshots")
-SCREENSHOT_KEYS = ['users', 'slug', 'start_url', 'inputs', 'end_url']
-SCREENSHOT_JSON_FILE = os.path.join(settings.SCREENSHOTS_DATA_PATH, 'screenshots.json')
-
 USER_TYPE_ADMIN = "admin"
 USER_TYPE_COACH = "coach"
 USER_TYPE_GUEST = "guest"
 USER_TYPE_STUDENT = "student"
 
-ADMIN_USERNAME = 'cy'
-ADMIN_PASSWORD = 'cy'
+ADMIN_USERNAME = 'admin'
+ADMIN_PASSWORD = 'admin'
+ADMIN_EMAIL = 'admin@example.com'
 COACH_USERNAME = 'coach'
 COACH_PASSWORD = 'password'
 STUDENT_USERNAME = 'student'
 STUDENT_PASSWORD = 'password'
+
+log = settings.LOG
 
 
 class Command(BaseCommand):
@@ -53,22 +54,39 @@ def make_sure_path_exists(path):
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
+
+
 # ENDREF:
 
 
 class Screenshot(KALiteDistributedBrowserTestCase):
-# class Screenshot():
-    logging.warn('==> screenshot %s' % (SCREENSHOT_PATH,))
+    # class Screenshot():
+    # log.info('==> screenshot %s' % (SCREENSHOTS_OUTPUT_PATH,))
 
     def __init__(self, *args, **kwargs):
 
-        # super(Screenshot, self).__init__(*args, **kwargs)
+        new_io = StringIO()
+        call_command("reset_db", interactive=False, stdout=new_io, router=settings.SCREENSHOTS_ROUTER)
+        # command_output = new_io.getvalue().strip()
+        call_command("syncdb", interactive=False, stdout=new_io, router=settings.SCREENSHOTS_ROUTER)
+        call_command("migrate", interactive=False, stdout=new_io, router=settings.SCREENSHOTS_ROUTER)
+        call_command("createsuperuser",
+                     username=ADMIN_USERNAME, email=ADMIN_EMAIL,
+                     interactive=False, stdout=new_io, router=settings.SCREENSHOTS_ROUTER)
+        u = User.objects.get(username=ADMIN_USERNAME)
+        u.set_password(ADMIN_PASSWORD)
+        u.save()
+
         self.persistent_browser = True
         self.max_wait_time = kwargs.get("max_wait_time", 30)
         # self.admin_user = User.objects.filter(is_superuser=True, is_active=True)[0]
-        self.admin_user = User.objects.filter(username=ADMIN_USERNAME, is_superuser=True, is_active=True)[0]
+        admins = User.objects.filter(username=ADMIN_USERNAME, is_superuser=True, is_active=True)
+        if not admins:
+            raise CommandError("No super user found!")
+        self.admin_user = admins[0]
         self.admin_pass = ADMIN_PASSWORD
         self.setUpClass()
+
         self.create_facility()
         self.create_student()
         self.create_teacher()
@@ -77,11 +95,8 @@ class Screenshot(KALiteDistributedBrowserTestCase):
         self.browser = webdriver.Firefox()
         self.browser.set_window_size(1024, 768)
 
-        # self.browser_login_user(ADMIN_USERNAME, ADMIN_PASSWORD)
-        self.browser_login_admin()
-        filename = self.make_filename(slug="admin-home")
-        self.browser.save_screenshot(filename)
-        logging.warn('==> live_server_url %s' % (self.live_server_url,))
+        log.info('==> live_server_url %s' % (self.live_server_url,))
+        log.info('==> Saving screenshots at %s ...' % (settings.SCREENSHOTS_OUTPUT_PATH,))
 
     def get_contents(self):
         """
@@ -89,9 +104,11 @@ class Screenshot(KALiteDistributedBrowserTestCase):
         """
         items = []
         try:
-            items = json.load(open(SCREENSHOT_JSON_FILE))
+            log.info('==> Fetching screenshots.json from %s ...' % (settings.SCREENSHOTS_JSON_FILE,))
+            items = json.load(open(settings.SCREENSHOTS_JSON_FILE))
         except Exception as exc:
-            logging.error("Cannot open `screenshots.json` at %s:\n  exception:  %s" % (SCREENSHOT_JSON_FILE, exc,))
+            log.error("Cannot open `screenshots.json` at %s:\n  exception:  %s" %
+                      (settings.SCREENSHOTS_JSON_FILE, exc,))
         return items
 
     def validate_json_item(self, shot, auto_login=True):
@@ -99,43 +116,44 @@ class Screenshot(KALiteDistributedBrowserTestCase):
         Validates a json item if keys are valid or not.
         """
         try:
-            values = [shot[key] for key in SCREENSHOT_KEYS]
+            values = [shot[key] for key in settings.SCREENSHOTS_JSON_KEYS]
             if values:
                 if auto_login:
-                    if USER_TYPE_ADMIN in shot["users"]:
+                    if USER_TYPE_STUDENT in shot["users"]:
+                        self.browser_login_student()
+                    elif USER_TYPE_COACH in shot["users"]:
+                        self.browser_login_teacher()
+                    elif USER_TYPE_ADMIN in shot["users"]:
                         self.browser_login_user(ADMIN_USERNAME, ADMIN_PASSWORD)
                 return True
         except Exception as exc:
-            logging.error("Screenshot JSON has invalid format: \n  json==%s\n  exception==%s" % (shot, exc,))
+            log.error("Screenshot JSON has invalid format: \n  json==%s\n  exception==%s" % (shot, exc,))
         return False
 
     def make_filename(self, slug):
-        make_sure_path_exists(SCREENSHOT_PATH)
-        filename = "%s/%s.png" % (SCREENSHOT_PATH, slug,)
+        make_sure_path_exists(settings.SCREENSHOTS_OUTPUT_PATH)
+        filename = "%s/%s.png" % (settings.SCREENSHOTS_OUTPUT_PATH, slug,)
         return filename
 
     def take(self, shot, browser=None):
         """
-        Take screenshot and save on SCREENSHOT_PATH.
+        Take screenshot and save on SCREENSHOTS_OUTPUT_PATH.
         """
         start_url = "%s%s" % (self.live_server_url, shot["start_url"],)
         # start_url = "%s%s" % ('', shot["start_url"],)
         self.browse_to(start_url)
 
-        # logging.warn('==> current_url %s' % (self.browser.current_url,))
-
         # TODO(cpauya): take actions to get to the `end_url`
         # TODO(cpauya): wait until we are at `end_url`
 
-        # make_sure_path_exists(SCREENSHOT_PATH)
-        # filename = "%s/%s.png" % (SCREENSHOT_PATH, shot["slug"],)
         filename = self.make_filename(slug=shot["slug"])
-        logging.warn('==> screenshot %s -- %s -- %s' % (filename, self.browser.current_url, self.browser.title,))
+        log.info('==> %s --titled-- "%s" --> %s.png ...' %
+                 (self.browser.current_url, self.browser.title, shot["slug"],))
         self.browser.save_screenshot(filename)
 
     def take_all(self, browser=None):
         """
-        Take screenshots for each item from json.
+        Take screenshots for each item from json grouped by user.
         """
         shots = self.get_contents()
         for shot in shots:
