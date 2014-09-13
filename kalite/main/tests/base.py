7ea1e4fb88f342c2875c9972da676aca456a3407
Aron Fyodor Asor <aronasorman@gmail.com>
2014-09-12 17:12:35 -0700

    fix kalite.updates tests

--- kalite/main/tests/base.py
+++ kalite/main/tests/base.py
@@ -19,7 +19,6 @@ from securesync.models import Device
 
 class MainTestCase(KALiteTestCase):
 
-
     def __init__(self, *args, **kwargs):
         self.content_root = tempfile.mkdtemp() + "/"
         self.cache_dir = tempfile.mkdtemp() + "/"
@@ -29,7 +28,6 @@ class MainTestCase(KALiteTestCase):
     def setUp(self, *args, **kwargs):
         self.setUp_fake_contentroot()
         self.setUp_fake_cache()
-        self.setUp_fake_device()
         return super(KALiteTestCase, self).setUp(*args, **kwargs)
 
     def setUp_fake_contentroot(self):
@@ -92,4 +90,3 @@ class MainTestCase(KALiteTestCase):
             fh.write("")
         self.assertTrue(os.path.exists(fake_video_file), "Make sure the video file was created, youtube_id='%s'." % youtube_id)
         return (fake_video_file, video_id, youtube_id)
-
