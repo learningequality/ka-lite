0fdbc389a5d970440c8169dbee64af6a386c964e
Aron Fyodor Asor <aronasorman@gmail.com>
2014-09-15 17:04:24 -0700

    remove create_test_admin

--- kalite/testing/base.py
+++ kalite/testing/base.py
@@ -6,33 +6,12 @@ for automated browser-based testing.
 
 from selenium import webdriver
 
-from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
-from django.test import Client, TestCase, LiveServerTestCase
-
-from kalite.shared.decorators.misc import deprecated
+from django.test import TestCase, LiveServerTestCase
 
 from .browser import setup_browser
 from .client import KALiteClient
 from .mixins import CreateDeviceMixin
-
-
-@deprecated
-def create_test_admin(username="admin", password="pass", email="admin@example.com"):
-    """Create a test user.
-    Taken from http://stackoverflow.com/questions/3495114/how-to-create-admin-user-in-django-tests-py"""
-
-    test_admin = User.objects.create_superuser(username, email, password)
-
-    # You'll need to log him in before you can send requests through the client
-    client = Client()
-    assert client.login(username=test_admin.username, password=password)
-
-    # set dummy password, so it can be passed around
-    test_admin.password = password
-    assert client.login(username=test_admin.username, password=password)
-
-    return test_admin
 
 
 class KALiteTestCase(CreateDeviceMixin, TestCase):
