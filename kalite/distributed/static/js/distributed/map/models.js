06b3fb648d51cb8ff54bc02f9829697d9d8383c5
Richard <richard.tibbles@gmail.com>
2014-09-15 18:37:03 -0700

    Updates Topic Model and Collection to return appropriate data for rendering.

--- kalite/distributed/static/js/distributed/map/models.js
+++ kalite/distributed/static/js/distributed/map/models.js
@@ -1,14 +1,36 @@
 // Models
-window.ExerciseNode = Backbone.Model.extend({
-    
+window.TopicNode = Backbone.Model.extend({
+
+    coordinates: function () {
+        var x_pos = this.get("x_pos") || 0;
+        var y_pos = this.get("y_pos") || 0;
+        if (x_pos!==0 && y_pos!==0) {
+            return [Number(this.get("x_pos")), Number(this.get("y_pos"))];
+        } else {
+            return null;
+        }
+        
+    }
 });
 
 // Collections
-window.ExerciseCollection = Backbone.Collection.extend({
-    url: ("ALL_EXERCISES_URL" in window)? ALL_EXERCISES_URL : null,
-    model: ExerciseNode,
+window.TopicCollection = Backbone.Collection.extend({
+    
+    model: TopicNode,
+
+    centerOfMass: function() {
+        var x = _.reduce(this.models, function(memo, model){return memo + model.get("x_pos");}, 0)/this.length;
+        var y = _.reduce(this.models, function(memo, model){return memo + model.get("y_pos");}, 0)/this.length;
+        return [x,y];
+    },
 
     parse: function(response) {
-        return _.values(response);
+        if (_.isObject(response)) {
+            if (_.isArray(response)) {
+                return response;
+            } else {
+                return _.values(response);
+            }
+        }
     }
 });
