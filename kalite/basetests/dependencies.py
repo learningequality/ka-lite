d1c0195cbdcd0146624514da517f07af241eaaec
Cyril Pauya <cpauya@gmail.com>
2014-10-30 17:26:33 +0800

    Initial management command and sqlite3 dependency test.

--- kalite/basetests/dependencies.py
+++ kalite/basetests/dependencies.py
@@ -1,33 +1,61 @@
 import logging
-import unittest
+import os
+import sys
+
+if __name__ == '__main__':
+    # Make sure we have access to the python packages.
+    PROJECT_PATH = os.path.dirname(os.path.realpath(__file__))
+    PROJECT_PYTHON_PATHS = [
+        os.path.join(PROJECT_PATH, "..", "..", "python-packages"),  # libraries (python-packages)
+    ]
+    sys.path = [os.path.realpath(p) for p in PROJECT_PYTHON_PATHS] + sys.path
+
+import unittest2
 
 
-class SqliteTests(unittest.TestCase):
+class SqliteTests(unittest2.TestCase):
+    """
+    For versions of Python 2.5 or newer that include sqlite3 in the standard library Django will now use a
+    pysqlite2 interface in preference to sqlite3 if it finds one is available.
+
+    REF: https://docs.djangoproject.com/en/1.5/ref/databases/#using-newer-versions-of-the-sqlite-db-api-2-0-driver
+    """
 
     def test_sqlite_is_installed(self):
-        logging.info("Testing SQLite3 is installed...")
-        self.assertEqual(True, False)
+        logging.info("Testing if SQLite3 is installed...")
+        try:
+            import sqlite3
+        except ImportError:
+            self.fail("SQLite3 is not installed.")
 
     def test_sqlite_version(self):
         logging.info("Testing SQLite3 version...")
         self.assertEqual(True, False)
 
+    def test_sqlite_path_is_writable(self):
+        logging.info("Testing SQLite3 path is writable...")
+        self.assertEqual(True, False)
 
-class DjangoTests(unittest.TestCase):
+
+class DjangoTests(unittest2.TestCase):
 
     def test_django_is_installed(self):
-        logging.info("Testing Django is installed...")
+        logging.info("Testing if Django is installed...")
         self.assertEqual(True, False)
 
     def test_django_version(self):
         logging.info("Testing Django version...")
         self.assertEqual(True, False)
 
+    def test_minimum_python_version(self):
+        logging.info("Testing the minimum Python version based on the Django version to use...")
+        self.assertEqual(True, False)
 
-class PackageTests(unittest.TestCase):
+
+class PackageTests(unittest2.TestCase):
 
     def test_packages_are_installed(self):
-        logging.info("Testing required Python packages are installed...")
+        logging.info("Testing if required Python packages are installed...")
         self.assertEqual(True, False)
 
     def test_packages_version(self):
@@ -36,4 +64,4 @@ class PackageTests(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest2.main()
---
+++ kalite/basetests/management/commands/check_dependencies.py
@@ -0,0 +1,54 @@
+import os
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+
+logging = settings.LOG
+
+
+class Command(BaseCommand):
+
+    def usage(self, argname):
+        return """check_dependencies
+        Check dependencies of the project on the current install."""
+
+    def handle(self, *args, **options):
+        self.check_sqlite()
+
+    def _log(self, msg, is_ok=False):
+        status = _("OK.")
+        if not is_ok:
+            status = _("FAILED!")
+        logging.info("%-120s %s" % (msg, status,))
+
+    def _log_ok(self, msg):
+        self._log(msg, is_ok=True)
+
+    def _log_fail(self, msg):
+        self._log(msg, is_ok=False)
+
+    def check_sqlite(self):
+        """
+        For versions of Python 2.5 or newer that include sqlite3 in the standard library Django will now use a
+        pysqlite2 interface in preference to sqlite3 if it finds one is available.
+
+        REF: https://docs.djangoproject.com/en/1.5/ref/databases/#using-newer-versions-of-the-sqlite-db-api-2-0-driver
+        """
+
+        # Can we import sqlite3?
+        msg = _("Testing if SQLite3 is installed")
+        try:
+            import sqlite3
+            self._log_ok(msg)
+        except ImportError:
+            self._log_fail(msg)
+
+        # TODO(cpauya): Is sqlite3 path writable?
+        sqlite_path = settings.DATABASES["default"]["NAME"]
+        msg = _('Testing writable SQLite3 database "%s"...') % sqlite_path
+        try:
+            is_ok = os.access(sqlite_path, os.W_OK)
+            self._log(msg, is_ok=is_ok)
+        except Exception:
+            self._log_fail(msg)
