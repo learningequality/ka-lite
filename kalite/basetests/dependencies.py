5e027b6f2e6d9642110b630ad301d4d823247ae0
Cyril Pauya <cpauya@gmail.com>
2014-11-03 16:27:07 +0800

    Create a `DependenciesTest` class from which the tests will inherit.

--- kalite/basetests/dependencies.py
+++ kalite/basetests/dependencies.py
@@ -21,30 +21,41 @@ import kalite
 os.environ["DJANGO_SETTINGS_MODULE"] = "kalite.settings"
 
 
-# Custom logging functions so we can customize the output.
-def _log(msg, delay=0):
-    sys.stdout.write(msg)
-    sys.stdout.flush()  # REF: https://answers.yahoo.com/question/index?qid=20110506145612AA1oU5Q
-    if delay:
-        import time
-        time.sleep(delay)
+def _tuple_to_str(t, delim="."):
+    return delim.join(str(i) for i in t)
 
 
-def _fail(msg=None):
-    if msg:
-        _log(msg)
-    else:
-        _log("FAIL!\n")
+class DependenciesTests(unittest2.TestCase):
+    """
+    Have a map of python and sqlite3 versions per django version released.
+    """
+
+    DJANGO_VERSION = (1, 5, 1,)  # TODO(cpauya): get from our django package
+
+    # REF: https://docs.djangoproject.com/en/1.5/releases/1.5/#python-compatibility
+    MINIMUM_PYTHON_VERSION = (2, 6, 5,)
+
+    # REF: https://docs.djangoproject.com/en/1.5/ref/databases/#sqlite-3-3-6-or-newer-strongly-recommended
+    MINIMUM_SQLITE_VERSION = (3, 3, 6,)
+
+    # Custom logging functions so we can customize the output.
+    def _log(self, msg, delay=0):
+        sys.stdout.write(msg)
+        sys.stdout.flush()  # REF: https://answers.yahoo.com/question/index?qid=20110506145612AA1oU5Q
+        if delay:
+            import time
+            time.sleep(delay)
+
+    def _fail(self, msg=" FAIL!\n", raise_fail=True):
+        self._log(msg)
+        if raise_fail:
+            self.fail(msg)
+
+    def _pass(self, msg=" OK!\n"):
+        self._log(msg)
 
 
-def _pass(msg=None):
-    if msg:
-        _log(msg)
-    else:
-        _log("OK!\n")
-
-
-class SqliteTests(unittest2.TestCase):
+class SqliteTests(DependenciesTests):
     """
     For versions of Python 2.5 or newer that include sqlite3 in the standard library Django will now use a
     pysqlite2 interface in preference to sqlite3 if it finds one is available.
@@ -54,16 +65,20 @@ class SqliteTests(unittest2.TestCase):
 
     def test_sqlite_is_installed(self):
         try:
-            _log("Testing if SQLite3 is installed...", 1)
+            self._log("Testing if SQLite3 is installed...", 1)
             import sqlite3
-            _pass()
+            self._pass()
         except ImportError:
-            _fail()
+            self._fail()
 
-    def test_sqlite_version(self):
-        _log("Testing SQLite3 version...", 1)
-        _pass()
-        # self.assertTrue(False)
+    def test_minimum_sqlite_version(self):
+        self._log("Testing minimum SQLite3 version %s for Django version %s..." %
+                  (_tuple_to_str(self.MINIMUM_SQLITE_VERSION), _tuple_to_str(self.DJANGO_VERSION),), 1)
+        from sqlite3 import version_info
+        if self.MINIMUM_SQLITE_VERSION <= version_info:
+            self._pass()
+        else:
+            self._fail()
 
     def test_sqlite_path_is_writable(self):
         try:
@@ -71,12 +86,12 @@ class SqliteTests(unittest2.TestCase):
             sqlite_path = settings.DATABASES["default"]["NAME"]
             msg = 'Testing writable SQLite3 database "%s"...' % sqlite_path
             try:
-                _log(msg, 1)
+                self._log(msg, 1)
                 is_ok = os.access(sqlite_path, os.W_OK)
                 if is_ok:
-                    _pass()
+                    self._pass()
                 else:
-                    _fail()
+                    self._fail(self)
             except Exception as exc:
                 msg = "SQLite database path is not writable: %s" % exc
                 logging.critical(msg)
@@ -85,19 +100,31 @@ class SqliteTests(unittest2.TestCase):
             self.fail("Settings cannot be imported: %s" % exc)
 
 
-class DjangoTests(unittest2.TestCase):
+class DjangoTests(DependenciesTests):
 
     def test_django_is_installed(self):
-        logging.info("Testing if Django is installed...")
-        self.assertTrue(False)
+        self._log("Testing if Django is installed...", 1)
+        try:
+            import django
+            self._pass()
+        except ImportError:
+            self._fail(self)
 
     def test_django_version(self):
-        logging.info("Testing Django version...")
-        self.assertTrue(False)
+        self._log("Testing Django %s version..." % _tuple_to_str(self.DJANGO_VERSION), 1)
+        try:
+            from django.utils.version import get_version
+            self._pass()
+        except ImportError:
+            self._fail(self)
 
     def test_minimum_python_version(self):
-        logging.info("Testing the minimum Python version based on the Django version to use...")
-        self.assertTrue(False)
+        self._log("Testing minimum Python version %s for Django version %s..." %
+                  (_tuple_to_str(self.MINIMUM_PYTHON_VERSION), _tuple_to_str(self.DJANGO_VERSION),), 1)
+        if sys.version_info >= self.MINIMUM_PYTHON_VERSION:
+            self._pass()
+        else:
+            self._fail(self)
 
 
 class PackagesTests(unittest2.TestCase):
@@ -151,7 +178,7 @@ class TestHandler(logging.StreamHandler):
 
 
 test_cases = (SqliteTests, DjangoTests, VersionTests, PathsTests, PackagesTests)
-# test_cases = (SqliteTests,)
+test_cases = (SqliteTests, DjangoTests,)
 
 
 def load_tests(loader, tests, pattern):
