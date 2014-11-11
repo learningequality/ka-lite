261b8e8962c1580905d24d2e260b9f84ab50d2a2
Danny Jim Allado <jim.,123>
2014-11-11 11:39:58 +0800

    Refactor `SessionIdleTimeout` in `session_timeout_middleware.py` to redirect to login page when session has timed out.

--- python-packages/django_snippets/session_timeout_middleware.py
+++ python-packages/django_snippets/session_timeout_middleware.py
@@ -3,8 +3,9 @@ from django.contrib import messages
 import datetime
 
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 
-import logging
 
 class SessionIdleTimeout:
     """
@@ -20,13 +21,19 @@ class SessionIdleTimeout:
                 current_datetime = datetime.datetime.now()
                 
                 # Timeout if idle time period is exceeded.
-                if (request.session.has_key('last_activity')
-                    and
+                # seconds =
+                if ('last_activity' in request.session and
                     (current_datetime - request.session['last_activity']).seconds >
-                    settings.SESSION_IDLE_TIMEOUT):
+                        settings.SESSION_IDLE_TIMEOUT):
                     logout(request)
-                    messages.add_message(request, messages.ERROR, 'Your session has been timed out.')
-                # Set last activity time in current session.
+                    messages.add_message(request, messages.ERROR, 'Your session has been timed out...')
+
+                    # Redirect to the login page if session has timed-out.
+                    redirect_to = reverse('login')
+                    response = HttpResponseRedirect(redirect_to)
+                    return response
                 else:
+                    # Set last activity time in current session.
                     request.session['last_activity'] = current_datetime
+
         return None
