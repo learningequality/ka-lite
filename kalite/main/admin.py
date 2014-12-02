7f0bba1fc7a53b620fd103106d693f502aafbd2e
Aron Fyodor Asor <aronasorman@users.noreply.github.com>
2014-12-02 12:04:43 -0800

    Revert "Hotfix/2271 export csv data quality issues"

--- kalite/main/admin.py
+++ kalite/main/admin.py
@@ -1,29 +1,26 @@
 """
 Manipulating models to expose to admins through the Django admin interface
 """
+from django.conf import settings
 from django.contrib import admin
 
 from .models import *
 
 
 class VideoLogAdmin(admin.ModelAdmin):
-    list_display = ("video_id", "user", "language", "points", "total_seconds_watched", "complete",
-                    "completion_timestamp",)
-    list_filter = ("completion_timestamp", "complete", "language", "user", "video_id",)
+    list_display = ("video_id", "user", "language", "points", "total_seconds_watched", "complete",)
+    list_filter = ("video_id", "user", "language", "complete",)
 admin.site.register(VideoLog, VideoLogAdmin)
-
 
 class ExerciseLogAdmin(admin.ModelAdmin):
     list_display = ("exercise_id", "user", "language", "streak_progress", "completion_timestamp", "complete",)
     list_filter = ("completion_timestamp", "complete", "language", "exercise_id", "user",)
 admin.site.register(ExerciseLog, ExerciseLogAdmin)
 
-
 class UserLogAdmin(admin.ModelAdmin):
     pass
 if UserLog.is_enabled():  # only enable admin if the feature is enabled.
     admin.site.register(UserLog, UserLogAdmin)
-
 
 class UserLogSummaryAdmin(admin.ModelAdmin):
     pass
