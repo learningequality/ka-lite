497082553bdea37e82c2d527c23751b165a417f7
Eduard Vincent Aban <arceduardvincent@gmail.com>
2014-12-03 12:54:38 +0800

    Refactor `i18nize_template` management command.

--- kalite/i18n/management/commands/i18nize_template.py
+++ kalite/i18n/management/commands/i18nize_template.py
@@ -4,40 +4,39 @@ import os
 import subprocess
 
 from optparse import make_option
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import AppCommand
+
+# logger = getLogger('management_commands')
 
 
-def django_module(app_name, parse_paths, i18nize_file):
+def i18nize_parser(app_name, parse_exts, i18nize_file):
     """
     Parse the django_app templates and its handlebars templates
     """
     # parse hbtemplates, handlebars
-    if parse_paths is not None:
-        for path in parse_paths:
-            if path == "hbtemplates":
-                for root, dirs, files in os.walk(app_name):
-                    for file in files:
-                        if file.endswith(".handlebars"):
-                            file_path = os.path.join(root, file)
-                            subprocess.call("i18nize-templates %s" % (file_path), shell=True)
-
-            else:
-                for root, dirs, files in os.walk(app_name):
-                    for file in files:
-                        if file.endswith(".html"):
-                            file_path = os.path.join(root, file)
-                            subprocess.call("i18nize-templates %s" % (file_path), shell=True)
-    else:
-    # Find a specific file to i18nize
-    # for filename in fnmatch.filter(files, pattern)
-        for root, dirs, files in os.walk(app_name):
-            for file in files:
+    for root, dirs, files in os.walk(app_name):
+        for file in files:
+            if i18nize_file is not None:
                 if file == i18nize_file[0]:
                     file_path = os.path.join(root, file)
                     subprocess.call("i18nize-templates %s" % (file_path), shell=True)
+            else:
+                for file_exts in parse_exts:
+                    if file.endswith(file_exts):
+                        file_path = os.path.join(root, file)
+                        subprocess.call("i18nize-templates %s" % (file_path), shell=True)
 
 
-class Command(BaseCommand):
+def handle_extensions(parse_file=None):
+    exts = [".handlebars", ".html"]
+    if parse_file == "hbtemplates":
+        exts = [".handlebars"]
+    if parse_file == "templates":
+        exts = [".html"]
+    return exts
+
+
+class Command(AppCommand):
     """
     This management command ask the module that will be i18nize.
     this cover the handlebars template and the templates
@@ -53,7 +52,7 @@ class Command(BaseCommand):
             python manage.py i18nize_template coachreports --parse hbtemplates
             python manage.py i18nize_template coachreports --parse-file facility-select.handlebars
     """
-    option_list = BaseCommand.option_list + (
+    option_list = AppCommand.option_list + (
        make_option('--parse',
             action='append',
             dest="parse",
@@ -64,30 +63,31 @@ class Command(BaseCommand):
             dest="parse_file",
             help='Select a specific file extention to be parse e.g(handlebars or html)'),
     )
+    help = ("Modify the specified template or handlebars to be i18n")
 
     def handle(self, *args, **options):
         extension_option = options.get("parse")
         i18nize_file = options.get("parse_file")
         parse_file = ["hbtemplates", "templates"]
         parse_flag = True
+        exts = handle_extensions()
         if extension_option is not None:
             if extension_option[0] in parse_file:
-                parse_file = extension_option
+                exts = handle_extensions(parse_file=extension_option[0])
             else:
                 parse_flag = False
 
         if parse_flag is True:
             for django_app in args:
-                if i18nize_file is not None:
-                    parse_file = None
+
                 try:
-                    self.stdout.write(">>> i18nize hbtemplate and templates")
-                    django_module(app_name=django_app, parse_paths=parse_file, i18nize_file=i18nize_file)
-                    self.stdout.write(">>> i18nize hbtemplate and templates done in %s" % (django_app))
+                    logging.warn(">>> i18nize hbtemplate and templates")
+                    i18nize_parser(app_name=django_app, parse_exts=exts, i18nize_file=i18nize_file)
+                    logging.warn(">>> i18nize hbtemplate and templates done in %s" % (django_app))
 
                 except:
-                    self.stdout.write("It seems you didn't have the tools for i18nze-templates"
+                    logging.warn("It seems you didn't have the tools for i18nze-templates"
                                       "Fork this tools to https://github.com/mrpau/i18nize_templates "
                                       "Install then setup.py to use this tool")
         else:
-            self.stdout.write("The file type specified is not available for i18nize.Did you miss type this?")
+            logging.error("The file type specified is not available for i18nize.Did you miss type this?")
