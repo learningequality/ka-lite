712589cc2514fa7493243aba54013845e4c30f3c
Danny Jim Allado <jim.,123>
2014-12-17 17:40:45 +0800

    Modify the progress_timestamp_test.py, making sure that the start_timestamp will not change, and progress_timestamp will change when we update contentlog

--- kalite/main/tests/progress_timestamp_test.py
+++ kalite/main/tests/progress_timestamp_test.py
@@ -12,10 +12,7 @@ class TestSaveContentLog(KALiteTestCase):
 
     CONTENT_ID = "712f11"
     POINTS = 3
-    COMPLETE = True
     COMPLETION_COUNTER = 1
-    TIME_SPENT = 11
-    PROGRESS_TIMESTAMP = str(datetime.now())
     CONTENT_SOURCE = ""
     CONTENT_KIND = "Document"
     USERNAME = "teststudent"
@@ -26,11 +23,9 @@ class TestSaveContentLog(KALiteTestCase):
         # create a facility and user that can be referred to in models across tests
         self.facility = Facility(name="Default Facility")
         self.facility.save()
-        print("==facility", self.facility)
         self.user = FacilityUser(username=self.USERNAME, facility=self.facility)
         self.user.set_password(self.PASSWORD)
         self.user.save()
-        print("==user", self.user)
 
         # create an initial ContentLog instance so we have something to update later
         self.contentlog = ContentLog(content_id=self.CONTENT_ID, user=self.user)
@@ -38,9 +33,11 @@ class TestSaveContentLog(KALiteTestCase):
         self.contentlog.content_kind = self.CONTENT_KIND,
         self.contentlog.content_source = self.CONTENT_SOURCE
         self.contentlog.save()
-        print("==>", self.contentlog)
 
-    def test_new_contentlog(self):
-        contentlogs = ContentLog.objects.filter(content_id=self.CONTENT_ID, user__username=self.USERNAME)
-        print("===>>>", contentlogs)
-        self.assertEqual(contentlogs.count(), 1)
+    def test_timestamp(self):
+            new_start_timestamp = ContentLog.objects.get(user=self.user)
+            new_start_timestamp.save()
+            # Make sure that the start_timestamp will not change when we update,
+            # only progress_timestamp will update.
+            self.assertEqual(new_start_timestamp.start_timestamp,self.contentlog.start_timestamp)
+            self.assertNotEqual(new_start_timestamp.progress_timestamp,self.contentlog.progress_timestamp)
