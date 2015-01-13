b0e445a047a9387846c8c59b7beea6f8bb1e5b7a
Cyril Pauya <cpauya@gmail.com>
2015-01-13 10:58:07 +0800

    Normalize the path and replace hard-coded slash character with `os.sep`.

--- python-packages/fle_utils/handlebars/loader.py
+++ python-packages/fle_utils/handlebars/loader.py
@@ -13,8 +13,6 @@ from django.core.exceptions import ImproperlyConfigured
 from django.utils._os import safe_join
 from django.utils.importlib import import_module
 
-from fle_utils.platforms import is_windows
-
 
 # At compile time, cache the directories to search.
 fs_encoding = sys.getfilesystemencoding() or sys.getdefaultencoding()
@@ -58,13 +56,13 @@ def load_template_sources(module_name):
 
     templates = []
 
-    if is_windows():
-        slash_character = "\\"
-    else:
-        slash_character = "/"
-
     for filepath in get_template_module_paths(module_name):
-        template_name = filepath.split(slash_character)[-1].split(".handlebars")[0]
+        # REF: http://stackoverflow.com/a/14334768/845481
+        # How to split a dos path into its components in Python
+        # On Windows, we might encounter path like c:\python2.7/python.exe, so using `normpath`.
+        filepath = os.path.normpath(filepath)
+        # Use specific path separator for the OS.
+        template_name = filepath.split(os.sep)[-1].split(".handlebars")[0]
         try:
             with open(filepath) as f:
                 # load the template text
