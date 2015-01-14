de12a8ac5ac49479d7d440506319dd9d95726e8b
Richard Tibbles <richard.tibbles@gmail.com>
2015-01-14 12:05:19 -0800

    Fixes error in student_testing api_urls.

--- kalite/student_testing/api_urls.py
+++ kalite/student_testing/api_urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url, include
 from .api_resources import TestLogResource, TestResource, CurrentUnitResource
 
-urlpatterns = patterns(
+urlpatterns = patterns('',
     url(r'^', include(TestLogResource().urls)),
     url(r'^', include(TestResource().urls)),
     url(r'^', include(CurrentUnitResource().urls)),
