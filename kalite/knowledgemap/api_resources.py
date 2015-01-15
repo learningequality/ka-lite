7dd683e66b5f02c900b0d15a9b3e3075fecad89e
66eli77 <66eli77@gmail.com>
2015-01-14 17:02:41 -0800

    repetitive code, should import the ExerciseSummaryResource from coachreports

--- kalite/knowledgemap/api_resources.py
+++ kalite/knowledgemap/api_resources.py
@@ -5,19 +5,9 @@ from tastypie.resources import ModelResource
 from kalite.shared.decorators import get_user_from_request
 
 from kalite.facility.api_resources import FacilityUserResource
+from kalite.coachreports.api_resources import ExerciseSummaryResource
 from kalite.shared.api_auth import UserObjectsOnlyAuthorization
 from kalite.main.models import ExerciseLog
-
-class ExerciseSummaryResource(ModelResource):
-    user = fields.ForeignKey(FacilityUserResource, 'user')
-
-    def obj_create(self, bundle, **kwargs):
-        is_admin = getattr(bundle.request, "is_admin", False)
-        user = getattr(bundle.request, "user", None)
-        if is_admin:
-            if user and getattr(user, 'is_superuser', False):
-                return None
-        return super(ExerciseLogResource, self).obj_create(bundle, **kwargs)
 
 class KnowledgeMapExerciseResource(ExerciseSummaryResource):
 
