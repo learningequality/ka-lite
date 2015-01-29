783d47151076cca412908437b27a04934f8bc171
Benjamin Bach <benjaoming@gmail.com>
2015-01-28 16:43:18 -0800

    use path alteration instead of doskeys and autorun

--- bin/windows/kalite.cmd
+++
@@ -1,2 +0,0 @@
-set "BIN_DIR=%~dp0"
-@doskey kalite call %BIN_DIR%\kalite.bat $*
--- bin/windows/setup.bat
+++
@@ -1,7 +0,0 @@
-rem THIS STUFF IS FOR WINDOWS PPL
-rem just call the environment's python
-
-rem detect location
-set "BIN_DIR=%~dp0"
-
-reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v Autorun /t reg_sz /d "%BIN_DIR%kalite.cmd"
