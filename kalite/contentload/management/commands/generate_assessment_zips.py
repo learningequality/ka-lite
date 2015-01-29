acc91bb85248d2926f301e07a959fbc9ca44a4c0
Jamie Alexandre <jamalex@gmail.com>
2015-01-28 17:45:35 -0800

    Fixes to assessment item image downloader (robustness and correctness).

--- kalite/contentload/management/commands/generate_assessment_zips.py
+++ kalite/contentload/management/commands/generate_assessment_zips.py
@@ -18,24 +18,17 @@ class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
         logging.info("fetching assessment items")
-        assessment_items_url = "https://s3.amazonaws.com/uploads.hipchat.com/86198%2F624195%2FtYo7Ez0tt3e1qQW%2Fassessmentitems.json"
 
-        # cache the assessmentitems
-        assessment_items_cache_path = os.path.join(settings.PROJECT_PATH, "assessmentitems.cache.json")
-        if os.path.isfile(assessment_items_cache_path):
-            assessment_items = json.load(open(assessment_items_cache_path))
-        else:
-            assessment_items = json.loads(requests.get(assessment_items_url).content)
-            json.dump(assessment_items, open(assessment_items_cache_path, "w"))
+        # load the assessmentitems
+        assessment_items = json.load(open(os.path.join(settings.PROJECT_PATH, "..", "data", "khan", "assessmentitems.json")))
 
         image_urls = all_image_urls(assessment_items)
 
         logging.info("rewriting image urls")
         new_assessment_items = localhosted_image_urls(assessment_items)
 
-        with open(os.path.join(settings.PROJECT_PATH, "..", "data", "khan", "assessmentitems.json"), "w") as f:
-            json.dump(new_assessment_items, f, indent=4)
-
+        # TODO(jamalex): We should migrate this away from direct-to-zip so that we can re-run it
+        # without redownloading all files. Not possible currently because ZipFile has no `delete`.
         logging.info("downloading images")
         with zipfile.ZipFile(ZIP_FILE_PATH, "w") as zf:
             write_assessment_to_zip(zf, new_assessment_items)
@@ -46,7 +39,7 @@ class Command(NoArgsCommand):
 
 def write_assessment_to_zip(zf, assessment_items):
     assessment_json_string = json.dumps(assessment_items)
-    zf.writestr("assessment_items.json", assessment_json_string)
+    zf.writestr("assessmentitems.json", assessment_json_string)
 
 
 def download_urls(zf, urls):
@@ -58,16 +51,17 @@ def download_urls(zf, urls):
 
 
 def download_url_to_zip(zipfile, url):
-    logging.info("downloading %s" % url)
     filename = os.path.basename(url)
-    r = requests.get(url)
+    url = url.replace("https://ka-", "http://ka-")
+    logging.info("downloading %s" % url)
     try:
+        r = requests.get(url)
         r.raise_for_status()
     # we don't want a failed image request to download, but we
     # want to inform the user of the error
-    except requests.exceptions.RequestException as e:
-        logging.error(str(r))
-        return None
+    except:
+        logging.error("Error when downloading from URL: %s" % url)
+        return
     zipfile.writestr(filename, r.content)
 
 
