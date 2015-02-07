34d58b354905d4508b7b62452129b705b917bdec
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-02-06 18:05:51 -0800

    Set default browser size for screenshots to sane values

--- kalite/distributed/management/commands/screenshots.py
+++ kalite/distributed/management/commands/screenshots.py
@@ -212,6 +212,7 @@ class Screenshot(KALiteBrowserTestCase, FacilityMixins, BrowserActionMixins):
 
         self.loginfo("==> Setting-up browser ...")
         super(Screenshot, self).setUp()
+        self.browser.set_window_size(1024, 768)
 
         self.loginfo("==> Browser %s successfully setup with live_server_url %s." %
                  (self.browser.name, self.live_server_url,))
