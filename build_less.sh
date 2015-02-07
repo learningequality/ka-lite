3906d96ea101c76cab5a1396d09e4fdd774e4f29
Benjamin Bach <benjaoming@gmail.com>
2015-02-07 10:00:05 -0800

    Don't touch the static/ dir

--- build_less.sh
+++ build_less.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
+# Commented out because original less file isn't there atm?
+# lessc -x kalite/static/perseus/lib/katex/katex.less kalite/static/perseus/lib/katex/katex.css
 
-lessc -x kalite/static/perseus/lib/katex/katex.less kalite/static/perseus/lib/katex/katex.css
-
-# TODO: 2 lines of same stuff in different locations
 lessc -x kalite/distributed/static/perseus/stylesheets/exercise-content-package/perseus.less kalite/distributed/static/perseus/stylesheets/exercise-content-package/perseus.css
-lessc -x kalite/static/perseus/stylesheets/exercise-content-package/perseus.less kalite/static/perseus/stylesheets/exercise-content-package/perseus.css
-
-# TODO: 2 lines of same stuff in different locations
 lessc -x kalite/distributed/static/perseus/stylesheets/perseus-admin-package/editor.less kalite/distributed/static/perseus/stylesheets/perseus-admin-package/editor.css
-lessc -x kalite/static/perseus/stylesheets/perseus-admin-package/editor.less kalite/static/perseus/stylesheets/perseus-admin-package/editor.css
