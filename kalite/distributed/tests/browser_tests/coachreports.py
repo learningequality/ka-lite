28d5a36bcb8364b3738aaf76fa9b176743a3e4e6
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-02-16 11:30:53 -0800

    Amend tests to use safer WebDriverWait

--- kalite/distributed/tests/browser_tests/coachreports.py
+++ kalite/distributed/tests/browser_tests/coachreports.py
@@ -1,7 +1,10 @@
 """
 Basic tests of coach reports, inside the browser
 """
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 from kalite.facility.models import FacilityGroup, FacilityUser
 from kalite.testing.base import KALiteBrowserTestCase
@@ -32,20 +35,20 @@ class TestTabularViewErrors(BrowserActionMixins, CreateAdminMixin, FacilityMixin
         url = "%s?topic=%s" % (self.url, self.topic,)
         self.browse_to(url)
         try:
-            elem = self.browser.find_element_by_css_selector('.alert-danger')
+            elem = WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "alert-danger")))
             # Check if error message is contained in the alert bubble container.
             self.assertTrue("No learner accounts in this group have been created." in elem.text)
-        except NoSuchElementException:
+        except TimeoutException:
             self.fail("Error message for 'no group selected' is not found.")
 
     def test_no_groups_with_topic_selected(self):
         self.browser_login_admin(**self.admin_data)
         self.browse_to(self.url)
         try:
-            elem = self.browser.find_element_by_css_selector('.alert-danger')
+            elem = WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "alert-danger")))
             # Check if error message is contained in the alert bubble container.
             self.assertTrue("Please select a topic." in elem.text)
-        except NoSuchElementException:
+        except TimeoutException:
             self.fail("Didn't find the error message with no users available, no topic selected.")
 
     def test_groups_group_selected_no_topic_selected(self):
@@ -53,10 +56,10 @@ class TestTabularViewErrors(BrowserActionMixins, CreateAdminMixin, FacilityMixin
         url = "%s?group_id=%s" % (self.url, self.group.id,)
         self.browse_to(url)
         try:
-            elem = self.browser.find_element_by_css_selector('.alert-danger')
+            elem = WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "alert-danger")))
             # Check if error message is contained in the alert bubble container.
             self.assertTrue("Please select a topic." in elem.text)
-        except NoSuchElementException:
+        except TimeoutException:
             self.fail("Didn't find the error message with no topic selected.")
 
     def test_groups_group_selected_topic_selected_no_users(self):
@@ -64,10 +67,10 @@ class TestTabularViewErrors(BrowserActionMixins, CreateAdminMixin, FacilityMixin
         url = "%s?topic=%s&group_id=%s" % (self.url, self.topic, self.group.id,)
         self.browse_to(url)
         try:
-            elem = self.browser.find_element_by_css_selector('.alert-danger')
+            elem = WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "alert-danger")))
             # Check if error message is contained in the alert bubble container.
             self.assertTrue("No learner accounts in this group have been created." in elem.text)
-        except NoSuchElementException:
+        except TimeoutException:
             self.fail("Didn't find the error message with no users available.")
 
     def test_groups_group_selected_topic_and_playlist_selected(self):
@@ -78,10 +81,10 @@ class TestTabularViewErrors(BrowserActionMixins, CreateAdminMixin, FacilityMixin
                                                        "test-playlist")
         self.browse_to(url)
         try:
-            elem = self.browser.find_element_by_css_selector('.alert-danger')
+            elem = WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "alert-danger")))
             # Check if error message is contained in the alert bubble container.
             self.assertTrue("Please select a topic." in elem.text)
-        except NoSuchElementException:
+        except TimeoutException:
             self.fail("Didn't find the error message with both topic and playlist selected.")
 
     def test_users_out_of_group(self):
@@ -93,10 +96,10 @@ class TestTabularViewErrors(BrowserActionMixins, CreateAdminMixin, FacilityMixin
         url = "%s?topic=%s&group_id=%s" % (self.url, self.topic, self.group.id,)
         self.browse_to(url)
         try:
-            elem = self.browser.find_element_by_css_selector('.alert-danger')
+            elem = WebDriverWait(self.browser, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "alert-danger")))
             # Check if error message is contained in the alert bubble container.
             self.assertTrue("No learner accounts in this group have been created." in elem.text)
-        except NoSuchElementException:
+        except TimeoutException:
             self.fail("Didn't find the error message with users out of group.")
 
     def test_success_with_group(self):
