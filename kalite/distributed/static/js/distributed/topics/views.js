589ab64a8d5fd52e6447628c2ccf9ea827078dfc
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-02-17 13:43:50 -0800

    Get rid off redundant clearAllMessages function, use existing clear_messages

--- kalite/distributed/static/js/distributed/topics/views.js
+++ kalite/distributed/static/js/distributed/topics/views.js
@@ -20,7 +20,7 @@ window.ContentAreaView = BaseView.extend({
     show_view: function(view) {
 
         // hide any messages being shown for the old view
-        clearAllMessages();
+        clear_messages();
 
         this.close();
         // set the new view as the current view
