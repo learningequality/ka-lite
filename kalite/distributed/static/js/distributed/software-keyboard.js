e67835e9dd8c4d53b6bbc89a1df74516173d3951
Richard Tibbles <richard.tibbles@gmail.com>
2015-03-03 20:21:16 +0530

    Enhanced keypad display. Added a space and brackets to the numpad.

--- kalite/distributed/static/js/distributed/software-keyboard.js
+++ kalite/distributed/static/js/distributed/software-keyboard.js
@@ -10,11 +10,12 @@ window.SoftwareKeyboardView = Backbone.View.extend({
 
 
     keys: [
-        [ "1", "2", "3" ],
-        [ "4", "5", "6" ],
-        [ "7", "8", "9" ],
-        [ "/", "0", "-" ],
-        [ ".", "c", "Del" ]
+        [ {display: "1", value: "1"}, {display: "2", value: "2"}, {display: "3", value: "3"} ],
+        [ {display: "4", value: "4"}, {display: "5", value: "5"}, {display: "6", value: "6"} ],
+        [ {display: "7", value: "7"}, {display: "8", value: "8"}, {display: "9", value: "9"} ],
+        [ {display: "(", value: "("}, {display: "0", value: "0"}, {display: ")", value: ")"} ],
+        [ {display: "/", value: "/"}, {display: ".", value: "."}, {display: "-", value: "-"} ],
+        [ {display: "c", value: "c"}, {display: "␣", value: " "}, {display: "Del", value: "Del"} ]
     ],
 
     initialize: function () {
