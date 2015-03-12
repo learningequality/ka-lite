e050b75813226ab82f74fbf68f5f0b1c050e4eaf
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-03-12 09:54:24 -0700

    Abbreviate mock

--- python-packages/securesync/tests/regression_tests.py
+++ python-packages/securesync/tests/regression_tests.py
@@ -14,14 +14,14 @@ class CentralServerDownMessageTest(TestCase):
     was returned by the central server, and pass it along.
     """
 
-    @patch('securesync.devices.views.urllib.urlopen')
+    @patch('urllib.urlopen')
     def test_error_message_when_central_server_down(self, mock):
         mock().getcode.return_value = 500
         error_msg = "Some error message ah!"
         expected_msg = "Central Server is not reachable, Please try after sometime."
         self.assertEqual(central_server_down_or_error(error_msg)['error_msg'], expected_msg)
     
-    @patch('securesync.devices.views.urllib.urlopen')
+    @patch('urllib.urlopen')
     def test_error_message_when_central_server_up(self, mock):
         mock().getcode.return_value = 200
         error_msg = "Some error message ah!"
