c0031088f1d33d3a121c90f4fa1c312feea5ad86
66eli77 <66eli77@gmail.com>
2015-03-12 16:32:42 -0700

    fixed #3264 the memory error due to downloading file bigger than device's RAM
    typo fixing
    
    fixing file deleting issue.
    
    need the file object for zipfile
    
    adding close file to release system resource
    
    remover file download

--- kalite/contentload/management/commands/unpack_assessment_zip.py
+++ kalite/contentload/management/commands/unpack_assessment_zip.py
@@ -52,13 +52,19 @@ class Command(BaseCommand):
 
         if is_valid_url(ziplocation):  # url; download the zip
             logging.warn("Downloading assessment item data from a remote server. Please be patient; this file is big, so this may take some time...")
-            r = requests.get(ziplocation)
-            r.raise_for_status()
-            f = StringIO.StringIO(r.content)
+            #this way we can download stuff larger than the device's RAM
+            r = requests.get(ziplocation, prefetch=False)
+            f = open("tobezipped", "w+")
+            for chunk in r.iter_content(chunk_size=1024): 
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    f.flush()
         else:                   # file; just open it normally
             f = open(ziplocation, "r")
 
         zf = zipfile.ZipFile(f, "r")
+
+        os.remove("tobezipped")
 
         extract_assessment_items_to_data_dir(zf)
         unpack_zipfile_to_khan_content(zf)
