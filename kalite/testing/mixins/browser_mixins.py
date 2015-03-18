09f6b2ead716def7164e0708255651636640469e
Richard Tibbles <richard.tibbles@gmail.com>
2015-03-17 23:38:22 -0700

    Import By for Browser mixins.

--- kalite/testing/mixins/browser_mixins.py
+++ kalite/testing/mixins/browser_mixins.py
@@ -1,6 +1,7 @@
 import time
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
