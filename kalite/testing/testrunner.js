10693660ba5c554af76456ee954e2ac8d09952c9
Richard Tibbles <richard.tibbles@gmail.com>
2015-03-24 13:19:42 -0700

    Adds karma config. Wait for page to load before starting QUnit tests.

--- kalite/testing/testrunner.js
+++ kalite/testing/testrunner.js
@@ -5,9 +5,10 @@ var qunitTest = QUnit.test;
 QUnit.test = window.test = function() {
   testCount += 1;
   qunitTest.apply(this, arguments);
-  console.log(testCount);
 };
 
-QUnit.begin(function(args) {
-  args.totalTests = testCount;
+$(function(){
+    QUnit.begin(function(args) {
+      args.totalTests = testCount;
+    });
 });
