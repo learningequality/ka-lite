d19c333f7832d576e4fb403f9929f4b8318b6c16
tk21 <teresado.21t@gmail.com>
2015-03-27 14:38:26 -0700

    Admin: Manage page works with intro.js. Start button is temporary.

--- kalite/distributed/static/js/distributed/intro-injector.js
+++ kalite/distributed/static/js/distributed/intro-injector.js
@@ -9,10 +9,12 @@ $(function() {
   //Parse YAML file into JSON to be used with intro.js !!! this is actually just a json object for nao.
   var jsonResponse = parseResponse(response);
 
+console.log(jsonResponse);
+
   //Load from our new JSON object, and attach to elements in page
   attachAttributes(jsonResponse);
 
-
+  console.log("uhh hello?");
 
 });
 
@@ -30,10 +32,11 @@ function startTour() {
 //that we can parse directly into an actual json object
 //
 function getIntro(relative_url) {
-  return {"management/zone/None/": [{"ul.nav li a.admin-only": [{"step": 1}, {"text": "Welcome! This is the landing page for admins. If at any point you would like to navigate back to this page, click on this tab!"}]}, {"li.facility": [{"step": 2}, {"text": "Clicking on this tab will show you a quick overview of all facilities and devices"}]}, {"a.create-facility": [{"step": 3}, {"text": "To add new facilities, click here..."}]}, {"content-container .row:nth-child(3)": [{"step": 4}, {"text": "Information on your device status will be shown here"}]}, {"a.create-facility": [{"step": 5}, {"text": "If you decide to register your device to our central club, click here"}]}, {"a.create-facility": [{"step": 6}, {"text": "Questions about anything on the page? Be sure to consult the user manual or FAQ for more detailed walk throughs!"}]}]};
+  return {"management/zone/None/": [{"a#manage.admin-only": [{"step": 1}, {"text": "Welcome! This is the landing page for admins. If at any point you would like to navigate back to this page, click on this tab!"}]}, {"li.facility": [{"step": 2}, {"text": "Clicking on this tab will show you a quick overview of all facilities and devices"}]}, {"a.create-facility": [{"step": 3}, {"text": "To add new facilities, click here..."}]}, {"content-container .row:nth-child(3)": [{"step": 4}, {"text": "Information on your device status will be shown here"}]}, {"a.create-facility": [{"step": 5}, {"text": "If you decide to register your device to our central club, click here"}]}, {"#not-registered": [{"step": 6}, {"text": "Questions about anything on the page? Be sure to consult the user manual or FAQ for more detailed walk throughs!"}]}]};
 }
 
-//Given a serialized JSON string, parse and return JSON object. 
+//Given JSON form of YAML file for this page, parse and return JSON object
+//that can be used for intro framework. 
 function parseResponse(response) {
   var url = Object.keys(response);
   var parsedResponse = {};
@@ -55,16 +58,16 @@ function parseResponse(response) {
       var newkey = null;
       var newvalue = null;
 
+      //Change the attributes to match the framework
       if (key === "step") {
         newkey = "data-step";
         newvalue = value;
       }
       else {
-        newkey = "text";
+        newkey = "data-intro";
         newvalue = value;
       }
 
-      //Return a new object that uses appropriate introjs syntax
       var newattr = {};
       newattr[newkey] = newvalue;
       return newattr; 
@@ -84,17 +87,12 @@ function parseResponse(response) {
 }
 //------------------------------------------------------------------
 // Attaches attributes for intro.js to HTML elements 
-//    by utilizing the json object.
+//    by iterating through elements from JSON object.
 //------------------------------------------------------------------
 function attachAttributes(jsonResponse) {
-  //iterates through the elements from the JSON object
-    //$(element).attr(data-step, 1)
-    //$(element).attr(data-intro, "some text heres and")
-  //attach attributes to these elements
-
   var url = Object.keys(jsonResponse);
-  
-  _.map(jsonResponse, function(element) {
+
+  _.map(jsonResponse[url], function(element) {
     //selectorKey = selector element to apply attributes to
     var selectorKey = Object.keys(element);
     var attributes = [];
@@ -104,26 +102,21 @@ function attachAttributes(jsonResponse) {
       //key = attribute to be added to the element
       //value = numbered step or description in tooltip
       var key = Object.keys(attribute);
-      key = key[0];
-      var value = attribute[key];
-
-      
+      key = String(key[0]);
+      var value = String(attribute[key]);
 
       //append the intro key-value pair to the element.
+      selectorKey = String(selectorKey);
       $(selectorKey).attr(key, value);
-
+      console.log("THIS IS SELECTOR KEY AT THE END THO:" +selectorKey);
     });
-  });
+  }); 
 
+
+/**
+  var selectorKey = "a#manage.admin-only";
+  var key = "data-step";
+  var value = 1;
+  $(selectorKey).attr(key, value); **/
 
 }
-
-
-//don't care about url, go through array and then 
-/**
-for each object in the array, attached to the URL thing
-1. get the selector
-  2. get the object in the dom corresponding to this selector
-  3. look at the array that is the value of the selector key
-4. 
-**/
