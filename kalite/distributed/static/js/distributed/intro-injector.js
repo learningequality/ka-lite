c05b115594655dd1bd65df730c5aa732ff4dabb3
tk21 <teresado.21t@gmail.com>
2015-03-27 12:13:15 -0700

    attach elements... in progress...

--- kalite/distributed/static/js/distributed/intro-injector.js
+++ kalite/distributed/static/js/distributed/intro-injector.js
@@ -10,7 +10,7 @@ $(function() {
   var jsonResponse = parseResponse(response);
 
   //Load from our new JSON object, and attach to elements in page
-  attachElements(jsonResponse);
+  attachAttributes(jsonResponse);
 
 
 
@@ -33,7 +33,7 @@ function getIntro(relative_url) {
   return {"management/zone/None/": [{"ul.nav li a.admin-only": [{"step": 1}, {"text": "Welcome! This is the landing page for admins. If at any point you would like to navigate back to this page, click on this tab!"}]}, {"li.facility": [{"step": 2}, {"text": "Clicking on this tab will show you a quick overview of all facilities and devices"}]}, {"a.create-facility": [{"step": 3}, {"text": "To add new facilities, click here..."}]}, {"content-container .row:nth-child(3)": [{"step": 4}, {"text": "Information on your device status will be shown here"}]}, {"a.create-facility": [{"step": 5}, {"text": "If you decide to register your device to our central club, click here"}]}, {"a.create-facility": [{"step": 6}, {"text": "Questions about anything on the page? Be sure to consult the user manual or FAQ for more detailed walk throughs!"}]}]};
 }
 
-//Parses YAML file into JSON
+//Given a serialized JSON string, parse and return JSON object. 
 function parseResponse(response) {
   var url = Object.keys(response);
   var parsedResponse = {};
@@ -86,55 +86,36 @@ function parseResponse(response) {
 // Attaches attributes for intro.js to HTML elements 
 //    by utilizing the json object.
 //------------------------------------------------------------------
-function attachElements(jsonResponse) {
+function attachAttributes(jsonResponse) {
+  //iterates through the elements from the JSON object
+    //$(element).attr(data-step, 1)
+    //$(element).attr(data-intro, "some text heres and")
+  //attach attributes to these elements
+
   var url = Object.keys(jsonResponse);
-  //var parsedResponse = {}; don't need to return a parsed response when attaching elements------
-
-  //Parse the array containing selector objects for a given URL
-  var newselector = [];
-  newselector = _.map(jsonResponse[url], function(element) {
-    var selectorKey = Object.keys(element);   //the element that we will be appending attributes to
+  
+  _.map(jsonResponse, function(element) {
+    //selectorKey = selector element to apply attributes to
+    var selectorKey = Object.keys(element);
     var attributes = [];
-    attributes = element[selectorKey];  
+    attributes = element[selectorKey];
 
-    //var test = document.getElementById()
-    $(selectorKey)
-
-    //Parse array of intro.js attributes for a given selector
-    var parsed = [];
-    parsed = _.map(attributes, function(attribute) {
+    _.map(attributes, function(attribute) {
+      //key = attribute to be added to the element
+      //value = numbered step or description in tooltip
       var key = Object.keys(attribute);
       key = key[0];
       var value = attribute[key];
+
       
-      var newkey = null;
-      var newvalue = null;
 
-      if (key === "step") {
-        newkey = "data-step";
-        newvalue = value;
-      }
-      else {
-        newkey = "text";
-        newvalue = value;
-      }
+      //append the intro key-value pair to the element.
+      $(selectorKey).attr(key, value);
 
-      //Return a new object that uses appropriate introjs syntax
-      var newattr = {};
-      newattr[newkey] = newvalue;
-      return newattr; 
-    }); 
-
-    //new object {selector: [{attr1: val}, {attr2: val}] } for introjs syntax
-    var newElement = {};
-    newElement[selectorKey] = parsed;
-    return newElement;
-
+    });
   });
 
-  //add new array of selector objects to json object
-  //parsedResponse[url] = newselector;
-  //return parsedResponse;
+
 }
 
 
