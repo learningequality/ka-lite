5ba8153db0167bcbaa6434f1c4e6781dfebb625c
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-03-31 12:46:09 -0700

    Remove unused imports

--- kalite/control_panel/features/environment.py
+++ kalite/control_panel/features/environment.py
@@ -5,13 +5,6 @@ could be used to bootstrap other integration tests in our project.
 It sets up a test server and test database by using the LiveServerTestCase
 machinery.
 """
-from behave import *
-from httplib import CannotSendRequest
-from selenium import webdriver
-from urlparse import urljoin
-
-from django.test.testcases import LiveServerTestCase
-
 from kalite.testing.behave_helpers import login_as_admin, logout
 from kalite.testing.base_environment import before_all, after_all
 
