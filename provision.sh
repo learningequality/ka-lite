34e152ebdc16eff6cedf0901922afd924485ef50
Aron Fyodor Asor <aron@learningequality.org>
2015-04-03 16:19:34 -0700

    install python-pip too

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Used by vagrant for provisioning
 
-sudo apt-get install -y python2.7 python2.7-dev
+sudo apt-get install -y python2.7 python2.7-dev python-pip
 pip install -r /kalite/requirements.txt
 pip install -r /kalite/dev_requirements.txt
