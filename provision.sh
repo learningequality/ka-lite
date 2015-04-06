a788f5fa0412c391f30a2777110b114d89bd1b40
Aron Fyodor Asor <aron@learningequality.org>
2015-04-06 13:17:55 -0700

    give full path to where the kalite installation is.

--- provision.sh
+++ provision.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Used by vagrant for provisioning
 
-sudo apt-get install -y python2.7 python2.7-dev python-pip
-pip install -r requirements.txt
-pip install -r dev_requirements.txt
+apt-get install -y python2.7 python2.7-dev python-pip
+pip install -r /home/vagrant/ka-lite/requirements.txt
+pip install -r /home/vagrant/ka-lite/dev_requirements.txt
