78df3888189aefd07578fdfafcfc9c3ce10ad18d
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-04-10 15:24:42 -0700

    Callback only needs to be set once

--- kalite/distributed/static/js/distributed/topics/views.js
+++ kalite/distributed/static/js/distributed/topics/views.js
@@ -284,6 +284,12 @@ window.TopicContainerInnerView = BaseView.extend({
         this.add_all_entries();
 
         this.state_model.set("current_level", options.level);
+
+        // resize the scrollable part of sidebar to the page height
+        $(window).resize(_.throttle(function() {
+            var height = $(window).height();
+            self.$(".slimScrollDiv, .sidebar").height(height);
+        }, 200));
     },
 
     render: function() {
@@ -299,11 +305,6 @@ window.TopicContainerInnerView = BaseView.extend({
             alwaysVisible: true
         });
 
-        // resize the scrollable part of sidebar to the page height
-        $(window).resize(_.throttle(function() {
-            var height = $(window).height();
-            self.$(".slimScrollDiv, .sidebar").height(height);
-        }, 200));
         $(window).resize();
 
         return this;
