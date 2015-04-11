f44502dd3c469fd323a0c6323f075c9d10fa69af
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-04-10 17:08:09 -0700

    First pass at test

---
+++ kalite/distributed/tests/javascript_unit_tests/topic_container_inner_view_tests.js
@@ -0,0 +1,17 @@
+module("TopicContainerInnerView tests", {
+  setup: function() {
+    return this.theView = new TopicContainerInnerView();
+  }
+});
+
+test("Resizes when the window is resized or scrolled", function() {
+  expect(2);
+  sinon.spy(this.theView, "window_scroll_callback")
+  sinon.spy(this.theView, "window_resize_callback")
+
+  $(window).resize();
+  $(window).scroll();
+
+  ok(this.theView.window_resize_callback.calledOnce());
+  ok(this.theView.window_scroll_callback.calledOnce());
+});
