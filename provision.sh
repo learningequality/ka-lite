6ad600c621cabae9ba338651c704d10fc6938935
Aron Fyodor Asor <aron@learningequality.org>
2015-04-15 15:01:19 -0700

    updates to the provisioning script.
    Add git, phantomjs and use the latest node version.

--- provision.sh
+++ provision.sh
@@ -2,8 +2,8 @@
 # Used by vagrant for provisioning
 
 apt-get update --fix-missing
-apt-get install -y python2.7 python2.7-dev python-pip
-curl -sL https://deb.nodesource.com/setup | bash -
+apt-get install -y python2.7 python2.7-dev python-pip git phantomjs
+curl -sL https://deb.nodesource.com/setup_0.12 | bash -
 apt-get install -y nodejs
 pip install -r /home/vagrant/ka-lite/requirements.txt
 pip install -r /home/vagrant/ka-lite/dev_requirements.txt
