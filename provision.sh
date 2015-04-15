d0a97930af7a9af10f089f3b50c3dbbdf0f1baeb
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-04-15 15:03:20 -0700

    Use requirements_dev.txt

--- provision.sh
+++ provision.sh
@@ -5,8 +5,7 @@ apt-get update --fix-missing
 apt-get install -y python2.7 python2.7-dev python-pip git phantomjs
 curl -sL https://deb.nodesource.com/setup_0.12 | bash -
 apt-get install -y nodejs
-pip install -r /home/vagrant/ka-lite/requirements.txt
-pip install -r /home/vagrant/ka-lite/dev_requirements.txt
+pip install -r /home/vagrant/ka-lite/requirements_dev.txt
 cd /home/vagrant/ka-lite
 # --no-bin-links option is for linux guests on windows hosts
 npm install --no-bin-links
