bace6b00d6e52c66ee3de95f323e1dc620b6185a
Christine Pham <christine.pham.t@gmail.com>
2015-04-16 22:30:42 +0100

    Set up views

---
+++ kalite/coachreports/static/js/coachreports/tabular_view.js
@@ -0,0 +1,14 @@
+var TabularReportView = BaseView.extend({
+
+    template: HB.template("tabular_reports/tabular-view")
+
+    initialize: function() {
+        _.bindAll(this);
+        this.render();
+    },
+
+    render: function() {
+        this.$el.html(this.template({students: students}));
+    }
+
+});
