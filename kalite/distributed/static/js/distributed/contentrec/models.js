b4b75ada9f24c773659a445a509cc6e8116cc10c
howdyitshelena <helena.qin@gmail.com>
2015-04-16 17:33:31 -0700

    content rec explore files

--- kalite/distributed/static/js/distributed/contentrec/models.js
+++ kalite/distributed/static/js/distributed/contentrec/models.js
@@ -1,1 +1,37 @@
 //Models
+
+window.ContentResumeModel = Backbone.Model.extend({
+	defaults: {
+		lesson_title: "",
+		lesson_description: "blahblah"
+	},
+
+	initialize: function() {
+        //this.channel = options.channel;
+        console.log(this.get('lesson_description'));
+    }
+});
+
+window.ContentExploreModel = Backbone.Model.extend({
+	defaults: {
+		lesson_title: "",
+		lesson_description: "blahblah"
+	},
+
+	initialize: function() {
+        //this.channel = options.channel;
+        console.log(this.get('lesson_description'));
+    }
+});
+
+window.ContentExploreLessonModel = Backbone.Model.extend({
+	defaults: {
+		interest_topic: "geometry",
+		lesson_title: "",
+		lesson_description: "blahblah"
+	},
+
+	initialize: function() {
+		console.log(this.get('lesson_description'));
+	}
+});
--- kalite/distributed/static/js/distributed/contentrec/views.js
+++ kalite/distributed/static/js/distributed/contentrec/views.js
@@ -8,7 +8,6 @@ window.ContentResumeView = BaseView.extend({
     template: HB.template("contentrec/content-resume"),
     
     initialize: function() {
-        this.model = new Backbone.Model();
         this.render();
     },
 
@@ -18,10 +17,81 @@ window.ContentResumeView = BaseView.extend({
 
 });
 
+/**
+ * View that wraps a lesson on the content explore card on the learn page
+ */
+window.ContentExploreLessonView = BaseView.extend({
+
+	template: HB.template("contentrec/content-explore-lesson"),
+
+	initialize: function() {
+		this.render();
+	},
+
+	render: function() {
+
+		this.$el.html(this.template(this.model.attributes));
+	}
+
+});
+
+/**
+ * View that wraps the content explore card on the learn page
+ */
+window.ContentExploreView = BaseView.extend({
+
+	template: HB.template("contentrec/content-explore"),
+
+	initialize: function() {
+		this.render();
+	},
+
+	render: function() {
+
+
+		this.contentExploreLessonModel = new ContentExploreLessonModel();
+
+		this.content_explore_lesson_1 = this.add_subview(ContentExploreLessonView, {
+			model: this.contentExploreLessonModel
+		});
+		this.content_explore_lesson_2 = this.add_subview(ContentExploreLessonView, {
+			model: this.contentExploreLessonModel
+		});
+		this.content_explore_lesson_3 = this.add_subview(ContentExploreLessonView, {
+			model: this.contentExploreLessonModel
+		});
+		this.content_explore_lesson_4 = this.add_subview(ContentExploreLessonView, {
+			model: this.contentExploreLessonModel
+		});
+
+		this.$el.html(this.template(this.model.attributes));
+
+		this.$("#content-explore-lessons").append(this.content_explore_lesson_1.el.childNodes);
+		this.$("#content-explore-lessons").append(this.content_explore_lesson_2.el.childNodes);
+		this.$("#content-explore-lessons").append(this.content_explore_lesson_3.el.childNodes);
+		this.$("#content-explore-lessons").append(this.content_explore_lesson_4.el.childNodes);
+	}
+
+});
+
+
+
 $(function(){
-	//window.content_resume = new ContentResumeView();
-	//$("#content-area").append(window.content_resume.$el);
-	window.content_resume = new ContentResumeView({
-		el: "#content-area"
-	});
+	window.contentResumeModel = new window.ContentResumeModel();
+	window.contentExploreModel = new window.ContentExploreModel();
+
+	//contentResumeModel.fetch().then(function(){
+		window.content_resume = new ContentResumeView({
+			model: contentResumeModel
+		});
+
+		$("#content-area").append(window.content_resume.el.childNodes);
+
+		window.content_explore = new ContentExploreView({
+			model: contentExploreModel
+		});
+
+		$("#content-area").append(window.content_explore.el.childNodes);
+
+	//});
 });
