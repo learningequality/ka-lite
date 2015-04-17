bdd15d6a7dcfb8424b05d30396e6672b77472c0b
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-04-16 17:28:48 -0700

    Remove pesky deprecated bat files

--- scripts/add_systemstart_task.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "SCRIPT_DIR=%~dp0"
-
-TITLE Adding task to start KA Lite at system start
-schtasks /create /tn "KALite" /tr "\"%SCRIPT_DIR%..\bin\windows\kalite.bat\" start" /sc onstart /ru SYSTEM
