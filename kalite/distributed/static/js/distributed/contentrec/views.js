1ca2c224017f992e6c5bf6930194e86338877a42
howdyitshelena <helena.qin@gmail.com>
2015-04-24 16:06:10 -0700

    adding next steps card

--- kalite/distributed/static/js/distributed/contentrec/views.js
+++ kalite/distributed/static/js/distributed/contentrec/views.js
@@ -18,11 +18,65 @@ window.ContentResumeView = BaseView.extend({
 });
 
 /**
- * View that wraps a lesson on the content explore card on the learn page
+ * View that wraps a lesson on the content next steps card on the learn page
  */
-window.ContentExploreLessonView = BaseView.extend({
+window.ContentNextStepsLessonView = BaseView.extend({
 
-	template: HB.template("contentrec/content-explore-lesson"),
+	template: HB.template("contentrec/content-nextsteps-lesson"),
+
+	initialize: function() {
+		this.render();
+	},
+
+	render: function() {
+
+		this.$el.html(this.template(this.model.attributes));
+	}
+
+});
+
+/**
+ * View that wraps the content next steps card on the learn page
+ */
+window.ContentNextStepsView = BaseView.extend({
+
+    template: HB.template("contentrec/content-nextsteps"),
+    
+    initialize: function() {
+    	if (typeof this.collection === "undefined") {
+    		this.collection = new ContentNextStepsCollection([
+    			{interest_topic: "Test Topic 1", lesson_title: "Test Lesson Title"},
+    			{interest_topic: "Test Topic 2", lesson_title: "Test Lesson Title 2"}
+    			]);
+    	}
+        this.render();
+    },
+
+    render: function() {
+    	this.contentNextStepsLessonModel = new ContentNextStepsLessonModel();
+
+        this.$el.html(this.template(this.model.attributes));
+        var container = document.createDocumentFragment();
+
+        for(i = 0; i < this.collection.length; i++) {
+			var name = new String('content_nextsteps_lesson_'+i);
+			this[name] = this.add_subview(ContentNextStepsLessonView, {
+				model: this.collection.models[i]
+			});
+			container.appendChild(this[name].el);
+		}
+
+		this.$("#content-nextsteps-lessons").append(container);
+    }
+
+});
+
+/**
+ * View that wraps a topic on the content explore card on the learn page
+ */
+window.ContentExploreTopicView = BaseView.extend({
+
+	template: HB.template("contentrec/content-explore-topic"),
 
 	initialize: function() {
 		this.render();
@@ -48,49 +102,45 @@ window.ContentExploreView = BaseView.extend({
 
 	render: function() {
 
+		this.contentExploreTopicModel = new ContentExploreTopicModel();
 
-		this.contentExploreLessonModel = new ContentExploreLessonModel();
-
-		this.content_explore_lesson_1 = this.add_subview(ContentExploreLessonView, {
-			model: this.contentExploreLessonModel
-		});
-		this.content_explore_lesson_2 = this.add_subview(ContentExploreLessonView, {
-			model: this.contentExploreLessonModel
-		});
-		this.content_explore_lesson_3 = this.add_subview(ContentExploreLessonView, {
-			model: this.contentExploreLessonModel
-		});
-		this.content_explore_lesson_4 = this.add_subview(ContentExploreLessonView, {
-			model: this.contentExploreLessonModel
-		});
+		this.contentExploreCollection = new ContentExploreCollection();
 
 		this.$el.html(this.template(this.model.attributes));
 
-		this.$("#content-explore-lessons").append(this.content_explore_lesson_1.el.childNodes);
-		this.$("#content-explore-lessons").append(this.content_explore_lesson_2.el.childNodes);
-		this.$("#content-explore-lessons").append(this.content_explore_lesson_3.el.childNodes);
-		this.$("#content-explore-lessons").append(this.content_explore_lesson_4.el.childNodes);
+		for(i = 1; i < 5; i++) {
+			var name = new String('content_explore_topic_'+i);
+			this.name = this.add_subview(ContentExploreTopicView, {
+				model: this.contentExploreTopicModel
+			});
+			this.contentExploreCollection.add(this.name.el.childNodes);
+			this.$("#content-explore-topics").append(this.name.el.childNodes);
+		}
 	}
 
 });
 
 
-
 $(function(){
 	window.contentResumeModel = new window.ContentResumeModel();
-	window.contentExploreModel = new window.ContentExploreModel();
+	window.contentNextStepsModel = new window.ContentNextStepsModel();
+	window.contentExploreCollection = new window.ContentExploreCollection();
 
 	//contentResumeModel.fetch().then(function(){
 		window.content_resume = new ContentResumeView({
 			model: contentResumeModel
 		});
 
-		$("#content-area").append(window.content_resume.el.childNodes);
-
-		window.content_explore = new ContentExploreView({
-			model: contentExploreModel
+		window.content_nextsteps = new ContentNextStepsView({
+			model: contentNextStepsModel
 		});
 
+		window.content_explore = new ContentExploreView({
+			model: contentExploreCollection
+		});
+
+		$("#content-area").append(window.content_resume.el.childNodes);
+		$("#content-area").append(window.content_nextsteps.el.childNodes);
 		$("#content-area").append(window.content_explore.el.childNodes);
 
 	//});
