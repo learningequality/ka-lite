105582017b42843591df40040f24ada8aa5b5071
Richard Tibbles <richard.tibbles@gmail.com>
2015-04-23 18:07:35 -0700

    Deduplicates the "I am on the homepage" step.

--- kalite/distributed/features/steps/search.py
+++ kalite/distributed/features/steps/search.py
@@ -7,10 +7,6 @@ from kalite.testing.behave_helpers import *
 
 TIMEOUT = 3
 
-@given("I am on the homepage")
-def step_impl(context):
-    go_to_homepage(context)
-
 @when("I search for 'Math'")
 def step_impl(context):
     search_for(context, "Math")
