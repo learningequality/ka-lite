a7b447e4656938d076a0110f67f120e8a602fc0a
Richard Tibbles <richard.tibbles@gmail.com>
2015-05-01 17:43:48 -0700

    Makes TopicNode Models and Collections work with new flat API endpoint.

--- kalite/distributed/static/js/distributed/topics/models.js
+++ kalite/distributed/static/js/distributed/topics/models.js
@@ -1,18 +1,30 @@
 // Models
 window.TopicNode = Backbone.Model.extend({
-    url: function() {
-        return ("ALL_TOPICS_URL" in window)? sprintf(ALL_TOPICS_URL, {channel_name: this.channel}) : null;
-    },
 
     initialize: function(options) {
+        
         if (this.get("entity_kind")===undefined && this.get("kind")!==undefined){
             this.set("entity_kind", this.get("kind"));
         }
-        this.channel = options.channel;
     }
 });
 
 // Collections
 window.TopicCollection = Backbone.Collection.extend({
-    model: TopicNode
+    model: TopicNode,
+
+    initialize: function(options) {
+
+        var self = this;
+        
+        this.parent = options.parent;
+
+        this.channel = options.channel;
+
+        this.listenToOnce(this, "sync", function() {self.loaded = true;})
+    },
+
+    url: function() {
+        return ("ALL_TOPICS_URL" in window)? setGetParam(sprintf(ALL_TOPICS_URL, {channel_name: this.channel}), "parent", this.parent) : null;
+    }
 });
