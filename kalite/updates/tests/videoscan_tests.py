72daacc2577ce7e053a7dc63bb2b913772e440f1
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-05-11 20:23:24 -0700

    Disconnect post_save to speed up test

--- kalite/updates/tests/videoscan_tests.py
+++ kalite/updates/tests/videoscan_tests.py
@@ -1,9 +1,11 @@
 from mock import patch
 
 from django.core.management import call_command
+from django.db.models.signals import post_save
 
 from kalite.testing.base import KALiteTestCase
 from kalite.updates.models import VideoFile
+from kalite import updates, caching
 
 class CacheInvalidationTestCase(KALiteTestCase):
     """
@@ -18,8 +20,10 @@ class CacheInvalidationTestCase(KALiteTestCase):
         # Since they don't actually exist they should automatically fail to be in `youtube_ids_in_filesystem` set.
         # `flagged_for_download=False` ensures `delete_objects_for_missing_videos` function attempts to delete them.
         # `percent_complete=100` ensures that the listener does something nontrivial.
+        post_save.disconnect(receiver=updates.invalidate_on_video_update, sender=VideoFile)
         VideoFile.objects.create(youtube_id="blah1", flagged_for_download=False, percent_complete=100)
         VideoFile.objects.create(youtube_id="blah2", flagged_for_download=False, percent_complete=100)
+        post_save.connect(receiver=updates.invalidate_on_video_update, sender=VideoFile)
 
     @patch('kalite.caching.invalidate_all_caches')
     def test_cache_invaldation_occurs_exactly_once(self, mock_func):
