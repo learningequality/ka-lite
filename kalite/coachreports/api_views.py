c14c462aab9245fd8d642c9f46d1b4234394b261
Richard Tibbles <richard.tibbles@gmail.com>
2015-05-13 17:03:57 -0700

    Adds ability to select via topic_id to coach_report API endpoint.

--- kalite/coachreports/api_views.py
+++ kalite/coachreports/api_views.py
@@ -6,6 +6,7 @@ from fle_utils.internet.classes import JsonResponse, JsonResponseMessage, JsonRe
 from kalite.main.models import ExerciseLog, VideoLog, ContentLog
 from kalite.facility.models import FacilityUser
 from kalite.shared.decorators.auth import require_admin
+from kalite.topic_tools import get_topic_leaves
 
 @require_admin
 def learner_logs(request):
@@ -15,6 +16,8 @@ def learner_logs(request):
     group_ids = request.GET.getlist("group_id")
 
     facility_ids = request.GET.getlist("facility_id")
+
+    topic_ids = request.GET.getlist("topic_id", ["root"])
 
     learners = FacilityUser.objects.filter(Q(group__pk__in=group_ids)|Q(pk__in=learner_ids)|Q(facility__pk__in=facility_ids))
 
@@ -27,14 +30,18 @@ def learner_logs(request):
         if log_type == "exercise":
             LogModel = ExerciseLog
             fields.extend(["exercise_id", "attempts", "struggling", "streak_progress", "attempts_before_completion"])
+            obj_id_field = "exercise_id__in"
         elif log_type == "video":
             LogModel = VideoLog
             fields.extend(["video_id", "total_seconds_watched"])
+            obj_id_field = "video_id__in"
         elif log_type == "content":
             LogModel = ContentLog
             fields.extend(["content_id", "progress"])
+            obj_id_field = "content_id__in"
         else:
             continue
-        output_logs.extend(LogModel.objects.filter(user__in=learners).values(*fields))
+        obj_ids = {obj_id_field: [obj.get("id") for topic_id in topic_ids for obj in get_topic_leaves(topic_id=topic_id, leaf_type=log_type.title())]}
+        output_logs.extend(LogModel.objects.filter(user__in=learners, **obj_ids).values(*fields))
 
     return JsonResponse(output_logs)
