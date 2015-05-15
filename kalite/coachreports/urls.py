c022146330283e84c9cf9da1b19910dda81b64e1
Richard Tibbles <richard.tibbles@gmail.com>
2015-05-15 12:29:41 -0700

    Burn down unused coach report code. Change link to landing_page rather than tabular view.

--- kalite/coachreports/urls.py
+++ kalite/coachreports/urls.py
@@ -12,21 +12,3 @@ urlpatterns = patterns(__package__ + '.views',
 
     url(r'^api/', include(api_urls)),
 )
-
-
-#Else block url patterns can't be empty because we have a reverse match for test_view/test_detail_view
-
-if "nalanda" in settings.CONFIG_PACKAGE:
-    urlpatterns += patterns(__package__ + '.views',
-        url(r'^test/$', 'test_view', {}, 'test_view'),
-        url(r'^test/(?P<test_id>\w+)/$', 'test_detail_view', {}, 'test_detail_view'),
-        url(r'^spending_report/$', 'spending_report_view', {}, 'spending_report_view'),
-        url(r'^spending_report/(?P<user_id>\w+)$', 'spending_report_detail_view', {}, 'spending_report_detail_view'),
-    )
-else:
-    urlpatterns += patterns(__package__ + '.views',
-        url(r'^/$', 'test_view', {}, 'test_view'),
-        url(r'^/$', 'test_detail_view', {}, 'test_detail_view'),
-        url(r'^/$', 'spending_report_view', {}, 'spending_report_view'),
-        url(r'^/(?P<user_id>\w+)$', 'spending_report_detail_view', {}, 'spending_report_detail_view'),
-    )
