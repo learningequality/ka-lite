15878e603d115f9cbcfe0621410ea8f58b56b7ff
66eli77 <66eli77@gmail.com>
2015-05-21 12:49:42 -0700

    added i18n translation support

--- kalite/distributed/forms.py
+++ kalite/distributed/forms.py
@@ -1,13 +1,14 @@
 from django import forms
+from django.utils.translation import ugettext as _
 
 class SuperuserForm(forms.Form):
     """Form that represents the schema for superuser creating API requests"""
 
     superusername = forms.CharField(max_length=40, min_length=1, label='Username', 
-        widget=forms.TextInput(attrs={'class':'form-control input-lg', 'placeholder':'Username'}))
+        widget=forms.TextInput(attrs={'class':'form-control input-lg', 'placeholder':_('Username')}))
 
     superpassword = forms.CharField(max_length=40, min_length=1, label='Password', 
-        widget=forms.TextInput(attrs={'class':'form-control input-lg', 'placeholder':'Password'}))
+        widget=forms.TextInput(attrs={'class':'form-control input-lg', 'placeholder':_('Password')}))
 
     superemail = forms.EmailField(max_length=40, label='E-mail', 
-        widget=forms.TextInput(attrs={'class':'form-control input-lg', 'placeholder':'E-mail'}))
+        widget=forms.TextInput(attrs={'class':'form-control input-lg', 'placeholder':_('E-mail')}))
