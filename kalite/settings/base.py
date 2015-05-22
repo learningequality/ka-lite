05a0edf8bd31cdfbc24bec69dd843db57605c532
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-05-22 14:17:17 -0700

    Remove unneeded option on DB

--- kalite/settings/base.py
+++ kalite/settings/base.py
@@ -260,7 +260,6 @@ DATABASES = getattr(local_settings, "DATABASES", {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": TT_DATABASE_PATH,
         "OPTIONS": {
-            "timeout": 60,
         },
     }
 })
