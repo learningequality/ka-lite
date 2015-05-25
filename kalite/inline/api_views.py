8ba9fec1b4c8bb078ce42bfee081f07eaad6c427
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-05-25 15:11:36 -0700

    Narrative api endpoint now matches url to pattern in narrative.yml

--- kalite/inline/api_views.py
+++ kalite/inline/api_views.py
@@ -2,6 +2,7 @@
 Views accessible as an API endpoint for inline.  All should return JsonResponses.
 """
 import os
+import re
 
 from kalite.shared.utils import open_json_or_yml
 from kalite import settings
@@ -10,7 +11,17 @@ from fle_utils.internet.classes import JsonResponse
 
 
 def narrative_view(request, narrative_id):
+    """
+    :param request: the request
+    :param narrative_id: the narrative id, a url to be matched
+    :return: a serialized JSON blob of the narrative dict
+    """
     filename = os.path.join(settings.CONTENT_DATA_PATH, "narratives")
-    narrative_json = open_json_or_yml(filename)
-    # TODO(MCGallaspy): This is incomplete. Needs to use the narrative_id.
-    return JsonResponse(narrative_json)
+    narratives = open_json_or_yml(filename)
+    the_narrative = {}
+    for key, narr in narratives.iteritems():
+        exp = re.compile(key)
+        if exp.search(narrative_id):
+            the_narrative[key] = narr
+            break
+    return JsonResponse(the_narrative)
