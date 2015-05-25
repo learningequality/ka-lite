37a8643f5add819336aa98f3360a847f9987858e
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-05-25 13:03:19 -0700

    Move narratives.yml file; use open_json_or_yml

--- kalite/inline/api_views.py
+++ kalite/inline/api_views.py
@@ -1,16 +1,16 @@
 """
 Views accessible as an API endpoint for inline.  All should return JsonResponses.
 """
-import yaml
-import json
+import os
+
+from kalite.shared.utils import open_json_or_yml
+from kalite import settings
 
 from fle_utils.internet.classes import JsonResponse
 
 
 def narrative_view(request, narrative_id):
-    f = open("kalite/inline/narratives.yaml", 'r')
-    narrative_json = yaml.safe_load(f)
-    f.close()
-
-    print narrative_json
+    filename = os.path.join(settings.CONTENT_DATA_PATH, "narratives")
+    narrative_json = open_json_or_yml(filename)
+    # TODO(MCGallaspy): This is incomplete. Needs to use the narrative_id.
     return JsonResponse(narrative_json)
