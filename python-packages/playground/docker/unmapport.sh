1e8cdbc146525be21886e584629682076ed806d6
Benjamin Bach <benjaoming@gmail.com>
2015-05-26 02:27:26 +0200

    Unbundle unused playground #3676

--- python-packages/playground/docker/unmapport.sh
+++
@@ -1,5 +0,0 @@
-for i in `sudo iptables -t nat -L PREROUTING --line-numbers | grep -E "(dpt:|ports )$1\b" | awk '{print $1}' | sort -r`; do
-    echo Unmapping "$i"
-    sudo iptables -t nat -D PREROUTING $i
-done
-
