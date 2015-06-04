92e37932148ab539e69b2e1605fa352593a38131
jtamiace <jtamiace@gmail.com>
2015-06-04 11:56:47 -0700

    fixes #3742: flickering learn tab

--- kalite/distributed/static/js/distributed/user/views.js
+++ kalite/distributed/static/js/distributed/user/views.js
@@ -362,7 +362,7 @@ window.ToggleNavbarView = BaseView.extend ({
     initialize: function() {
 
         _.bindAll(this);
-        this.listenTo(this.model, "change", this.render);
+        this.listenTo(this.model, "change:client_server_time_diff", this.render);
         $("topnav").append(this.template());
         $(window).on("resize", this.collapsed_nav);
     },
