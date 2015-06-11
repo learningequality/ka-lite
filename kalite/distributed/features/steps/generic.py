860dff8ff7edd3fb2c1befa3f5db6f5a280ea8e0
66eli77 <66eli77@gmail.com>
2015-06-10 18:58:29 -0700

    create superuser when on homepage

--- kalite/distributed/features/steps/generic.py
+++ kalite/distributed/features/steps/generic.py
@@ -1,6 +1,9 @@
 from behave import *
 from kalite.testing.behave_helpers import *
+from django.contrib.auth.models import User
 
 @given("I am on the homepage")
 def step_impl(context):
-    go_to_homepage(context)
+	if not User.objects.exists():
+		User.objects.create_superuser(username='superusername', password='superpassword', email='super@email.com')
+	go_to_homepage(context)
