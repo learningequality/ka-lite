7146299835f07104a499662c6052f0392c0f630c
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-06-11 16:44:52 -0700

    Oh my gosh, use an in-memory database for screenshots

--- kalite/settings/__init__.py
+++ kalite/settings/__init__.py
@@ -136,9 +136,6 @@ if sys.getdefaultencoding() != DEFAULT_ENCODING:
 SCREENSHOTS_OUTPUT_PATH = os.path.join(USER_DATA_ROOT, "data", "screenshots")
 SCREENSHOTS_EXTENSION = ".png"
 
-SCREENSHOTS_DATABASE_NAME = "screenshot-data.sqlite"
-SCREENSHOTS_DATABASE_PATH = os.path.join(SCREENSHOTS_OUTPUT_PATH, SCREENSHOTS_DATABASE_NAME)
-
 SCREENSHOTS_JSON_PATH = os.path.join(os.path.dirname(__file__), "data")
 SCREENSHOTS_JSON_FILE = os.path.join(SCREENSHOTS_JSON_PATH, 'screenshots.json')
 SCREENSHOTS_ROUTER = 'default'
@@ -149,10 +146,7 @@ if 'screenshots' in sys.argv:
     DATABASES = {
         SCREENSHOTS_ROUTER: {
             "ENGINE": getattr(local_settings, "DATABASE_TYPE", SQLITE3_ENGINE),
-            "NAME": SCREENSHOTS_DATABASE_PATH,
-            "OPTIONS": {
-                "timeout": 60,
-            },
+            "NAME": ":memory:",
         }
     }
 
