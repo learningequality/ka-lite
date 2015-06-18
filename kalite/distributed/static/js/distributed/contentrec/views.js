f5d6132fc6a08375263ebeea47adec2ed0078096
Richard Tibbles <richard.tibbles@gmail.com>
2015-06-18 14:24:09 +0100

    Fixes #3884. Only displays existing recommendations to user.

--- kalite/distributed/static/js/distributed/contentrec/views.js
+++ kalite/distributed/static/js/distributed/contentrec/views.js
@@ -18,9 +18,18 @@ window.HomepageWrapper = BaseView.extend({
 
     render: function() {
         this.$el.html(this.template());
-        this.$("#resume").append(this.content_resume.el);
-        this.$("#nextsteps").append(this.content_nextsteps.el);
-        this.$("#explore").append(this.content_explore.el);
+
+        if (this.content_resume) {
+            this.$("#resume").append(this.content_resume.el);
+        }
+
+        if (this.content_nextsteps) {
+            this.$("#nextsteps").append(this.content_nextsteps.el);
+        }
+
+        if (this.content_explore) {
+            this.$("#explore").append(this.content_explore.el);
+        }
     },
 
     data_load: function() {
@@ -30,17 +39,23 @@ window.HomepageWrapper = BaseView.extend({
         
         var exploreCollection = new window.SuggestedContentCollection(this.collection.where({explore:true}));
         
-        this.content_resume = new ContentResumeView({
-            collection:resumeCollection
-        });
+        if (resumeCollection.length > 0) {
+            this.content_resume = new ContentResumeView({
+                collection:resumeCollection
+            });
+        }
 
-        this.content_nextsteps = new ContentNextStepsView({
-            collection:nextStepsCollection
-        });
+        if (nextStepsCollection.length > 0) {
+            this.content_nextsteps = new ContentNextStepsView({
+                collection:nextStepsCollection
+            });
+        }
 
-        this.content_explore = new ContentExploreView({
-            collection:exploreCollection
-        });
+        if (exploreCollection.length > 0) {
+            this.content_explore = new ContentExploreView({
+                collection:exploreCollection
+            });
+        }
                 
         this.render();
     }
