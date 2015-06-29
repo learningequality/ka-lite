76a47dfaf258c019a701edf1faed9812b23cdb37
Benjamin Bach <benjaoming@gmail.com>
2015-06-29 16:49:12 +0200

    upgrade django-debug-toolbar settings to match latest version

--- kalite/project/settings/dev.py
+++ kalite/project/settings/dev.py
@@ -53,19 +53,22 @@ MIDDLEWARE_CLASSES += [
 #######################################
 
 DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+#    'debug_toolbar.panels.redirects.RedirectsPanel',  # This one is very annoying
 )
 DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-    'HIDE_DJANGO_SQL': False,
+#    'INTERCEPT_REDIRECTS': False,  # Deprecated in recent django-debug-toolbar
+#    'HIDE_DJANGO_SQL': False,  # Removed
     'ENABLE_STACKTRACES': True,
 }
 # Debug toolbar must be set in conjunction with CACHE_TIME=0
