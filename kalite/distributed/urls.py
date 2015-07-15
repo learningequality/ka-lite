e87e459091174c25921788d35177cb51d14f2af8
Benjamin Bach <benjaoming@gmail.com>
2015-07-15 23:41:30 +0200

    KHAN_ASSESSMENT_ITEM_ROOT should be respected for user data, too

--- kalite/distributed/urls.py
+++ kalite/distributed/urls.py
@@ -46,12 +46,11 @@ urlpatterns += patterns('',
 # TODO: This should only be in DEBUG settings and the HTTP server should be
 # serving it otherwise. Cherrypy is currently serving it through modifications
 # in kalite/django_cherrypy_wsgiserver/cherrypyserver.py
-if settings.ASSESSMENT_ITEMS_SYSTEM_WIDE:
-    urlpatterns += patterns('',
-        url(r'^%s/khan(?P<path>.*)$' % settings.CONTENT_URL[1:], 'django.views.static.serve', {
-            'document_root': contentload_settings.KHAN_ASSESSMENT_ITEM_ROOT,
-        }),
-    )
+urlpatterns += patterns('',
+    url(r'^%s/khan(?P<path>.*)$' % settings.CONTENT_URL[1:], 'django.views.static.serve', {
+        'document_root': contentload_settings.KHAN_ASSESSMENT_ITEM_ROOT,
+    }),
+)
 
 urlpatterns += patterns('',
     url(r'^%s(?P<path>.*)$' % settings.CONTENT_URL[1:], 'django.views.static.serve', {
