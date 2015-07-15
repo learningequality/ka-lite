7ac3f3fcd02ab8d0b7383c47b816efd29999d0f7
Sushrut Girdhari <developer@girdhari.in>
2015-07-16 00:14:25 +0530

    Fixed indentation issues

--- backup/__init__.py
+++ backup/__init__.py
@@ -12,32 +12,32 @@ backup_dirpath = expanduser("~")
 backup_dirpath = os.path.join(backup_dirpath, 'ka-lite-backups')
 
 def file_rename():
-	filename ='default.backup'	
-	time = datetime.datetime.now().ctime()
-	path = os.path.join(backup_dirpath, filename)
-	target = os.path.join(backup_dirpath, time + '.backup')
+    filename ='default.backup'
+    time = datetime.datetime.now().ctime()
+    path = os.path.join(backup_dirpath, filename)
+    target = os.path.join(backup_dirpath, time + '.backup')
 
-	if path.exists():
-		os.rename(path, target)
+    if path.exists():
+        os.rename(path, target)
 
 def setup_backup():
-	try:
-		command = call_command('dbbackup')
-		force_job(command, frequency="DAILY")
-	except KeyError
+    try:
+        command = call_command('dbbackup')
+        force_job(command, frequency="DAILY")
+    except KeyError
 
 def restore_list():
     file_list = os.listdir(backup_dirpath) #Retrieve the filenames present in the backup_dirpath
-    filelist = [] 
-    filenumber = 0 
-    
+    filelist = []
+    filenumber = 0
+
     for eachfile in file_list:
         filelist.append(eachfile) #Store files one by one
         print filelist.index(eachfile), eachfile #show file number: index
-        filenumber += 1 
-    
+        filenumber += 1
+
     print "Please enter file-number to restore:\n"
-    
+
     backup_option = int(raw_input())
     restoref = filelist[backup_option] #file to be restored based on x
     backup_filepath = os.path.join(backup_dirpath, restoref) #create file path
@@ -45,7 +45,6 @@ def restore_list():
 
 def rotate_backups():
     now = time.time()
-    print backup_dirpath
     for fp in os.listdir(backup_dirpath):
         if os.stat(os.path.join(backup_dirpath, fp)).st_mtime < now - 7 * 86400:
             os.remove(os.path.join(backup_dirpath, fp))
