5a5ddac32f1b9c79b7b8f7da2946eef2f04c43e6
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-08-04 17:32:58 -0700

    Implement more steps

--- kalite/distributed/features/steps/content_rating.py
+++ kalite/distributed/features/steps/content_rating.py
@@ -10,6 +10,7 @@ STAR_CONTAINER_IDS = (
 )
 SUBMIT_CLASS = "rating-submit"
 STAR_RATING_OPTION_CLASS = "star-rating-option"
+STAR_INNER_WRAPPER_CLASS = "star-rating-inner-wrapper"
 TEXT_INPUT_CLASS = "rating-text-feedback"
 
 @then(u'my feedback is displayed')
@@ -26,6 +27,24 @@ def impl(context):
 def impl(context):
     feedback_form_container = find_id_with_wait(context, RATING_CONTAINER_ID, wait_time=60)
     assert elem_is_visible_with_wait(context, feedback_form_container), "Rating form is not visible."
+
+@when(u'I alter a star rating')
+def impl(context):
+    inner_wrapper = find_css_class_with_wait(context, STAR_INNER_WRAPPER_CLASS)
+    options = context.options = inner_wrapper.find_elements_by_class_name(STAR_RATING_OPTION_CLASS)
+    cur_val = context.old_val = len([el for el in options if "activated" in el.get_attribute("class")])
+    new_val = cur_val + 1 if cur_val < 5 else 1  # Keep the new value between 1 and 5
+    new_option_el = [el for el in options if int(el.get_attribute("data-val")) == new_val].pop()
+    new_option_el.click()
+
+@then(u'the altered rating is displayed')
+def impl(context):
+    new_val = len([el for el in context.options if "activated" in el.get_attribute("class")])
+    assert context.old_val != new_val, \
+        "Star rating has value {new_val}, which should be different from the old value {old_val}".format(
+            old_val=context.old_val,
+            new_val=new_val,
+        )
 
 @given(u'some user feedback exists')
 def impl(context):
@@ -56,7 +75,10 @@ def impl(context):
 
 @given(u'I have filled out a feedback form')
 def impl(context):
-    assert False
+    context.execute_steps(u'''
+        Given I am on a content page
+        When I fill out the form
+    ''')
 
 @then(u'I see an edit button')
 def impl(context):
@@ -89,14 +111,14 @@ def enter_star_ratings(context):
         rate_id(context, id_)
 
 
-def rate_id(context, id, val=3):
+def rate_id(context, id_, val=3):
     """
     Enter a star rating given the id of the container
     :param context: behave context
     :param id: id of the container element
     :return: nothing
     """
-    container = find_id_with_wait(context, id)
+    container = find_id_with_wait(context, id_)
 
     def get_rating_el(driver):
         try:
