f415d094776bdf24a3ad04326edc8887335dd842
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-08-25 11:56:59 -0700

    Add get_uuid method

--- kalite/main/content_rating_models.py
+++ kalite/main/content_rating_models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.conf import settings
 from django.db import models
 
@@ -21,3 +23,13 @@ class ContentRating(DeferredCountSyncedModel):
     quality = models.IntegerField(blank=False, default=0)
     difficulty = models.IntegerField(blank=False, default=0)
     text = models.TextField(blank=True)
+
+    def get_uuid(self):
+        assert self.user is not None and self.user.id is not None, "User ID required for get_uuid"
+        assert self.content_id is not None, "Content id required for get_uuid"
+        assert self.content_kind is not None, "Content kind required for get_uuid"
+        assert self.content_source is not None, "Content source required for get_uuid"
+
+        namespace = uuid.UUID(self.user.id)
+        hashtext = ":".join([self.__class__.__name__, self.content_source, self.content_kind, self.content_id])
+        return uuid.uuid5(namespace, hashtext.encode("utf-8")).hex
