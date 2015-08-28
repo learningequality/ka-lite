447aeb832b51c5c86aad35eb5c2f7975bceee60b
Aron Fyodor Asor <aronasorman@gmail.com>
2015-08-28 09:11:27 -0700

    cleanup create_content_db.

--- kalite/distributed/management/commands/create_content_db.py
+++ kalite/distributed/management/commands/create_content_db.py
@@ -5,12 +5,6 @@ import json
 from sqlitedict import SqliteDict
 
 logging = settings.LOG
-EXERCISES_FILEPATH = os.path.join(settings.CHANNEL_DATA_PATH, "exercises.json")
-EXERCISES_SQLITEPATH = os.path.join(
-    settings.CHANNEL_DATA_PATH, "exercises.sqlite")
-CONTENT_FILEPATH = os.path.join(settings.CHANNEL_DATA_PATH, "contents.json")
-CONTENT_SQLITEPATH = os.path.join(
-    settings.CHANNEL_DATA_PATH, "contents.sqlite")
 
 
 class Command(BaseCommand):
@@ -18,7 +12,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         logging.info("Converting...")
 
-        convert(CONTENT_FILEPATH, CONTENT_SQLITEPATH)
+        convert(settings.CONTENT_FILEPATH, settings.CONTENT_CACHE_FILEPATH)
 
 
 def convert(jsonfilepath, sqlitefilepath):
