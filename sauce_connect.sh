48bdeb344bff9cd718b417d6ec849e469df34bc6
Michael Gallaspy <gallaspy.michael@gmail.com>
2015-09-14 17:47:03 -0700

    Use unix line endings; really fix syntax

--- sauce_connect.sh
+++ sauce_connect.sh
@@ -2,12 +2,14 @@
 cd sc-*-linux && ./bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY --tunnel-identifier $CIRCLE_BUILD_NUM-$CIRCLE_NODE_INDEX --readyfile ~/sauce_is_ready &
 I=0
 while 1; do
-    if [[ -e ~/sauce_is_ready ]]; then
+    if [ -e ~/sauce_is_ready ]
+    then
         break;
     fi
     sleep 1
     I=$(( I + 1 ))
-    if [[ $(( I % 60 )) -eq 0 ]]; then
+    if [ $(( I % 60 )) -eq 0 ]
+    then
         killall --wait sc
         ./bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY --tunnel-identifier $CIRCLE_BUILD_NUM-$CIRCLE_NODE_INDEX --readyfile ~/sauce_is_ready &
     fi
