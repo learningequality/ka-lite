aa5c0c1f0ac9eea687569a7a043e404bcf09060a
Richard Tibbles <richard.tibbles@gmail.com>
2015-10-12 14:23:24 -0700

    Adds additional requires for force_sync.

--- kalite/control_panel/static/js/control_panel/utils/force_sync.js
+++ kalite/control_panel/static/js/control_panel/utils/force_sync.js
@@ -1,4 +1,6 @@
 var api = require("utils/api");
+var sprintf = require("sprintf-js").sprintf;
+var messages = require("messages");
 
 module.exports = function force_sync(zone_id, device_id) {
     // Simple function that calls the API endpoint to force a data sync,
@@ -9,6 +11,6 @@ module.exports = function force_sync(zone_id, device_id) {
             msg += sprintf(gettext("After syncing completes, visit the <a href='%(devman_url)s'>device management page</a> to view results."), {
                 devman_url: Urls.device_management(zone_id, device_id)
             });
-            show_message("success", msg);
+            messages.show_message("success", msg);
         });
 };
