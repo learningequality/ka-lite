e48f7ad2902f829175c1660d30235ffdfa03ec1a
Benjamin Bach <benjaoming@gmail.com>
2015-10-14 14:18:55 +0200

    Remove broken clock set feature

--- kalite/distributed/api_views.py
+++ kalite/distributed/api_views.py
@@ -28,38 +28,6 @@ from kalite.main.models import ExerciseLog, VideoLog, ContentLog
 from kalite.shared.decorators.auth import require_admin
 
 
-
-@require_admin
-@api_handle_error_with_json
-def time_set(request):
-    """
-    Receives a date-time string and sets the system date-time
-    RPi only.
-    """
-
-    if not settings.ENABLE_CLOCK_SET:
-        return JsonResponseMessageError(_("Time reset can only be done on Raspberry Pi systems."), status=403)
-
-    # Form does all the data validation - including ensuring that the data passed is a proper date time.
-    # This is necessary to prevent arbitrary code being run on the system.
-    form = DateTimeForm(data=simplejson.loads(request.body))
-    if not form.is_valid():
-        return JsonResponseMessageError(_("Could not read date and time: Unrecognized input data format."), status=400)
-
-    try:
-
-        if os.system('sudo date +%%F%%T -s "%s"' % form.data["date_time"]):
-            raise PermissionDenied
-
-    except PermissionDenied as e:
-        return JsonResponseMessageError(
-            _("System permissions prevented time setting, please run with root permissions."), status=500)
-
-    now = datetime.datetime.now().isoformat(" ").split(".")[0]
-
-    return JsonResponseMessage(_("System time was reset successfully."))
-
-
 # Functions below here focused on users
 
 
