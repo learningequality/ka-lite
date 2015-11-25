8711b5cba9179f9b8e14f62b24db350f60e9e61a
Anusha <anusha.varadharajan@gmail.com>
2015-11-25 15:07:51 -0800

    Added a video player and fixed the CSS for audio player

--- kalite/distributed/static/js/distributed/bundle_modules/vectorvideo.js
+++ kalite/distributed/static/js/distributed/bundle_modules/vectorvideo.js
@@ -1,1 +1,11 @@
-module.exports = require("vectorvideo/views");
+var $ = require("base/jquery");
+var file = require("vectorvideo/views");
+var Models = require("content/models");
+var data_model = new Models.ContentDataModel({content_urls: {stream: ""}});
+var log_model = new Models.ContentLogModel();
+var vectorVideoView = new file.VectorVideoView({data_model: data_model, log_model: log_model});
+vectorVideoView.render();
+console.log(vectorVideoView);
+$(function(){
+$("#content-area").append(vectorVideoView.el);
+});
