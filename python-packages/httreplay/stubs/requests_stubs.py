0b01ac7f54f2612cbc055ac19193036f42f742cf
Benjamin Bach <benjaoming@gmail.com>
2015-11-27 14:30:34 +0100

    remove unused package httreplay

--- python-packages/httreplay/stubs/requests_stubs.py
+++
@@ -1,7 +0,0 @@
-from requests.packages.urllib3.connectionpool import VerifiedHTTPSConnection
-from .base import ReplayHTTPSConnection
-
-
-class ReplayRequestsHTTPSConnection(
-        ReplayHTTPSConnection, VerifiedHTTPSConnection):
-    _baseclass = VerifiedHTTPSConnection
