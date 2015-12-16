05476767fa14d47c34cfb309842a50f23cf69c79
Aron Fyodor Asor <aronasorman@gmail.com>
2015-12-16 10:20:24 -0800

    Revert "have only one return statement."
    This reverts commit 1f159ed1087c67312aafd745878c549fe30083e5.

--- kalite/topic_tools/content_models.py
+++ kalite/topic_tools/content_models.py
@@ -231,6 +231,7 @@ def get_topic_nodes(parent=None, ids=None, **kwargs):
             Item.path,
             Item.slug,
         ).join(Parent, on=(Item.parent == Parent.pk)).where(selector)
+        return values
     elif ids:
         values = Item.select(
             Item.title,
@@ -242,8 +243,7 @@ def get_topic_nodes(parent=None, ids=None, **kwargs):
             Item.path,
             Item.slug,
         ).where(Item.id.in_(ids))
-
-    return values
+        return values
 
 
 @parse_data
