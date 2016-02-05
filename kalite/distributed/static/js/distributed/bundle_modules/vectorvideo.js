5de37a1da3870a372da5e5721f57e467e321a7c7
Anusha Varadharajan <anushavaradharajan@anushas-mbp.dynamic.ucsd.edu>
2016-02-04 21:32:07 -0800

    Need to fix

--- kalite/distributed/static/js/distributed/bundle_modules/vectorvideo.js
+++ kalite/distributed/static/js/distributed/bundle_modules/vectorvideo.js
@@ -2,7 +2,13 @@ var $ = require("base/jQuery");
 var file = require("vectorvideo/views");
 var Paper = require("../../../../../../node_modules/paper/dist/paper-full.js");
 var Models = require("content/models");
-var data_model = new Models.ContentDataModel({content_urls: {stream: "https://raw.githubusercontent.com/christianmemije/audio_file/master/audio.mp3"}});
+var data_model = new Models.ContentDataModel({
+		content_urls: {stream: "https://raw.githubusercontent.com/christianmemije/audio_file/master/audio.mp3"},
+		past_time: 0,
+		past_object: 0,
+		current_object: 0,
+		is_playing: false
+	});
 var log_model = new Models.ContentLogModel();
 window.vectorVideoView = new file.VectorVideoView({data_model: data_model, log_model: log_model});
 //vectorVideoView.render();
