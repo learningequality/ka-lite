f0ddeb74503e60fa539117229f6c127e355dd2b7
Giovanni Wong <deargiovanni89@gmail.com>
2016-02-06 00:33:46 -0800

    benchmark process finished, code review

--- benchmark.py
+++ benchmark.py
@@ -1,59 +1,27 @@
-import psutil
-import multiprocessing
+import benchmarkclient, benchmarkserver
+from benchmarkclient import BenchmarkClient
+from benchmarkserver import BenchmarkingServer
 
 
-class BenchmarkingProcess(multiprocessing.Process):
-    """
-    Receive messages from the main kalite process on whether to start or stop tracking, and how often.
-
-    Write the results in a file.
-    """
-
-    """
-
-    when flag is passed,create BenchmarkingThread object and call start_benchmark function
-    timer function will be inside this flag too, or can be its own seperate flag
-    benchmark writes to file, appending to it instead of overwriting the previous benchmark 
-    call stop benchmark to end benchmark,
-    call kill process after its finished???
-    """
 
 
-    def __init__(self, pid, ipc):
-        self.Benchmarking = True
-        self.pid = pid
-        self.ipc = ipc
-        super(BenchmarkingProcess, self).__init__()
-        #multiprocessing.Process.__init__(self)
+def benchmark_client(f):
+  """decorator function for benchmarkclient.py
+  
 
-    def run():
-        while True:
-            msg = get_message()
-            if msg == "start":
-                _start_benchmark()
-            elif msg == "stop":
-                break
-            else:
-                continue
+  f --  the function that decorator wraps
+  The wrapper starts the benchmark server
+  and benchmarks the function that decorator is wrapped in
+  """
+  
+  def wrapper(*args, **kwargs):
+    if not kwargs['benchmark']:
+      f(*args, **kwargs)
+    else:
+      p2 = BenchmarkingServer()
+      p2.start()
+      with BenchmarkClient() as c:
+        f(*args, **kwargs)
 
-    def start_benchmark(self):
-        self.ipc.add('start')
-
-    def stop_benchmark(self):
-        self.ipc.add('stop')   
-
-    def _start_benchmark(self):
-        process = psutil.Process(self.pid)
-        f = open('benchmarkresults',"a")
-        cpu_usage = psutil.cpu_percent(percpu=True)
-        mem_usage = process.get_memory_info()[0] / float(2 ** 20)
-    #   disk_io = psutil.disk_usage(path)
-        benchmark_values = '{}\n'.format(cpu_usage)
-        f.write(benchmark_values)
-        f.close()
-
-    def get_message(self):
-        self.ipc.pop(0)
-
-   
+  return wrapper
 
--- kalite/topic_tools/content_models.py
+++ kalite/topic_tools/content_models.py
@@ -62,7 +62,7 @@ class AssessmentItem(Model):
     id = CharField(max_length=50, primary_key=True)
     item_data = TextField()  # A serialized JSON blob
     author_names = CharField(max_length=200)  # A serialized JSON list
-    __slots__ = ["id","item_data","author_names"]
+
 
 
 def parse_model_data(item):
@@ -466,9 +466,8 @@ def iterator_content_items(ids=None, **kwargs):
         items = Item.select().where(Item.id.in_(ids)).dicts().iterator()
     else:
         items = Item.select().dicts().iterator()
-
-    imap = itertools.imap
-    mapped_items = imap(unparse_model_data, items)
+    
+    mapped_items = itertools.imap(unparse_model_data, items)
     updated_mapped_items = update_content_availability(mapped_items)
 
     for path, update in updated_mapped_items:
@@ -481,8 +480,7 @@ def iterator_content_items_by_youtube_id(ids=None, **kwargs):
     else:
         items = Item.select().dicts().iterator()
 
-    imap = itertools.imap
-    mapped_items = imap(unparse_model_data, items)
+    mapped_items = itertools.imap(unparse_model_data, items)
     updated_mapped_items = update_content_availability(mapped_items)
 
     for path, update in updated_mapped_items:
