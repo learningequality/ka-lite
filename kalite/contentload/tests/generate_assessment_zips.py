734b64fbb134c3c1fc6ac744e2e99ececd6b5e75
Benjamin Bach <benjaoming@gmail.com>
2016-02-21 23:05:33 +0100

    fix tests to match latest content db

--- kalite/contentload/tests/generate_assessment_zips.py
+++ kalite/contentload/tests/generate_assessment_zips.py
@@ -37,7 +37,10 @@ class TestUrlConversion(TestCase):
 
     def test_content_link_converted(self):
         link_string = "(and so that is the correct answer).**\\n\\n[Watch this video to review](https://www.khanacademy.org/humanities/history/ancient-medieval/Ancient/v/standard-of-ur-c-2600-2400-b-c-e)"
-        expected_string = "(and so that is the correct answer).**\\n\\n[Watch this video to review](/learn/khan/test-prep/ap-art-history/ancient-mediterranean-ap/ancient-near-east-a/standard-of-ur-c-2600-2400-b-c-e/)"
+        expected_string = u"(and so that is the correct answer).**\\n\\n[Watch this video to review](/learn/khan/test-prep/ap-art-history/ancient-mediterranean-AP/ancient-near-east-a/standard-of-ur-c-2600-2400-b-c-e/)"
+        self.maxDiff = None
+        print expected_string
+        print mod.convert_urls(link_string)
         self.assertEqual(expected_string, mod.convert_urls(link_string))
 
     def test_bad_content_link_removed(self):
