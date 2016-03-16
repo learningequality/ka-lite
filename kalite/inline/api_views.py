7eb10a4db886113eaa45f6f15675599933992a53
Michael Gallaspy <gallaspy.michael@gmail.com>
2016-03-16 12:47:51 -0700

    Disable inline help for unavailable pages
    If the user clicks the button and no help is available,
    show a warning message and disable the button.
    
    Also fixes the pattern matching for the management page.

--- kalite/inline/api_views.py
+++ kalite/inline/api_views.py
@@ -4,10 +4,12 @@ Views accessible as an API endpoint for inline.  All should return JsonResponses
 import os
 import re
 
+from django.utils.translation import ugettext as _
+
 from kalite.shared.utils import open_json_or_yml
 from kalite import settings
 
-from fle_utils.internet.classes import JsonResponse
+from fle_utils.internet.classes import JsonResponse, JsonResponseMessageWarning
 
 
 def narrative_view(request, narrative_id):
@@ -24,5 +26,8 @@ def narrative_view(request, narrative_id):
         if exp.search(narrative_id):
             the_narrative[key] = narr
             break
+
+    if not the_narrative:
+        return JsonResponseMessageWarning(_("No inline help is available for this page."), status=404)
 
     return JsonResponse(the_narrative)
