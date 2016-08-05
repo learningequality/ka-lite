722164b0cb865ed2f935b9081cb289f17fc14e71
Benjamin Bach <benjamin@learningequality.org>
2016-08-05 17:07:23 +0200

    bump le version

--- kalite/version.py
+++ kalite/version.py
@@ -5,7 +5,7 @@ import os
 # Must also be of the form N.N.N for internal use, where N is a non-negative integer
 MAJOR_VERSION = "0"
 MINOR_VERSION = "16"
-PATCH_VERSION = "8dev"
+PATCH_VERSION = "8"
 VERSION = "%s.%s.%s" % (MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 SHORTVERSION = "%s.%s" % (MAJOR_VERSION, MINOR_VERSION)
 
