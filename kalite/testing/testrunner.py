bef67e75625b9fb8a1e8d41d06d28f40d161ffdb
Benjamin Bach <benjamin@learningequality.org>
2016-12-13 01:10:27 +0100

    Suppress harmless broken pipe errors cluttering test output

--- kalite/testing/testrunner.py
+++ kalite/testing/testrunner.py
@@ -23,6 +23,37 @@ from kalite.testing.base import DjangoBehaveTestCase
 from kalite.topic_tools.base import database_exists, database_path
 
 
+# Because Selenium browser tests will cause lots of pipe errors, suppress
+# them from test output
+def patch_broken_pipe_error():
+    """Monkey Patch BaseServer.handle_error to not write
+    a stacktrace to stderr on broken pipe.
+    http://stackoverflow.com/a/22618740/362702"""
+    import sys
+    from SocketServer import BaseServer
+    from wsgiref import handlers
+
+    handle_error = BaseServer.handle_error
+    log_exception = handlers.BaseHandler.log_exception
+
+    def is_broken_pipe_error():
+        __, err, __ = sys.exc_info()
+        return repr(err) == "error(32, 'Broken pipe')"
+
+    def my_handle_error(self, request, client_address):
+        if not is_broken_pipe_error():
+            handle_error(self, request, client_address)
+
+    def my_log_exception(self, exc_info):
+        if not is_broken_pipe_error():
+            log_exception(self, exc_info)
+
+    BaseServer.handle_error = my_handle_error
+    handlers.BaseHandler.log_exception = my_log_exception
+
+patch_broken_pipe_error()
+
+
 def get_app_dir(app_module):
     app_dir = os.path.dirname(app_module.__file__)
     if os.path.basename(app_dir) == 'models':
