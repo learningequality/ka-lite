a14d66da32462d55558cedcbae19b4538eefa15d
Richard Amodia <amodia18@gmail.com>
2016-12-16 16:29:42 +0800

    Remove disable logging in windows.

--- kalite/settings/base.py
+++ kalite/settings/base.py
@@ -110,13 +110,6 @@ LOGGING = {
     }
 }
 
-# Disable the logging output on Windows, as it's broken, and was causing problems.
-# See: https://github.com/learningequality/ka-lite/issues/5030
-# TODO(jamalex): if we can get logging working properly on Windows, this can be removed
-# if os.name == "nt":
-#     LOGGING["handlers"]["console"]["class"] = "django.utils.log.NullHandler"
-
-
 DB_TEMPLATE_DIR = os.path.join(
     os.path.split(os.path.dirname(os.path.realpath(__file__)))[0],
     "database",
