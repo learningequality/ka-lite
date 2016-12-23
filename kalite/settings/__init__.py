9e07468dcb9ae9ca70660363ea68784cffbcfc92
Benjamin Bach <benjamin@learningequality.org>
2016-12-23 17:06:59 +0100

    Remove CONFIG_PACKAGE, maintain Nalanda settings in its own settings module

--- kalite/settings/__init__.py
+++ kalite/settings/__init__.py
@@ -1,51 +1,11 @@
 import sys
 import warnings
 from kalite import version
-from kalite.shared.exceptions import RemovedInKALite_v016_Error
-
-
-##############################
-# Functions for querying settings
-##############################
-def package_selected(package_name):
-    global CONFIG_PACKAGE
-    return bool(CONFIG_PACKAGE) and bool(package_name) and package_name.lower() in CONFIG_PACKAGE
-
-# for extracting assessment item resources
-ASSESSMENT_ITEMS_ZIP_URL = "https://learningequality.org/downloads/ka-lite/%s/content/assessment.zip" % version.SHORTVERSION
-
 
 from .base import *
 
 
 CHERRYPY_PORT = HTTP_PORT
-
-########################
-# IMPORTANT: Do not add new settings below this line
-#
-# Everything that follows is overriding default settings, depending on CONFIG_PACKAGE
-########################
-
-# A deprecated setting that shouldn't be used
-CONFIG_PACKAGE = []
-if isinstance(CONFIG_PACKAGE, basestring):
-    CONFIG_PACKAGE = [CONFIG_PACKAGE]
-CONFIG_PACKAGE = [cp.lower() for cp in CONFIG_PACKAGE]
-
-
-if CONFIG_PACKAGE:
-    raise RemovedInKALite_v016_Error("CONFIG_PACKAGE is outdated, use a settings module from kalite.project.settings")
-
-if package_selected("nalanda"):
-    TURN_OFF_MOTIVATIONAL_FEATURES = True
-    RESTRICTED_TEACHER_PERMISSIONS = True
-    FIXED_BLOCK_EXERCISES = 5
-    QUIZ_REPEATS = 3
-    UNIT_POINTS = 2000
-
-if package_selected("UserRestricted"):
-    LOG.info("UserRestricted package selected.")
-    DISABLE_SELF_ADMIN = True  # hard-code facility app setting.
 
 
 # set the default encoding
