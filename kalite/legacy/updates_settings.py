427313f78f1b9a64d8dc4edf3739ab8724d14f85
Benjamin Bach <benjamin@learningequality.org>
2016-12-23 16:58:58 +0100

    Remove unused settings for old git updates

--- kalite/legacy/updates_settings.py
+++ kalite/legacy/updates_settings.py
@@ -22,15 +22,3 @@ This will be cleaned up in KA Lite 0.14
 # Should be a function that receives a video file (youtube ID), and returns a URL to a video stream
 BACKUP_VIDEO_SOURCE = None
 BACKUP_THUMBNAIL_SOURCE = None
-
-# This is the standard method... but doesn't work because we cannot load
-# kalite.i18n while loading settings because of its __init__.py
-# from pkgutil import get_data
-# I18N_DATA_PATH = get_data("kalite.i18n", "data")
-
-
-# UNUSED - REMOVE
-# settings for when we're updating the server through Git
-GIT_UPDATE_REPO_URL = "https://github.com/learningequality/ka-lite.git"
-GIT_UPDATE_BRANCH = "master"
-GIT_UPDATE_REMOTE_NAME = "updates"
