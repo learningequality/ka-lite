8fc90172e1ffb8d59218281d5631dbfa6f70e5ef
Benjamin Bach <benjamin@learningequality.org>
2017-07-22 02:55:53 +0200

    Strip absolute paths from messages to make generated messages consistent while keeping the file name context

--- kalite/i18n/management/commands/makemessages.py
+++ kalite/i18n/management/commands/makemessages.py
@@ -6,10 +6,16 @@ For now (11/12/15) we'll just deal with the existing output. In the future, we
 should revert makemessages to vanilla Django and again do our custom actions
 *on top of* the vanilla output.
 """
+import logging
+import re
+import os
+
+import kalite
+
 from django.conf import settings
 from django.core.management.commands import makemessages
 
-logging = settings.LOG
+logger = logging.getLogger(__name__)
 
 IGNORE_PATTERNS = [
     "*/node_modules*",
@@ -33,7 +39,29 @@ class Command(makemessages.Command):
         for ignore_pattern in IGNORE_PATTERNS:
             if ignore_pattern not in options["ignore_patterns"]:
                 options["ignore_patterns"].append(ignore_pattern)
-        logging.debug("Ignoring the following patterns: {}".format(options["ignore_patterns"]))
+        logger.debug("Ignoring the following patterns: {}".format(options["ignore_patterns"]))
 
-        logging.debug("Calling base makemessages command.")
+        logger.debug("Calling base makemessages command.")
         super(Command, self).handle_noargs(*args, **options)
+
+        # Make all the path annotations relative
+        domain = options.get('domain', "django")
+        django_po = os.path.join(
+            os.path.dirname(kalite.__file__),
+            "locale",
+            "en",
+            "LC_MESSAGES",
+            domain + ".po"
+        )
+        
+        msgid_re = re.compile(r"^\#\:\s+(.+)kalite.+\:\d+\s*$", re.MULTILINE)
+        
+        contents = open(django_po, "r").read()
+        matches = msgid_re.search(contents)
+        if not matches:
+            raise RuntimeError("No translations found")
+        absolute_path = matches.group(1)
+        
+        logger.info("Removing {} from {}.po".format(absolute_path, domain))
+        open(django_po, "w").write(contents.replace(absolute_path, ""))
+        
