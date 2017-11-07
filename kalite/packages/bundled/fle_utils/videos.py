94e1f739a9c2749d0af8713f42b4a09c806d9745
Benjamin Bach <benjamin@learningequality.org>
2017-11-07 13:38:38 +0100

    Move download functions from `fle_utils.videos` to `kalite.updates.videos`

--- kalite/packages/bundled/fle_utils/videos.py
+++ kalite/packages/bundled/fle_utils/videos.py
@@ -1,71 +1,7 @@
 """
+Legacy module, do not use
 """
-import glob
-import logging
-import os
-import socket
 
-from general import ensure_dir
-from internet.download import callback_percent_proxy, download_file, URLNotFound, DownloadCancelled
-
+# This is used in the Central Server for redirects. 
 OUTSIDE_DOWNLOAD_BASE_URL = "http://s3.amazonaws.com/KA-youtube-converted/"  # needed for redirects
 OUTSIDE_DOWNLOAD_URL = OUTSIDE_DOWNLOAD_BASE_URL + "%s/%s"  # needed for default behavior, below
-
-logger = logging.getLogger(__name__)
-
-
-def download_video(youtube_id, download_path="../content/", download_url=OUTSIDE_DOWNLOAD_URL, format="mp4", callback=None):
-    """Downloads the video file to disk (note: this does NOT invalidate any of the cached html files in KA Lite)"""
-
-    ensure_dir(download_path)
-
-    video_filename = "%(id)s.%(format)s" % {"id": youtube_id, "format": format}
-    thumb_filename = "%(id)s.png" % {"id": youtube_id}
-
-    url = download_url % (video_filename, video_filename)
-    thumb_url = download_url % (video_filename, thumb_filename)
-
-    filepath = os.path.join(download_path, video_filename)
-    thumb_filepath = os.path.join(download_path, thumb_filename)
-
-    try:
-        response = download_file(url, filepath, callback_percent_proxy(callback, end_percent=95))
-        if (
-                not os.path.isfile(filepath) or
-                "content-length" not in response.headers or
-                not os.path.getsize(filepath) == int(response.headers['content-length'])):
-            raise URLNotFound("Video was not found, tried: {}".format(url))
-
-        response = download_file(thumb_url, thumb_filepath, callback_percent_proxy(callback, start_percent=95, end_percent=100))
-        if (
-                not os.path.isfile(thumb_filepath) or
-                "content-length" not in response.headers or
-                not os.path.getsize(thumb_filepath) == int(response.headers['content-length'])):
-            raise URLNotFound("Thumbnail was not found, tried: {}".format(thumb_url))
-
-    except DownloadCancelled:
-        delete_downloaded_files(youtube_id, download_path)
-        raise
-
-    except (socket.timeout, IOError) as e:
-        logging.exception(e)
-        logging.info("Timeout -- Network UnReachable")
-        delete_downloaded_files(youtube_id, download_path)
-        raise
-
-    except Exception as e:
-        logging.exception(e)
-        delete_downloaded_files(youtube_id, download_path)
-        raise
-
-
-def delete_downloaded_files(youtube_id, download_path):
-    files_deleted = 0
-    for filepath in glob.glob(os.path.join(download_path, youtube_id + ".*")):
-        try:
-            os.remove(filepath)
-            files_deleted += 1
-        except OSError:
-            pass
-    if files_deleted:
-        return True
--- kalite/updates/videos.py
+++ kalite/updates/videos.py
@@ -1,18 +1,23 @@
 """
 """
+import glob
+import logging
 import os
+import socket
 
 from django.conf import settings
-logging = settings.LOG
+from fle_utils.general import ensure_dir
+from fle_utils.internet.download import callback_percent_proxy, download_file, URLNotFound, DownloadCancelled
 
-from fle_utils import videos  # keep access to all functions
+
+logger = logging.getLogger(__name__)
 
 
 def get_local_video_size(youtube_id, default=None):
     try:
         return os.path.getsize(os.path.join(settings.CONTENT_ROOT, "%s.mp4" % youtube_id))
     except Exception as e:
-        logging.debug(str(e))
+        logger.debug(str(e))
         return default
 
 
@@ -20,8 +25,59 @@ def download_video(youtube_id, format="mp4", callback=None):
     """Downloads the video file to disk (note: this does NOT invalidate any of the cached html files in KA Lite)"""
 
     download_url = ("http://%s/download/videos/" % (settings.CENTRAL_SERVER_HOST)) + "%s/%s"
-    return videos.download_video(youtube_id, settings.CONTENT_ROOT, download_url, format, callback)
+
+    download_path=settings.CONTENT_ROOT
+    
+    ensure_dir(download_path)
+
+    video_filename = "%(id)s.%(format)s" % {"id": youtube_id, "format": format}
+    thumb_filename = "%(id)s.png" % {"id": youtube_id}
+
+    url = download_url % (video_filename, video_filename)
+    thumb_url = download_url % (video_filename, thumb_filename)
+
+    filepath = os.path.join(download_path, video_filename)
+    thumb_filepath = os.path.join(download_path, thumb_filename)
+
+    try:
+        response = download_file(url, filepath, callback_percent_proxy(callback, end_percent=95))
+        if (
+                not os.path.isfile(filepath) or
+                "content-length" not in response.headers or
+                not os.path.getsize(filepath) == int(response.headers['content-length'])):
+            raise URLNotFound("Video was not found, tried: {}".format(url))
+
+        response = download_file(thumb_url, thumb_filepath, callback_percent_proxy(callback, start_percent=95, end_percent=100))
+        if (
+                not os.path.isfile(thumb_filepath) or
+                "content-length" not in response.headers or
+                not os.path.getsize(thumb_filepath) == int(response.headers['content-length'])):
+            raise URLNotFound("Thumbnail was not found, tried: {}".format(thumb_url))
+
+    except DownloadCancelled:
+        delete_downloaded_files(youtube_id, download_path)
+        raise
+
+    except (socket.timeout, IOError) as e:
+        logger.exception(e)
+        logger.info("Timeout -- Network UnReachable")
+        delete_downloaded_files(youtube_id, download_path)
+        raise
+
+    except Exception as e:
+        logger.exception(e)
+        delete_downloaded_files(youtube_id, download_path)
+        raise
 
 
 def delete_downloaded_files(youtube_id):
-    return videos.delete_downloaded_files(youtube_id, settings.CONTENT_ROOT)
+    download_path = settings.CONTENT_ROOT
+    files_deleted = 0
+    for filepath in glob.glob(os.path.join(download_path, youtube_id + ".*")):
+        try:
+            os.remove(filepath)
+            files_deleted += 1
+        except OSError:
+            pass
+    if files_deleted:
+        return True
