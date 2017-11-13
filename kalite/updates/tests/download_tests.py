fa5a36f2b88d1588aa6456e3d40af430323c0053
Benjamin Bach <benjamin@learningequality.org>
2017-11-13 16:30:39 +0100

    Add test of videodownload job

--- kalite/updates/tests/download_tests.py
+++ kalite/updates/tests/download_tests.py
@@ -1,8 +1,10 @@
 from .base import UpdatesTestCase
-from kalite.updates.videos import download_video
+from kalite.updates.videos import download_video, delete_downloaded_files
 from fle_utils.internet.download import URLNotFound
 from kalite.topic_tools.content_models import get_content_item,\
     annotate_content_models_by_youtube_id
+from kalite.updates.download_track import VideoQueue
+from django.core.management import call_command
 
 
 class TestDownload(UpdatesTestCase):
@@ -22,3 +24,17 @@ class TestDownload(UpdatesTestCase):
     def test_download_unavailable(self):
         with self.assertRaises(URLNotFound):
             download_video(self.content_unavailable_item.youtube_id)
+
+    def test_download_command(self):
+        delete_downloaded_files(self.real_video.youtube_id)
+        # Check that it's been marked unavailable
+        updated = get_content_item(content_id=self.real_video.id)
+        annotate_content_models_by_youtube_id(youtube_ids=[self.real_video])
+        self.assertFalse(updated['available'])
+        queue = VideoQueue()
+        queue.add_files({self.real_video.youtube_id: self.real_video.title}, language="en")
+        call_command("videodownload")
+        annotate_content_models_by_youtube_id(youtube_ids=[self.real_video.youtube_id])
+        # Check that it's been marked available
+        updated = get_content_item(content_id=self.real_video.id)
+        self.assertTrue(updated['available'])
