8c952f433c67edd596fd536b3cf7625e17adf503
Benjamin Bach <benjamin@learningequality.org>
2017-11-13 15:55:36 +0100

    Remove redundant Using context

--- kalite/updates/tests/base.py
+++ kalite/updates/tests/base.py
@@ -2,7 +2,6 @@ import random
 
 from kalite.testing.base import KALiteTestCase
 from kalite.topic_tools.content_models import set_database, Item
-from peewee import Using
 
 
 # Some small videos from the content database 2017-11-13
@@ -14,20 +13,20 @@ def add_test_content_videos(instance, db):
     
     youtube_id = random.choice(TEST_YOUTUBE_IDS)
     
-    with Using(db, [Item], with_transaction=False):
-        real_video = Item.create(
-            title="A real video from KA/Youtube",
-            description="This video exists",
-            available=False,
-            kind="Video",
-            id=youtube_id,
-            youtube_id=youtube_id,
-            slug="youtube-vid",
-            path=instance.content_unavailable_content_path,
-            parent=random.choice(instance.content_subsubtopics).pk,
-        )
-        instance.real_video = real_video
-        instance.content_videos.append(real_video)
+    real_video = Item.create(
+        title="A real video from KA/Youtube",
+        description="This video exists",
+        available=False,
+        kind="Video",
+        format="mp4",
+        id=youtube_id,
+        youtube_id=youtube_id,
+        slug="youtube-vid",
+        path="real/video",
+        parent=random.choice(instance.content_subsubtopics).pk,
+    )
+    instance.real_video = real_video
+    instance.content_videos.append(real_video)
 
 
 class UpdatesTestCase(KALiteTestCase):
